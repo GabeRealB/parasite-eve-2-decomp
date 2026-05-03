c2e9ef8e3f37c941d13c385e9f5984f30148165a
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-05-03 15:41:51 +0200

    Link libsn/fsinit

--- src/main/3B44C.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-INCLUDE_ASM("main/nonmatchings/3B44C", func_8004AC4C);
-
-INCLUDE_ASM("main/nonmatchings/3B44C", func_8004AC58);
-
-INCLUDE_ASM("main/nonmatchings/3B44C", func_8004ACAC);
-
-INCLUDE_ASM("main/nonmatchings/3B44C", func_8004ACF0);
---
+++ src/main/3B458.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+INCLUDE_ASM("main/nonmatchings/3B458", func_8004AC58);
+
+INCLUDE_ASM("main/nonmatchings/3B458", func_8004ACAC);
+
+INCLUDE_ASM("main/nonmatchings/3B458", func_8004ACF0);
