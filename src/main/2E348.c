7548af86f7b06990f89e3240fccdc085232f3583
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-02 22:44:52 +0200

    matched func_8003DE58 1

--- src/main/2E348.c
+++ src/main/2E348.c
@@ -1,9 +1,13 @@
 #include "common.h"
 
+#include "main/unknown_syms.h"
+
 INCLUDE_ASM("main/nonmatchings/2E348", func_8003DB48);
 
 INCLUDE_ASM("main/nonmatchings/2E348", func_8003DE14);
 
-INCLUDE_ASM("main/nonmatchings/2E348", func_8003DE58);
+void func_8003DE58(void) {
+    func_8003DB48(0x1010);
+}
 
 INCLUDE_ASM("main/nonmatchings/2E348", func_8003DE78);
