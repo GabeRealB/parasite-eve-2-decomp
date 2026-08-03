a4cd90279cd0ca8873cfb61784be859d7f2601c2
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-03 09:32:59 +0200

    matched func_8003C748 4

--- src/main/2C160.c
+++ src/main/2C160.c
@@ -1,5 +1,7 @@
 #include "common.h"
 
+#include "main/game.h"
+
 INCLUDE_ASM("main/nonmatchings/2C160", func_8003B960);
 
 INCLUDE_ASM("main/nonmatchings/2C160", func_8003BD34);
@@ -12,7 +14,18 @@ INCLUDE_ASM("main/nonmatchings/2C160", func_8003C6D8);
 
 INCLUDE_ASM("main/nonmatchings/2C160", func_8003C728);
 
-INCLUDE_ASM("main/nonmatchings/2C160", func_8003C748);
+void func_8003C748(MATRIX* arg0, volatile SVECTOR* arg1) {
+    register short t4 asm("t4");
+    register short t5 asm("t5");
+    register short t6 asm("t6");
+
+    t4 = arg0->m[0][1];
+    t5 = arg0->m[1][1];
+    t6 = arg0->m[2][1];
+    arg1->vx = t4;
+    arg1->vy = t5;
+    arg1->vz = t6;
+}
 
 INCLUDE_ASM("main/nonmatchings/2C160", func_8003C768);
 
