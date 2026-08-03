517d471413f128d2d2a7d4a4931e41f21a0758d1
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-03 16:31:46 +0200

    matched func_8003DE14 1

--- src/main/2E348.c
+++ src/main/2E348.c
@@ -4,7 +4,21 @@
 
 INCLUDE_ASM("main/nonmatchings/2E348", func_8003DB48);
 
-INCLUDE_ASM("main/nonmatchings/2E348", func_8003DE14);
+void func_8003DE14(s32 arg0, s32 arg1, s32 arg2) {
+    if (arg0 < 0) {
+        D_80070F68.field_48[1].isbg = 0;
+        D_80070F68.field_48[0].isbg = 0;
+        return;
+    }
+    D_80070F68.field_48[1].isbg = 1;
+    D_80070F68.field_48[0].isbg = 1;
+    D_80070F68.field_48[1].r0 = arg0;
+    D_80070F68.field_48[0].r0 = arg0;
+    D_80070F68.field_48[1].g0 = arg1;
+    D_80070F68.field_48[0].g0 = arg1;
+    D_80070F68.field_48[1].b0 = arg2;
+    D_80070F68.field_48[0].b0 = arg2;
+}
 
 void func_8003DE58(void) {
     func_8003DB48(0x1010);
