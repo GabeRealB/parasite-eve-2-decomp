c2f988416af2b7bd0a0a2334981e316e65b25dac
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-03 17:07:06 +0200

    matched func_8004AC58 9

--- src/main/3B458.c
+++ src/main/3B458.c
@@ -2,7 +2,25 @@
 
 #include "main/unknown_syms.h"
 
-INCLUDE_ASM("main/nonmatchings/3B458", func_8004AC58);
+void func_8004AC58(s32 arg0, s32 arg1)
+{
+    s32 idx;
+    register u8 *ptr asm("v0");
+    register s32 val asm("v1");
+    register s32 nibble asm("a0");
+
+    idx = arg0 / 2;
+    if (arg0 & 1) {
+        ptr = &D_80073980[idx];
+        nibble = arg1 & 0xF;
+        val = ptr[4] & 0xF0;
+    } else {
+        ptr = &D_80073980[idx];
+        nibble = arg1 << 4;
+        val = ptr[4] & 0xF;
+    }
+    ptr[4] = val | nibble;
+}
 
 s32 func_8004ACAC(s32 arg0)
 {
