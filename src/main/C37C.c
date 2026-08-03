65c64ae32c3140b4c0320d6b90ed37049166b9dc
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-03 14:07:32 +0200

    matched func_8001D5CC 1

--- src/main/C37C.c
+++ src/main/C37C.c
@@ -113,7 +113,20 @@ void func_8001D5C4(void)
 {
 }
 
-INCLUDE_ASM("main/nonmatchings/C37C", func_8001D5CC);
+void func_8001D5CC(void)
+{
+    GStruct3* p;
+    u8        sp10;
+
+    p = &D_80068FA0;
+    if (p->field_21A >= 0) {
+        sp10 = p->field_21A;
+        p->field_20E = 2;
+        func_8001D2B0(0x81, 0, &sp10);
+    } else {
+        p->field_20E = 1;
+    }
+}
 
 void func_8001D628(void)
 {
