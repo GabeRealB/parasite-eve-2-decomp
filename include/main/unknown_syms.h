4283c39043bb9089cddb1bf3847f6e8e071191f8
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-03 13:01:14 +0200

    matched func_8003E438 1

--- include/main/unknown_syms.h
+++ include/main/unknown_syms.h
@@ -106,7 +106,7 @@ extern void      func_8002C9E0(s32 arg0);
 extern void      func_8002CCB8(GStruct0* arg0);
 extern GStruct0*     func_8002CB04(GStruct2* arg0, s32 arg1, s32 arg2, GStruct0Node* arg3);
 extern GStruct0*     func_8002CFA0(GStruct2* arg0, s32 arg1, s32 arg2, s32 arg3);
-extern void          func_8002CFDC(s32 arg0, s32 arg1, s32 arg2, s32 arg3);
+extern GStruct0*     func_8002CFDC(s32 arg0, s32 arg1, s32 arg2, s32 arg3);
 extern GStruct0Node* func_8002D40C(void);
 extern void          func_8002D41C(GStruct0Node* node);
 extern void          func_8002D248(GStruct0Node* node);
--- src/main/2E7B0.c
+++ src/main/2E7B0.c
@@ -9,7 +9,17 @@ INCLUDE_ASM("main/nonmatchings/2E7B0", func_8003E210);
 
 INCLUDE_ASM("main/nonmatchings/2E7B0", func_8003E324);
 
-INCLUDE_ASM("main/nonmatchings/2E7B0", func_8003E438);
+GStruct0* func_8003E438(s32 arg0, s32 arg1, s32 arg2, s32 arg3)
+{
+    GStruct0Node* saved;
+    GStruct0* ret;
+
+    saved = func_8002D40C();
+    func_8002D41C(&D_800716E0);
+    ret = func_8002CFDC(arg0, arg1, arg2, arg3);
+    func_8002D41C(saved);
+    return ret;
+}
 
 GStruct0* func_8003E4BC(GStruct2* arg0, s32 arg1, s32 arg2, s32 arg3)
 {
