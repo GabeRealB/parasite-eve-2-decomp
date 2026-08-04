b5f2a6d1d2e66546d1431e88bc42ca5bb1f484af
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-05 01:33:35 +0200

    matched func_80035ED4 2

--- src/main/mc.c
+++ src/main/mc.c
@@ -1246,7 +1246,35 @@ void func_80035E48(Task* arg0, McWork* arg1)
     }
 }
 
-INCLUDE_ASM("main/nonmatchings/mc", func_80035ED4);
+void func_80035ED4(Task* arg0, McWork* arg1)
+{
+    register McWork*   a1 asm("s1");
+    register Task*     a0 asm("s0");
+    register UiObject* obj asm("s0");
+    register s32       modeIdx asm("s1");
+    register s32       ret asm("s3");
+    McPromptPair*      entry;
+    McPromptPair*      base;
+
+    a0          = arg0;
+    a1          = arg1;
+    a1->field_8 = 1;
+    if (MemCardAccept(a1->field_C) != 0) {
+        a1->field_4  = 0;
+        a0->field_30 = a0->field_30 + 1;
+    } else {
+        a1->field_4 = a1->field_4 + 1;
+    }
+    obj           = a0->field_20;
+    modeIdx       = a1->field_8;
+    ret           = func_80048E10(obj, 1);
+    obj->field_2E = 0;
+    func_80048E38(obj, D_8001398C);
+    base  = Mc_PromptTable;
+    entry = &base[modeIdx];
+    func_8002FDCC(obj, obj->field_1C + 2, -2, entry->field_0, ret, 1, 0);
+    func_8002FDCC(obj, obj->field_1C + 2, 0xF, entry->field_4, ret, 1, 0);
+}
 
 void func_80035FD8(Task* arg0, McWork* arg1)
 {
