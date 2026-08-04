b3ccfc167eae6417a4e7a6f1f39d7c3bbc69b6de
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-04 19:00:28 +0200

    matched func_80035844 1

--- src/main/21FDC.c
+++ src/main/21FDC.c
@@ -786,7 +786,27 @@ INCLUDE_ASM("main/nonmatchings/21FDC", func_80035684);
 
 INCLUDE_ASM("main/nonmatchings/21FDC", func_80035764);
 
-INCLUDE_ASM("main/nonmatchings/21FDC", func_80035844);
+void func_80035844(GStruct0* arg0, GStruct21* arg1)
+{
+    s32        ret;
+    GStruct37* obj;
+    GStruct62* entry;
+    GStruct62* base;
+
+    arg1->field_4 -= 1;
+    if (arg1->field_4 <= 0) {
+        arg0->field_30 = 0xF;
+    }
+    arg1->field_8 = 4;
+    obj           = arg0->field_20;
+    ret           = func_80048E10(obj, 1);
+    obj->field_2E = 0;
+    func_80048E38(obj, D_8001398C);
+    base  = D_80060D08;
+    entry = &base[4];
+    func_8002FDCC(obj, obj->field_1C + 2, -2, entry->field_0, ret, 1, 0);
+    func_8002FDCC(obj, obj->field_1C + 2, 0xF, entry->field_4, ret, 1, 0);
+}
 
 void func_8003591C(GStruct0* arg0, GStruct21* arg1)
 {
