1f6317c002d94e554f9739f8159b841acf8391bc
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-05 02:26:01 +0200

    matched func_800362A4 1

--- src/main/mc.c
+++ src/main/mc.c
@@ -1459,7 +1459,30 @@ void func_800361C0(Task* arg0, McWork* arg1)
     arg0->field_30 = arg0->field_30 + 1;
 }
 
-INCLUDE_ASM("main/nonmatchings/mc", func_800362A4);
+void func_800362A4(Task* arg0, McWork* arg1)
+{
+    s32           ret;
+    UiObject*     obj;
+    McPromptPair* entry;
+    McPromptPair* base;
+    s32           idx;
+
+    if (MemCardReadData((unsigned long*)arg1->field_18, arg1->field_1C << 7, arg1->field_20) != 0) {
+        arg1->field_4  = 0;
+        arg0->field_30 = arg0->field_30 + 1;
+    } else {
+        arg1->field_4 = arg1->field_4 + 1;
+    }
+    obj           = arg0->field_20;
+    idx           = arg1->field_8;
+    ret           = func_80048E10(obj, 1);
+    obj->field_2E = 0;
+    func_80048E38(obj, D_8001398C);
+    base  = Mc_PromptTable;
+    entry = &base[idx];
+    func_8002FDCC(obj, obj->field_1C + 2, -2, entry->field_0, ret, 1, 0);
+    func_8002FDCC(obj, obj->field_1C + 2, 0xF, entry->field_4, ret, 1, 0);
+}
 
 void func_800363AC(Task* arg0, McWork* arg1)
 {
