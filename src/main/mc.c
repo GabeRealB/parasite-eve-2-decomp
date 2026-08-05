af04aae62f54829dcc01c0007d2d08bfbe116bbe
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-05 11:04:37 +0200

    matched func_8003429C 1

--- src/main/mc.c
+++ src/main/mc.c
@@ -836,7 +836,36 @@ void func_800340A4(Task* arg0, McWork* arg1)
 
 INCLUDE_ASM("main/nonmatchings/mc", func_8003415C);
 
-INCLUDE_ASM("main/nonmatchings/mc", func_8003429C);
+void func_8003429C(Task* arg0, McWork* arg1)
+{
+    s32           ret;
+    s32           idx;
+    UiObject*     obj;
+    McPromptPair* entry;
+    McPromptPair* base;
+
+    if (MemCardSync(1, (long*)&arg1->field_10, (long*)&arg1->field_14) != 0) {
+        arg1->field_4  = 0;
+        arg0->field_30 = arg0->field_30 + 1;
+    } else {
+        arg1->field_4 = arg1->field_4 + 1;
+    }
+    idx           = arg1->field_8;
+    obj           = arg0->field_20;
+    ret           = func_80048E10(obj, 1);
+    obj->field_2E = 0;
+    func_80048E38(obj, D_8001398C);
+    base  = Mc_PromptTable;
+    entry = &base[idx];
+    func_8002FDCC(obj, obj->field_1C + 2, -2, entry->field_0, ret, 1, 0);
+    func_8002FDCC(obj, obj->field_1C + 2, 0xF, entry->field_4, ret, 1, 0);
+    if (arg1->field_0 > 0) {
+        arg1->field_0 -= 2;
+    }
+    if (arg1->field_0 < 0) {
+        arg1->field_0 += 2;
+    }
+}
 
 void func_800343D0(Task* arg0, McWork* arg1)
 {
