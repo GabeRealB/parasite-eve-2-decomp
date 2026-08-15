53b6d8ddc46dc220737a174b86f37c6dd6a021bf
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-15 13:49:39 +0200

    matched func_8009FCDC 1

--- src/gameplay/gameplay.c
+++ src/gameplay/gameplay.c
@@ -241,7 +241,22 @@ INCLUDE_ASM("gameplay/nonmatchings/gameplay", func_8009FC44);
 
 INCLUDE_ASM("gameplay/nonmatchings/gameplay", func_8009FC90);
 
-INCLUDE_ASM("gameplay/nonmatchings/gameplay", func_8009FCDC);
+u32* func_8009FCDC(TmdScratchModelBlock* arg0, s32 arg1, u32* arg2)
+{
+    u8* prims;
+    s32 stride;
+
+    prims = arg0->field_0;
+    if (arg0->field_1C-- > 0) {
+        stride = arg0->field_18;
+        do {
+            arg2  += stride;
+            prims += 0x14;
+        } while (arg0->field_1C-- > 0);
+    }
+    arg0->field_0 = prims;
+    return arg2;
+}
 
 INCLUDE_ASM("gameplay/nonmatchings/gameplay", func_8009FD28);
 
