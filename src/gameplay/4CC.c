7357f7f0c854528b2896949bdd6107abe5a61d40
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-15 17:45:43 +0200

    matched func_800BF398 1

--- src/gameplay/4CC.c
+++ src/gameplay/4CC.c
@@ -1,8 +1,11 @@
 #include "common.h"
 
+#include <psyq/libgte.h>
+
 #include "gameplay/268.h"
 #include "gameplay/4CC.h"
 #include "main/task.h"
+#include "main/ui.h"
 
 INCLUDE_ASM("gameplay/nonmatchings/4CC", func_800BC634);
 
@@ -42,7 +45,36 @@ s32 func_800BF334(s32 arg0, s32 arg1)
     return ret;
 }
 
-INCLUDE_ASM("gameplay/nonmatchings/4CC", func_800BF398);
+void func_800BF398(UiObject* arg0, Task* arg1)
+{
+    UiObject* parent;
+
+    parent = arg1->parent->spawnArg2;
+    switch (arg0->field_2E) {
+        case -1:
+            if (parent->owner->flags) {
+                parent->status = 1;
+                Ui_TeardownTree(arg0, arg0->owner);
+            } else {
+                Ui_TeardownTree(arg0, arg0->owner);
+                parent->status   = 0;
+                parent->field_2E = -1;
+            }
+            break;
+        case 6:
+            parent->status = 1;
+            Ui_TeardownTree(arg0, arg0->owner);
+            break;
+        case 0x23:
+            Ui_TeardownTree(arg0, arg0->owner);
+            parent->field_2E = 0x23;
+            break;
+        case 38:
+        case 39:
+            parent->field_2E = arg0->field_2E;
+            break;
+    }
+}
 
 INCLUDE_ASM("gameplay/nonmatchings/4CC", func_800BF464);
 
