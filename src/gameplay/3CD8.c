43f09e9f76f54564d3d7a1db5b0a096f8c5f0c8d
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-15 10:52:34 +0200

    matched func_800E3DD8 1

--- src/gameplay/3CD8.c
+++ src/gameplay/3CD8.c
@@ -133,7 +133,14 @@ void func_800E3D8C(s32 arg0, s32 arg1)
     }
 }
 
-INCLUDE_ASM("gameplay/nonmatchings/3CD8", func_800E3DD8);
+void func_800E3DD8(s32 arg0, s32 arg1, s32 arg2)
+{
+    if (arg0 & 0xF000000) {
+        arg0 &= 0xF0FFFFFF;
+        arg0 |= Game_Session->field_7 << 24;
+    }
+    SndEvt_EnqueueType6(arg0, (s8)arg1, (s8)arg2);
+}
 
 s32 func_800E3E30(s32 arg0)
 {
