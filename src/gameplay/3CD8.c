56014559df66a35582a157347293a08b82b90fc5
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-15 05:38:59 +0200

    matched func_800E3CEC 1

--- src/gameplay/3CD8.c
+++ src/gameplay/3CD8.c
@@ -13,6 +13,7 @@
 extern WipSysConfig D_80073B88;
 extern TaskDesc     D_8010FAEC[];
 extern s32          D_8010FB90[];
+extern u16          D_80112D68[];
 extern s32          D_801155A8;
 extern u16          D_801155AE;
 extern Task*        D_80115674;
@@ -76,7 +77,10 @@ INCLUDE_ASM("gameplay/nonmatchings/3CD8", func_800E3BBC);
 
 INCLUDE_ASM("gameplay/nonmatchings/3CD8", func_800E3C6C);
 
-INCLUDE_ASM("gameplay/nonmatchings/3CD8", func_800E3CEC);
+void func_800E3CEC(s32* arg0)
+{
+    *arg0 = D_80112D68[Mc_SaveData.field_22 - 1] + D_80073B88.field_21;
+}
 
 INCLUDE_ASM("gameplay/nonmatchings/3CD8", func_800E3D24);
 
