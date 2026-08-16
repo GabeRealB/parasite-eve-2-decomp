72677be6555534898a7d7ad5a83d621a4a2f84a8
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-16 22:05:40 +0200

    matched func_8009EECC 1

--- src/gameplay/gameplay.c
+++ src/gameplay/gameplay.c
@@ -392,7 +392,25 @@ u32* func_8009ED90(TmdScratchModelBlock* arg0, s32 arg1, u32* arg2)
 
 INCLUDE_ASM("gameplay/nonmatchings/gameplay", func_8009EE28);
 
-INCLUDE_ASM("gameplay/nonmatchings/gameplay", func_8009EECC);
+u32* func_8009EECC(TmdScratchModelBlock* arg0, s32 arg1, u32* arg2)
+{
+    POLY_GT3* poly;
+
+    poly = (POLY_GT3*)arg0->field_0;
+    if (arg0->field_1C-- > 0) {
+        do {
+            *(s32*)&poly->u0 = arg2[4];
+            *(s32*)&poly->u1 = arg2[5];
+            *(u16*)&poly->u2 = *(u16*)&arg2[6];
+            poly->tpage     += arg0->field_70;
+            poly->clut      += arg0->field_72;
+            poly++;
+            arg2 += arg0->field_18;
+        } while (arg0->field_1C-- > 0);
+    }
+    arg0->field_0 = (u8*)poly;
+    return arg2;
+}
 
 INCLUDE_ASM("gameplay/nonmatchings/gameplay", func_8009EF64);
 
