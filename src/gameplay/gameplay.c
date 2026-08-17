d51d97845fd02d4605d22ce0ad4bdbd21a2be29b
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-17 02:53:19 +0200

    matched func_8009F3F8 1

--- src/gameplay/gameplay.c
+++ src/gameplay/gameplay.c
@@ -559,7 +559,26 @@ u32* func_8009F360(TmdScratchModelBlock* arg0, s32 arg1, u32* arg2)
     return arg2;
 }
 
-INCLUDE_ASM("gameplay/nonmatchings/gameplay", func_8009F3F8);
+u32* func_8009F3F8(TmdScratchModelBlock* arg0, s32 arg1, u32* arg2)
+{
+    POLY_FT4* poly;
+
+    poly = (POLY_FT4*)arg0->field_0;
+    if (arg0->field_1C-- > 0) {
+        do {
+            *(s32*)&poly->u0 = arg2[2];
+            *(s32*)&poly->u1 = arg2[3];
+            *(u16*)&poly->u2 = *(u16*)&arg2[4];
+            *(u16*)&poly->u3 = ((u16*)&arg2[4])[1];
+            poly->tpage     += arg0->field_70;
+            poly->clut      += arg0->field_72;
+            poly++;
+            arg2 += arg0->field_18;
+        } while (arg0->field_1C-- > 0);
+    }
+    arg0->field_0 = (u8*)poly;
+    return arg2;
+}
 
 u32* func_8009F49C(TmdScratchModelBlock* arg0, s32 arg1, u32* arg2)
 {
