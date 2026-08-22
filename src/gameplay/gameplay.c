8ab78f1d1000f143dfcfa42b825dd5b4cbcdac93
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-22 06:10:53 +0200

    matched func_8009D0DC 1

--- src/gameplay/gameplay.c
+++ src/gameplay/gameplay.c
@@ -1090,7 +1090,79 @@ u32* func_8009CED0(TmdScratchModelBlock* arg0, s32 arg1, u32* arg2)
     return arg2;
 }
 
-INCLUDE_ASM("gameplay/nonmatchings/gameplay", func_8009D0DC);
+u32* func_8009D0DC(TmdScratchModelBlock* arg0, s32 arg1, u32* arg2)
+{
+    TmdScratchModelBlock* ws;
+    POLY_GT4*             poly;
+    s32*                  opz;
+    DisplayState*         ds;
+    register u32          mask asm("t2");
+    u32                   maskHi;
+    u32                   clipMask;
+    s32*                  flg;
+    u16*                  rec;
+    u8*                   verts;
+    u8*                   norms;
+
+    ws   = arg0;
+    poly = (POLY_GT4*)ws->field_0;
+    if (ws->field_1C-- > 0) {
+        flg      = &ws->field_24;
+        clipMask = 0x80000000;
+        opz      = &ws->field_28;
+        ds       = &Display_State;
+        mask     = 0xFFFFFF;
+        maskHi   = 0xFF000000;
+        do {
+            rec   = (u16*)arg2;
+            verts = (u8*)ws->field_8;
+            gte_ldv3(verts + (rec[0] & 0xFFF8), verts + (rec[1] & 0xFFF8), verts + (rec[2] & 0xFFF8));
+            gte_rtpt_real();
+            gte_stflg(flg);
+            if ((ws->field_24 & clipMask) == 0) {
+                gte_nclip_real();
+                gte_stopz(opz);
+                gte_ldrgb(arg2 + 4);
+                gte_stsxy3_gt4(poly);
+                gte_ldv0((u8*)ws->field_8 + (rec[3] & 0xFFF8));
+                gte_rtps_real();
+                gte_stflg(flg);
+                if ((ws->field_24 & clipMask) == 0) {
+                    if (ws->field_28 > 0) {
+                        goto draw;
+                    }
+                    gte_nclip_real();
+                    gte_stopz(opz);
+                    if (ws->field_28 < 0) {
+                    draw:
+                        gte_stsxy2(&poly->x3);
+                        gte_avsz4_real();
+                        norms = (u8*)ws->field_C;
+                        gte_ldv3(norms + (rec[4] & 0xFFF8), norms + (rec[5] & 0xFFF8), norms + (rec[6] & 0xFFF8));
+                        gte_ncct_real();
+                        gte_strgb3_gt4(poly);
+                        gte_ldv0((u8*)ws->field_C + (rec[7] & 0xFFF8));
+                        gte_nccs_real();
+                        gte_strgb(&poly->r3);
+                        setlen(poly, 0xC);
+                        setcode(poly, 0x3C);
+                        if (ws->field_80->field_C & 2) {
+                            setcode(poly, 0x3E);
+                        }
+                        gte_stotz(opz);
+                        poly->tag = (poly->tag & maskHi) | (*(u_long*)(((((u32)ws->field_28 << ds->field_128) >> 2) & 0xFFC) + (s32)ws->field_14) & mask);
+                        *(u_long*)(((((u32)ws->field_28 << ds->field_128) >> 2) & 0xFFC) + (s32)ws->field_14) =
+                            (*(u_long*)(((((u32)ws->field_28 << ds->field_128) >> 2) & 0xFFC) + (s32)ws->field_14) & maskHi) | ((u32)poly & mask);
+                    }
+                }
+            }
+            poly++;
+            arg2 += ws->field_18;
+        } while (ws->field_1C-- > 0);
+    }
+    ws->field_0 = (u8*)poly;
+    return arg2;
+}
 
 u32* func_8009D388(TmdScratchModelBlock* arg0, s32 arg1, u32* arg2)
 {
