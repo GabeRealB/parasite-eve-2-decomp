73eb190b65b70c2ed1802c9a48242e75db09c08b
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-30 06:12:28 +0200

    matched func_shelter_1f_tent_8017F484 1

--- src/rooms/shelter_1f_tent/shelter_1f_tent.c
+++ src/rooms/shelter_1f_tent/shelter_1f_tent.c
@@ -35,31 +35,3 @@ INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent", func_shelter_1
 INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent", func_shelter_1f_tent_8017F380);
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent", func_shelter_1f_tent_8017F448);
-
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent", func_shelter_1f_tent_8017F484);
-
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent", func_shelter_1f_tent_8017F9F0);
-
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent", func_shelter_1f_tent_8017FBC4);
-
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent", func_shelter_1f_tent_8017FC54);
-
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent", func_shelter_1f_tent_8017FC5C);
-
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent", func_shelter_1f_tent_8017FCA0);
-
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent", func_shelter_1f_tent_8017FD54);
-
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent", func_shelter_1f_tent_8017FDA8);
-
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent", func_shelter_1f_tent_8017FDB8);
-
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent", func_shelter_1f_tent_8017FE10);
-
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent", func_shelter_1f_tent_8017FFAC);
-
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent", func_shelter_1f_tent_801807F0);
-
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent", func_shelter_1f_tent_80180B90);
-
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent", func_shelter_1f_tent_80181010);
---
+++ src/rooms/shelter_1f_tent/shelter_1f_tent_3.c
@@ -0,0 +1,27 @@
+#include "common.h"
+
+INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent_3", func_shelter_1f_tent_8017F9F0);
+
+INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent_3", func_shelter_1f_tent_8017FBC4);
+
+INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent_3", func_shelter_1f_tent_8017FC54);
+
+INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent_3", func_shelter_1f_tent_8017FC5C);
+
+INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent_3", func_shelter_1f_tent_8017FCA0);
+
+INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent_3", func_shelter_1f_tent_8017FD54);
+
+INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent_3", func_shelter_1f_tent_8017FDA8);
+
+INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent_3", func_shelter_1f_tent_8017FDB8);
+
+INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent_3", func_shelter_1f_tent_8017FE10);
+
+INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent_3", func_shelter_1f_tent_8017FFAC);
+
+INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent_3", func_shelter_1f_tent_801807F0);
+
+INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent_3", func_shelter_1f_tent_80180B90);
+
+INCLUDE_ASM("rooms/nonmatchings/shelter_1f_tent/shelter_1f_tent_3", func_shelter_1f_tent_80181010);
