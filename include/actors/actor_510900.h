8c983342d68bbf9465e0cea5b49cb7862497f821
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-30 14:34:10 +0200

    matched func_actor_510900_8013B6A0 1

--- include/actors/actor_510900.h
+++ include/actors/actor_510900.h
@@ -3,16 +3,42 @@
 
 #include "common.h"
 
+typedef struct Actor510900Coord {
+    /* 0x00 */ s32  field_0;
+    /* 0x04 */ byte pad_4[0x4C];
+    /* 0x50 */ s32  field_50;
+} Actor510900Coord;
+
+typedef struct Actor510900Obj2C {
+    /* 0x00 */ byte              pad_0[8];
+    /* 0x08 */ Actor510900Coord* field_8;
+    /* 0x0C */ s16               field_C;
+} Actor510900Obj2C;
+
 typedef struct Actor510900Work {
-    /* 0x000 */ byte pad_0[0x58E];
+    /* 0x000 */ byte pad_0[0x584];
+    /* 0x584 */ s16  field_584;
+    /* 0x586 */ byte pad_586[8];
     /* 0x58E */ s16  field_58E;
+    /* 0x590 */ byte pad_590[0x14];
+    /* 0x5A4 */ s16  field_5A4;
 } Actor510900Work;
 
 typedef struct Actor510900 {
-    /* 0x00 */ byte             pad_0[0x1C];
-    /* 0x1C */ Actor510900Work* field_1C;
+    /* 0x00 */ byte              pad_0[0x1C];
+    /* 0x1C */ Actor510900Work*  field_1C;
+    /* 0x20 */ byte              pad_20[0xC];
+    /* 0x2C */ Actor510900Obj2C* field_2C;
 } Actor510900;
 
+typedef struct Actor510900Ctx {
+    /* 0x00 */ byte pad_0[0x14];
+    /* 0x14 */ u8   field_14;
+    /* 0x15 */ byte pad_15[0x37];
+    /* 0x4C */ u8   field_4C;
+} Actor510900Ctx;
+
+void func_actor_510900_8013B6A0(Actor510900Ctx* arg0, Actor510900* arg1);
 void func_actor_510900_8013B870(Actor510900* arg0);
 
 #endif
--- src/actors/actor_510900/actor_510900.c
+++ src/actors/actor_510900/actor_510900.c
@@ -1,5 +1,24 @@
 #include "common.h"
 
+#include "actors/actor_510900.h"
+
+void Gp_UpdateCoord(Actor510900Coord* arg0);
+void func_actor_510900_80135744(Actor510900* arg0);
+void func_actor_510900_8013864C(Actor510900* arg0);
+void func_actor_510900_801387F4(Actor510900* arg0);
+void func_actor_510900_80138978(Actor510900* arg0);
+void func_actor_510900_80138A9C(Actor510900* arg0);
+void func_actor_510900_80138BF0(Actor510900* arg0);
+void func_actor_510900_80138D38(Actor510900* arg0);
+void func_actor_510900_80138F44(Actor510900* arg0);
+void func_actor_510900_8013B804(Actor510900* arg0);
+void func_actor_510900_8013BB20(Actor510900* arg0);
+void func_actor_510900_8013BBE4(Actor510900* arg0);
+void func_actor_510900_8013BC38(Actor510900* arg0, Actor510900Coord* arg1);
+void func_actor_510900_8013BC80(Actor510900* arg0);
+
+extern u8 D_801153F4;
+
 INCLUDE_RODATA("actors/nonmatchings/actor_510900/actor_510900", D_actor_510900_80131E20);
 
 INCLUDE_ASM("actors/nonmatchings/actor_510900/actor_510900", func_actor_510900_80131F24);
@@ -102,6 +121,70 @@ INCLUDE_ASM("actors/nonmatchings/actor_510900/actor_510900", func_actor_510900_8
 
 INCLUDE_ASM("actors/nonmatchings/actor_510900/actor_510900", func_actor_510900_8013B658);
 
-INCLUDE_ASM("actors/nonmatchings/actor_510900/actor_510900", func_actor_510900_8013B6A0);
+void func_actor_510900_8013B6A0(Actor510900Ctx* arg0, Actor510900* arg1)
+{
+    Actor510900Coord* temp_s1;
+    Actor510900Obj2C* temp_a1;
+    Actor510900Work*  temp_s2;
+    s32               one;
+    s32               sp;
+
+    temp_s2 = arg1->field_1C;
+    temp_a1 = arg1->field_2C;
+    temp_s1 = temp_a1->field_8;
+    if (temp_s2->field_5A4 != 0) {
+        sp  = D_801153F4;
+        one = 1;
+        if (sp == one) {
+            goto case1;
+        }
+        if (sp >= 2) {
+            goto ge2;
+        }
+        if (sp == 0) {
+            goto case0;
+        }
+        goto default_body;
+    ge2:
+        if (sp == 2) {
+            goto case2;
+        }
+        goto default_body;
+    case0:
+        temp_a1->field_C = 0;
+        arg0->field_14   = 8;
+        goto default_body;
+    case1:
+        func_actor_510900_8013BBE4(arg1);
+        func_actor_510900_8013BC38(arg1, temp_s1);
+        return;
+    case2:
+        temp_a1->field_C = 0x80;
+        arg0->field_14   = one;
+        return;
+    default_body:
+        if (arg0->field_4C != 0) {
+            func_actor_510900_8013B804(arg1);
+        }
+        func_actor_510900_80135744(arg1);
+        func_actor_510900_8013864C(arg1);
+        func_actor_510900_8013B870(arg1);
+        func_actor_510900_801387F4(arg1);
+        func_actor_510900_80138978(arg1);
+        func_actor_510900_80138A9C(arg1);
+        func_actor_510900_8013BB20(arg1);
+        func_actor_510900_80138BF0(arg1);
+        if (temp_s2->field_584 != 0) {
+            func_actor_510900_80138D38(arg1);
+        }
+        temp_s1->field_0                  = 0;
+        arg1->field_2C->field_8->field_50 = 0;
+        Gp_UpdateCoord(temp_s1);
+        func_actor_510900_8013BBE4(arg1);
+        func_actor_510900_8013BC38(arg1, temp_s1);
+        func_actor_510900_80138F44(arg1);
+        func_actor_510900_8013BC80(arg1);
+    }
+}
 
 INCLUDE_ASM("actors/nonmatchings/actor_510900/actor_510900", func_actor_510900_8013B804);
