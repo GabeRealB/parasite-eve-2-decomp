f8c03961c8650f0e6b09f1ff28e2b1aeb3e94223
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-30 05:05:40 +0200

    matched func_acropolis_square_80181228 1

---
+++ src/rooms/acropolis_square/acropolis_square_3.c
@@ -0,0 +1,41 @@
+#include "common.h"
+
+INCLUDE_ASM("rooms/nonmatchings/acropolis_square/acropolis_square_3", func_acropolis_square_80181794);
+
+INCLUDE_ASM("rooms/nonmatchings/acropolis_square/acropolis_square_3", func_acropolis_square_801819BC);
+
+INCLUDE_RODATA("rooms/nonmatchings/acropolis_square/acropolis_square_3", D_acropolis_square_8017D6B4);
+
+INCLUDE_ASM("rooms/nonmatchings/acropolis_square/acropolis_square_3", func_acropolis_square_80181AEC);
+
+INCLUDE_ASM("rooms/nonmatchings/acropolis_square/acropolis_square_3", func_acropolis_square_80181DD0);
+
+INCLUDE_ASM("rooms/nonmatchings/acropolis_square/acropolis_square_3", func_acropolis_square_80182048);
+
+INCLUDE_ASM("rooms/nonmatchings/acropolis_square/acropolis_square_3", func_acropolis_square_801820D8);
+
+INCLUDE_ASM("rooms/nonmatchings/acropolis_square/acropolis_square_3", func_acropolis_square_80182108);
+
+INCLUDE_ASM("rooms/nonmatchings/acropolis_square/acropolis_square_3", func_acropolis_square_80182110);
+
+INCLUDE_ASM("rooms/nonmatchings/acropolis_square/acropolis_square_3", func_acropolis_square_80182148);
+
+INCLUDE_ASM("rooms/nonmatchings/acropolis_square/acropolis_square_3", func_acropolis_square_80182200);
+
+INCLUDE_ASM("rooms/nonmatchings/acropolis_square/acropolis_square_3", func_acropolis_square_80182260);
+
+INCLUDE_ASM("rooms/nonmatchings/acropolis_square/acropolis_square_3", func_acropolis_square_801822A4);
+
+INCLUDE_ASM("rooms/nonmatchings/acropolis_square/acropolis_square_3", func_acropolis_square_80182308);
+
+INCLUDE_ASM("rooms/nonmatchings/acropolis_square/acropolis_square_3", func_acropolis_square_80182360);
+
+INCLUDE_ASM("rooms/nonmatchings/acropolis_square/acropolis_square_3", func_acropolis_square_801823DC);
+
+INCLUDE_ASM("rooms/nonmatchings/acropolis_square/acropolis_square_3", func_acropolis_square_801825DC);
+
+INCLUDE_ASM("rooms/nonmatchings/acropolis_square/acropolis_square_3", func_acropolis_square_8018344C);
+
+void func_acropolis_square_8018345C(void)
+{
+}
