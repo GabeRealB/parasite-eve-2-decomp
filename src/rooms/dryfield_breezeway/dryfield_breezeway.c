ae5ecd5fe5d32d3b02b0d57c117f282707c64d1c
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-30 19:51:13 +0200

    matched func_dryfield_breezeway_8017DCE4 1

--- src/rooms/dryfield_breezeway/dryfield_breezeway.c
+++ src/rooms/dryfield_breezeway/dryfield_breezeway.c
@@ -1,5 +1,9 @@
 #include "common.h"
 
+#include "gameplay/3CD8.h"
+#include "main/gameflag.h"
+#include "main/task.h"
+
 INCLUDE_ASM("rooms/nonmatchings/dryfield_breezeway/dryfield_breezeway", func_dryfield_breezeway_8017D638);
 
 INCLUDE_RODATA("rooms/nonmatchings/dryfield_breezeway/dryfield_breezeway", D_dryfield_breezeway_8017D5C0);
@@ -18,7 +22,29 @@ INCLUDE_ASM("rooms/nonmatchings/dryfield_breezeway/dryfield_breezeway", func_dry
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_breezeway/dryfield_breezeway", func_dryfield_breezeway_8017DC3C);
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_breezeway/dryfield_breezeway", func_dryfield_breezeway_8017DCE4);
+void func_dryfield_breezeway_8017DCE4(Task* task)
+{
+    switch (task->state) {
+        case 0:
+            Gp_RunCapCmd(task->spawnArg1, 0);
+            task->state++;
+            break;
+        case 1:
+            if (Gp_CapBusy() == 0) {
+                if (GameFlag_GetNibble(0x56) != 4) {
+                    if (Gp_GetCapEventKey() == 0xB) {
+                        GameFlag_SetNibble(0x56, 2);
+                    }
+                    if (GameFlag_GetNibble(0x56) == 5) {
+                        GameFlag_SetNibble(0x56, 6);
+                    }
+                }
+                Gp_MsgPlayerWeapon(1);
+                Task_Kill(task);
+            }
+            break;
+    }
+}
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_breezeway/dryfield_breezeway", func_dryfield_breezeway_8017DDB0);
 
