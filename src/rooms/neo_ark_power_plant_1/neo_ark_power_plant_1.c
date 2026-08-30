4cf946909700e37c954bd16601734ad7bd7af2de
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-30 09:32:46 +0200

    matched func_neo_ark_power_plant_1_8017D7F8 1

--- src/rooms/neo_ark_power_plant_1/neo_ark_power_plant_1.c
+++ src/rooms/neo_ark_power_plant_1/neo_ark_power_plant_1.c
@@ -2,9 +2,11 @@
 
 #include <psyq/libgte.h>
 
+#include "gameplay/3A34.h"
 #include "gameplay/3CD8.h"
 #include "gameplay/D4.h"
 #include "main/gameflag.h"
+#include "main/task.h"
 
 extern s32 Gp_LcgState;
 
@@ -21,7 +23,48 @@ INCLUDE_ASM("rooms/nonmatchings/neo_ark_power_plant_1/neo_ark_power_plant_1", fu
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_power_plant_1/neo_ark_power_plant_1", func_neo_ark_power_plant_1_8017D7B4);
 
-INCLUDE_ASM("rooms/nonmatchings/neo_ark_power_plant_1/neo_ark_power_plant_1", func_neo_ark_power_plant_1_8017D7F8);
+s32 func_neo_ark_power_plant_1_8017D7F8(Task* task, s32 msgId, s32 arg2, s32 arg3)
+{
+    s32 cmd;
+
+    switch (arg2) {
+        case 2:
+            if (GameFlag_GetNibble(0xDE) == 0) {
+                cmd = 2;
+            } else {
+                cmd = 5;
+            }
+            Gp_RunCapCmd1(cmd);
+            break;
+        case 3:
+            if (Gp_StateF0.field_0 == 2) {
+                cmd = 7;
+            } else if (GameFlag_GetNibble(0x148) != 0) {
+                cmd = 6;
+            } else {
+                cmd = 3;
+            }
+            Gp_RunCapCmd1(cmd);
+            break;
+        case 9:
+            if (GameFlag_GetNibble(0xDF) == 0) {
+                cmd = 9;
+            } else {
+                cmd = 0xB;
+            }
+            Gp_RunCapCmd1(cmd);
+            break;
+        case 12:
+            if (GameFlag_GetNibble(0xDF) != 0) {
+                cmd = 0xA;
+            } else {
+                cmd = 0xC;
+            }
+            Gp_RunCapCmd1(cmd);
+            break;
+    }
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_power_plant_1/neo_ark_power_plant_1", func_neo_ark_power_plant_1_8017D8C8);
 
