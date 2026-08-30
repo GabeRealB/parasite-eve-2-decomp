df5195085a9c0fdf566a88141c89fdd34f5f82e2
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-30 21:43:43 +0200

    matched Room_Snd01 1

--- src/rooms/lib/room_snd01.c
+++ src/rooms/lib/room_snd01.c
@@ -1,3 +1,31 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/lib/room_snd01", Room_Snd01);
+#include "gameplay/3CD8.h"
+
+#include "main/task.h"
+
+s32 Room_Snd01(Task* task, s32 msgId, s32 arg2, s32 arg3)
+{
+    switch (arg2) {
+        case 0x8:
+            Gp_EnqueueStageSnd6(0x52020008, 0, 0);
+            break;
+        case 0x9:
+            Gp_EnqueueStageSnd6(0x52020009, 0, 0);
+            /* fallthrough */
+        case 0xC:
+            Gp_EnqueueStageSnd6(0x5202000C, 0, 0);
+            break;
+        case 0x65:
+            if (Gp_GetCapEventKey() == 1) {
+                Gp_EnqueueStageSnd6(0x5202000D, 0, 0);
+            }
+            break;
+        case 0x78:
+            if (Gp_GetCapEventKey() == 0) {
+                Gp_EnqueueStageSnd6(0x5202000D, 0, 0);
+            }
+            break;
+    }
+    return 0;
+}
