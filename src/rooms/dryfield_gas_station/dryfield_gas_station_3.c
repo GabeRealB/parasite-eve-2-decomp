8caa4d5cd7b8fb41aa8f1ad94f58cbaf7af11593
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-30 07:57:49 +0200

    matched func_dryfield_gas_station_8017FB94 1

--- src/rooms/dryfield_gas_station/dryfield_gas_station_3.c
+++ src/rooms/dryfield_gas_station/dryfield_gas_station_3.c
@@ -1,8 +1,54 @@
 #include "common.h"
 
+#include "gameplay/3CD8.h"
+
 INCLUDE_ASM("rooms/nonmatchings/dryfield_gas_station/dryfield_gas_station_3", func_dryfield_gas_station_8017FA20);
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_gas_station/dryfield_gas_station_3", func_dryfield_gas_station_8017FB94);
+/// Maps a cap (cutscene) script event key to the stage sound it should play in
+/// the gas station, then enqueues it as a type-6 sound event. Event key 0x83
+/// only plays if a cap script is still reporting an event key. Keys with no
+/// sound are ignored. Always returns 0.
+s32 func_dryfield_gas_station_8017FB94(s32 arg0, s32 arg1, s32 arg2)
+{
+    s32 id;
+
+    switch (arg2) {
+        case 5:
+            id = 0x52010005;
+            goto play;
+        case 7:
+            id = 0x52010007;
+            goto play;
+        case 0xA:
+            id = 0x5201000A;
+            goto play;
+        case 0xD:
+            id = 0x5201000D;
+            goto play;
+        case 0x11:
+            id = 0x52010011;
+            goto play;
+        case 0x13:
+            id = 0x52010013;
+            goto play;
+        case 0x6D:
+        case 0x82:
+            id = 0x5201000B;
+            goto play;
+        case 0x73:
+            id = 0x5201000E;
+            goto play;
+        case 0x83:
+            if (Gp_GetCapEventKey() == 0) {
+                break;
+            }
+            id = 0x52010012;
+        play:
+            Gp_EnqueueStageSnd6(id, 0, 0);
+            break;
+    }
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_gas_station/dryfield_gas_station_3", func_dryfield_gas_station_8017FCBC);
 
