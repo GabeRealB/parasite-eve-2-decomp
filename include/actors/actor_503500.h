18b8f8b31fad3229f1cebe15f1a62123f879da51
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-30 22:26:46 +0200

    matched func_actor_503500_80136304 1

--- include/actors/actor_503500.h
+++ include/actors/actor_503500.h
@@ -3,16 +3,26 @@
 
 #include "common.h"
 
+#include "gameplay/3A34.h"
+
 typedef struct Actor503500Work {
     /* 0x000 */ byte pad_0[0xF0];
     /* 0x0F0 */ s8   field_F0;
     /* 0x0F1 */ byte pad_F1[0x130];
     /* 0x221 */ s8   field_221;
+    /* 0x222 */ byte pad_222[0x58E];
+    /* 0x7B0 */ s16  field_7B0;
+    /* 0x7B2 */ u16  field_7B2;
+    /* 0x7B4 */ byte pad_7B4[0x16];
+    /* 0x7CA */ s16  field_7CA;
+    /* 0x7CC */ byte pad_7CC[0x14];
+    /* 0x7E0 */ s8   field_7E0;
 } Actor503500Work;
 
 typedef struct Actor503500 {
     /* 0x00 */ byte             pad_0[0x1C];
     /* 0x1C */ Actor503500Work* field_1C;
+    /* 0x20 */ GpObj5D*         field_20;
 } Actor503500;
 
 void func_actor_503500_8013F8AC(Actor503500* arg0);
