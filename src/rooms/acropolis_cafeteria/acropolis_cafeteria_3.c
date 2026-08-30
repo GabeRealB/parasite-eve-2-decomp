395195fb18e618835856bc7b6cd030152d9e0524
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 00:33:47 +0200

    matched func_acropolis_cafeteria_8017E47C 1

--- src/rooms/acropolis_cafeteria/acropolis_cafeteria_3.c
+++ src/rooms/acropolis_cafeteria/acropolis_cafeteria_3.c
@@ -2,8 +2,6 @@
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_cafeteria/acropolis_cafeteria_3", func_acropolis_cafeteria_8017DF68);
 
-INCLUDE_RODATA("rooms/nonmatchings/acropolis_cafeteria/acropolis_cafeteria_3", jtbl_acropolis_cafeteria_8017D670);
-
 INCLUDE_RODATA("rooms/nonmatchings/acropolis_cafeteria/acropolis_cafeteria_3", jtbl_acropolis_cafeteria_8017D688);
 
 INCLUDE_RODATA("rooms/nonmatchings/acropolis_cafeteria/acropolis_cafeteria_3", D_acropolis_cafeteria_8017D69C);
