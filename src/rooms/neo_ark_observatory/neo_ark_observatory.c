0fb38e50c710987e3eb3815ed0ae21b579bcb7e8
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-30 20:01:08 +0200

    matched func_neo_ark_observatory_8017FB1C 1

--- src/rooms/neo_ark_observatory/neo_ark_observatory.c
+++ src/rooms/neo_ark_observatory/neo_ark_observatory.c
@@ -1,5 +1,8 @@
 #include "common.h"
 
+#include "gameplay/3CD8.h"
+#include "main/task.h"
+
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_observatory/neo_ark_observatory", func_neo_ark_observatory_8017D6F4);
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_observatory/neo_ark_observatory", func_neo_ark_observatory_8017D8A8);
@@ -18,7 +21,29 @@ INCLUDE_ASM("rooms/nonmatchings/neo_ark_observatory/neo_ark_observatory", func_n
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_observatory/neo_ark_observatory", func_neo_ark_observatory_8017FA98);
 
-INCLUDE_ASM("rooms/nonmatchings/neo_ark_observatory/neo_ark_observatory", func_neo_ark_observatory_8017FB1C);
+void func_neo_ark_observatory_8017FB1C(Task* task)
+{
+    switch (task->state) {
+        case 0:
+            Gp_CapFile = 0;
+            Gp_LoadCapFile(1);
+            func_800E6D4C(0x300, 0);
+            Gp_SpawnIfCapIdle(task->spawnArg1, 0);
+            task->state++;
+            break;
+        case 1:
+            if (Gp_CapBusy() != 0) {
+                break;
+            }
+            task->state++;
+            break;
+        case 2:
+            Gp_MsgPlayerWeapon(1);
+            Gp_ResetCap();
+            Task_Kill(task);
+            break;
+    }
+}
 
 INCLUDE_RODATA("rooms/nonmatchings/neo_ark_observatory/neo_ark_observatory", D_neo_ark_observatory_8017D698);
 
