3a4e3834be22dfa282b93729a0c3b514d56249a5
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-30 17:20:09 +0200

    matched func_actor_450200_80132220 1

--- src/actors/actor_450200/actor_450200.c
+++ src/actors/actor_450200/actor_450200.c
@@ -1,5 +1,13 @@
 #include "common.h"
 
+#include "gameplay/3CD8.h"
+#include "main/gameflag.h"
+
+extern s32 D_actor_450200_80138870;
+extern s32 D_actor_450200_80138A68;
+extern s32 D_actor_450200_80138C60;
+extern s32 D_actor_450200_80138E88;
+
 INCLUDE_ASM("actors/nonmatchings/actor_450200/actor_450200", func_actor_450200_80131E24);
 
 INCLUDE_ASM("actors/nonmatchings/actor_450200/actor_450200", func_actor_450200_80131FA8);
@@ -12,7 +20,26 @@ INCLUDE_ASM("actors/nonmatchings/actor_450200/actor_450200", func_actor_450200_8
 
 INCLUDE_ASM("actors/nonmatchings/actor_450200/actor_450200", func_actor_450200_8013219C);
 
-INCLUDE_ASM("actors/nonmatchings/actor_450200/actor_450200", func_actor_450200_80132220);
+void func_actor_450200_80132220(void)
+{
+    switch (GameFlag_GetNibble(0x101)) {
+        case 0:
+            func_800E8614((s32)&D_actor_450200_80138870, 0);
+            GameFlag_SetNibble(0x101, 1);
+            break;
+        case 1:
+            func_800E8614((s32)&D_actor_450200_80138A68, 0);
+            GameFlag_SetNibble(0x101, 2);
+            break;
+        case 2:
+            func_800E8614((s32)&D_actor_450200_80138C60, 0);
+            GameFlag_SetNibble(0x101, 3);
+            break;
+        case 3:
+            func_800E8614((s32)&D_actor_450200_80138E88, 0);
+            break;
+    }
+}
 
 INCLUDE_ASM("actors/nonmatchings/actor_450200/actor_450200", func_actor_450200_801322F8);
 
