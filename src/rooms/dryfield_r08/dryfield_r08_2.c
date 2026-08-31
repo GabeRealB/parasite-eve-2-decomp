14acee672e11a22dbbd548639c407d423d207baa
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 22:04:51 +0200

    matched func_dryfield_r08_8017F334 1

--- src/rooms/dryfield_r08/dryfield_r08_2.c
+++ src/rooms/dryfield_r08/dryfield_r08_2.c
@@ -1,8 +1,12 @@
 #include "common.h"
+extern s32 D_dryfield_r08_80180C24;
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_r08/dryfield_r08_2", func_dryfield_r08_8017EB68);
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_r08/dryfield_r08_2", func_dryfield_r08_8017F334);
+void func_dryfield_r08_8017F334(s32 arg0)
+{
+    D_dryfield_r08_80180C24 = arg0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_r08/dryfield_r08_2", func_dryfield_r08_8017F340);
 
