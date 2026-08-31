6749f51c38e0552264f1fd3ca7edf19d77f17c0d
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 19:41:07 +0200

    matched func_shelter_1f_heliport_s4_8017D624 1

--- src/rooms/shelter_1f_heliport_s4/shelter_1f_heliport_s4.c
+++ src/rooms/shelter_1f_heliport_s4/shelter_1f_heliport_s4.c
@@ -5,7 +5,10 @@ s32 func_shelter_1f_heliport_s4_8017D61C(void)
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_heliport_s4/shelter_1f_heliport_s4", func_shelter_1f_heliport_s4_8017D624);
+s32 func_shelter_1f_heliport_s4_8017D624(void)
+{
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_1f_heliport_s4/shelter_1f_heliport_s4", func_shelter_1f_heliport_s4_8017D62C);
 
