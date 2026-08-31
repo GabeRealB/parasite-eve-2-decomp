6d08bb99b3aed09f4fb74001cc7702e9b82b7e91
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 22:03:38 +0200

    matched func_tonfa_baton_8011DA48 1

--- src/weapons/tonfa_baton/tonfa_baton.c
+++ src/weapons/tonfa_baton/tonfa_baton.c
@@ -1,12 +1,28 @@
 #include "common.h"
 
+#include <psyq/libgte.h>
+
+#include "main/session.h"
 #include "main/task.h"
+
+void WeaponsShared8011db78(Task* task);
 
 INCLUDE_ASM("weapons/nonmatchings/tonfa_baton/tonfa_baton", func_tonfa_baton_8011D1EC);
 
 INCLUDE_ASM("weapons/nonmatchings/tonfa_baton/tonfa_baton", func_tonfa_baton_8011D6B0);
 
-INCLUDE_ASM("weapons/nonmatchings/tonfa_baton/tonfa_baton", func_tonfa_baton_8011DA48);
+void func_tonfa_baton_8011DA48(Task* arg0)
+{
+    GameActorExt* extra;
+    s32*          ptr;
+
+    extra              = (GameActorExt*)arg0->extra;
+    ptr                = extra->field_8;
+    arg0->state        = arg0->state + 1;
+    arg0->exitCallback = WeaponsShared8011db78;
+    *ptr               = 0;
+    extra->field_C     = 0;
+}
 
 INCLUDE_ASM("weapons/nonmatchings/tonfa_baton/tonfa_baton", func_tonfa_baton_8011DA74);
 
