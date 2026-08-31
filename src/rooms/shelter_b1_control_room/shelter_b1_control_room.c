e13d2c237e2355945dd2984392db6bf8f0ac7767
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 00:03:23 +0200

    matched func_shelter_b1_control_room_8017ECCC 1

--- src/rooms/shelter_b1_control_room/shelter_b1_control_room.c
+++ src/rooms/shelter_b1_control_room/shelter_b1_control_room.c
@@ -4,7 +4,10 @@ INCLUDE_ASM("rooms/nonmatchings/shelter_b1_control_room/shelter_b1_control_room"
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b1_control_room/shelter_b1_control_room", func_shelter_b1_control_room_8017D7B8);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b1_control_room/shelter_b1_control_room", func_shelter_b1_control_room_8017ECCC);
+s32 func_shelter_b1_control_room_8017ECCC(void)
+{
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b1_control_room/shelter_b1_control_room", func_shelter_b1_control_room_8017ECD4);
 
