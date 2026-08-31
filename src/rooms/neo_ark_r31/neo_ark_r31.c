471a4a32fb376122f901d0115ca948125209ae52
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 01:19:12 +0200

    matched func_neo_ark_r31_8017D8B0 1

--- src/rooms/neo_ark_r31/neo_ark_r31.c
+++ src/rooms/neo_ark_r31/neo_ark_r31.c
@@ -2,7 +2,10 @@
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_r31/neo_ark_r31", func_neo_ark_r31_8017D5D0);
 
-INCLUDE_ASM("rooms/nonmatchings/neo_ark_r31/neo_ark_r31", func_neo_ark_r31_8017D8B0);
+s32 func_neo_ark_r31_8017D8B0(void)
+{
+    return 0;
+}
 
 INCLUDE_RODATA("rooms/nonmatchings/neo_ark_r31/neo_ark_r31", D_neo_ark_r31_8017D5C0);
 
