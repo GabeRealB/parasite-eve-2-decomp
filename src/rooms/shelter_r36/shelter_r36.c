89916627cb174610d812cfc259878bc4e5a8332a
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 07:41:06 +0200

    matched func_shelter_r36_8017D5E8 1

--- src/rooms/shelter_r36/shelter_r36.c
+++ src/rooms/shelter_r36/shelter_r36.c
@@ -1,6 +1,53 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_r36/shelter_r36", func_shelter_r36_8017D5E8);
+#include "gameplay/3CD8.h"
+#include "main/gameflag.h"
+#include "main/session.h"
+#include "main/task.h"
+
+extern s32 D_shelter_r36_8017DF2C;
+extern s32 D_shelter_r36_8017E5A4;
+extern s32 D_shelter_r36_8017E664;
+extern s32 D_shelter_r36_8017E8BC;
+
+void func_shelter_r36_8017D5E8(Task* task)
+{
+    s32 state;
+    s16 slot;
+
+    state = task->state;
+    switch (state) {
+        case 0:
+            if (GameFlag_GetNibble(0x113) == 0) {
+                slot = 1;
+            } else {
+                Gp_CapFile = 0;
+                Gp_LoadCapFile(3);
+                func_800E6D4C(0x140, 0x100);
+                slot = 2;
+            }
+            Gp_StartCapSlot(slot, 0, 0);
+            func_800E8634((s32)&D_shelter_r36_8017DF2C, 0, (s32)&D_shelter_r36_8017E5A4);
+            task->state++;
+            break;
+        case 1:
+            if (Game_Session->field_1 == 0) {
+                if (GameFlag_GetNibble(0x113) == 0) {
+                    Game_Session->field_128 = 0xFF;
+                    Game_Session->field_12E = state;
+                    Task_Kill(task);
+                } else {
+                    Gp_ResetCap();
+                    task->state++;
+                }
+            }
+            break;
+        case 2:
+            func_800E8634((s32)&D_shelter_r36_8017E664, 0, (s32)&D_shelter_r36_8017E8BC);
+            Task_Kill(task);
+            break;
+    }
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_r36/shelter_r36", func_shelter_r36_8017D738);
 
