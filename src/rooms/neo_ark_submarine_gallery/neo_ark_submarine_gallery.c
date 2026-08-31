e2584095f9e9bb49244d6828fef9c0b7962543c3
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 23:56:08 +0200

    matched func_neo_ark_submarine_gallery_8017EED8 1

--- src/rooms/neo_ark_submarine_gallery/neo_ark_submarine_gallery.c
+++ src/rooms/neo_ark_submarine_gallery/neo_ark_submarine_gallery.c
@@ -1,4 +1,6 @@
 #include "common.h"
+#include "main/display.h"
+#include "gameplay/gameplay.h"
 
 INCLUDE_RODATA("rooms/nonmatchings/neo_ark_submarine_gallery/neo_ark_submarine_gallery", D_neo_ark_submarine_gallery_8017D5C0);
 
@@ -34,7 +36,15 @@ INCLUDE_ASM("rooms/nonmatchings/neo_ark_submarine_gallery/neo_ark_submarine_gall
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_submarine_gallery/neo_ark_submarine_gallery", func_neo_ark_submarine_gallery_8017EC24);
 
-INCLUDE_ASM("rooms/nonmatchings/neo_ark_submarine_gallery/neo_ark_submarine_gallery", func_neo_ark_submarine_gallery_8017EED8);
+void func_neo_ark_submarine_gallery_8017EED8(Task* arg0)
+{
+    if (Game_Session->field_9 != 4) {
+        arg0->killCountdown = 0;
+    } else {
+        arg0->killCountdown = 0x780;
+    }
+    arg0->state = (s32)(arg0->state + 1);
+}
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_submarine_gallery/neo_ark_submarine_gallery", func_neo_ark_submarine_gallery_8017EF14);
 
