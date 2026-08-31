b1765a911066b2f014ece66d39318d34fe39f2a0
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 05:47:34 +0200

    matched func_neo_ark_pyramid_8017DA44 1

--- src/rooms/neo_ark_pyramid/neo_ark_pyramid_2.c
+++ src/rooms/neo_ark_pyramid/neo_ark_pyramid_2.c
@@ -1,8 +1,31 @@
 #include "common.h"
 
+#include "gameplay/1A8.h"
+#include "gameplay/3CD8.h"
+
+#include "main/gameflag.h"
+#include "main/task.h"
+
+extern TaskDesc D_neo_ark_pyramid_8017FC0C;
+
+void func_neo_ark_pyramid_8017DAC0(s32 arg0);
+
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_pyramid/neo_ark_pyramid_2", func_neo_ark_pyramid_8017DA3C);
 
-INCLUDE_ASM("rooms/nonmatchings/neo_ark_pyramid/neo_ark_pyramid_2", func_neo_ark_pyramid_8017DA44);
+s32 func_neo_ark_pyramid_8017DA44(Task* task, s32 msgId, GpMsg13EF* arg2)
+{
+    if (arg2->field_2 == 1) {
+        func_neo_ark_pyramid_8017DAC0(0);
+        if (GameFlag_GetNibble(0xEC) == 4) {
+            Gp_SpawnIfCapIdle(3, 1);
+        } else {
+            Gp_MsgPlayerWeapon(0);
+            Gp_MsgPlayer3F3(0);
+            Task_SpawnFromTable(&D_neo_ark_pyramid_8017FC0C, 0, 0, 0);
+        }
+    }
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_pyramid/neo_ark_pyramid_2", func_neo_ark_pyramid_8017DAC0);
 
