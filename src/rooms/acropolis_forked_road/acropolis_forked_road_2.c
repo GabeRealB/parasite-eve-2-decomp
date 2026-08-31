ea9aa469925b6f898bc1d6f8cd3d2e419cee9c7d
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 21:52:56 +0200

    matched func_acropolis_forked_road_8017D970 1

--- src/rooms/acropolis_forked_road/acropolis_forked_road_2.c
+++ src/rooms/acropolis_forked_road/acropolis_forked_road_2.c
@@ -1,4 +1,8 @@
 #include "common.h"
+#include "main/fs.h"
+#include "gameplay/gameplay.h"
+extern s32      D_acropolis_forked_road_80180F3C;
+extern TaskDesc D_acropolis_forked_road_80180F44;
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_forked_road/acropolis_forked_road_2", func_acropolis_forked_road_8017D858);
 
@@ -6,7 +10,13 @@ INCLUDE_ASM("rooms/nonmatchings/acropolis_forked_road/acropolis_forked_road_2", 
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_forked_road/acropolis_forked_road_2", func_acropolis_forked_road_8017D92C);
 
-INCLUDE_ASM("rooms/nonmatchings/acropolis_forked_road/acropolis_forked_road_2", func_acropolis_forked_road_8017D970);
+void func_acropolis_forked_road_8017D970(void)
+{
+    if ((D_acropolis_forked_road_80180F3C == 0) && (Game_Session->field_8 == 2)) {
+        D_acropolis_forked_road_80180F3C = 1;
+        Task_SpawnFromTable(&D_acropolis_forked_road_80180F44, 2, 0, 0);
+    }
+}
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_forked_road/acropolis_forked_road_2", func_acropolis_forked_road_8017D9CC);
 
