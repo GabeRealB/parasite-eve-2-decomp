f9ad3a3a2f62622d5a5185f3f1d9423bbf7d9070
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 22:20:41 +0200

    matched func_shelter_b3_dumping_hole_8017D870 1

--- src/rooms/shelter_b3_dumping_hole/shelter_b3_dumping_hole_2.c
+++ src/rooms/shelter_b3_dumping_hole/shelter_b3_dumping_hole_2.c
@@ -1,5 +1,11 @@
 #include "common.h"
+#include "main/task.h"
+extern TaskDesc D_shelter_b3_dumping_hole_80189ADC;
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b3_dumping_hole/shelter_b3_dumping_hole_2", func_shelter_b3_dumping_hole_8017D870);
+s32 func_shelter_b3_dumping_hole_8017D870(void)
+{
+    Task_SpawnFromTable(&D_shelter_b3_dumping_hole_80189ADC, 0, 0, 0);
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b3_dumping_hole/shelter_b3_dumping_hole_2", func_shelter_b3_dumping_hole_8017D8A0);
