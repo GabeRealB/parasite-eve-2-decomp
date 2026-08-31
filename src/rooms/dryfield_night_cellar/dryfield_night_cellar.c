121103c30e0d1c753ba155c6f37f0ea3780485f3
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 22:28:47 +0200

    matched func_dryfield_night_cellar_8017D62C 1

--- src/rooms/dryfield_night_cellar/dryfield_night_cellar.c
+++ src/rooms/dryfield_night_cellar/dryfield_night_cellar.c
@@ -1,6 +1,9 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_night_cellar/dryfield_night_cellar", func_dryfield_night_cellar_8017D62C);
+s32 func_dryfield_night_cellar_8017D62C(void)
+{
+    return 0;
+}
 
 INCLUDE_RODATA("rooms/nonmatchings/dryfield_night_cellar/dryfield_night_cellar", D_dryfield_night_cellar_8017D5C0);
 
