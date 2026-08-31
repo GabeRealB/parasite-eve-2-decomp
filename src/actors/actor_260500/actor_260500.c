4bb08d818b343c5a3b4e81e76beddda0c83a962a
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 21:47:00 +0200

    matched func_actor_260500_80149E38 1

--- src/actors/actor_260500/actor_260500.c
+++ src/actors/actor_260500/actor_260500.c
@@ -1,6 +1,16 @@
 #include "common.h"
+#include "gameplay/3CD8.h"
 
-INCLUDE_ASM("actors/nonmatchings/actor_260500/actor_260500", func_actor_260500_80149E38);
+void func_actor_260500_80149E38(s32 arg0)
+{
+    if (arg0 != 0) {
+        Gp_CapFile = 0;
+        Gp_LoadCapFile(2);
+        func_800E6D4C(0x340, 0);
+        return;
+    }
+    Gp_ResetCap();
+}
 
 INCLUDE_ASM("actors/nonmatchings/actor_260500/actor_260500", func_actor_260500_80149E80);
 
