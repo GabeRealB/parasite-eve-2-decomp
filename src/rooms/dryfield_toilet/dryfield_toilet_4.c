c227825804d933857f004ad11de38ecdc1a075a6
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 23:55:34 +0200

    matched func_dryfield_toilet_8017E64C 1

--- src/rooms/dryfield_toilet/dryfield_toilet_4.c
+++ src/rooms/dryfield_toilet/dryfield_toilet_4.c
@@ -1,10 +1,22 @@
 #include "common.h"
+#include "main/task.h"
+extern s32 D_80115730;
+extern s32 D_80115734;
+extern s32 D_80115754;
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_toilet/dryfield_toilet_4", func_dryfield_toilet_8017DCF0);
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_toilet/dryfield_toilet_4", func_dryfield_toilet_8017DEF4);
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_toilet/dryfield_toilet_4", func_dryfield_toilet_8017E64C);
+void func_dryfield_toilet_8017E64C(Task* arg0)
+{
+    if (arg0->state == 0) {
+        D_80115734  = 0x6028A;
+        D_80115730  = 0x6028B;
+        D_80115754  = 0x6028C;
+        arg0->state = 1;
+    }
+}
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_toilet/dryfield_toilet_4", func_dryfield_toilet_8017E69C);
 
