b0a99ad5c66426899f58c4e18d855647cf3a2ad6
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 22:19:37 +0200

    matched func_neo_ark_pyramid_8017DAC0 1

--- src/rooms/neo_ark_pyramid/neo_ark_pyramid_2.c
+++ src/rooms/neo_ark_pyramid/neo_ark_pyramid_2.c
@@ -5,6 +5,7 @@
 
 #include "main/gameflag.h"
 #include "main/task.h"
+extern s32 D_neo_ark_pyramid_801818A4;
 
 extern TaskDesc D_neo_ark_pyramid_8017FC0C;
 
@@ -30,7 +31,10 @@ s32 func_neo_ark_pyramid_8017DA44(Task* task, s32 msgId, GpMsg13EF* arg2)
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/neo_ark_pyramid/neo_ark_pyramid_2", func_neo_ark_pyramid_8017DAC0);
+void func_neo_ark_pyramid_8017DAC0(s32 arg0)
+{
+    D_neo_ark_pyramid_801818A4 = (((GameFlag_GetNibble(0xEC) - 4) << 0xC) / 12) + arg0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_pyramid/neo_ark_pyramid_2", func_neo_ark_pyramid_8017DB18);
 
