6837e75ac8ffe87167036ed44af7b4826d9e2a2a
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 21:29:51 +0200

    matched func_actor_136100_801348C8 1

--- src/actors/actor_136100/actor_136100.c
+++ src/actors/actor_136100/actor_136100.c
@@ -1,4 +1,8 @@
 #include "common.h"
+#include "main/task.h"
+extern s16 D_actor_136100_8013F17C;
+
+extern TaskDesc D_actor_136100_80140744;
 
 INCLUDE_ASM("actors/nonmatchings/actor_136100/actor_136100", func_actor_136100_80131EC4);
 
@@ -52,9 +56,16 @@ INCLUDE_ASM("actors/nonmatchings/actor_136100/actor_136100", func_actor_136100_8
 
 INCLUDE_ASM("actors/nonmatchings/actor_136100/actor_136100", func_actor_136100_80134898);
 
-INCLUDE_ASM("actors/nonmatchings/actor_136100/actor_136100", func_actor_136100_801348C8);
+void func_actor_136100_801348C8(void)
+{
+    Task_SpawnFromTable(&D_actor_136100_80140744, 5, 9, 0);
+}
 
-INCLUDE_ASM("actors/nonmatchings/actor_136100/actor_136100", func_actor_136100_801348F8);
+void func_actor_136100_801348F8(void)
+{
+    D_actor_136100_8013F17C = 1;
+    SetDispMask(1);
+}
 
 INCLUDE_ASM("actors/nonmatchings/actor_136100/actor_136100", func_actor_136100_80134924);
 
