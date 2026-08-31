7b54bace1bf5afd4032faa3ccff6bdf5aa1957d2
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 23:56:11 +0200

    matched func_neo_ark_submarine_tunnel_8017F48C 1

--- src/rooms/neo_ark_submarine_tunnel/neo_ark_submarine_tunnel_2.c
+++ src/rooms/neo_ark_submarine_tunnel/neo_ark_submarine_tunnel_2.c
@@ -1,4 +1,8 @@
 #include "common.h"
+#include "main/task.h"
+extern s32 D_80115730;
+extern s32 D_80115734;
+extern s32 D_80115754;
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_submarine_tunnel/neo_ark_submarine_tunnel_2", func_neo_ark_submarine_tunnel_8017F2C8);
 
@@ -18,7 +22,15 @@ void func_neo_ark_submarine_tunnel_8017F414(void)
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_submarine_tunnel/neo_ark_submarine_tunnel_2", func_neo_ark_submarine_tunnel_8017F434);
 
-INCLUDE_ASM("rooms/nonmatchings/neo_ark_submarine_tunnel/neo_ark_submarine_tunnel_2", func_neo_ark_submarine_tunnel_8017F48C);
+void func_neo_ark_submarine_tunnel_8017F48C(Task* arg0)
+{
+    if (arg0->state == 0) {
+        D_80115734  = 0x6027F;
+        D_80115730  = 0x60280;
+        D_80115754  = 0x60281;
+        arg0->state = 1;
+    }
+}
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_submarine_tunnel/neo_ark_submarine_tunnel_2", func_neo_ark_submarine_tunnel_8017F4DC);
 
