4e62ee838115fc886d3592875024c6a3c1c1c3dc
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 23:13:04 +0200

    matched func_neo_ark_power_plant_1_8017D7AC 1

--- src/rooms/neo_ark_power_plant_1/neo_ark_power_plant_1.c
+++ src/rooms/neo_ark_power_plant_1/neo_ark_power_plant_1.c
@@ -2,7 +2,10 @@
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_power_plant_1/neo_ark_power_plant_1", func_neo_ark_power_plant_1_8017D5EC);
 
-INCLUDE_ASM("rooms/nonmatchings/neo_ark_power_plant_1/neo_ark_power_plant_1", func_neo_ark_power_plant_1_8017D7AC);
+s32 func_neo_ark_power_plant_1_8017D7AC(void)
+{
+    return 0;
+}
 
 INCLUDE_RODATA("rooms/nonmatchings/neo_ark_power_plant_1/neo_ark_power_plant_1", D_neo_ark_power_plant_1_8017D5C0);
 
