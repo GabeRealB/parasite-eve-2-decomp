e66ea7829ff9c7988adcafbaa7a3380e715c073b
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 23:28:19 +0200

    matched func_neo_ark_altar_8017D8BC 1

--- src/rooms/neo_ark_altar/neo_ark_altar_2.c
+++ src/rooms/neo_ark_altar/neo_ark_altar_2.c
@@ -1,6 +1,9 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/neo_ark_altar/neo_ark_altar_2", func_neo_ark_altar_8017D8BC);
+s32 func_neo_ark_altar_8017D8BC(void)
+{
+    return 0;
+}
 
 INCLUDE_RODATA("rooms/nonmatchings/neo_ark_altar/neo_ark_altar_2", jtbl_neo_ark_altar_8017D618);
 
