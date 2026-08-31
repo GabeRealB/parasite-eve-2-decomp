00470aedec30c30d7577faefda1cefbd70c51e88
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 19:23:14 +0200

    matched func_dryfield_night_general_store_8017DE2C 1

--- src/rooms/dryfield_night_general_store/dryfield_night_general_store_3.c
+++ src/rooms/dryfield_night_general_store/dryfield_night_general_store_3.c
@@ -5,7 +5,10 @@ s32 func_dryfield_night_general_store_8017DE24(void)
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_night_general_store/dryfield_night_general_store_3", func_dryfield_night_general_store_8017DE2C);
+s32 func_dryfield_night_general_store_8017DE2C(void)
+{
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_night_general_store/dryfield_night_general_store_3", func_dryfield_night_general_store_8017DE34);
 
