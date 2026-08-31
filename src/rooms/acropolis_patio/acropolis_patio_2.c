3f5ff0cf60eb4c2b623c640750b0dbcbecf5ea3c
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 06:31:11 +0200

    matched func_acropolis_patio_8017DD80 1

--- src/rooms/acropolis_patio/acropolis_patio_2.c
+++ src/rooms/acropolis_patio/acropolis_patio_2.c
@@ -1,8 +1,33 @@
 #include "common.h"
 
+#include "main/gameflag.h"
+#include "main/task.h"
+
+#include "gameplay/3CD8.h"
+
 INCLUDE_ASM("rooms/nonmatchings/acropolis_patio/acropolis_patio_2", func_acropolis_patio_8017DD4C);
 
-INCLUDE_ASM("rooms/nonmatchings/acropolis_patio/acropolis_patio_2", func_acropolis_patio_8017DD80);
+void func_acropolis_patio_8017DD80(Task* task)
+{
+    s32 state;
+
+    state = task->state;
+    switch (state) {
+        case 0:
+            Gp_RunCapCmd1(6);
+            task->state = task->state + 1;
+            return;
+        case 1:
+            task->state = 2;
+            return;
+        case 2:
+            if (Gp_GetCapEventKey() == 1) {
+                GameFlag_SetNibble(0x15, 1);
+            }
+            Task_Kill(task);
+            return;
+    }
+}
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_patio/acropolis_patio_2", func_acropolis_patio_8017DE2C);
 
