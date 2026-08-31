1f143406620e7fd4547fada1dffd1e035d6298f5
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 21:52:35 +0200

    matched func_acropolis_bridge_8017DD24 1

--- src/rooms/acropolis_bridge/acropolis_bridge_3.c
+++ src/rooms/acropolis_bridge/acropolis_bridge_3.c
@@ -1,4 +1,6 @@
 #include "common.h"
+#include "main/task.h"
+extern s32 D_acropolis_bridge_801917A0;
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_bridge/acropolis_bridge_3", func_acropolis_bridge_8017DA0C);
 
@@ -14,4 +16,14 @@ INCLUDE_ASM("rooms/nonmatchings/acropolis_bridge/acropolis_bridge_3", func_acrop
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_bridge/acropolis_bridge_3", func_acropolis_bridge_8017DC68);
 
-INCLUDE_ASM("rooms/nonmatchings/acropolis_bridge/acropolis_bridge_3", func_acropolis_bridge_8017DD24);
+void func_acropolis_bridge_8017DD24(Task* arg0)
+{
+    if (D_acropolis_bridge_801917A0 == 0) {
+        Gp_MsgPlayerWeapon(1);
+        Gp_MsgPlayer3F3(1);
+        Task_Kill(arg0);
+        return;
+    }
+    Gp_MsgPlayer3F3(1);
+    arg0->state += 1;
+}
