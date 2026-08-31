39a2b7dbf2f04b00f908568e192f7becf5fc2d60
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 00:51:24 +0200

    matched func_dryfield_night_motel_room_2_8017D668 1

--- src/rooms/dryfield_night_motel_room_2/dryfield_night_motel_room_2.c
+++ src/rooms/dryfield_night_motel_room_2/dryfield_night_motel_room_2.c
@@ -2,7 +2,10 @@
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_night_motel_room_2/dryfield_night_motel_room_2", func_dryfield_night_motel_room_2_8017D660);
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_night_motel_room_2/dryfield_night_motel_room_2", func_dryfield_night_motel_room_2_8017D668);
+s32 func_dryfield_night_motel_room_2_8017D668(void)
+{
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_night_motel_room_2/dryfield_night_motel_room_2", func_dryfield_night_motel_room_2_8017D670);
 
