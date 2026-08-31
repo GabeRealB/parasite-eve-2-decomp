a4dfe7a136bbeb8336d3a24f8917a32a850b3d16
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 23:04:00 +0200

    matched func_neo_ark_eve_elevator_8017D670 1

--- src/rooms/neo_ark_eve_elevator/neo_ark_eve_elevator.c
+++ src/rooms/neo_ark_eve_elevator/neo_ark_eve_elevator.c
@@ -7,7 +7,10 @@ s32 func_neo_ark_eve_elevator_8017D668(void)
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/neo_ark_eve_elevator/neo_ark_eve_elevator", func_neo_ark_eve_elevator_8017D670);
+s32 func_neo_ark_eve_elevator_8017D670(void)
+{
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_eve_elevator/neo_ark_eve_elevator", func_neo_ark_eve_elevator_8017D678);
 
