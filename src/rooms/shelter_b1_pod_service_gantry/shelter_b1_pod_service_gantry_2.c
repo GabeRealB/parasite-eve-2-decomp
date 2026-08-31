24e610dc2679e4ccbcb640a463ee16ffe70231dc
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 22:19:58 +0200

    matched func_shelter_b1_pod_service_gantry_8017D81C 1

--- src/rooms/shelter_b1_pod_service_gantry/shelter_b1_pod_service_gantry_2.c
+++ src/rooms/shelter_b1_pod_service_gantry/shelter_b1_pod_service_gantry_2.c
@@ -1,4 +1,7 @@
 #include "common.h"
+#include "main/fs.h"
+#include "gameplay/gameplay.h"
+extern void D_shelter_b1_pod_service_gantry_8017FAF4;
 
 s32 func_shelter_b1_pod_service_gantry_8017D80C(void)
 {
@@ -10,7 +13,22 @@ s32 func_shelter_b1_pod_service_gantry_8017D814(void)
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b1_pod_service_gantry/shelter_b1_pod_service_gantry_2", func_shelter_b1_pod_service_gantry_8017D81C);
+void func_shelter_b1_pod_service_gantry_8017D81C(Task* arg0)
+{
+    TaskIdMap* temp_v0;
+
+    arg0->field_24 = &D_shelter_b1_pod_service_gantry_8017FAF4;
+    Game_SetPtrSlot(arg0, 7);
+    temp_v0     = Mem_Malloc(8U, false);
+    arg0->idMap = temp_v0;
+    if (temp_v0 == NULL) {
+        Task_Kill(arg0);
+        return;
+    }
+    Mem_Set(temp_v0, 0U, 8U);
+    SetDispMask(0);
+    arg0->state += 1;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b1_pod_service_gantry/shelter_b1_pod_service_gantry_2", func_shelter_b1_pod_service_gantry_8017D89C);
 
