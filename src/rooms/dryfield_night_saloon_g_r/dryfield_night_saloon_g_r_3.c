0f6bffe765e92fc456d6e839675ceae0ad4bd092
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 22:04:41 +0200

    matched func_dryfield_night_saloon_g_r_8017E698 1

--- src/rooms/dryfield_night_saloon_g_r/dryfield_night_saloon_g_r_3.c
+++ src/rooms/dryfield_night_saloon_g_r/dryfield_night_saloon_g_r_3.c
@@ -1,4 +1,6 @@
 #include "common.h"
+#include "main/task.h"
+extern TaskDesc D_dryfield_night_saloon_g_r_80185068;
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_night_saloon_g_r/dryfield_night_saloon_g_r_3", func_dryfield_night_saloon_g_r_8017E050);
 
@@ -10,6 +12,10 @@ INCLUDE_ASM("rooms/nonmatchings/dryfield_night_saloon_g_r/dryfield_night_saloon_
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_night_saloon_g_r/dryfield_night_saloon_g_r_3", func_dryfield_night_saloon_g_r_8017E564);
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_night_saloon_g_r/dryfield_night_saloon_g_r_3", func_dryfield_night_saloon_g_r_8017E698);
+s32 func_dryfield_night_saloon_g_r_8017E698(void)
+{
+    Display_InitModeObj(&D_dryfield_night_saloon_g_r_80185068, 0, 0, 0);
+    return 1;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_night_saloon_g_r/dryfield_night_saloon_g_r_3", func_dryfield_night_saloon_g_r_8017E6C8);
