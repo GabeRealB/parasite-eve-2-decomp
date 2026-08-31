be7a9a005b5c1cb1bdadc73e4f9b4f2d409ae108
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 01:30:36 +0200

    matched func_dryfield_night_back_street_8017D734 1

--- src/rooms/dryfield_night_back_street/dryfield_night_back_street.c
+++ src/rooms/dryfield_night_back_street/dryfield_night_back_street.c
@@ -12,7 +12,10 @@ s32 func_dryfield_night_back_street_8017D72C(void)
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_night_back_street/dryfield_night_back_street", func_dryfield_night_back_street_8017D734);
+s32 func_dryfield_night_back_street_8017D734(void)
+{
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_night_back_street/dryfield_night_back_street", func_dryfield_night_back_street_8017D73C);
 
