c3097805a789adcd2ff67cf5c1aa0a57105d465c
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 22:05:48 +0200

    matched Room_Script03 1

--- src/rooms/lib/room_script03.c
+++ src/rooms/lib/room_script03.c
@@ -1,3 +1,14 @@
 #include "common.h"
+#include "main/task.h"
 
-INCLUDE_ASM("rooms/nonmatchings/lib/room_script03", Room_Script03);
+void Room_Script03(Task* arg0)
+{
+    u16 temp_v0;
+
+    Fade_DrawOverlay(0xFF, 0xFF, 0xFF, 2);
+    temp_v0             = arg0->killCountdown + 4;
+    arg0->killCountdown = temp_v0;
+    if ((s16)temp_v0 >= 0x100) {
+        Task_Kill(arg0);
+    }
+}
