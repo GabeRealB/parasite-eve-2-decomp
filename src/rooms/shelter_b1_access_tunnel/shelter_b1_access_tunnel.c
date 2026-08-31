9f2c69d73eddba7a636e5f9c15a64d94329d8866
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 18:23:09 +0200

    matched func_shelter_b1_access_tunnel_8017DCA4 1

--- src/rooms/shelter_b1_access_tunnel/shelter_b1_access_tunnel.c
+++ src/rooms/shelter_b1_access_tunnel/shelter_b1_access_tunnel.c
@@ -20,7 +20,10 @@ INCLUDE_ASM("rooms/nonmatchings/shelter_b1_access_tunnel/shelter_b1_access_tunne
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b1_access_tunnel/shelter_b1_access_tunnel", func_shelter_b1_access_tunnel_8017DA68);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b1_access_tunnel/shelter_b1_access_tunnel", func_shelter_b1_access_tunnel_8017DCA4);
+s32 func_shelter_b1_access_tunnel_8017DCA4(void)
+{
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b1_access_tunnel/shelter_b1_access_tunnel", func_shelter_b1_access_tunnel_8017DCAC);
 
