3f3fdb20317a4776784983cbfeacddcf993bd92c
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 22:17:34 +0200

    matched func_dryfield_motel_lobby_8017F43C 1

--- src/rooms/dryfield_motel_lobby/dryfield_motel_lobby_5.c
+++ src/rooms/dryfield_motel_lobby/dryfield_motel_lobby_5.c
@@ -1,6 +1,9 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_motel_lobby/dryfield_motel_lobby_5", func_dryfield_motel_lobby_8017F43C);
+s32 func_dryfield_motel_lobby_8017F43C(void)
+{
+    return 0;
+}
 
 s32 func_dryfield_motel_lobby_8017F444(void)
 {
