224d2cf59c725c16e4b3616637db605d4b57c47f
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 21:53:34 +0200

    matched func_acropolis_helicopter_landing_pad_8017E5E8 1

--- src/rooms/acropolis_helicopter_landing_pad/acropolis_helicopter_landing_pad_2.c
+++ src/rooms/acropolis_helicopter_landing_pad/acropolis_helicopter_landing_pad_2.c
@@ -8,7 +8,10 @@ INCLUDE_ASM("rooms/nonmatchings/acropolis_helicopter_landing_pad/acropolis_helic
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_helicopter_landing_pad/acropolis_helicopter_landing_pad_2", func_acropolis_helicopter_landing_pad_8017E5B8);
 
-INCLUDE_ASM("rooms/nonmatchings/acropolis_helicopter_landing_pad/acropolis_helicopter_landing_pad_2", func_acropolis_helicopter_landing_pad_8017E5E8);
+void func_acropolis_helicopter_landing_pad_8017E5E8(void)
+{
+    Task_SpawnFromTable(&D_acropolis_helicopter_landing_pad_80184DA0, 1, 0, 0);
+}
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_helicopter_landing_pad/acropolis_helicopter_landing_pad_2", func_acropolis_helicopter_landing_pad_8017E618);
 
