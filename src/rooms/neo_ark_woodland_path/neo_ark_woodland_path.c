cd91f38dd4c8639c13d705dc748dd1386efa50b4
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 19:56:56 +0200

    matched func_neo_ark_woodland_path_8017E888 1

--- src/rooms/neo_ark_woodland_path/neo_ark_woodland_path.c
+++ src/rooms/neo_ark_woodland_path/neo_ark_woodland_path.c
@@ -6,7 +6,10 @@ INCLUDE_ASM("rooms/nonmatchings/neo_ark_woodland_path/neo_ark_woodland_path", fu
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_woodland_path/neo_ark_woodland_path", func_neo_ark_woodland_path_8017E2E8);
 
-INCLUDE_ASM("rooms/nonmatchings/neo_ark_woodland_path/neo_ark_woodland_path", func_neo_ark_woodland_path_8017E888);
+s32 func_neo_ark_woodland_path_8017E888(void)
+{
+    return 0;
+}
 
 INCLUDE_RODATA("rooms/nonmatchings/neo_ark_woodland_path/neo_ark_woodland_path", D_neo_ark_woodland_path_8017D614);
 
