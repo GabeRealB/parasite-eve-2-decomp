6e1681fe1756efd3a3688cd99c4013336e1da080
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 20:36:16 +0200

    matched func_shelter_b1_pod_service_gantry_8017D80C 1

--- src/rooms/shelter_b1_pod_service_gantry/shelter_b1_pod_service_gantry_2.c
+++ src/rooms/shelter_b1_pod_service_gantry/shelter_b1_pod_service_gantry_2.c
@@ -1,6 +1,9 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b1_pod_service_gantry/shelter_b1_pod_service_gantry_2", func_shelter_b1_pod_service_gantry_8017D80C);
+s32 func_shelter_b1_pod_service_gantry_8017D80C(void)
+{
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b1_pod_service_gantry/shelter_b1_pod_service_gantry_2", func_shelter_b1_pod_service_gantry_8017D814);
 
