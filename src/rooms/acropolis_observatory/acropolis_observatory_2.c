c971a08bd7fa1b59fd192ed32192f692f4779578
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 21:54:08 +0200

    matched func_acropolis_observatory_8017D8AC 1

--- src/rooms/acropolis_observatory/acropolis_observatory_2.c
+++ src/rooms/acropolis_observatory/acropolis_observatory_2.c
@@ -1,10 +1,24 @@
 #include "common.h"
+#include "main/fs.h"
+#include "gameplay/gameplay.h"
+extern s32      D_acropolis_observatory_8017E7D8;
+extern TaskDesc D_acropolis_observatory_8017E7DC;
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_observatory/acropolis_observatory_2", func_acropolis_observatory_8017D7C4);
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_observatory/acropolis_observatory_2", func_acropolis_observatory_8017D834);
 
-INCLUDE_ASM("rooms/nonmatchings/acropolis_observatory/acropolis_observatory_2", func_acropolis_observatory_8017D8AC);
+void func_acropolis_observatory_8017D8AC(void)
+{
+    if ((D_acropolis_observatory_8017E7D8 == 0) && (Game_Session->field_8 == 3)) {
+        D_acropolis_observatory_8017E7D8 = 1;
+        Task_SpawnFromTable(&D_acropolis_observatory_8017E7DC, 1, 0, 0);
+    }
+    if ((D_acropolis_observatory_8017E7D8 == 0) && (Game_Session->field_8 == 4)) {
+        D_acropolis_observatory_8017E7D8 = 1;
+        Task_SpawnFromTable(&D_acropolis_observatory_8017E7DC, 0, 0, 0);
+    }
+}
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_observatory/acropolis_observatory_2", func_acropolis_observatory_8017D950);
 
