59f682a4f37691ae756c237630ddc397da7a1eb1
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 22:04:28 +0200

    matched func_dryfield_dilapidated_house_8017EA10 1

--- src/rooms/dryfield_dilapidated_house/dryfield_dilapidated_house_2.c
+++ src/rooms/dryfield_dilapidated_house/dryfield_dilapidated_house_2.c
@@ -1,4 +1,7 @@
 #include "common.h"
+#include "main/display.h"
+extern Task*    D_dryfield_dilapidated_house_801857E8;
+extern TaskDesc D_dryfield_dilapidated_house_80186854;
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_dilapidated_house/dryfield_dilapidated_house_2", func_dryfield_dilapidated_house_8017E8E8);
 
@@ -6,4 +9,14 @@ INCLUDE_ASM("rooms/nonmatchings/dryfield_dilapidated_house/dryfield_dilapidated_
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_dilapidated_house/dryfield_dilapidated_house_2", func_dryfield_dilapidated_house_8017E9A4);
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_dilapidated_house/dryfield_dilapidated_house_2", func_dryfield_dilapidated_house_8017EA10);
+void func_dryfield_dilapidated_house_8017EA10(s32 arg0)
+{
+    if (arg0 != 0) {
+        D_dryfield_dilapidated_house_801857E8 = Task_SpawnFromTable(&D_dryfield_dilapidated_house_80186854, 0, 3, Game_GetPtrSlot(3));
+        return;
+    }
+    if (D_dryfield_dilapidated_house_801857E8 != NULL) {
+        Task_Kill(D_dryfield_dilapidated_house_801857E8);
+        D_dryfield_dilapidated_house_801857E8 = NULL;
+    }
+}
