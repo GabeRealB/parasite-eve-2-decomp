a2ae8e63d8d7e83ce42719731cfd9cdddd33b732
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 19:37:42 +0200

    matched func_shelter_1f_heliport_s4_8017D61C 1

--- src/rooms/shelter_1f_heliport_s4/shelter_1f_heliport_s4.c
+++ src/rooms/shelter_1f_heliport_s4/shelter_1f_heliport_s4.c
@@ -1,6 +1,9 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_heliport_s4/shelter_1f_heliport_s4", func_shelter_1f_heliport_s4_8017D61C);
+s32 func_shelter_1f_heliport_s4_8017D61C(void)
+{
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_1f_heliport_s4/shelter_1f_heliport_s4", func_shelter_1f_heliport_s4_8017D624);
 
