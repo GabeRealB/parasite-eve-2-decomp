2cee6a89de93b1dc688764914cbc0ecd55775a21
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 22:21:01 +0200

    matched func_shelter_b6_nursery_8017FFF4 1

--- src/rooms/shelter_b6_nursery/shelter_b6_nursery_7.c
+++ src/rooms/shelter_b6_nursery/shelter_b6_nursery_7.c
@@ -1,8 +1,17 @@
 #include "common.h"
+#include "main/task.h"
+extern TaskDesc D_shelter_b6_nursery_80185000;
+extern s32      D_shelter_b6_nursery_8018797C;
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b6_nursery/shelter_b6_nursery_7", func_shelter_b6_nursery_8017FF9C);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b6_nursery/shelter_b6_nursery_7", func_shelter_b6_nursery_8017FFF4);
+void func_shelter_b6_nursery_8017FFF4(void)
+{
+    if (D_shelter_b6_nursery_8018797C == 0) {
+        D_shelter_b6_nursery_8018797C = 1;
+        Task_SpawnFromTable(&D_shelter_b6_nursery_80185000, 0, 0, 0);
+    }
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b6_nursery/shelter_b6_nursery_7", func_shelter_b6_nursery_80180038);
 
