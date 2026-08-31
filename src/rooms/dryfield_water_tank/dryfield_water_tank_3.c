8983f383b66a39e97a9de517ec27e33c624a2497
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 22:04:55 +0200

    matched func_dryfield_water_tank_8017EC38 1

--- src/rooms/dryfield_water_tank/dryfield_water_tank_3.c
+++ src/rooms/dryfield_water_tank/dryfield_water_tank_3.c
@@ -1,4 +1,6 @@
 #include "common.h"
+#include "main/task.h"
+extern TaskDesc D_dryfield_water_tank_80184DF4;
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_water_tank/dryfield_water_tank_3", func_dryfield_water_tank_8017E174);
 
@@ -20,7 +22,10 @@ INCLUDE_ASM("rooms/nonmatchings/dryfield_water_tank/dryfield_water_tank_3", func
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_water_tank/dryfield_water_tank_3", func_dryfield_water_tank_8017EBA0);
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_water_tank/dryfield_water_tank_3", func_dryfield_water_tank_8017EC38);
+void func_dryfield_water_tank_8017EC38(u32 arg0)
+{
+    Task_SpawnFromTable(&D_dryfield_water_tank_80184DF4, arg0 & 0xFFFF, (s32)(arg0 >> 0x10), 0);
+}
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_water_tank/dryfield_water_tank_3", func_dryfield_water_tank_8017EC6C);
 
