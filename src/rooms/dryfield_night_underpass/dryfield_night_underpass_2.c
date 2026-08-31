222cd86036b69dd7a368453f2d47225913ad3c85
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 20:52:33 +0200

    matched func_dryfield_night_underpass_8017D908 1

--- src/rooms/dryfield_night_underpass/dryfield_night_underpass_2.c
+++ src/rooms/dryfield_night_underpass/dryfield_night_underpass_2.c
@@ -2,7 +2,10 @@
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_night_underpass/dryfield_night_underpass_2", func_dryfield_night_underpass_8017D900);
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_night_underpass/dryfield_night_underpass_2", func_dryfield_night_underpass_8017D908);
+s32 func_dryfield_night_underpass_8017D908(void)
+{
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_night_underpass/dryfield_night_underpass_2", func_dryfield_night_underpass_8017D910);
 
