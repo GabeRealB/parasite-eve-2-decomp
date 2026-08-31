a2c8fb570fbe5a4f3972f139efd8a84856437bd0
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 21:37:41 +0200

    matched func_actor_151000_80131EE0 1

--- src/actors/actor_151000/actor_151000.c
+++ src/actors/actor_151000/actor_151000.c
@@ -1,8 +1,17 @@
 #include "common.h"
+#include "main/task.h"
+extern TaskDesc D_actor_151000_80133360;
+extern s32      D_actor_151000_8013D378;
 
 INCLUDE_ASM("actors/nonmatchings/actor_151000/actor_151000", func_actor_151000_80131E2C);
 
-INCLUDE_ASM("actors/nonmatchings/actor_151000/actor_151000", func_actor_151000_80131EE0);
+void func_actor_151000_80131EE0(s32 arg0)
+{
+    D_actor_151000_8013D378 = arg0;
+    if (arg0 != 0) {
+        Task_SpawnFromTable(&D_actor_151000_80133360, 0, 0, 0);
+    }
+}
 
 INCLUDE_ASM("actors/nonmatchings/actor_151000/actor_151000", func_actor_151000_80131F1C);
 
