12d46e6650f62fd39026fc34bc79b69034393ee9
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 23:56:02 +0200

    matched func_neo_ark_south_promenade_8017D6D0 1

--- src/rooms/neo_ark_south_promenade/neo_ark_south_promenade.c
+++ src/rooms/neo_ark_south_promenade/neo_ark_south_promenade.c
@@ -1,4 +1,8 @@
 #include "common.h"
+#include "main/task.h"
+extern s32 D_8011572C;
+extern s32 D_80115750;
+extern s32 D_80115758;
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_south_promenade/neo_ark_south_promenade", func_neo_ark_south_promenade_8017D61C);
 
@@ -14,6 +18,14 @@ INCLUDE_RODATA("rooms/nonmatchings/neo_ark_south_promenade/neo_ark_south_promena
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_south_promenade/neo_ark_south_promenade", func_neo_ark_south_promenade_8017D678);
 
-INCLUDE_ASM("rooms/nonmatchings/neo_ark_south_promenade/neo_ark_south_promenade", func_neo_ark_south_promenade_8017D6D0);
+void func_neo_ark_south_promenade_8017D6D0(Task* arg0)
+{
+    if (arg0->state == 0) {
+        D_80115758  = 0x601DE;
+        D_8011572C  = 0x601FA;
+        D_80115750  = 0x60216;
+        arg0->state = 1;
+    }
+}
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_south_promenade/neo_ark_south_promenade", func_neo_ark_south_promenade_8017D720);
