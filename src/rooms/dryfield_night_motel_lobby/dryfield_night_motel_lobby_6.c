da10a6148c757f53b676895823e127ebe9c421b2
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 08:28:10 +0200

    matched func_dryfield_night_motel_lobby_8017FC6C 1

--- src/rooms/dryfield_night_motel_lobby/dryfield_night_motel_lobby_6.c
+++ src/rooms/dryfield_night_motel_lobby/dryfield_night_motel_lobby_6.c
@@ -1,8 +1,28 @@
 #include "common.h"
 
+#include "gameplay/1A8.h"
+#include "gameplay/3CD8.h"
+
+#include "main/gameflag.h"
+#include "main/task.h"
+
+extern TaskDesc D_dryfield_night_motel_lobby_801827FC;
+
 INCLUDE_ASM("rooms/nonmatchings/dryfield_night_motel_lobby/dryfield_night_motel_lobby_6", func_dryfield_night_motel_lobby_8017FB7C);
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_night_motel_lobby/dryfield_night_motel_lobby_6", func_dryfield_night_motel_lobby_8017FC6C);
+s32 func_dryfield_night_motel_lobby_8017FC6C(Task* task, s32 msgId, GpMsg13EF* arg2)
+{
+    if (arg2->field_2 == 1) {
+        if (GameFlag_GetNibble(0x74) == 0) {
+            Gp_MsgPlayerWeapon(0);
+            Gp_MsgPlayer3F3(0);
+            Task_SpawnFromTable(&D_dryfield_night_motel_lobby_801827FC, 0, 0, 0);
+        } else {
+            Gp_RunCapCmd1(8);
+        }
+    }
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_night_motel_lobby/dryfield_night_motel_lobby_6", func_dryfield_night_motel_lobby_8017FCDC);
 
