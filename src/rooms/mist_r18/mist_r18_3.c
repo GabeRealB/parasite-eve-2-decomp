9481c56464ff397dfd3efcbb3e531f588ba95cc4
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 22:19:23 +0200

    matched func_mist_r18_8017EBB8 1

--- src/rooms/mist_r18/mist_r18_3.c
+++ src/rooms/mist_r18/mist_r18_3.c
@@ -1,4 +1,6 @@
 #include "common.h"
+#include "main/fs.h"
+#include "main/session.h"
 #include "main/task.h"
 extern Task* D_mist_r18_80186E98;
 
@@ -16,6 +18,11 @@ INCLUDE_ASM("rooms/nonmatchings/mist_r18/mist_r18_3", func_mist_r18_8017EA98);
 
 INCLUDE_ASM("rooms/nonmatchings/mist_r18/mist_r18_3", func_mist_r18_8017EB48);
 
-INCLUDE_ASM("rooms/nonmatchings/mist_r18/mist_r18_3", func_mist_r18_8017EBB8);
+void func_mist_r18_8017EBB8(void)
+{
+    Game_Session->field_52 = 1;
+    CdCmd_StartOverlay(1U, 0x1EU, 0xBU);
+    CdCmd_EnqueueReplaceOverlay82();
+}
 
 INCLUDE_ASM("rooms/nonmatchings/mist_r18/mist_r18_3", func_mist_r18_8017EBF8);
