9fc112439477193a81f01bccf2b4e41a63dd169a
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 18:02:25 +0200

    matched func_dryfield_g_r_kitchen_8017D8BC 1

--- src/rooms/dryfield_g_r_kitchen/dryfield_g_r_kitchen.c
+++ src/rooms/dryfield_g_r_kitchen/dryfield_g_r_kitchen.c
@@ -6,7 +6,10 @@ INCLUDE_RODATA("rooms/nonmatchings/dryfield_g_r_kitchen/dryfield_g_r_kitchen", D
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_g_r_kitchen/dryfield_g_r_kitchen", func_dryfield_g_r_kitchen_8017D74C);
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_g_r_kitchen/dryfield_g_r_kitchen", func_dryfield_g_r_kitchen_8017D8BC);
+s32 func_dryfield_g_r_kitchen_8017D8BC(void)
+{
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_g_r_kitchen/dryfield_g_r_kitchen", func_dryfield_g_r_kitchen_8017D8C4);
 
