497105473d9c157510e0fe2a303968a154465c63
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 23:21:18 +0200

    matched func_dryfield_toilet_8017D8B8 1

--- src/rooms/dryfield_toilet/dryfield_toilet_2.c
+++ src/rooms/dryfield_toilet/dryfield_toilet_2.c
@@ -1,6 +1,9 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_toilet/dryfield_toilet_2", func_dryfield_toilet_8017D8B8);
+s32 func_dryfield_toilet_8017D8B8(void)
+{
+    return 0;
+}
 
 s32 func_dryfield_toilet_8017D8C0(void)
 {
