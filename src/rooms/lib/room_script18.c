42e548226115c219c1d6f22c4476b683d00dc638
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 22:06:13 +0200

    matched Room_Script18 1

--- src/rooms/lib/room_script18.c
+++ src/rooms/lib/room_script18.c
@@ -1,3 +1,12 @@
 #include "common.h"
+#include "main/task.h"
 
-INCLUDE_ASM("rooms/nonmatchings/lib/room_script18", Room_Script18);
+void Room_Script18(Task* arg0)
+{
+    if (Gp_CapBusy() == 0) {
+        if (Gp_GetCapEventKey() != 0xC) {
+            GameFlag_SetNibble(arg0->spawnArg1, 2);
+        }
+        Task_Kill(arg0);
+    }
+}
