b5619067fab29c1790691726857bee5f8894def1
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 08:14:51 +0200

    matched func_actor_450200_801322F8 1

--- src/actors/actor_450200/actor_450200.c
+++ src/actors/actor_450200/actor_450200.c
@@ -2,11 +2,18 @@
 
 #include "gameplay/3CD8.h"
 #include "main/gameflag.h"
+#include "main/session.h"
+#include "main/task.h"
 
-extern s32 D_actor_450200_80138870;
-extern s32 D_actor_450200_80138A68;
-extern s32 D_actor_450200_80138C60;
-extern s32 D_actor_450200_80138E88;
+extern void func_8017FA98(s32);
+
+extern s32      D_actor_450200_80138870;
+extern s32      D_actor_450200_80138A68;
+extern s32      D_actor_450200_80138C60;
+extern s32      D_actor_450200_80138E88;
+extern s32      D_actor_450200_80139098;
+extern TaskDesc D_actor_450200_80137A60;
+extern Task*    D_actor_450200_801401E0;
 
 INCLUDE_ASM("actors/nonmatchings/actor_450200/actor_450200", func_actor_450200_80131E24);
 
@@ -41,7 +48,17 @@ void func_actor_450200_80132220(void)
     }
 }
 
-INCLUDE_ASM("actors/nonmatchings/actor_450200/actor_450200", func_actor_450200_801322F8);
+void func_actor_450200_801322F8(void)
+{
+    if (GameFlag_GetNibble(0xD7) != 0) {
+        func_800E8614((s32)&D_actor_450200_80139098, 1);
+    } else {
+        func_8017FA98(0);
+    }
+    if (Game_GetPtrSlot(0xA) != NULL) {
+        D_actor_450200_801401E0 = Task_SpawnFromTable(&D_actor_450200_80137A60, 2, 0, 0);
+    }
+}
 
 INCLUDE_ASM("actors/nonmatchings/actor_450200/actor_450200", func_actor_450200_80132368);
 
