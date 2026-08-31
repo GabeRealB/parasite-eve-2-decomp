7750b47df9b067687b7bc3a63e9ca33a2eeaf630
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 22:07:55 +0200

    matched func_mine_cavern_8017E2D8 1

--- src/rooms/mine_cavern/mine_cavern_4.c
+++ src/rooms/mine_cavern/mine_cavern_4.c
@@ -2,4 +2,7 @@
 
 INCLUDE_ASM("rooms/nonmatchings/mine_cavern/mine_cavern_4", func_mine_cavern_8017E18C);
 
-INCLUDE_ASM("rooms/nonmatchings/mine_cavern/mine_cavern_4", func_mine_cavern_8017E2D8);
+void func_mine_cavern_8017E2D8(void)
+{
+    SndEvt_EnqueueType2(0, 0x64);
+}
