b8bb5ac7dd843ef0edd3c0faa0278e667138381f
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 01:01:20 +0200

    matched func_shelter_b1_south_maintenance_walkway_8017D9D0 1

--- src/rooms/shelter_b1_south_maintenance_walkway/shelter_b1_south_maintenance_walkway.c
+++ src/rooms/shelter_b1_south_maintenance_walkway/shelter_b1_south_maintenance_walkway.c
@@ -6,7 +6,10 @@ INCLUDE_ASM("rooms/nonmatchings/shelter_b1_south_maintenance_walkway/shelter_b1_
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b1_south_maintenance_walkway/shelter_b1_south_maintenance_walkway", func_shelter_b1_south_maintenance_walkway_8017D790);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b1_south_maintenance_walkway/shelter_b1_south_maintenance_walkway", func_shelter_b1_south_maintenance_walkway_8017D9D0);
+s32 func_shelter_b1_south_maintenance_walkway_8017D9D0(void)
+{
+    return 0;
+}
 
 s32 func_shelter_b1_south_maintenance_walkway_8017D9D8(void)
 {
