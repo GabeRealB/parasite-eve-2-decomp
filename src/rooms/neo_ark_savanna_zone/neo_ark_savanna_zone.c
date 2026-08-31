93227692a4ea6bd1c7c1b108019ee2f64a493b90
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 23:56:00 +0200

    matched func_neo_ark_savanna_zone_8017D9AC 1

--- src/rooms/neo_ark_savanna_zone/neo_ark_savanna_zone.c
+++ src/rooms/neo_ark_savanna_zone/neo_ark_savanna_zone.c
@@ -1,4 +1,8 @@
 #include "common.h"
+#include "gameplay/3CD8.h"
+extern s32 D_8011572C;
+extern s32 D_80115750;
+extern s32 D_80115758;
 
 INCLUDE_RODATA("rooms/nonmatchings/neo_ark_savanna_zone/neo_ark_savanna_zone", D_neo_ark_savanna_zone_8017D5C0);
 
@@ -20,6 +24,15 @@ void func_neo_ark_savanna_zone_8017D94C(void)
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_savanna_zone/neo_ark_savanna_zone", func_neo_ark_savanna_zone_8017D954);
 
-INCLUDE_ASM("rooms/nonmatchings/neo_ark_savanna_zone/neo_ark_savanna_zone", func_neo_ark_savanna_zone_8017D9AC);
+void func_neo_ark_savanna_zone_8017D9AC(Task* arg0)
+{
+    if (arg0->state == 0) {
+        D_80115758          = 0x601DD;
+        D_8011572C          = 0x601F9;
+        D_80115750          = 0x60215;
+        Gp_State1C->field_A = 2;
+        arg0->state         = 1;
+    }
+}
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_savanna_zone/neo_ark_savanna_zone", func_neo_ark_savanna_zone_8017DA0C);
