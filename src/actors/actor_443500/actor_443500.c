c7913d2c106a81d8fd2d3cc9c402d23bb5c7a94e
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 21:48:17 +0200

    matched func_actor_443500_80131E84 1

--- src/actors/actor_443500/actor_443500.c
+++ src/actors/actor_443500/actor_443500.c
@@ -12,7 +12,18 @@ void func_actor_443500_80131E3C(s32 arg0)
     Gp_ResetCap();
 }
 
-INCLUDE_ASM("actors/nonmatchings/actor_443500/actor_443500", func_actor_443500_80131E84);
+void func_actor_443500_80131E84(s32 arg0)
+{
+    if (GameFlag_GetNibble(0xDF) > 0) {
+        if (arg0 != 0) {
+            Gp_CapFile = 0;
+            Gp_LoadCapFile(1);
+            func_800E6D4C(0x240, 0x100);
+            return;
+        }
+        Gp_ResetCap();
+    }
+}
 
 INCLUDE_ASM("actors/nonmatchings/actor_443500/actor_443500", func_actor_443500_80131EE4);
 
