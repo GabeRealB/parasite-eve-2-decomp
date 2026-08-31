acf6873d480a5d4ef15083978b6462c557d3a3c5
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 18:37:56 +0200

    matched func_dryfield_driveway_8017DDB0 1

--- src/rooms/dryfield_driveway/dryfield_driveway_2.c
+++ src/rooms/dryfield_driveway/dryfield_driveway_2.c
@@ -4,7 +4,10 @@ INCLUDE_ASM("rooms/nonmatchings/dryfield_driveway/dryfield_driveway_2", func_dry
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_driveway/dryfield_driveway_2", func_dryfield_driveway_8017DCC0);
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_driveway/dryfield_driveway_2", func_dryfield_driveway_8017DDB0);
+s32 func_dryfield_driveway_8017DDB0(void)
+{
+    return 0;
+}
 
 s32 func_dryfield_driveway_8017DDB8(void)
 {
