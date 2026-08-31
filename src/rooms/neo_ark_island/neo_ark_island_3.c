01afa84a8c9db949f66c49f9182786945ddd5412
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 23:55:48 +0200

    matched func_neo_ark_island_8017FB2C 1

--- src/rooms/neo_ark_island/neo_ark_island_3.c
+++ src/rooms/neo_ark_island/neo_ark_island_3.c
@@ -1,5 +1,21 @@
 #include "common.h"
+#include "main/task.h"
+extern s32 D_8011572C;
+extern s32 D_80115738;
+extern s32 D_8011574C;
+extern s32 D_80115750;
+extern s32 D_80115758;
 
-INCLUDE_ASM("rooms/nonmatchings/neo_ark_island/neo_ark_island_3", func_neo_ark_island_8017FB2C);
+void func_neo_ark_island_8017FB2C(Task* arg0)
+{
+    if (arg0->state == 0) {
+        D_80115758  = 0x601DB;
+        D_8011572C  = 0x601F7;
+        D_80115750  = 0x60213;
+        D_8011574C  = 0x60178;
+        D_80115738  = 0x60179;
+        arg0->state = 1;
+    }
+}
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_island/neo_ark_island_3", func_neo_ark_island_8017FB9C);
