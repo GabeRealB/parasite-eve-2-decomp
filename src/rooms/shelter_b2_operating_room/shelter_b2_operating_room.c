e5159b2d4a291f1b4b5f8e8dd79235b395c245b4
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 06:36:03 +0200

    matched func_shelter_b2_operating_room_8017DCA4 1

--- src/rooms/shelter_b2_operating_room/shelter_b2_operating_room.c
+++ src/rooms/shelter_b2_operating_room/shelter_b2_operating_room.c
@@ -1,5 +1,9 @@
 #include "common.h"
 
+#include "main/gameflag.h"
+
+#include "gameplay/3CD8.h"
+
 INCLUDE_ASM("rooms/nonmatchings/shelter_b2_operating_room/shelter_b2_operating_room", func_shelter_b2_operating_room_8017D628);
 
 INCLUDE_RODATA("rooms/nonmatchings/shelter_b2_operating_room/shelter_b2_operating_room", D_shelter_b2_operating_room_8017D5C0);
@@ -12,7 +16,18 @@ INCLUDE_ASM("rooms/nonmatchings/shelter_b2_operating_room/shelter_b2_operating_r
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b2_operating_room/shelter_b2_operating_room", func_shelter_b2_operating_room_8017DC9C);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b2_operating_room/shelter_b2_operating_room", func_shelter_b2_operating_room_8017DCA4);
+s32 func_shelter_b2_operating_room_8017DCA4(s32 arg0, s32 arg1, s32 arg2)
+{
+    switch (arg2) {
+        case 4:
+            Gp_SpawnIfCapIdle(GameFlag_GetNibble(0xC7) == 0 ? 4 : 0x10, 0);
+            break;
+        case 5:
+            Gp_SpawnIfCapIdle(GameFlag_GetNibble(0xC7) != 0 ? 0xF : 5, 0);
+            break;
+    }
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b2_operating_room/shelter_b2_operating_room", func_shelter_b2_operating_room_8017DD0C);
 
