7f0a003d04fba1610a0febb9fee6e91349ee0c82
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 22:08:01 +0200

    matched func_mine_cavern_8017E360 1

--- src/rooms/mine_cavern/mine_cavern_5.c
+++ src/rooms/mine_cavern/mine_cavern_5.c
@@ -1,4 +1,6 @@
 #include "common.h"
+#include "main/fs.h"
+#include "gameplay/3A34.h"
 
 INCLUDE_ASM("rooms/nonmatchings/mine_cavern/mine_cavern_5", func_mine_cavern_8017E330);
 
@@ -6,7 +8,16 @@ void func_mine_cavern_8017E358(void)
 {
 }
 
-INCLUDE_ASM("rooms/nonmatchings/mine_cavern/mine_cavern_5", func_mine_cavern_8017E360);
+void func_mine_cavern_8017E360(void)
+{
+    Game_Session->field_9 = 4;
+    Gp_StateF0.field_0    = 0;
+    Gp_StateF0.field_5    = 0;
+    Gp_StateF0.field_6    = 0;
+    Gp_StateF0.field_8    = 0;
+    Gp_StateF0.field_C    = 0;
+    Gp_StateF0.field_10   = 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/mine_cavern/mine_cavern_5", func_mine_cavern_8017E394);
 
