738ad40a372f33f7677e8115c15f7129a6a932e5
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 03:28:59 +0200

    matched func_neo_ark_observatory_80180124 2

--- src/rooms/neo_ark_observatory/neo_ark_observatory.c
+++ src/rooms/neo_ark_observatory/neo_ark_observatory.c
@@ -46,5 +46,3 @@ void func_neo_ark_observatory_8017FB1C(Task* task)
 }
 
 INCLUDE_RODATA("rooms/nonmatchings/neo_ark_observatory/neo_ark_observatory", D_neo_ark_observatory_8017D698);
-
-INCLUDE_RODATA("rooms/nonmatchings/neo_ark_observatory/neo_ark_observatory", jtbl_neo_ark_observatory_8017D6A4);
