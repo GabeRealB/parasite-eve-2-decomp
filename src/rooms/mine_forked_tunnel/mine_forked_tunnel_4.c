f6ed36775f071e8acd1c18a369853a899deb4e83
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 08:59:34 +0200

    matched func_mine_forked_tunnel_8017E2E0 1

--- src/rooms/mine_forked_tunnel/mine_forked_tunnel_4.c
+++ src/rooms/mine_forked_tunnel/mine_forked_tunnel_4.c
@@ -2,7 +2,11 @@
 
 #include "gameplay/3CD8.h"
 
+#include "main/gameflag.h"
 #include "main/task.h"
+
+extern s32 D_mine_forked_tunnel_801831AC;
+extern s32 D_mine_forked_tunnel_801834F4;
 
 void func_mine_forked_tunnel_8017E48C(s32 arg0);
 
@@ -10,7 +14,27 @@ INCLUDE_ASM("rooms/nonmatchings/mine_forked_tunnel/mine_forked_tunnel_4", func_m
 
 INCLUDE_ASM("rooms/nonmatchings/mine_forked_tunnel/mine_forked_tunnel_4", func_mine_forked_tunnel_8017E2B4);
 
-INCLUDE_ASM("rooms/nonmatchings/mine_forked_tunnel/mine_forked_tunnel_4", func_mine_forked_tunnel_8017E2E0);
+void func_mine_forked_tunnel_8017E2E0(Task* arg0)
+{
+    s32 state;
+
+    state = arg0->state;
+    switch (state) {
+        case 0:
+            Gp_RunCapCmd1(1);
+            arg0->state = arg0->state + 1;
+            break;
+        case 1:
+            if (Gp_CapBusy() == 0) {
+                if (Gp_GetCapEventKey() == state) {
+                    func_800E8634((s32)&D_mine_forked_tunnel_801831AC, 0, (s32)&D_mine_forked_tunnel_801834F4);
+                    GameFlag_SetNibble(0x75, 1);
+                }
+                Task_Kill(arg0);
+            }
+            break;
+    }
+}
 
 void func_mine_forked_tunnel_8017E38C(Task* arg0)
 {
