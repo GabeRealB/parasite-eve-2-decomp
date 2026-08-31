b348c39236085de26e8de318b2c4055a2542f3ad
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-08-31 22:02:41 +0200

    matched func_neo_ark_r26_8017D694 1

--- src/rooms/neo_ark_r26/neo_ark_r26_2.c
+++ src/rooms/neo_ark_r26/neo_ark_r26_2.c
@@ -1,6 +1,9 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/neo_ark_r26/neo_ark_r26_2", func_neo_ark_r26_8017D694);
+s32 func_neo_ark_r26_8017D694(void)
+{
+    return 0;
+}
 
 s32 func_neo_ark_r26_8017D69C(void)
 {
