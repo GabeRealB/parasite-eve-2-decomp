0a944cbe3e1833f44172a7ca31d46e314a9eec0a
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 02:45:53 +0200

    matched func_shelter_r36_8017D914 1

--- src/rooms/shelter_r36/shelter_r36_2.c
+++ src/rooms/shelter_r36/shelter_r36_2.c
@@ -1,6 +1,9 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_r36/shelter_r36_2", func_shelter_r36_8017D914);
+s32 func_shelter_r36_8017D914(void)
+{
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_r36/shelter_r36_2", func_shelter_r36_8017D91C);
 
