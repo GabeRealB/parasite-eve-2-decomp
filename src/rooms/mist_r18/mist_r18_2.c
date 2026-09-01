4b909552db95dd8d773a26ebfefc6b4afed8a2b9
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 11:34:27 +0200

    matched func_mist_r18_8017E7F0 1

--- src/rooms/mist_r18/mist_r18_2.c
+++ src/rooms/mist_r18/mist_r18_2.c
@@ -1,5 +1,15 @@
 #include "common.h"
 
+#include "main/session.h"
+#include "main/task.h"
+
+void func_mist_r18_8017DF80(s32 arg0);
+void func_mist_r18_8017E144(s16 arg0);
+
+/// Spawn descriptor handed to entry 5 of `D_mist_r18_80184F04`.
+extern s32      D_mist_r18_80184EE4;
+extern TaskDesc D_mist_r18_80184F04;
+
 void func_mist_r18_8017E39C(void)
 {
 }
@@ -16,7 +26,10 @@ INCLUDE_ASM("rooms/nonmatchings/mist_r18/mist_r18_2", func_mist_r18_8017E6D8);
 
 INCLUDE_ASM("rooms/nonmatchings/mist_r18/mist_r18_2", func_mist_r18_8017E784);
 
-INCLUDE_ASM("rooms/nonmatchings/mist_r18/mist_r18_2", func_mist_r18_8017E7F0);
+void func_mist_r18_8017E7F0(void)
+{
+    Task_SpawnFromTable(&D_mist_r18_80184F04, 5, 0, (s32)&D_mist_r18_80184EE4);
+}
 
 INCLUDE_ASM("rooms/nonmatchings/mist_r18/mist_r18_2", func_mist_r18_8017E824);
 
