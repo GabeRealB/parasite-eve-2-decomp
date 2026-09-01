3d14e20dd06265a9e9d9a48107d6c4943478ddc0
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 03:11:40 +0200

    matched func_shelter_1f_guardroom_8017D73C 1

--- src/rooms/shelter_1f_guardroom/shelter_1f_guardroom.c
+++ src/rooms/shelter_1f_guardroom/shelter_1f_guardroom.c
@@ -6,4 +6,7 @@ INCLUDE_RODATA("rooms/nonmatchings/shelter_1f_guardroom/shelter_1f_guardroom", D
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_1f_guardroom/shelter_1f_guardroom", func_shelter_1f_guardroom_8017D5E8);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_guardroom/shelter_1f_guardroom", func_shelter_1f_guardroom_8017D73C);
+s32 func_shelter_1f_guardroom_8017D73C(void)
+{
+    return 0;
+}
