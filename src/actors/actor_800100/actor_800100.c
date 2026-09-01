2494e6e16dcb0570057aff45c7de9ffa52a981a8
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 09:05:35 +0200

    matched func_actor_800100_80166DF0 1

--- src/actors/actor_800100/actor_800100.c
+++ src/actors/actor_800100/actor_800100.c
@@ -249,7 +249,17 @@ void func_actor_800100_80166DD0(GpActorWork* arg0)
     actor->field_960 = 0;
 }
 
-INCLUDE_ASM("actors/nonmatchings/actor_800100/actor_800100", func_actor_800100_80166DF0);
+void func_actor_800100_80166DF0(GpActorWork* arg0)
+{
+    GameActor* actor;
+
+    actor            = arg0->actor;
+    actor->field_956 = 6;
+    actor->field_954 = 0;
+    actor->field_95C = 0;
+    actor->field_95E = 0;
+    actor->field_97E = 1;
+}
 
 INCLUDE_ASM("actors/nonmatchings/actor_800100/actor_800100", func_actor_800100_80166E14);
 
