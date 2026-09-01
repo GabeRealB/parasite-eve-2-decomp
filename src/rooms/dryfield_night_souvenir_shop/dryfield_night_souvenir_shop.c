4f42f859f589660c5d30c936e6c7c49e46e3307e
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 21:21:37 +0200

    matched func_dryfield_night_souvenir_shop_8017DFF4 1

--- src/rooms/dryfield_night_souvenir_shop/dryfield_night_souvenir_shop.c
+++ src/rooms/dryfield_night_souvenir_shop/dryfield_night_souvenir_shop.c
@@ -1,5 +1,11 @@
 #include "common.h"
 
+#include "gameplay/gameplay.h"
+#include "main/task.h"
+#include "main/tmd.h"
+
+void func_dryfield_night_souvenir_shop_8017D6B4(GsCOORDINATE2* coord, s32 arg1);
+
 INCLUDE_ASM("rooms/nonmatchings/dryfield_night_souvenir_shop/dryfield_night_souvenir_shop", func_dryfield_night_souvenir_shop_8017D610);
 
 void func_dryfield_night_souvenir_shop_8017D654(void)
@@ -12,4 +18,12 @@ INCLUDE_ASM("rooms/nonmatchings/dryfield_night_souvenir_shop/dryfield_night_souv
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_night_souvenir_shop/dryfield_night_souvenir_shop", func_dryfield_night_souvenir_shop_8017D6B4);
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_night_souvenir_shop/dryfield_night_souvenir_shop", func_dryfield_night_souvenir_shop_8017DFF4);
+void func_dryfield_night_souvenir_shop_8017DFF4(Task* arg0)
+{
+    GsCOORDINATE2* coord;
+
+    coord = (GsCOORDINATE2*)((TmdObject*)arg0->extra)->field_8;
+    Gp_UpdateCoord(coord);
+    func_dryfield_night_souvenir_shop_8017D6B4(coord, 0);
+    func_dryfield_night_souvenir_shop_8017D6B4(coord, 8);
+}
