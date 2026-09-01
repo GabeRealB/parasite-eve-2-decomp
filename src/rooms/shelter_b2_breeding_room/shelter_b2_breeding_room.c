82318b2997886794b2e3e945985306eb537b9c43
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 23:20:14 +0200

    matched func_shelter_b2_breeding_room_8017D658 1

--- src/rooms/shelter_b2_breeding_room/shelter_b2_breeding_room.c
+++ src/rooms/shelter_b2_breeding_room/shelter_b2_breeding_room.c
@@ -2,7 +2,10 @@
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b2_breeding_room/shelter_b2_breeding_room", func_shelter_b2_breeding_room_8017D5F8);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b2_breeding_room/shelter_b2_breeding_room", func_shelter_b2_breeding_room_8017D658);
+s32 func_shelter_b2_breeding_room_8017D658(void)
+{
+    return 0;
+}
 
 INCLUDE_RODATA("rooms/nonmatchings/shelter_b2_breeding_room/shelter_b2_breeding_room", D_shelter_b2_breeding_room_8017D5C0);
 
