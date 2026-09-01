51fec92b5f2406d0966f256514b539ce3eb1a8df
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 08:26:04 +0200

    matched func_actor_342000_801642D4 1

--- src/actors/actor_342000/actor_342000.c
+++ src/actors/actor_342000/actor_342000.c
@@ -108,7 +108,14 @@ void func_actor_342000_801642B4(s16 arg0)
     work->field_6A = 0;
 }
 
-INCLUDE_ASM("actors/nonmatchings/actor_342000/actor_342000", func_actor_342000_801642D4);
+void func_actor_342000_801642D4(s16 arg0)
+{
+    Actor342000EventWork* work;
+
+    work           = (Actor342000EventWork*)D_actor_342000_80165070->idMap;
+    work->field_70 = arg0;
+    work->field_72 = 0;
+}
 
 INCLUDE_ASM("actors/nonmatchings/actor_342000/actor_342000", func_actor_342000_801642F4);
 
