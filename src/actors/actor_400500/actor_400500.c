d7634577fc4dc1e570903d0b9de05737bc2e2636
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 03:19:08 +0200

    matched func_actor_400500_8013DFE4 1

--- src/actors/actor_400500/actor_400500.c
+++ src/actors/actor_400500/actor_400500.c
@@ -555,4 +555,15 @@ void func_actor_400500_8013DF74(Task* arg0)
     work->field_A06 = work->field_A06 + 1;
 }
 
-INCLUDE_ASM("actors/nonmatchings/actor_400500/actor_400500", func_actor_400500_8013DFE4);
+void func_actor_400500_8013DFE4(Task* arg0)
+{
+    Actor400500Work* work;
+    u16              frame;
+
+    work            = (Actor400500Work*)arg0->idMap;
+    frame           = work->field_A04 + 1;
+    work->field_A04 = frame;
+    if ((s16)frame >= 0x12D) {
+        Gp_DestroyEnemy(arg0->spawnArg2, arg0);
+    }
+}
