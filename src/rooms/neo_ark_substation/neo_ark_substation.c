ac3c4664d8936afe7d342f53c1cd33aa5b3c37bb
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 05:00:55 +0200

    matched func_neo_ark_substation_8017D71C 1

--- src/rooms/neo_ark_substation/neo_ark_substation.c
+++ src/rooms/neo_ark_substation/neo_ark_substation.c
@@ -6,7 +6,10 @@ INCLUDE_RODATA("rooms/nonmatchings/neo_ark_substation/neo_ark_substation", D_neo
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_substation/neo_ark_substation", func_neo_ark_substation_8017D610);
 
-INCLUDE_ASM("rooms/nonmatchings/neo_ark_substation/neo_ark_substation", func_neo_ark_substation_8017D71C);
+s32 func_neo_ark_substation_8017D71C(void)
+{
+    return 0;
+}
 
 INCLUDE_RODATA("rooms/nonmatchings/neo_ark_substation/neo_ark_substation", jtbl_neo_ark_substation_8017D5EC);
 
