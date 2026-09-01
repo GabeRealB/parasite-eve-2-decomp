78c5867cfcfb43c834bc9f2211578c6a92e6614f
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 20:55:51 +0200

    matched func_mine_mesa_8017DD44 1

--- src/rooms/mine_mesa/mine_mesa_2.c
+++ src/rooms/mine_mesa/mine_mesa_2.c
@@ -1,7 +1,9 @@
 #include "common.h"
 #include "main/task.h"
+#include "gameplay/3A34.h"
 extern TaskDesc D_mine_mesa_80189B2C;
 extern Task*    D_mine_mesa_80189B4C;
+extern GpObj4A  D_mine_mesa_801890EC[4];
 
 INCLUDE_ASM("rooms/nonmatchings/mine_mesa/mine_mesa_2", func_mine_mesa_8017D8F8);
 
@@ -13,7 +15,13 @@ INCLUDE_ASM("rooms/nonmatchings/mine_mesa/mine_mesa_2", func_mine_mesa_8017DBC4)
 
 INCLUDE_ASM("rooms/nonmatchings/mine_mesa/mine_mesa_2", func_mine_mesa_8017DC80);
 
-INCLUDE_ASM("rooms/nonmatchings/mine_mesa/mine_mesa_2", func_mine_mesa_8017DD44);
+void func_mine_mesa_8017DD44(void)
+{
+    Gp_UnlinkObj4A(0, &D_mine_mesa_801890EC[0]);
+    Gp_UnlinkObj4A(0, &D_mine_mesa_801890EC[1]);
+    Gp_UnlinkObj4A(0, &D_mine_mesa_801890EC[2]);
+    Gp_UnlinkObj4A(0, &D_mine_mesa_801890EC[3]);
+}
 
 INCLUDE_ASM("rooms/nonmatchings/mine_mesa/mine_mesa_2", func_mine_mesa_8017DD98);
 
