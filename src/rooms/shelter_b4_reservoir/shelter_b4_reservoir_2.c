47917e6bbb85129b000b202f4515ec297d5fffe0
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 07:31:51 +0200

    matched func_shelter_b4_reservoir_8017E864 1

--- src/rooms/shelter_b4_reservoir/shelter_b4_reservoir_2.c
+++ src/rooms/shelter_b4_reservoir/shelter_b4_reservoir_2.c
@@ -1,8 +1,15 @@
 #include "common.h"
 
+extern void func_shelter_b4_reservoir_8017E068(void);
+void        func_shelter_b4_reservoir_8017E8E4(void);
+
 INCLUDE_ASM("rooms/nonmatchings/shelter_b4_reservoir/shelter_b4_reservoir_2", func_shelter_b4_reservoir_8017E7C8);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b4_reservoir/shelter_b4_reservoir_2", func_shelter_b4_reservoir_8017E864);
+void func_shelter_b4_reservoir_8017E864(void)
+{
+    func_shelter_b4_reservoir_8017E068();
+    func_shelter_b4_reservoir_8017E8E4();
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b4_reservoir/shelter_b4_reservoir_2", func_shelter_b4_reservoir_8017E88C);
 
