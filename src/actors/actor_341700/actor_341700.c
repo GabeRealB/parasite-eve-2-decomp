7a4545fc6963ed4eb9e6bb60993c24314e1c966f
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 02:56:28 +0200

    matched func_actor_341700_80168F9C 1

--- src/actors/actor_341700/actor_341700.c
+++ src/actors/actor_341700/actor_341700.c
@@ -295,7 +295,22 @@ void func_actor_341700_80168F5C(Task* arg0)
     }
 }
 
-INCLUDE_ASM("actors/nonmatchings/actor_341700/actor_341700", func_actor_341700_80168F9C);
+void func_actor_341700_80168F9C(Task* arg0)
+{
+    Actor341700Work* work;
+    Actor341700Work* work2;
+
+    work = (Actor341700Work*)arg0->idMap;
+    if ((func_actor_341700_80168468() << 0x10) != 0) {
+        func_actor_341700_801681C4(arg0, 0);
+        work2            = (Actor341700Work*)arg0->idMap;
+        work2->field_426 = 8;
+        work2->field_41C = 0x10;
+        work2->field_418 = 0xF;
+        work2->field_414 = 1;
+        work->field_422  = work->field_422 + 1;
+    }
+}
 
 INCLUDE_ASM("actors/nonmatchings/actor_341700/actor_341700", func_actor_341700_80169018);
 
