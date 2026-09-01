1fdd4ee213b34c6591cfbdf624dd94bbffa26252
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 05:31:47 +0200

    matched func_actor_143400_80131E90 1

--- src/actors/actor_143400/actor_143400.c
+++ src/actors/actor_143400/actor_143400.c
@@ -1,6 +1,8 @@
 #include "common.h"
 #include "gameplay/3CD8.h"
 
+extern s8 D_8007272D;
+
 void func_actor_143400_80131E24(s32 arg0)
 {
     if (arg0 != 0) {
@@ -14,6 +16,9 @@ void func_actor_143400_80131E24(s32 arg0)
 
 INCLUDE_ASM("actors/nonmatchings/actor_143400/actor_143400", func_actor_143400_80131E6C);
 
-INCLUDE_ASM("actors/nonmatchings/actor_143400/actor_143400", func_actor_143400_80131E90);
+void func_actor_143400_80131E90(s8 arg0)
+{
+    D_8007272D = arg0;
+}
 
 INCLUDE_RODATA("actors/nonmatchings/actor_143400/actor_143400", D_actor_143400_80131E20);
