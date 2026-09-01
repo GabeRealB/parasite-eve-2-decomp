8b2c1b49195698a689fc920d3536fd1c8a4f96c7
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 20:37:56 +0200

    matched func_dryfield_night_saloon_g_r_8017E0A8 1

--- src/rooms/dryfield_night_saloon_g_r/dryfield_night_saloon_g_r_3.c
+++ src/rooms/dryfield_night_saloon_g_r/dryfield_night_saloon_g_r_3.c
@@ -1,10 +1,17 @@
 #include "common.h"
+#include "main/session.h"
 #include "main/task.h"
 extern TaskDesc D_dryfield_night_saloon_g_r_80185068;
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_night_saloon_g_r/dryfield_night_saloon_g_r_3", func_dryfield_night_saloon_g_r_8017E050);
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_night_saloon_g_r/dryfield_night_saloon_g_r_3", func_dryfield_night_saloon_g_r_8017E0A8);
+extern u8 D_8007216D;
+
+void func_dryfield_night_saloon_g_r_8017E0A8(u8 arg0)
+{
+    D_8007216D            = arg0;
+    Game_Session->field_5 = arg0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_night_saloon_g_r/dryfield_night_saloon_g_r_3", func_dryfield_night_saloon_g_r_8017E0C0);
 
