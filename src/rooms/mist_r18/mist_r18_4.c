3a8ecaa6c652a605103b1ec12032a67d927ed58c
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 06:02:40 +0200

    matched func_mist_r18_8017ECC0 1

--- src/rooms/mist_r18/mist_r18_4.c
+++ src/rooms/mist_r18/mist_r18_4.c
@@ -1,8 +1,13 @@
 #include "common.h"
 
+extern s8 D_80071090;
+
 INCLUDE_ASM("rooms/nonmatchings/mist_r18/mist_r18_4", func_mist_r18_8017EC98);
 
-INCLUDE_ASM("rooms/nonmatchings/mist_r18/mist_r18_4", func_mist_r18_8017ECC0);
+void func_mist_r18_8017ECC0(s8 arg0)
+{
+    D_80071090 = arg0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/mist_r18/mist_r18_4", func_mist_r18_8017ECCC);
 
