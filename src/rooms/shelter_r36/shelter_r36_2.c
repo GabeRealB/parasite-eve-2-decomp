5f072d8d27722c5f454fc5c93ef1b25369f3de96
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 02:50:31 +0200

    matched func_shelter_r36_8017D91C 1

--- src/rooms/shelter_r36/shelter_r36_2.c
+++ src/rooms/shelter_r36/shelter_r36_2.c
@@ -5,6 +5,9 @@ s32 func_shelter_r36_8017D914(void)
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_r36/shelter_r36_2", func_shelter_r36_8017D91C);
+s32 func_shelter_r36_8017D91C(void)
+{
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_r36/shelter_r36_2", func_shelter_r36_8017D924);
