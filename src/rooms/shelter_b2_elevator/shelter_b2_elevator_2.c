cfd256d42b35ab9c82c6e2e70e8d533df04c1617
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 04:35:11 +0200

    matched func_shelter_b2_elevator_8017DAA8 1

--- src/rooms/shelter_b2_elevator/shelter_b2_elevator_2.c
+++ src/rooms/shelter_b2_elevator/shelter_b2_elevator_2.c
@@ -1,6 +1,9 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b2_elevator/shelter_b2_elevator_2", func_shelter_b2_elevator_8017DAA8);
+s32 func_shelter_b2_elevator_8017DAA8(void)
+{
+    return 0;
+}
 
 s32 func_shelter_b2_elevator_8017DAB0(void)
 {
