8e6755de09fba3a4042d2c8aa2374c0297264f3a
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 05:59:50 +0200

    matched func_neo_ark_observatory_80180DAC 1

--- src/rooms/neo_ark_observatory/neo_ark_observatory_3.c
+++ src/rooms/neo_ark_observatory/neo_ark_observatory_3.c
@@ -1,3 +1,8 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/neo_ark_observatory/neo_ark_observatory_3", func_neo_ark_observatory_80180DAC);
+extern s16 D_neo_ark_observatory_80187A3C;
+
+void func_neo_ark_observatory_80180DAC(s16 arg0)
+{
+    D_neo_ark_observatory_80187A3C = arg0;
+}
