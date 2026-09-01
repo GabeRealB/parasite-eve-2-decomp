e91effa8074a927f4ddd80d67e513f0a1698c399
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 03:17:52 +0200

    matched func_actor_215100_8014C58C 1

--- src/actors/actor_215100/actor_215100.c
+++ src/actors/actor_215100/actor_215100.c
@@ -237,7 +237,11 @@ void func_actor_215100_8014C538(s16 arg0, s16 arg1, s16 arg2)
     Task_SpawnFromTable(&D_actor_215100_801544FC, 0, arg2, 0);
 }
 
-INCLUDE_ASM("actors/nonmatchings/actor_215100/actor_215100", func_actor_215100_8014C58C);
+void func_actor_215100_8014C58C(s16 arg0, s16 arg1, s16 arg2)
+{
+    func_actor_215100_8014B2B8(arg0, arg1, 0xD0);
+    Display_InitModeObj(&D_actor_215100_80154508, arg2, 0, 0);
+}
 
 INCLUDE_ASM("actors/nonmatchings/actor_215100/actor_215100", func_actor_215100_8014C5E0);
 
