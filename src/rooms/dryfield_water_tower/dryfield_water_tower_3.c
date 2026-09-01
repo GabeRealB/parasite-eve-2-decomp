b2b662c551958c9179ae66f5bc6acd9a18a1228d
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 07:02:09 +0200

    matched func_dryfield_water_tower_8017FBC8 1

--- src/rooms/dryfield_water_tower/dryfield_water_tower_3.c
+++ src/rooms/dryfield_water_tower/dryfield_water_tower_3.c
@@ -1,5 +1,13 @@
 #include "common.h"
 
+#include "main/task.h"
+
+/// Scratch state of the room's cap script, stored at `Task::idMap`.
+typedef struct {
+    /* 0x00 */ u8  pad_0[0x6C];
+    /* 0x6C */ s16 field_6C;
+} DryfieldWaterTowerState;
+
 INCLUDE_ASM("rooms/nonmatchings/dryfield_water_tower/dryfield_water_tower_3", func_dryfield_water_tower_8017F8E8);
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_water_tower/dryfield_water_tower_3", func_dryfield_water_tower_8017F908);
@@ -10,7 +18,10 @@ INCLUDE_ASM("rooms/nonmatchings/dryfield_water_tower/dryfield_water_tower_3", fu
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_water_tower/dryfield_water_tower_3", func_dryfield_water_tower_8017FB4C);
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_water_tower/dryfield_water_tower_3", func_dryfield_water_tower_8017FBC8);
+void func_dryfield_water_tower_8017FBC8(Task* task)
+{
+    ((DryfieldWaterTowerState*)task->idMap)->field_6C = 1;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_water_tower/dryfield_water_tower_3", func_dryfield_water_tower_8017FBD8);
 
