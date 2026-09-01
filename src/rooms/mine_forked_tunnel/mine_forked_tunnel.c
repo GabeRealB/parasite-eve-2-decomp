a84c8910e3248043ee070ae24ce9cc959559a86a
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 11:14:35 +0200

    matched func_mine_forked_tunnel_8017DC50 1

--- src/rooms/mine_forked_tunnel/mine_forked_tunnel.c
+++ src/rooms/mine_forked_tunnel/mine_forked_tunnel.c
@@ -1,5 +1,9 @@
 #include "common.h"
 
+#include "main/task.h"
+
+#include "gameplay/1BC.h"
+
 INCLUDE_ASM("rooms/nonmatchings/mine_forked_tunnel/mine_forked_tunnel", func_mine_forked_tunnel_8017D5E8);
 
 INCLUDE_ASM("rooms/nonmatchings/mine_forked_tunnel/mine_forked_tunnel", func_mine_forked_tunnel_8017D724);
@@ -12,7 +16,10 @@ INCLUDE_RODATA("rooms/nonmatchings/mine_forked_tunnel/mine_forked_tunnel", D_min
 
 INCLUDE_ASM("rooms/nonmatchings/mine_forked_tunnel/mine_forked_tunnel", func_mine_forked_tunnel_8017DBE4);
 
-INCLUDE_ASM("rooms/nonmatchings/mine_forked_tunnel/mine_forked_tunnel", func_mine_forked_tunnel_8017DC50);
+void func_mine_forked_tunnel_8017DC50(Task* arg0)
+{
+    Gp_EnemyTaskExit(arg0);
+}
 
 INCLUDE_ASM("rooms/nonmatchings/mine_forked_tunnel/mine_forked_tunnel", func_mine_forked_tunnel_8017DC70);
 
