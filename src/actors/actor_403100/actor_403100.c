b8f2a646bf4722f5bf421658ce22b677a6f4863f
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 02:54:17 +0200

    matched func_actor_403100_8013ED50 1

--- src/actors/actor_403100/actor_403100.c
+++ src/actors/actor_403100/actor_403100.c
@@ -560,7 +560,24 @@ void func_actor_403100_8013ED48(void)
 {
 }
 
-INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100", func_actor_403100_8013ED50);
+void func_actor_403100_8013ED50(Task* arg0)
+{
+    GsCOORDINATE2* coord;
+
+    coord                              = (GsCOORDINATE2*)((TmdObject*)arg0->extra)->field_8;
+    D_actor_403100_80155808->field_618 = 0x1400;
+    D_actor_403100_80155808->field_5E2 = 0x10;
+    D_actor_403100_80155808->field_5DE = 3;
+    D_actor_403100_80155808->field_5DA = 2;
+    func_actor_403100_801327CC();
+    D_actor_403100_80155808->field_82  = 0;
+    coord->coord.t[0]                  = -0x2710;
+    coord->coord.t[1]                  = -0x258;
+    coord->coord.t[2]                  = -0x2328;
+    D_actor_403100_80155808->field_600 = 0;
+    D_actor_403100_80155808->field_5EC = 0;
+    D_actor_403100_80155808->field_5FA = D_actor_403100_80155808->field_5FA + 1;
+}
 
 INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100", func_actor_403100_8013EDDC);
 
