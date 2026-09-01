a9de86cb5cf3893484d0e589872bb84242b8ebf2
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 11:34:27 +0200

    matched func_mist_r18_8017EC98 1

--- src/rooms/mist_r18/mist_r18_4.c
+++ src/rooms/mist_r18/mist_r18_4.c
@@ -1,8 +1,21 @@
 #include "common.h"
 
-extern s8 D_80071090;
+#include "main/session.h"
 
-INCLUDE_ASM("rooms/nonmatchings/mist_r18/mist_r18_4", func_mist_r18_8017EC98);
+void func_80179FC8(s32 arg0, s32 arg1);
+
+extern s8 D_80071090;
+extern s8 D_801156F9;
+
+/// Set by `func_mist_r18_8017D960` when the alternate cutscene branch ran.
+extern s32 D_mist_r18_80186EA0;
+
+void func_mist_r18_8017EC98(void)
+{
+    if (Game_Session->field_4D != 1) {
+        D_801156F9 = 0;
+    }
+}
 
 void func_mist_r18_8017ECC0(s8 arg0)
 {
