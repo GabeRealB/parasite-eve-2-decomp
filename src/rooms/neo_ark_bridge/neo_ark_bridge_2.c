2abeace5e8e357aca2bc4efd42180d52567a056b
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 04:51:51 +0200

    matched func_neo_ark_bridge_8017E880 1

--- src/rooms/neo_ark_bridge/neo_ark_bridge_2.c
+++ src/rooms/neo_ark_bridge/neo_ark_bridge_2.c
@@ -2,7 +2,10 @@
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_bridge/neo_ark_bridge_2", func_neo_ark_bridge_8017E878);
 
-INCLUDE_ASM("rooms/nonmatchings/neo_ark_bridge/neo_ark_bridge_2", func_neo_ark_bridge_8017E880);
+s32 func_neo_ark_bridge_8017E880(void)
+{
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_bridge/neo_ark_bridge_2", func_neo_ark_bridge_8017E888);
 
