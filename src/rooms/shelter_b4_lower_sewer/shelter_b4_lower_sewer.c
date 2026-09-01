023b3650106550c21160b77f6bef77c4e8262e82
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 03:39:37 +0200

    matched func_shelter_b4_lower_sewer_8017D654 1

--- src/rooms/shelter_b4_lower_sewer/shelter_b4_lower_sewer.c
+++ src/rooms/shelter_b4_lower_sewer/shelter_b4_lower_sewer.c
@@ -1,6 +1,9 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b4_lower_sewer/shelter_b4_lower_sewer", func_shelter_b4_lower_sewer_8017D654);
+s32 func_shelter_b4_lower_sewer_8017D654(void)
+{
+    return 0;
+}
 
 s32 func_shelter_b4_lower_sewer_8017D65C(void)
 {
