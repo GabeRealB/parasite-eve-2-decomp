34181c8e58df80273d772aa20e43cfa6be512095
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-01 04:11:24 +0200

    matched func_neo_ark_forest_zone_8017D7DC 1

--- src/rooms/neo_ark_forest_zone/neo_ark_forest_zone.c
+++ src/rooms/neo_ark_forest_zone/neo_ark_forest_zone.c
@@ -4,7 +4,10 @@ INCLUDE_RODATA("rooms/nonmatchings/neo_ark_forest_zone/neo_ark_forest_zone", D_n
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_forest_zone/neo_ark_forest_zone", func_neo_ark_forest_zone_8017D644);
 
-INCLUDE_ASM("rooms/nonmatchings/neo_ark_forest_zone/neo_ark_forest_zone", func_neo_ark_forest_zone_8017D7DC);
+s32 func_neo_ark_forest_zone_8017D7DC(void)
+{
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/neo_ark_forest_zone/neo_ark_forest_zone", func_neo_ark_forest_zone_8017D7E4);
 
