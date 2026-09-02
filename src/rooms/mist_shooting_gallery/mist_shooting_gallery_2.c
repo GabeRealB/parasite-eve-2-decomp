44183eb7b7a5138038fd4f835865ea58393e0dac
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 22:54:10 +0200

    matched func_mist_shooting_gallery_8018055C 4

--- src/rooms/mist_shooting_gallery/mist_shooting_gallery_2.c
+++ src/rooms/mist_shooting_gallery/mist_shooting_gallery_2.c
@@ -148,7 +148,51 @@ INCLUDE_ASM("rooms/nonmatchings/mist_shooting_gallery/mist_shooting_gallery_2", 
 
 INCLUDE_ASM("rooms/nonmatchings/mist_shooting_gallery/mist_shooting_gallery_2", func_mist_shooting_gallery_801801E4);
 INCLUDE_ASM("rooms/nonmatchings/mist_shooting_gallery/mist_shooting_gallery_2", func_mist_shooting_gallery_80180390);
-INCLUDE_ASM("rooms/nonmatchings/mist_shooting_gallery/mist_shooting_gallery_2", func_mist_shooting_gallery_8018055C);
+void func_mist_shooting_gallery_8018055C(DialogPrompt* prompt, UiObject* obj)
+{
+    MistShootingGalleryCourseMenu menu;
+    MistShootingGalleryCourse*    course;
+    s32                           row;
+    s32                           list;
+    s32                           mode;
+
+    row  = prompt->field_8;
+    menu = D_mist_shooting_gallery_8017DADC;
+
+    list = 4;
+    if (Mc_SaveData.field_E != 0) {
+        list = Mc_SaveData.field_F;
+    }
+    if (Gp_IsDebugAttachRoom() == 0) {
+        list += 5;
+    }
+
+    course              = &menu.lists[list][row];
+    menu.req.x          = obj->baseX + (u16)prompt->field_18;
+    menu.req.y          = (prompt->field_1A - 3) + obj->baseY;
+    menu.req.otIndex    = (s16)obj->drawOrder + 1;
+    menu.req.field_8    = prompt->field_1C;
+    menu.req.glyphTable = 4;
+    menu.req.field_E    = 1;
+    menu.req.centerMode = 0;
+    func_8002E53C(&menu.req, course->name);
+
+    mode = prompt->field_C;
+    if (mode == 1) {
+        if (Pad_CheckButtons(0, 1, Pad_MaskConfirm) != 0) {
+            if (obj->owner->spawnArg1 != prompt->field_8) {
+                SndEvt_EnqueueType6(0x16, 0, 0);
+                if (obj->owner->flags != course->id) {
+                    SndEvt_EnqueueType2(0, 0x3C);
+                    obj->owner->state = mode;
+                    obj->owner->flags = course->id;
+                    CdCmd_DropPending();
+                }
+                obj->owner->spawnArg1 = prompt->field_8;
+            }
+        }
+    }
+}
 INCLUDE_ASM("rooms/nonmatchings/mist_shooting_gallery/mist_shooting_gallery_2", func_mist_shooting_gallery_80180728);
 void func_mist_shooting_gallery_80180A00(Task* task)
 {
