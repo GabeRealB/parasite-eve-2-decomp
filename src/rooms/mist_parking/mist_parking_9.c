b099cbdfb78763c1c36a345e29fcee39de0fbda0
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 15:11:06 +0200

    matched func_mist_parking_8018357C 1

--- src/rooms/mist_parking/mist_parking_9.c
+++ src/rooms/mist_parking/mist_parking_9.c
@@ -1,12 +1,18 @@
 #include "common.h"
 
+#include "gameplay/268.h"
 #include "main/display.h"
 #include "main/fs.h"
+#include "main/mc.h"
 #include "main/mem.h"
 #include "main/pad.h"
 #include "main/session.h"
+#include "main/sound.h"
 #include "main/stream.h"
 #include "main/task.h"
+
+extern s16 D_80071076;
+
 extern s32 D_mist_parking_80195320;
 
 extern TaskDesc D_mist_parking_8018D75C;
@@ -21,7 +27,19 @@ void func_mist_parking_8018354C(void)
     Task_SpawnFromTable(&D_mist_parking_8018D75C, 3, 0, 0);
 }
 
-INCLUDE_ASM("rooms/nonmatchings/mist_parking/mist_parking_9", func_mist_parking_8018357C);
+void func_mist_parking_8018357C(Task* arg0)
+{
+    func_800BC4E4();
+    Mc_SaveData.field_7 = 2;
+    Mc_SaveData.field_6 = 1;
+    Mc_SaveData.field_8 = 1;
+    Mc_SaveData.field_5 = 1;
+    D_80071076          = 1;
+    Fs_BeginBootLoad(&Mc_SaveData.field_4, 1);
+    SndEvt_EnqueueType7(0x80000000, 0);
+    Task_Spawn(0, 0x11, 0, 0);
+    Task_Kill(arg0);
+}
 
 void func_mist_parking_80183600(void)
 {
