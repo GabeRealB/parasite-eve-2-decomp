80ca78b732fc7bb3ab6347fd19698a80dcd2b257
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 16:04:04 +0200

    matched func_mist_parking_801834D4 1

--- src/rooms/mist_parking/mist_parking_9.c
+++ src/rooms/mist_parking/mist_parking_9.c
@@ -17,6 +17,8 @@ extern s16 D_80071076;
 
 extern s32 D_mist_parking_8018F4AC;
 extern s32 D_mist_parking_8018F5E4;
+extern s32 D_mist_parking_8018FA4C;
+extern s32 D_mist_parking_8018FB3C;
 extern s32 D_mist_parking_8019531C;
 
 extern s32 D_mist_parking_80195320;
@@ -36,7 +38,17 @@ void func_mist_parking_8018345C(Task* arg0)
     }
 }
 
-INCLUDE_ASM("rooms/nonmatchings/mist_parking/mist_parking_9", func_mist_parking_801834D4);
+void func_mist_parking_801834D4(Task* arg0)
+{
+    if (Game_Session->field_1 == 0 && Gp_CapBusy() == 0) {
+        if (D_mist_parking_8019531C == 2) {
+            func_800E8614((s32)&D_mist_parking_8018FB3C, 1);
+        } else {
+            func_800E8614((s32)&D_mist_parking_8018FA4C, 1);
+        }
+        Task_Kill(arg0);
+    }
+}
 
 void func_mist_parking_8018354C(void)
 {
