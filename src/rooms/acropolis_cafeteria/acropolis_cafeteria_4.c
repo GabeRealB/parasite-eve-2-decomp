222c6dc537c8e6ed7c5efcb93f3f2dfc89706089
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 04:20:20 +0200

    matched func_acropolis_cafeteria_8017E27C 1

--- src/rooms/acropolis_cafeteria/acropolis_cafeteria_4.c
+++ src/rooms/acropolis_cafeteria/acropolis_cafeteria_4.c
@@ -1,5 +1,8 @@
 #include "common.h"
 #include "main/fs.h"
+#include "main/session.h"
+#include "main/task.h"
+
 extern RECT D_acropolis_cafeteria_80184168;
 extern RECT D_acropolis_cafeteria_80184170;
 
@@ -9,7 +12,10 @@ INCLUDE_ASM("rooms/nonmatchings/acropolis_cafeteria/acropolis_cafeteria_4", func
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_cafeteria/acropolis_cafeteria_4", func_acropolis_cafeteria_8017E22C);
 
-INCLUDE_ASM("rooms/nonmatchings/acropolis_cafeteria/acropolis_cafeteria_4", func_acropolis_cafeteria_8017E27C);
+void func_acropolis_cafeteria_8017E27C(s32 arg0)
+{
+    ((GameActor*)((Task*)Game_GetPtrSlot(3))->idMap)->field_930 = arg0;
+}
 
 void func_acropolis_cafeteria_8017E2B0(void)
 {
