dcebd05f02d5428f80545db3eb3ea511c6bada71
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 09:32:18 +0200

    matched func_acropolis_west_elevator_hall_8017F568 1

--- src/rooms/acropolis_west_elevator_hall/acropolis_west_elevator_hall_2.c
+++ src/rooms/acropolis_west_elevator_hall/acropolis_west_elevator_hall_2.c
@@ -1,6 +1,24 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/acropolis_west_elevator_hall/acropolis_west_elevator_hall_2", func_acropolis_west_elevator_hall_8017F568);
+#include "gameplay/D4.h"
+
+#include "main/session.h"
+#include "main/task.h"
+
+extern GpMsgEntry D_acropolis_west_elevator_hall_801849CC[];
+extern TaskDesc   D_acropolis_west_elevator_hall_80184568[];
+extern Task*      D_acropolis_west_elevator_hall_80186AE4[];
+
+void func_acropolis_west_elevator_hall_8017F568(Task* arg0)
+{
+    arg0->field_24 = D_acropolis_west_elevator_hall_801849CC;
+    Game_SetPtrSlot(arg0, 7);
+    D_acropolis_west_elevator_hall_80186AE4[0] =
+        Task_SpawnFromTable(D_acropolis_west_elevator_hall_80184568, 0, 0, -1);
+    D_acropolis_west_elevator_hall_80186AE4[1] =
+        Task_SpawnFromTable(D_acropolis_west_elevator_hall_80184568, 1, 0, 1);
+    arg0->state = (s32)(arg0->state + 1);
+}
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_west_elevator_hall/acropolis_west_elevator_hall_2", func_acropolis_west_elevator_hall_8017F5F4);
 
