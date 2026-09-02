3f3a35007ba45488492778ed913b420d35fba3c9
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 07:40:42 +0200

    matched func_shelter_1f_bulwark_8017DBD4 1

--- src/rooms/shelter_1f_bulwark/shelter_1f_bulwark.c
+++ src/rooms/shelter_1f_bulwark/shelter_1f_bulwark.c
@@ -1,5 +1,12 @@
 #include "common.h"
 
+#include "gameplay/D4.h"
+
+#include "main/session.h"
+#include "main/task.h"
+
+extern GpMsgEntry D_shelter_1f_bulwark_8018032C[];
+
 INCLUDE_RODATA("rooms/nonmatchings/shelter_1f_bulwark/shelter_1f_bulwark", D_shelter_1f_bulwark_8017D5C0);
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_1f_bulwark/shelter_1f_bulwark", func_shelter_1f_bulwark_8017D61C);
@@ -25,7 +32,12 @@ s32 func_shelter_1f_bulwark_8017DBCC(void)
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_bulwark/shelter_1f_bulwark", func_shelter_1f_bulwark_8017DBD4);
+void func_shelter_1f_bulwark_8017DBD4(Task* arg0)
+{
+    arg0->field_24 = D_shelter_1f_bulwark_8018032C;
+    Game_SetPtrSlot(arg0, 7);
+    arg0->state = (s32)(arg0->state + 1);
+}
 
 void func_shelter_1f_bulwark_8017DC18(void)
 {
