3688652120836ee9b705a7195dc34aa100c22f61
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 04:14:25 +0200

    matched func_acropolis_cafeteria_8017E6B8 1

--- src/rooms/acropolis_cafeteria/acropolis_cafeteria_6.c
+++ src/rooms/acropolis_cafeteria/acropolis_cafeteria_6.c
@@ -1,6 +1,19 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/acropolis_cafeteria/acropolis_cafeteria_6", func_acropolis_cafeteria_8017E6B8);
+#include "gameplay/gameplay.h"
+#include "main/display.h"
+#include "main/task.h"
+
+extern s8       D_8007106B;
+extern TaskDesc D_acropolis_cafeteria_80184178;
+
+void func_acropolis_cafeteria_8017E6B8(Task* arg0)
+{
+    Display_SpawnWithOt(&D_acropolis_cafeteria_80184178, 2, 0, 0);
+    D_8007106B = 1;
+    Gp_SpawnViewTasks();
+    Task_Kill(arg0);
+}
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_cafeteria/acropolis_cafeteria_6", func_acropolis_cafeteria_8017E708);
 
