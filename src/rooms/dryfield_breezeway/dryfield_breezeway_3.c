567f17057198a8af7f57bc35ac5c148f7d1321c2
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 06:07:28 +0200

    matched func_dryfield_breezeway_8017FBEC 1

--- src/rooms/dryfield_breezeway/dryfield_breezeway_3.c
+++ src/rooms/dryfield_breezeway/dryfield_breezeway_3.c
@@ -1,5 +1,7 @@
 #include "common.h"
 
+#include <psyq/libgte.h>
+
 INCLUDE_ASM("rooms/nonmatchings/dryfield_breezeway/dryfield_breezeway_3", func_dryfield_breezeway_8017FA80);
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_breezeway/dryfield_breezeway_3", func_dryfield_breezeway_8017FAD0);
@@ -8,7 +10,16 @@ INCLUDE_ASM("rooms/nonmatchings/dryfield_breezeway/dryfield_breezeway_3", func_d
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_breezeway/dryfield_breezeway_3", func_dryfield_breezeway_8017FBC8);
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_breezeway/dryfield_breezeway_3", func_dryfield_breezeway_8017FBEC);
+s16 func_dryfield_breezeway_8017FBEC(s32 arg0, s32 arg1, s32 arg2, s32 arg3)
+{
+    SVECTOR vec;
+
+    vec.vx = arg2 - arg0;
+    vec.vy = arg3 - arg1;
+    vec.vz = 0;
+    VectorNormalSS(&vec, &vec);
+    return ratan2(vec.vx, vec.vy);
+}
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_breezeway/dryfield_breezeway_3", func_dryfield_breezeway_8017FC38);
 
