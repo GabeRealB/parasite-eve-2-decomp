25255319c69cc10b893c6b79a87eabf10144d4fc
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 04:25:45 +0200

    matched func_dryfield_night_motel_balcony_8017E0C8 1

--- src/rooms/dryfield_night_motel_balcony/dryfield_night_motel_balcony_2.c
+++ src/rooms/dryfield_night_motel_balcony/dryfield_night_motel_balcony_2.c
@@ -1,6 +1,21 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_night_motel_balcony/dryfield_night_motel_balcony_2", func_dryfield_night_motel_balcony_8017E0C8);
+#include "gameplay/gameplay.h"
+#include "main/display.h"
+#include "main/sound.h"
+#include "main/task.h"
+
+extern s8       D_8007106B;
+extern TaskDesc D_dryfield_night_motel_balcony_80182834;
+
+void func_dryfield_night_motel_balcony_8017E0C8(Task* arg0)
+{
+    Display_SpawnWithOt(&D_dryfield_night_motel_balcony_80182834, 1, 0, 0);
+    D_8007106B = 1;
+    Gp_SpawnViewTasks();
+    SndEvt_EnqueueType7(0x60010001, 1);
+    Task_Kill(arg0);
+}
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_night_motel_balcony/dryfield_night_motel_balcony_2", func_dryfield_night_motel_balcony_8017E128);
 
