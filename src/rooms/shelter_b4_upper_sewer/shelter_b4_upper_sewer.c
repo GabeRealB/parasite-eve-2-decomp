47fb9ccd4c00197b61cf630df4255060e59ff69d
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 08:02:23 +0200

    matched func_shelter_b4_upper_sewer_8017DB58 1

--- src/rooms/shelter_b4_upper_sewer/shelter_b4_upper_sewer.c
+++ src/rooms/shelter_b4_upper_sewer/shelter_b4_upper_sewer.c
@@ -3,6 +3,8 @@
 extern u8 D_8007216C;
 extern u8 D_shelter_b4_upper_sewer_80188D2C;
 
+extern void func_shelter_b4_upper_sewer_8017E59C(s32);
+
 INCLUDE_RODATA("rooms/nonmatchings/shelter_b4_upper_sewer/shelter_b4_upper_sewer", D_shelter_b4_upper_sewer_8017D5C0);
 
 INCLUDE_RODATA("rooms/nonmatchings/shelter_b4_upper_sewer/shelter_b4_upper_sewer", D_shelter_b4_upper_sewer_8017D5C4);
@@ -25,7 +27,14 @@ s32 func_shelter_b4_upper_sewer_8017DB50(void)
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b4_upper_sewer/shelter_b4_upper_sewer", func_shelter_b4_upper_sewer_8017DB58);
+s32 func_shelter_b4_upper_sewer_8017DB58(s32 arg0, s32 arg1, s32 arg2)
+{
+    if (arg2 == 4) {
+        func_shelter_b4_upper_sewer_8017E59C(1);
+        SndEvt_EnqueueType6(0x542C0004, 0, 0);
+    }
+    return 0;
+}
 
 void func_shelter_b4_upper_sewer_8017DB94(void)
 {
