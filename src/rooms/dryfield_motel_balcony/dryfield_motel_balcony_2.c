622a9cf1debb74cd741de21a97d662d1f3256ca6
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 13:50:22 +0200

    matched func_dryfield_motel_balcony_8017E66C 1

--- src/rooms/dryfield_motel_balcony/dryfield_motel_balcony_2.c
+++ src/rooms/dryfield_motel_balcony/dryfield_motel_balcony_2.c
@@ -1,9 +1,96 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_motel_balcony/dryfield_motel_balcony_2", func_dryfield_motel_balcony_8017E66C);
+#include "gameplay/3CD8.h"
+#include "gameplay/3FB8.h"
+#include "main/display.h"
+#include "main/mem.h"
+#include "main/task.h"
+#include "main/tmd.h"
+#include "rooms/dryfield_motel_balcony.h"
 
+#include <psyq/inline_c.h>
+#include <psyq/libgpu.h>
+#include <psyq/libgs.h>
+#include <psyq/libgte.h>
+
+#define gte_rtps_real() __asm__ volatile("nop; nop; .word 0x4A180001")
+
+extern void func_dryfield_motel_balcony_8017EF44(GsCOORDINATE2* arg0, s16 arg1);
+extern void Room_Draw09(GsCOORDINATE2* arg0, s16 arg1, s32 arg2, u8* arg3);
+extern void func_dryfield_motel_balcony_8017F7E8(GsCOORDINATE2* arg0, s16 arg1, u8* arg2);
+
+void func_dryfield_motel_balcony_8017E66C(GsCOORDINATE2* arg0, s16 arg1, u8* arg2)
+{
+    register RoomFanScratch* block asm("s2");
+    register POLY_G4*        prim asm("s0");
+    register s32             ang asm("s3");
+    register void**          scratch asm("a1");
+    register u8*             head asm("a2");
+    s32                      otz;
+    s32                      radius;
+    s32                      t;
+    s32                      t2;
+    u16                      vz;
+
+    scratch = (void**)G_SCRATCH_HEAD;
+    head    = *scratch;
+    {
+        register u16 vx asm("v0");
+        vx                                       = *(u16*)&arg0->workm.t[0];
+        ((RoomFanScratch*)(head - 0x18))->vec.vx = vx;
+    }
+    {
+        register u8* tmp asm("v0");
+        tmp   = head - 0x18;
+        block = (RoomFanScratch*)tmp;
+    }
+    block->vec.vy = *(u16*)&arg0->workm.t[1];
+    vz            = *(u16*)&arg0->workm.t[2];
+    *scratch      = block;
+    block->vec.vz = vz;
+
+    gte_SetTransMatrix(&GsWSMATRIX);
+    gte_SetRotMatrix(&GsWSMATRIX);
+    gte_ldv0(&block->vec);
+    gte_rtps_real();
+    gte_stsxy(&((RoomFanScratch*)(head - 0x18))->sx);
+    gte_stflg(&((RoomFanScratch*)(head - 0x18))->flag);
+    if (block->flag >= 0) {
+        gte_stszotz(&((RoomFanScratch*)(head - 0x18))->otz);
+        USE_REG(head);
+        otz           = block->otz + 1;
+        radius        = (arg1 * 64) / otz;
+        block->otz    = otz;
+        block->radius = radius;
+
+        ang = 0;
+        do {
+            prim           = (POLY_G4*)Gpu_PrimCursor;
+            Gpu_PrimCursor = (DR_TPAGE*)(prim + 1);
+            setPolyG4(prim);
+            setRGB0(prim, 0, 0, 0);
+            setRGB1(prim, 0, 0, 0);
+            setRGB2(prim, arg2[0], arg2[1], arg2[2]);
+            setRGB3(prim, 0, 0, 0);
+            prim->x0 = *(u16*)&block->sx + ((block->radius * rsin(ang)) >> 12);
+            t        = ang + 0x100;
+            prim->y0 = *(u16*)&block->sy + ((block->radius * rcos(ang)) >> 12);
+            prim->x1 = *(u16*)&block->sx + ((block->radius * rsin(t)) >> 12);
+            prim->y1 = *(u16*)&block->sy + ((block->radius * rcos(t)) >> 12);
+            t2       = ang + 0x200;
+            prim->x2 = *(u16*)&block->sx;
+            prim->y2 = *(u16*)&block->sy;
+            prim->x3 = *(u16*)&block->sx + ((block->radius * rsin(t2)) >> 12);
+            prim->y3 = *(u16*)&block->sy + ((block->radius * rcos(t2)) >> 12);
+            ang      = t2;
+            addPrim((u_long*)(((((u32)block->otz << Display_State.field_128) >> 2) & 0xFFC) + (s32)Gpu_CurrentOt),
+                    prim);
+            Gp_AddTpageShift((P_TAG*)prim, 1, block->otz);
+            SOFT_USE_REG(t2);
+        } while (ang < 0x1000);
+    }
+    *(void**)G_SCRATCH_HEAD = (u8*)*(void**)G_SCRATCH_HEAD + 0x18;
+}
 INCLUDE_ASM("rooms/nonmatchings/dryfield_motel_balcony/dryfield_motel_balcony_2", func_dryfield_motel_balcony_8017EA00);
-
 INCLUDE_ASM("rooms/nonmatchings/dryfield_motel_balcony/dryfield_motel_balcony_2", func_dryfield_motel_balcony_8017ED98);
-
 INCLUDE_ASM("rooms/nonmatchings/dryfield_motel_balcony/dryfield_motel_balcony_2", func_dryfield_motel_balcony_8017EF44);
