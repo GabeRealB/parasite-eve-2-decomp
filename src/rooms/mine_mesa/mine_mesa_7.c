08b4f1d868e607fe684c55a972fc535dcdb8d71e
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 02:11:44 +0200

    matched func_mine_mesa_8017EB38 1

--- src/rooms/mine_mesa/mine_mesa_7.c
+++ src/rooms/mine_mesa/mine_mesa_7.c
@@ -1,6 +1,16 @@
 #include "common.h"
+#include "main/task.h"
 
-INCLUDE_ASM("rooms/nonmatchings/mine_mesa/mine_mesa_7", func_mine_mesa_8017EB38);
+extern Task* D_mine_mesa_80189B54;
+extern Task* D_mine_mesa_80189B58;
+extern Task* D_mine_mesa_80189B5C;
+
+void func_mine_mesa_8017EB38(void)
+{
+    D_mine_mesa_80189B54 = NULL;
+    D_mine_mesa_80189B58 = NULL;
+    D_mine_mesa_80189B5C = NULL;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/mine_mesa/mine_mesa_7", func_mine_mesa_8017EB54);
 
