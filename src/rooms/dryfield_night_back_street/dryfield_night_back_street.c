1ba5b76ef7fc91d243d9b207ea0e1eb4214ea8b3
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 07:32:19 +0200

    matched func_dryfield_night_back_street_8017D73C 1

--- src/rooms/dryfield_night_back_street/dryfield_night_back_street.c
+++ src/rooms/dryfield_night_back_street/dryfield_night_back_street.c
@@ -1,5 +1,12 @@
 #include "common.h"
 
+#include "gameplay/D4.h"
+
+#include "main/session.h"
+#include "main/task.h"
+
+extern GpMsgEntry D_dryfield_night_back_street_80180324[];
+
 INCLUDE_ASM("rooms/nonmatchings/dryfield_night_back_street/dryfield_night_back_street", func_dryfield_night_back_street_8017D5D8);
 
 s32 func_dryfield_night_back_street_8017D724(void)
@@ -17,7 +24,12 @@ s32 func_dryfield_night_back_street_8017D734(void)
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_night_back_street/dryfield_night_back_street", func_dryfield_night_back_street_8017D73C);
+void func_dryfield_night_back_street_8017D73C(Task* arg0)
+{
+    arg0->field_24 = D_dryfield_night_back_street_80180324;
+    Game_SetPtrSlot(arg0, 7);
+    arg0->state = (s32)(arg0->state + 1);
+}
 
 void func_dryfield_night_back_street_8017D780(void)
 {
