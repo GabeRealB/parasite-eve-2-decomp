000f4724a4ab9a0558eba8c395993380444dbba6
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 09:50:04 +0200

    matched func_dryfield_night_water_tank_8017D94C 1

--- src/rooms/dryfield_night_water_tank/dryfield_night_water_tank_2.c
+++ src/rooms/dryfield_night_water_tank/dryfield_night_water_tank_2.c
@@ -1,12 +1,20 @@
 #include "common.h"
 
+#include "gameplay/268.h"
+#include "main/session.h"
+
 INCLUDE_ASM("rooms/nonmatchings/dryfield_night_water_tank/dryfield_night_water_tank_2", func_dryfield_night_water_tank_8017D73C);
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_night_water_tank/dryfield_night_water_tank_2", func_dryfield_night_water_tank_8017D76C);
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_night_water_tank/dryfield_night_water_tank_2", func_dryfield_night_water_tank_8017D870);
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_night_water_tank/dryfield_night_water_tank_2", func_dryfield_night_water_tank_8017D94C);
+void func_dryfield_night_water_tank_8017D94C(void)
+{
+    if (Game_Session->field_9 == 0xB) {
+        Gp_MarkPlayTime();
+    }
+}
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_night_water_tank/dryfield_night_water_tank_2", func_dryfield_night_water_tank_8017D984);
 
