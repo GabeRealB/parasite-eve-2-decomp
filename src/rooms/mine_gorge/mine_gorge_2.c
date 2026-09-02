9cc3396c47e876aa56f305e2d68f941f1e20069f
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 09:03:30 +0200

    matched func_mine_gorge_8017D7F4 1

--- src/rooms/mine_gorge/mine_gorge_2.c
+++ src/rooms/mine_gorge/mine_gorge_2.c
@@ -2,6 +2,12 @@
 
 INCLUDE_ASM("rooms/nonmatchings/mine_gorge/mine_gorge_2", func_mine_gorge_8017D784);
 
-INCLUDE_ASM("rooms/nonmatchings/mine_gorge/mine_gorge_2", func_mine_gorge_8017D7F4);
+s32 func_mine_gorge_8017D7F4(s32 arg0, s32 arg1, s32 arg2)
+{
+    if (arg2 == 0xA) {
+        SndEvt_EnqueueType6(0x54050000 | arg2, 0, 0);
+    }
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/mine_gorge/mine_gorge_2", func_mine_gorge_8017D828);
