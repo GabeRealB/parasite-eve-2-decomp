5f5881b47f82283ad858a855366e9b469f38f29f
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 09:12:53 +0200

    matched func_acropolis_promenade_8017D8E0 2

--- src/rooms/acropolis_promenade/acropolis_promenade_2.c
+++ src/rooms/acropolis_promenade/acropolis_promenade_2.c
@@ -1,6 +1,16 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/acropolis_promenade/acropolis_promenade_2", func_acropolis_promenade_8017D8E0);
+s32 func_acropolis_promenade_8017D8E0(s32 arg0, s32 arg1, s32 arg2)
+{
+    if (arg2 == 5) {
+        if (Gp_GetCurBit2Flag(0x15) != 2) {
+            Gp_StartCapSlot(5, 1, 0);
+        } else {
+            Gp_RunCapCmd1(9);
+        }
+    }
+    return 0;
+}
 
 void func_acropolis_promenade_8017D930(void)
 {
