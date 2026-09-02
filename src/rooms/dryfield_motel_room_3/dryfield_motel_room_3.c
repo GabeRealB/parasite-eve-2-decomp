ad0035a0944f3ba2fe1ed8998069c16c1394871e
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 07:01:38 +0200

    matched func_dryfield_motel_room_3_8017D610 1

--- src/rooms/dryfield_motel_room_3/dryfield_motel_room_3.c
+++ src/rooms/dryfield_motel_room_3/dryfield_motel_room_3.c
@@ -1,6 +1,18 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_motel_room_3/dryfield_motel_room_3", func_dryfield_motel_room_3_8017D610);
+#include "gameplay/D4.h"
+
+#include "main/session.h"
+#include "main/task.h"
+
+extern GpMsgEntry D_dryfield_motel_room_3_8017D6B4[];
+
+void func_dryfield_motel_room_3_8017D610(Task* arg0)
+{
+    arg0->field_24 = D_dryfield_motel_room_3_8017D6B4;
+    Game_SetPtrSlot(arg0, 7);
+    arg0->state = (s32)(arg0->state + 1);
+}
 
 void func_dryfield_motel_room_3_8017D654(void)
 {
