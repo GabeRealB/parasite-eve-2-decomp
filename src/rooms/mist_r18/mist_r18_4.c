f47af34a8a3c0bb0fb943033e8232ac1990f3244
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 09:22:15 +0200

    matched func_mist_r18_8017ECF4 1

--- src/rooms/mist_r18/mist_r18_4.c
+++ src/rooms/mist_r18/mist_r18_4.c
@@ -1,6 +1,9 @@
 #include "common.h"
 
+#include "gameplay/3CD8.h"
+
 #include "main/session.h"
+#include "main/task.h"
 
 void func_80179FC8(s32 arg0, s32 arg1);
 
@@ -9,6 +12,13 @@ extern s8 D_801156F9;
 
 /// Set by `func_mist_r18_8017D960` when the alternate cutscene branch ran.
 extern s32 D_mist_r18_80186EA0;
+
+extern s32 D_mist_r18_8018522C;
+extern s32 D_mist_r18_8018639C;
+extern s32 D_mist_r18_80186E90;
+extern s32 D_mist_r18_80186E94;
+extern s32 D_mist_r18_80186E98;
+extern s32 D_mist_r18_80186E9C;
 
 void func_mist_r18_8017EC98(void)
 {
@@ -27,6 +37,15 @@ void func_mist_r18_8017ECCC(void)
     func_80179FC8(0, D_mist_r18_80186EA0);
 }
 
-INCLUDE_ASM("rooms/nonmatchings/mist_r18/mist_r18_4", func_mist_r18_8017ECF4);
+void func_mist_r18_8017ECF4(Task* arg0)
+{
+    D_mist_r18_80186E90 = 0;
+    D_mist_r18_80186E94 = 0;
+    D_mist_r18_80186E98 = 0;
+    Game_SetPtrSlot(arg0, 7);
+    func_800E8634((s32)&D_mist_r18_8018522C, 0, (s32)&D_mist_r18_8018639C);
+    arg0->state         = (s32)(arg0->state + 1);
+    D_mist_r18_80186E9C = 1;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/mist_r18/mist_r18_4", func_mist_r18_8017ED64);
