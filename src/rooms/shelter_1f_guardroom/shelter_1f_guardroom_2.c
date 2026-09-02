c92e11fae4b02c53440ddffe44c7302d91a79168
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 04:58:45 +0200

    matched func_shelter_1f_guardroom_8017D824 2

--- src/rooms/shelter_1f_guardroom/shelter_1f_guardroom_2.c
+++ src/rooms/shelter_1f_guardroom/shelter_1f_guardroom_2.c
@@ -1,5 +1,15 @@
 #include "common.h"
 
+#include "gameplay/D4.h"
+
+#include "main/gameflag.h"
+#include "main/session.h"
+#include "main/task.h"
+
+extern GpMsgEntry D_shelter_1f_guardroom_8017DA30[];
+
+void func_shelter_1f_guardroom_8017D9CC(s32);
+
 INCLUDE_ASM("rooms/nonmatchings/shelter_1f_guardroom/shelter_1f_guardroom_2", func_shelter_1f_guardroom_8017D788);
 
 s32 func_shelter_1f_guardroom_8017D7E8(void)
@@ -9,7 +19,13 @@ s32 func_shelter_1f_guardroom_8017D7E8(void)
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_1f_guardroom/shelter_1f_guardroom_2", func_shelter_1f_guardroom_8017D7F0);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_guardroom/shelter_1f_guardroom_2", func_shelter_1f_guardroom_8017D824);
+void func_shelter_1f_guardroom_8017D824(Task* arg0)
+{
+    arg0->field_24 = D_shelter_1f_guardroom_8017DA30;
+    Game_SetPtrSlot(arg0, 7);
+    func_shelter_1f_guardroom_8017D9CC(GameFlag_GetNibble(0xB2) & 0xFF);
+    arg0->state = (s32)(arg0->state + 1);
+}
 
 void func_shelter_1f_guardroom_8017D878(void)
 {
