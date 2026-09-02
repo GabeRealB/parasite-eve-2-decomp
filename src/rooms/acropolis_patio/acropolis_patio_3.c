7d2afdffcf16e790b4e1b196ee6fc3dfb1b186e1
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 14:47:49 +0200

    matched func_acropolis_patio_8017DFE4 1

--- src/rooms/acropolis_patio/acropolis_patio_3.c
+++ src/rooms/acropolis_patio/acropolis_patio_3.c
@@ -1,5 +1,15 @@
 #include "common.h"
 
+extern u8 D_801153F4;
+
 INCLUDE_ASM("rooms/nonmatchings/acropolis_patio/acropolis_patio_3", func_acropolis_patio_8017DF8C);
 
-INCLUDE_ASM("rooms/nonmatchings/acropolis_patio/acropolis_patio_3", func_acropolis_patio_8017DFE4);
+void func_acropolis_patio_8017DFE4(s32 arg0)
+{
+    if (arg0 != 0) {
+        D_801153F4 = 0;
+        Gp_ArmStateF0(1);
+        return;
+    }
+    D_801153F4 = 1;
+}
