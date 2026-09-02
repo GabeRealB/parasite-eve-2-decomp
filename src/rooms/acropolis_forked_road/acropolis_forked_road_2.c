4a6dc9e2feca54579762b4e14b45b2f8f6ebcaa7
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 10:02:53 +0200

    matched func_acropolis_forked_road_8017D858 1

--- src/rooms/acropolis_forked_road/acropolis_forked_road_2.c
+++ src/rooms/acropolis_forked_road/acropolis_forked_road_2.c
@@ -4,7 +4,20 @@
 extern s32      D_acropolis_forked_road_80180F3C;
 extern TaskDesc D_acropolis_forked_road_80180F44;
 
-INCLUDE_ASM("rooms/nonmatchings/acropolis_forked_road/acropolis_forked_road_2", func_acropolis_forked_road_8017D858);
+s32 func_acropolis_forked_road_8017D858(s32 arg0, s32 arg1, s32 arg2)
+{
+    s32 cmd;
+
+    if (arg2 == 3) {
+        if ((Gp_GetCurBit2Flag(0x18) == 0) || (Gp_GetCurBit2Flag(0x18) == 1)) {
+            cmd = 3;
+        } else {
+            cmd = 4;
+        }
+        Gp_RunCapCmd1(cmd);
+    }
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_forked_road/acropolis_forked_road_2", func_acropolis_forked_road_8017D8A8);
 
