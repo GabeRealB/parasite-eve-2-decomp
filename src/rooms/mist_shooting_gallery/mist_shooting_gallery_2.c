91cf5a59b816e65eb2e1889584c5f426af85014a
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 22:54:11 +0200

    matched func_mist_shooting_gallery_80180B64 11

--- src/rooms/mist_shooting_gallery/mist_shooting_gallery_2.c
+++ src/rooms/mist_shooting_gallery/mist_shooting_gallery_2.c
@@ -366,7 +366,137 @@ s32 func_mist_shooting_gallery_80180B34(void)
     return 1;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/mist_shooting_gallery/mist_shooting_gallery_2", func_mist_shooting_gallery_80180B64);
+void func_mist_shooting_gallery_80180B64(Task* arg0)
+{
+    u8 param1[8];
+    u8 param2[8];
+
+    switch (arg0->state) {
+        case 0:
+            param1[3] = 0;
+            param1[2] = 0;
+            param2[0] = 0;
+            param2[1] = 0;
+            param2[2] = 0;
+            param2[3] = 0;
+            switch (D_80072177) {
+                case 0:
+                    param1[0] = 0x29;
+                    break;
+                case 1:
+                    param1[0] = 0x2A;
+                    break;
+                case 2:
+                    param1[0] = 0x2B;
+                    break;
+                case 3:
+                    param1[0] = 0x2C;
+                    break;
+            }
+            CdCmd_Enqueue(0x21, param1, param2);
+            arg0->state++;
+            return;
+
+        case 1:
+            if (CdCmd_IsIdle() & 0xFFFF) {
+                D_80071068[0]       = 1;
+                arg0->killCountdown = 0;
+                arg0->state++;
+                return;
+            }
+            return;
+
+        case 2: {
+            TILE*     p;
+            DR_TPAGE* dr;
+            u8        color;
+
+            p              = (TILE*)Gpu_PrimCursor;
+            color          = ~(u8)arg0->killCountdown;
+            Gpu_PrimCursor = (DR_TPAGE*)(p + 1);
+            setlen(p, 3);
+            setcode(p, 0x62);
+            p->r0 = color;
+            p->g0 = color;
+            p->b0 = color;
+            p->x0 = -0xA0;
+            p->y0 = -0x78;
+            p->w  = 0x140;
+            p->h  = 0xF0;
+
+            addPrim(Gpu_CurrentOt, p);
+            dr             = Gpu_PrimCursor;
+            Gpu_PrimCursor = dr + 1;
+            setlen(dr, 1);
+            dr->code[0] = 0xE1000240;
+            addPrim(Gpu_CurrentOt, dr);
+
+            arg0->killCountdown += 8;
+            if (arg0->killCountdown >= 0x11) {
+                SetDispMask(1);
+            }
+            if (arg0->killCountdown < 0x100) {
+                return;
+            }
+            arg0->killCountdown = 0;
+            arg0->state++;
+            return;
+        }
+
+        case 3:
+            arg0->killCountdown += 1;
+            if (arg0->killCountdown < 0x97 && Pad_CheckFlag800() == 0) {
+                return;
+            }
+            arg0->killCountdown = 0;
+            arg0->state++;
+            return;
+
+        case 4: {
+            TILE*     p;
+            DR_TPAGE* dr;
+            u8        color;
+
+            p              = (TILE*)Gpu_PrimCursor;
+            color          = (u8)arg0->killCountdown;
+            Gpu_PrimCursor = (DR_TPAGE*)(p + 1);
+            setlen(p, 3);
+            setcode(p, 0x62);
+            p->r0 = color;
+            p->g0 = color;
+            p->b0 = color;
+            p->x0 = -0xA0;
+            p->y0 = -0x78;
+            p->w  = 0x140;
+            p->h  = 0xF0;
+
+            addPrim(Gpu_CurrentOt, p);
+            dr             = Gpu_PrimCursor;
+            Gpu_PrimCursor = dr + 1;
+            setlen(dr, 1);
+            dr->code[0] = 0xE1000240;
+            addPrim(Gpu_CurrentOt, dr);
+
+            arg0->killCountdown += 8;
+            if (arg0->killCountdown < 0x100) {
+                return;
+            }
+            Mem_Set(Fs_ImgBuffers, 0, 0x25800);
+            SetDispMask(0);
+            arg0->state++;
+            return;
+        }
+
+        case 5:
+            D_80071068[0] = 0;
+            Task_Kill(arg0);
+            Display_ResetHeapWrapper();
+            return;
+
+        default:
+            return;
+    }
+}
 void func_mist_shooting_gallery_80180F2C(Task* arg0)
 {
     u8          slotParam[4];
