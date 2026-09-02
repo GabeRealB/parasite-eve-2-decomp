aec27669fcf7f5ab39028f480426f656ebec35f0
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-02 03:27:36 +0200

    matched func_shelter_b1_pod_access_tunnel_8017E704 1

--- src/rooms/shelter_b1_pod_access_tunnel/shelter_b1_pod_access_tunnel_3.c
+++ src/rooms/shelter_b1_pod_access_tunnel/shelter_b1_pod_access_tunnel_3.c
@@ -1,6 +1,7 @@
 #include "common.h"
 #include "main/task.h"
 extern TaskDesc D_shelter_b1_pod_access_tunnel_80182D2C;
+extern TaskDesc D_801348D8;
 
 void func_shelter_b1_pod_access_tunnel_8017E41C(s32 arg0)
 {
@@ -20,7 +21,10 @@ INCLUDE_ASM("rooms/nonmatchings/shelter_b1_pod_access_tunnel/shelter_b1_pod_acce
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b1_pod_access_tunnel/shelter_b1_pod_access_tunnel_3", func_shelter_b1_pod_access_tunnel_8017E66C);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b1_pod_access_tunnel/shelter_b1_pod_access_tunnel_3", func_shelter_b1_pod_access_tunnel_8017E704);
+void func_shelter_b1_pod_access_tunnel_8017E704(void)
+{
+    Task_SpawnFromTable(&D_801348D8, 0, 0, 0);
+}
 
 void func_shelter_b1_pod_access_tunnel_8017E734(s32 arg0)
 {
