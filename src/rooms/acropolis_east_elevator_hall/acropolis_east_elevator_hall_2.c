8a6ec71789ecd6ff0772d277c2670a70c99e0ada
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-04 01:24:20 +0200

    matched func_acropolis_east_elevator_hall_8017F478 1

--- src/rooms/acropolis_east_elevator_hall/acropolis_east_elevator_hall_2.c
+++ src/rooms/acropolis_east_elevator_hall/acropolis_east_elevator_hall_2.c
@@ -1,5 +1,13 @@
 #include "common.h"
 
+#include "gameplay/3CD8.h"
+#include "gameplay/D4.h"
+#include "main/session.h"
+#include "main/task.h"
+
+extern s32 D_acropolis_east_elevator_hall_80185C8C;
+extern s32 D_acropolis_east_elevator_hall_801862F4;
+
 INCLUDE_ASM("rooms/nonmatchings/acropolis_east_elevator_hall/acropolis_east_elevator_hall_2", func_acropolis_east_elevator_hall_8017F378);
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_east_elevator_hall/acropolis_east_elevator_hall_2", func_acropolis_east_elevator_hall_8017F420);
@@ -9,7 +17,14 @@ void func_acropolis_east_elevator_hall_8017F450(void)
     Gp_StartCapSlot(0x10, 1, 0);
 }
 
-INCLUDE_ASM("rooms/nonmatchings/acropolis_east_elevator_hall/acropolis_east_elevator_hall_2", func_acropolis_east_elevator_hall_8017F478);
+void func_acropolis_east_elevator_hall_8017F478(Task* task)
+{
+    task->field_24 = &D_acropolis_east_elevator_hall_801862F4;
+    Game_SetPtrSlot(task, 7);
+    Gp_MsgSlot4Chain(0, 1);
+    Gp_DispatchMsg((Task*)Gp_LookupSlot4(0), 0x7D3, (s32)&D_acropolis_east_elevator_hall_80185C8C, 0);
+    task->state++;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_east_elevator_hall/acropolis_east_elevator_hall_2", func_acropolis_east_elevator_hall_8017F4E8);
 
