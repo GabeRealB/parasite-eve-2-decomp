f9492a204be2e0c7d92a40022822298f7416f75e
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-03 23:36:23 +0200

    matched func_acropolis_hallway_8017D734 1

--- src/rooms/acropolis_hallway/acropolis_hallway_2.c
+++ src/rooms/acropolis_hallway/acropolis_hallway_2.c
@@ -1,6 +1,19 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/acropolis_hallway/acropolis_hallway_2", func_acropolis_hallway_8017D734);
+#include "main/sound.h"
+
+s32 func_acropolis_hallway_8017D734(s32 arg0, s32 arg1, s32 arg2)
+{
+    switch (arg2) { /* irregular */
+        case 6:
+            SndEvt_EnqueueType6(0x51070006, 0, 0);
+            break;
+        case 7:
+            SndEvt_EnqueueType6(0x51070007, 0, 0);
+            break;
+    }
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_hallway/acropolis_hallway_2", func_acropolis_hallway_8017D784);
 
