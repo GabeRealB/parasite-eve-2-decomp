82d4e6a7e5cca48290888d149278e376db01d423
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-04 19:19:05 +0200

    matched func_acropolis_promenade_8017D9E0 1

--- src/rooms/acropolis_promenade/acropolis_promenade_2.c
+++ src/rooms/acropolis_promenade/acropolis_promenade_2.c
@@ -1,5 +1,13 @@
 #include "common.h"
 
+#include "main/session.h"
+#include "main/task.h"
+
+extern u8       D_80115598;
+extern s32      D_acropolis_promenade_80180E74;
+extern TaskDesc D_acropolis_promenade_80180EA4;
+extern Task*    D_acropolis_promenade_801862D8;
+
 s32 func_acropolis_promenade_8017D8E0(s32 arg0, s32 arg1, s32 arg2)
 {
     if (arg2 == 5) {
@@ -20,7 +28,14 @@ INCLUDE_ASM("rooms/nonmatchings/acropolis_promenade/acropolis_promenade_2", func
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_promenade/acropolis_promenade_2", func_acropolis_promenade_8017D988);
 
-INCLUDE_ASM("rooms/nonmatchings/acropolis_promenade/acropolis_promenade_2", func_acropolis_promenade_8017D9E0);
+void func_acropolis_promenade_8017D9E0(Task* arg0)
+{
+    arg0->field_24 = &D_acropolis_promenade_80180E74;
+    Game_SetPtrSlot(arg0, 7);
+    D_acropolis_promenade_801862D8 = Task_SpawnFromTable(&D_acropolis_promenade_80180EA4, 0, 0, 0);
+    arg0->state                    = (s32)(arg0->state + 1);
+    D_80115598                     = 1;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_promenade/acropolis_promenade_2", func_acropolis_promenade_8017DA4C);
 
