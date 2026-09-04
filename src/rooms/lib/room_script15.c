c9af5af7f46ab82dd00b819ea98275fa20885b8b
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-04 18:36:52 +0200

    matched Room_Script15 1

--- src/rooms/lib/room_script15.c
+++ src/rooms/lib/room_script15.c
@@ -1,3 +1,18 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/lib/room_script15", Room_Script15);
+#include "gameplay/1A8.h"
+#include "gameplay/3CD8.h"
+#include "main/gameflag.h"
+#include "main/sound.h"
+#include "main/task.h"
+
+s32 Room_Script15(Task* task, s32 msgId, GpMsg13EF* arg2)
+{
+    if ((arg2->field_2 == 1) && (GameFlag_GetNibble(0x2C) == 0)) {
+        Gp_SpawnIfCapIdle(0xB, 1);
+        GameFlag_SetNibble(0x2C, 1);
+        func_800E3FAC(0xA2, 0xA);
+        SndEvt_EnqueueType6(0x5217000A, 0, 0);
+    }
+    return 0;
+}
