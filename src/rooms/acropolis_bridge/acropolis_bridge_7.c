025624c55bf1913ea1c9434a21dafa18d3d98e59
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-04 21:21:00 +0200

    matched func_acropolis_bridge_8017F658 1

--- src/rooms/acropolis_bridge/acropolis_bridge_7.c
+++ src/rooms/acropolis_bridge/acropolis_bridge_7.c
@@ -1,5 +1,14 @@
 #include "common.h"
 
+#include "main/display.h"
+#include "main/session.h"
+#include "main/task.h"
+
+extern s16 D_80114D08;
+extern s32 D_acropolis_bridge_801917A8;
+
+void func_acropolis_bridge_8017E60C(s32 arg0, s32 arg1);
+
 INCLUDE_ASM("rooms/nonmatchings/acropolis_bridge/acropolis_bridge_7", func_acropolis_bridge_8017F280);
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_bridge/acropolis_bridge_7", func_acropolis_bridge_8017F2D0);
@@ -14,7 +23,17 @@ INCLUDE_ASM("rooms/nonmatchings/acropolis_bridge/acropolis_bridge_7", func_acrop
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_bridge/acropolis_bridge_7", func_acropolis_bridge_8017F544);
 
-INCLUDE_ASM("rooms/nonmatchings/acropolis_bridge/acropolis_bridge_7", func_acropolis_bridge_8017F658);
+void func_acropolis_bridge_8017F658(Task* task)
+{
+    Display_ReleaseRef();
+    func_acropolis_bridge_8017E60C(0xFFF, 0);
+    Task_Kill((Task*)task->spawnArg2);
+    Task_RequestKill(task, D_acropolis_bridge_801917A8);
+    Game_Session->field_1  = 0;
+    Game_Session->field_68 = 0;
+    Game_Session->field_66 = 0;
+    D_80114D08             = 0xA;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_bridge/acropolis_bridge_7", func_acropolis_bridge_8017F6D4);
 
