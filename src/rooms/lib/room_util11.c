3d18508dc953f412feda180fcb4aed20d813387a
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-04 18:43:17 +0200

    matched Room_Util11 1

--- src/rooms/lib/room_util11.c
+++ src/rooms/lib/room_util11.c
@@ -1,3 +1,9 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/lib/room_util11", Room_Util11);
+#include "main/session.h"
+
+s32 Room_Util11(s32 arg0, s32 arg1, GBytes8* arg2, GBytes8* arg3)
+{
+    *arg3 = *arg2;
+    return 1;
+}
