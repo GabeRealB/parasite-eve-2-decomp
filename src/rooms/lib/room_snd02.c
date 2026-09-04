6d540ac53bf38102a76da1eab43566ddb36b8f54
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-04 19:28:49 +0200

    matched Room_Snd02 1

--- src/rooms/lib/room_snd02.c
+++ src/rooms/lib/room_snd02.c
@@ -1,3 +1,13 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/lib/room_snd02", Room_Snd02);
+#include "gameplay/3CD8.h"
+
+#include "main/task.h"
+
+s32 Room_Snd02(Task* task, s32 msgId, s32 arg2, s32 arg3)
+{
+    if (arg2 == 7) {
+        Gp_EnqueueStageSnd6(0x52030000 | 7, 0, 0);
+    }
+    return 0;
+}
