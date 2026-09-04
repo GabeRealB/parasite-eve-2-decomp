47638e42b11a8cb6e47552614f4c12920eee17fa
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-04 12:45:23 +0200

    matched func_acropolis_west_elevator_hall_8017F4C0 1

--- src/rooms/acropolis_west_elevator_hall/acropolis_west_elevator_hall.c
+++ src/rooms/acropolis_west_elevator_hall/acropolis_west_elevator_hall.c
@@ -1,6 +1,10 @@
 #include "common.h"
 
+#include "gameplay/1A8.h"
+#include "main/gameflag.h"
 #include "main/task.h"
+
+extern s8 D_8007272D;
 
 extern Task* D_acropolis_west_elevator_hall_80186AE4[];
 
@@ -34,7 +38,16 @@ s32 func_acropolis_west_elevator_hall_8017F498(void)
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/acropolis_west_elevator_hall/acropolis_west_elevator_hall", func_acropolis_west_elevator_hall_8017F4C0);
+s32 func_acropolis_west_elevator_hall_8017F4C0(Task* task, s32 msgId, GpSaveLoc* src, GpSaveLoc* dst)
+{
+    *dst = *src;
+    if (*(u16*)src == 1 && GameFlag_GetNibble(0x21) == 0 && src->field_5 == 0) {
+        GameFlag_SetNibble(0x21, 1);
+        D_8007272D   = 1;
+        dst->field_2 = 7;
+    }
+    return 1;
+}
 
 INCLUDE_RODATA("rooms/nonmatchings/acropolis_west_elevator_hall/acropolis_west_elevator_hall", D_acropolis_west_elevator_hall_8017D5EC);
 
