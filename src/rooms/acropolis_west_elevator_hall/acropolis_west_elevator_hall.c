0cde54e98c668b378827089146f0dfebaba5f438
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-04 12:38:08 +0200

    matched func_acropolis_west_elevator_hall_8017F498 1

--- src/rooms/acropolis_west_elevator_hall/acropolis_west_elevator_hall.c
+++ src/rooms/acropolis_west_elevator_hall/acropolis_west_elevator_hall.c
@@ -1,5 +1,9 @@
 #include "common.h"
 
+#include "main/task.h"
+
+extern Task* D_acropolis_west_elevator_hall_80186AE4[];
+
 INCLUDE_ASM("rooms/nonmatchings/acropolis_west_elevator_hall/acropolis_west_elevator_hall", func_acropolis_west_elevator_hall_8017D5FC);
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_west_elevator_hall/acropolis_west_elevator_hall", func_acropolis_west_elevator_hall_8017D7B0);
@@ -18,7 +22,12 @@ INCLUDE_ASM("rooms/nonmatchings/acropolis_west_elevator_hall/acropolis_west_elev
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_west_elevator_hall/acropolis_west_elevator_hall", func_acropolis_west_elevator_hall_8017F470);
 
-INCLUDE_ASM("rooms/nonmatchings/acropolis_west_elevator_hall/acropolis_west_elevator_hall", func_acropolis_west_elevator_hall_8017F498);
+s32 func_acropolis_west_elevator_hall_8017F498(void)
+{
+    D_acropolis_west_elevator_hall_80186AE4[0]->spawnArg1 = -1;
+    D_acropolis_west_elevator_hall_80186AE4[1]->spawnArg1 = -1;
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_west_elevator_hall/acropolis_west_elevator_hall", func_acropolis_west_elevator_hall_8017F4C0);
 
