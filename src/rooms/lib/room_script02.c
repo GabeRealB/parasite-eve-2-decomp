5bb7b863fbc695322245fda14d54f00a1bbacbc5
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-04 21:38:04 +0200

    matched Room_Script02 26

--- src/rooms/lib/room_script02.c
+++ src/rooms/lib/room_script02.c
@@ -1,3 +1,25 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/lib/room_script02", Room_Script02);
+#include "main/gameflag.h"
+
+#include "rooms/room_common.h"
+
+s32 Room_Script02(s32 arg0, s32 arg1, RoomEventMsg* in, RoomEventMsg* out)
+{
+    s32 val;
+    s32 n;
+
+    *out = *in;
+    if (in->msgId == 2 && in->field_5 == 0) {
+        n = GameFlag_GetNibble(0x7A);
+        if (n < 4) {
+            val = 3;
+            TOUCH_REG(val);
+            val = GameFlag_GetNibble(0x61) + 1;
+        } else {
+            val = 3;
+        }
+        out->field_3 = val;
+    }
+    return 1;
+}
