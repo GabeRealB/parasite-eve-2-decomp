34b4781018c493d53edef657d728ae704d33e0ac
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-04 18:23:45 +0200

    matched Room_Script20 1

--- src/rooms/lib/room_script20.c
+++ src/rooms/lib/room_script20.c
@@ -1,3 +1,11 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/lib/room_script20", Room_Script20);
+#include "gameplay/3CD8.h"
+
+s32 Room_Script20(s32 arg0, s32 arg1, s32 arg2)
+{
+    if (arg2 == 4) {
+        Gp_RunCapCmd1(4);
+    }
+    return 0;
+}
