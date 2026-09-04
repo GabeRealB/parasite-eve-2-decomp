d23ac563403109c4db781512beb6aca5f94d1637
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-04 21:17:24 +0200

    matched func_acropolis_bridge_8017DE94 1

--- src/rooms/acropolis_bridge/acropolis_bridge_5.c
+++ src/rooms/acropolis_bridge/acropolis_bridge_5.c
@@ -1,10 +1,21 @@
 #include "common.h"
 
+#include "main/gameflag.h"
+#include "main/session.h"
+#include "main/task.h"
+
+void func_acropolis_bridge_8017F2D0(s32 arg0);
+
 INCLUDE_ASM("rooms/nonmatchings/acropolis_bridge/acropolis_bridge_5", func_acropolis_bridge_8017DD9C);
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_bridge/acropolis_bridge_5", func_acropolis_bridge_8017DDEC);
 
-INCLUDE_ASM("rooms/nonmatchings/acropolis_bridge/acropolis_bridge_5", func_acropolis_bridge_8017DE94);
+void func_acropolis_bridge_8017DE94(Task* arg0)
+{
+    func_acropolis_bridge_8017F2D0(GameFlag_GetNibble(0x10) & 0xFF);
+    Game_Session->field_68 = 0;
+    arg0->state            = (s32)(arg0->state + 1);
+}
 
 INCLUDE_ASM("rooms/nonmatchings/acropolis_bridge/acropolis_bridge_5", func_acropolis_bridge_8017DEE4);
 
