75b86496bc2705c6de02312e655775667bc060cd
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-06 01:34:50 +0200

    matched func_combustion_801308E0 1

--- src/pe/combustion/combustion.c
+++ src/pe/combustion/combustion.c
@@ -1,5 +1,10 @@
 #include "common.h"
 
+#include "gameplay/3CD8.h"
+#include "gameplay/gameplay.h"
+#include "main/task.h"
+#include "main/tmd.h"
+
 INCLUDE_ASM("pe/nonmatchings/combustion/combustion", func_combustion_8012EF34);
 
 INCLUDE_ASM("pe/nonmatchings/combustion/combustion", func_combustion_8012F2BC);
@@ -16,6 +21,19 @@ INCLUDE_ASM("pe/nonmatchings/combustion/combustion", func_combustion_80130184);
 
 INCLUDE_ASM("pe/nonmatchings/combustion/combustion", func_combustion_801305F8);
 
-INCLUDE_ASM("pe/nonmatchings/combustion/combustion", func_combustion_801308E0);
+void func_combustion_801308E0(Task* arg0)
+{
+    GsCOORDINATE2* coord;
+
+    if (arg0->state != 0) {
+        Gp_ReleaseState1CMem(arg0->spawnArg2, arg0);
+        return;
+    }
+    coord = ((TmdObject*)arg0->extra)->field_8;
+    Gp_UpdateCoord(coord);
+    Gp_SpawnEff(0x8006001B, coord, 1, 0);
+    Gp_SpawnEff(0x8006001B, coord, -1, 0);
+    arg0->state = arg0->state + 1;
+}
 
 INCLUDE_RODATA("pe/nonmatchings/combustion/combustion", D_combustion_8012EF30);
