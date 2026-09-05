6cc610d3526f85e251e69a24bdb324b5c70cb53e
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-05 13:34:05 +0200

    matched func_map_akropolis_80179FC8 1

--- src/mapui/map_akropolis/map_akropolis.c
+++ src/mapui/map_akropolis/map_akropolis.c
@@ -1,6 +1,11 @@
 #include "common.h"
 
-extern s32 D_map_akropolis_8017A9A8;
+#include "gameplay/268.h"
+#include "main/display.h"
+
+extern s32      D_map_akropolis_8017A9A8;
+extern s32      D_map_akropolis_8017A9AC[4];
+extern TaskDesc D_map_akropolis_8017AA00;
 
 INCLUDE_ASM("mapui/nonmatchings/map_akropolis/map_akropolis", func_map_akropolis_80179C50);
 
@@ -10,7 +15,23 @@ INCLUDE_ASM("mapui/nonmatchings/map_akropolis/map_akropolis", func_map_akropolis
 
 INCLUDE_ASM("mapui/nonmatchings/map_akropolis/map_akropolis", func_map_akropolis_80179E8C);
 
-INCLUDE_ASM("mapui/nonmatchings/map_akropolis/map_akropolis", func_map_akropolis_80179FC8);
+s32 func_map_akropolis_80179FC8(s32 arg0, s32 arg1)
+{
+    s32* p;
+    s32  i;
+
+    if (arg1 == 0) {
+        i = 0;
+        p = D_map_akropolis_8017A9AC;
+        do {
+            Gp_SetCollectedBit(*p++);
+            i++;
+        } while (i < 4);
+        D_map_akropolis_8017A9A8 = 0;
+    }
+    Display_InitModeObj(&D_map_akropolis_8017AA00, 0, 0, 0);
+    return 1;
+}
 
 s32 func_map_akropolis_8017A038(void)
 {
