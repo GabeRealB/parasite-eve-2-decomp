bac1e4c6feb1c9846d60be8b5ae096bca0a10720
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-05 23:18:21 +0200

    matched func_acropolis_forked_road_8017D8A8 1

--- src/rooms/acropolis_forked_road/acropolis_forked_road_2.c
+++ src/rooms/acropolis_forked_road/acropolis_forked_road_2.c
@@ -1,8 +1,15 @@
 #include "common.h"
 
+#include "gameplay/1A8.h"
+#include "gameplay/3CD8.h"
 #include "gameplay/gameplay.h"
 
 #include "main/fs.h"
+#include "main/gameflag.h"
+#include "main/session.h"
+#include "main/task.h"
+
+extern s32 D_acropolis_forked_road_801820B8;
 
 s32 func_acropolis_forked_road_8017D858(s32 arg0, s32 arg1, s32 arg2)
 {
@@ -19,7 +26,19 @@ s32 func_acropolis_forked_road_8017D858(s32 arg0, s32 arg1, s32 arg2)
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/acropolis_forked_road/acropolis_forked_road_2", func_acropolis_forked_road_8017D8A8);
+s32 func_acropolis_forked_road_8017D8A8(Task* task, s32 msgId, GpMsg13EF* arg2)
+{
+    u8 temp;
+
+    if (arg2->field_2 == 1 && (GameFlag_GetNibble(9) & 2)) {
+        temp = Game_Session->field_9;
+        if (((temp == 4) || (temp == 8)) && (GameFlag_GetNibble(0xCC) == 0)) {
+            func_800E8614((s32)&D_acropolis_forked_road_801820B8, 1);
+            GameFlag_SetNibble(0xCC, 1);
+        }
+    }
+    return 1;
+}
 
 INCLUDE_RODATA("rooms/nonmatchings/acropolis_forked_road/acropolis_forked_road_2", jtbl_acropolis_forked_road_8017D5D0);
 
