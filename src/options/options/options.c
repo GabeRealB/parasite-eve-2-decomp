11023ec6c17edabc23ec331108a12458f304ffd2
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-05 21:56:38 +0200

    matched func_options_801D4B64 1

--- src/options/options/options.c
+++ src/options/options/options.c
@@ -12,6 +12,8 @@ extern u8           D_options_801D5B2C[];
 extern u8           D_options_801D5BAC[];
 extern u8           D_options_801D5DA4[];
 extern u8           D_options_801D5DDC[];
+extern UiList       D_options_801D5EB0;
+extern UiList       D_options_801D5ED8;
 extern UiObjectDesc D_options_801D5EFC;
 
 INCLUDE_ASM("options/nonmatchings/options/options", func_options_801D404C);
@@ -26,7 +28,55 @@ INCLUDE_ASM("options/nonmatchings/options/options", func_options_801D4724);
 
 INCLUDE_ASM("options/nonmatchings/options/options", func_options_801D4944);
 
-INCLUDE_ASM("options/nonmatchings/options/options", func_options_801D4B64);
+void func_options_801D4B64(Task* task)
+{
+    UiList*   list;
+    UiObject* obj;
+    UiObject* child;
+    s32       status;
+    s32       result;
+
+    list = &D_options_801D5EB0;
+    obj  = task->spawnArg2;
+    if (task->spawnArg1 == 1) {
+        list = &D_options_801D5ED8;
+    }
+    if (task->state == 0) {
+        Ui_LayoutListPanel(list, (UiPanel*)obj);
+        task->state += 1;
+        if (task->spawnArg1 == 1) {
+            Ui_UpdateLayoutSize((UiPanel*)obj, 0xC0, 0);
+            obj->field_E = -((s16)obj->field_12 / 2);
+            obj->field_C = -((s16)obj->field_10 / 2);
+        }
+    }
+    obj->field_2E = 0;
+    Ui_DrawText((UiPanel*)obj, "Option");
+    Ui_UpdateListNoAnim(list, obj);
+    status = obj->status;
+    if (status == 1) {
+        if (Pad_CheckButtons(0, 1, Pad_MaskCancel) != 0) {
+            SndEvt_EnqueueType6(4, 0, 0);
+            obj->field_2C = status;
+            obj->field_2E = 6;
+        } else if (Pad_CheckButtons(0, 1, Pad_MaskMenu) != 0) {
+            obj->field_2E = -1;
+        }
+    }
+    if (task->firstChild != NULL) {
+        child  = task->firstChild->spawnArg2;
+        result = child->field_2E;
+        switch (result) {
+            case 6:
+                Ui_TeardownTree(child, child->owner);
+                obj->status = 1;
+                break;
+            case -1:
+                obj->field_2E = result;
+                break;
+        }
+    }
+}
 
 INCLUDE_ASM("options/nonmatchings/options/options", func_options_801D4D0C);
 
