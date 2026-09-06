e23b0f6f2aaa826a7a7d5cbec976416d3aea6cbc
wyrel <wyrelade@gmail.com>
2026-09-07 00:46:43 +0300

    matched func_shelter_1f_guardroom_8017D788, func_shelter_1f_guardroom_8017D7F0
    8017D788: on event 2, gates on GameFlag 0xB2 - either MsgPlayerWeapon + spawn
    or Gp_RunCapCmd1.
    8017D7F0: on event 3, SndEvt_EnqueueType6.

--- src/rooms/shelter_1f_guardroom/shelter_1f_guardroom_2.c
+++ src/rooms/shelter_1f_guardroom/shelter_1f_guardroom_2.c
@@ -1,23 +1,43 @@
 #include "common.h"
 
+#include "gameplay/3CD8.h"
 #include "gameplay/D4.h"
 
 #include "main/gameflag.h"
 #include "main/session.h"
+#include "main/sound.h"
 #include "main/task.h"
 
 extern GpMsgEntry D_shelter_1f_guardroom_8017DA30[];
+extern TaskDesc   D_shelter_1f_guardroom_8017DA60;
 
 void func_shelter_1f_guardroom_8017D9CC(s32);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_guardroom/shelter_1f_guardroom_2", func_shelter_1f_guardroom_8017D788);
+s32 func_shelter_1f_guardroom_8017D788(s32 arg0, s32 arg1, s32 arg2)
+{
+    if (arg2 == 2) {
+        if (GameFlag_GetNibble(0xB2) == 0) {
+            Gp_MsgPlayerWeapon(0);
+            Task_SpawnFromTable(&D_shelter_1f_guardroom_8017DA60, 0, 0, 0);
+        } else {
+            Gp_RunCapCmd1(3);
+        }
+    }
+    return 0;
+}
 
 s32 func_shelter_1f_guardroom_8017D7E8(void)
 {
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_guardroom/shelter_1f_guardroom_2", func_shelter_1f_guardroom_8017D7F0);
+s32 func_shelter_1f_guardroom_8017D7F0(s32 arg0, s32 arg1, s32 arg2)
+{
+    if (arg2 == 3) {
+        SndEvt_EnqueueType6(0x55060003, 0, 0);
+    }
+    return 0;
+}
 
 void func_shelter_1f_guardroom_8017D824(Task* arg0)
 {
