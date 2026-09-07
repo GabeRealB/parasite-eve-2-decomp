9f01839494e49503663ea77b0153711e943ef6bd
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-07 17:11:15 +0200

    matched func_actor_341700_8016A8F4 1

--- src/actors/actor_341700/actor_341700_7.c
+++ src/actors/actor_341700/actor_341700_7.c
@@ -18,4 +18,17 @@ void func_actor_341700_8016A8EC(void)
 {
 }
 
-INCLUDE_ASM("actors/nonmatchings/actor_341700/actor_341700_7", func_actor_341700_8016A8F4);
+void func_actor_341700_8016A8F4(Task* arg0)
+{
+    Actor341700Work* work;
+    GpEnemy*         enemy;
+
+    enemy = (GpEnemy*)arg0->spawnArg2;
+    work  = (Actor341700Work*)arg0->idMap;
+    SndEvt_EnqueueType7(((enemy->field_8 >> 0xC) << 8) | 0x402C0002, 0xF);
+    if ((Gp_StateF0.field_1F & 0xF) == (((GpEnemy*)arg0->spawnArg2)->field_8 >> 0xC)) {
+        Gp_StateF0.field_1F = 0;
+    }
+    Gp_UnlinkNode(&enemy->node);
+    work->field_420 = work->field_420 + 1;
+}
--- src/actors/actor_342400/actor_342400_6.c
+++ src/actors/actor_342400/actor_342400_6.c
@@ -1,5 +1,10 @@
 #include "common.h"
+
+#include "main/sound.h"
 #include "main/task.h"
+
+#include "gameplay/1BC.h"
+
 #include "actors/actor_342400.h"
 
 INCLUDE_ASM("actors/nonmatchings/actor_342400/actor_342400_6", func_actor_342400_8016B104);
@@ -30,7 +35,20 @@ INCLUDE_ASM("actors/nonmatchings/actor_342400/actor_342400_6", func_actor_342400
 
 INCLUDE_ASM("actors/nonmatchings/actor_342400/actor_342400_6", func_actor_342400_8016B914);
 
-INCLUDE_ASM("actors/nonmatchings/actor_342400/actor_342400_6", func_actor_342400_8016B9A4);
+void func_actor_342400_8016B9A4(Task* arg0)
+{
+    Actor342400Work* work;
+    GpEnemy*         enemy;
+
+    enemy = (GpEnemy*)arg0->spawnArg2;
+    work  = (Actor342400Work*)arg0->idMap;
+    SndEvt_EnqueueType7(((enemy->field_8 >> 0xC) << 8) | 0x402C0002, 0xF);
+    if ((Gp_StateF0.field_1F & 0xF) == (((GpEnemy*)arg0->spawnArg2)->field_8 >> 0xC)) {
+        Gp_StateF0.field_1F = 0;
+    }
+    Gp_UnlinkNode(&enemy->node);
+    work->field_420 = work->field_420 + 1;
+}
 
 INCLUDE_ASM("actors/nonmatchings/actor_342400/actor_342400_6", func_actor_342400_8016BA3C);
 
