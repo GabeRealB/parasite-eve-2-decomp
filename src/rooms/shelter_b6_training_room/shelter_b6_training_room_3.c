dac396a3c411585b91b40d237fe2a7f77976a022
wyrel <wyrelade@gmail.com>
2026-09-07 20:00:37 +0300

    matched func_shelter_b6_training_room_8017D940 1

--- src/rooms/shelter_b6_training_room/shelter_b6_training_room_3.c
+++ src/rooms/shelter_b6_training_room/shelter_b6_training_room_3.c
@@ -1,3 +1,11 @@
 #include "common.h"
+#include "main/task.h"
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b6_training_room/shelter_b6_training_room_3", func_shelter_b6_training_room_8017D940);
+#include "rooms/rooms_shared_801807d4.h"
+
+extern TaskDesc D_shelter_b6_training_room_801839A8;
+
+void func_shelter_b6_training_room_8017D940(void)
+{
+    RoomsShared801807d4Task = Task_SpawnFromTable(&D_shelter_b6_training_room_801839A8, 0, 0, 0);
+}
