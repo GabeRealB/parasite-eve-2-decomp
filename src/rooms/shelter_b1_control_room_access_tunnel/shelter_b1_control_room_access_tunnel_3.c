e8f182de0e6f1b609bcbe0e9180e73a1dd0c99d3
wyrel <wyrelade@gmail.com>
2026-09-07 11:48:56 +0300

    matched func_shelter_b1_control_room_access_tunnel_8017E1BC 1

--- src/rooms/shelter_b1_control_room_access_tunnel/shelter_b1_control_room_access_tunnel_3.c
+++ src/rooms/shelter_b1_control_room_access_tunnel/shelter_b1_control_room_access_tunnel_3.c
@@ -1,3 +1,46 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b1_control_room_access_tunnel/shelter_b1_control_room_access_tunnel_3", func_shelter_b1_control_room_access_tunnel_8017E1BC);
+#include "gameplay/D4.h"
+
+#include "rooms/room_common.h"
+
+#include "main/task.h"
+
+extern s32 D_8011572C;
+extern s32 D_80115730;
+extern s32 D_80115734;
+extern s32 D_80115750;
+extern s32 D_80115754;
+extern s32 D_80115758;
+
+extern SVECTOR D_shelter_b1_control_room_access_tunnel_80181E9C[];
+extern SVECTOR D_shelter_b1_control_room_access_tunnel_80181EAC[];
+
+void func_shelter_b1_control_room_access_tunnel_8017E1BC(Task* arg0)
+{
+    u8 view;
+
+    if (arg0->state == 0) {
+        D_80115758  = 0x601CF;
+        D_8011572C  = 0x601EB;
+        D_80115750  = 0x60207;
+        D_80115734  = 0x60279;
+        D_80115730  = 0x6027A;
+        D_80115754  = 0x6027B;
+        arg0->state = 1;
+    }
+    view = Gp_GetViewIndex();
+    switch (view) {
+        case 2: {
+            SVECTOR* p = D_shelter_b1_control_room_access_tunnel_80181E9C;
+            Room_Draw11(&p[0], 0x200, 0x400);
+            Room_Draw11(&p[4], 0x200, 0x400);
+            Room_Draw25(&p[8], 0x200);
+        } break;
+        case 3: {
+            SVECTOR* p = D_shelter_b1_control_room_access_tunnel_80181EAC;
+            Room_Draw11(&p[0], 0x200, -0x400);
+            Room_Draw11(&p[4], 0x200, -0x400);
+        } break;
+    }
+}
