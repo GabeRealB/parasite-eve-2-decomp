939e5d45e016523d2981cb981d2e4560ec242d88
wyrel <wyrelade@gmail.com>
2026-09-07 19:26:52 +0300

    matched func_shelter_b1_golem_freezer_1_8017D624 1

--- src/rooms/shelter_b1_golem_freezer_1/shelter_b1_golem_freezer_1.c
+++ src/rooms/shelter_b1_golem_freezer_1/shelter_b1_golem_freezer_1.c
@@ -1,6 +1,16 @@
 #include "common.h"
+#include "main/session.h"
+#include "rooms/room_common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b1_golem_freezer_1/shelter_b1_golem_freezer_1", func_shelter_b1_golem_freezer_1_8017D624);
+extern void func_80131E70(void);
+
+s32 func_shelter_b1_golem_freezer_1_8017D624(s32 arg0, s32 arg1, RoomEventMsg* msg)
+{
+    if (msg->field_2 == 1 && Game_Session->field_9 == 0x15) {
+        func_80131E70();
+    }
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b1_golem_freezer_1/shelter_b1_golem_freezer_1", func_shelter_b1_golem_freezer_1_8017D66C);
 
