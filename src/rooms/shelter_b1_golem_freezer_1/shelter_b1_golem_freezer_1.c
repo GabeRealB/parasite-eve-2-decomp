a9bebf6e77941d9d8c990ad1c885be099dcb8b96
wyrel <wyrelade@gmail.com>
2026-09-09 20:52:12 +0300

    matched func_shelter_b1_golem_freezer_1_8017D66C 1

--- src/rooms/shelter_b1_golem_freezer_1/shelter_b1_golem_freezer_1.c
+++ src/rooms/shelter_b1_golem_freezer_1/shelter_b1_golem_freezer_1.c
@@ -1,8 +1,16 @@
 #include "common.h"
+
+#include "gameplay/D4.h"
+
 #include "main/session.h"
+#include "main/task.h"
 #include "rooms/room_common.h"
 
+extern GpMsgEntry D_shelter_b1_golem_freezer_1_8017E6A8[];
+
 extern void func_80131E70(void);
+extern void func_80131E24(void);
+void        func_shelter_b1_golem_freezer_1_8017D744(s32 arg0);
 
 s32 func_shelter_b1_golem_freezer_1_8017D624(s32 arg0, s32 arg1, RoomEventMsg* msg)
 {
@@ -12,7 +20,16 @@ s32 func_shelter_b1_golem_freezer_1_8017D624(s32 arg0, s32 arg1, RoomEventMsg* m
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b1_golem_freezer_1/shelter_b1_golem_freezer_1", func_shelter_b1_golem_freezer_1_8017D66C);
+void func_shelter_b1_golem_freezer_1_8017D66C(Task* arg0)
+{
+    arg0->field_24 = D_shelter_b1_golem_freezer_1_8017E6A8;
+    Game_SetPtrSlot(arg0, 7);
+    if (Game_Session->field_9 == 0x15) {
+        func_80131E24();
+    }
+    func_shelter_b1_golem_freezer_1_8017D744(0);
+    arg0->state = arg0->state + 1;
+}
 
 INCLUDE_RODATA("rooms/nonmatchings/shelter_b1_golem_freezer_1/shelter_b1_golem_freezer_1", D_shelter_b1_golem_freezer_1_8017D5C0);
 
