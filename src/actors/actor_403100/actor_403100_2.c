a6dfd7a754bad6edb79d8bbae6951e41b09da25c
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-10 16:41:30 +0200

    matched func_actor_403100_8013D0B8 4

--- src/actors/actor_403100/actor_403100_2.c
+++ src/actors/actor_403100/actor_403100_2.c
@@ -286,7 +286,18 @@ void func_actor_403100_8013D06C(void)
     }
 }
 
-INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100_2", func_actor_403100_8013D0B8);
+void func_actor_403100_8013D0B8(s16 arg0, s16 arg1, s16 arg2, s16 arg3)
+{
+    Actor403100MsgPos msg;
+
+    msg.pos.vx = arg0;
+    msg.pos.vy = arg1;
+    msg.pos.vz = arg2;
+    msg.rot.vx = 0;
+    msg.rot.vy = arg3;
+    msg.rot.vz = 0;
+    Gp_DispatchMsg(Game_GetPtrSlot(3), 0x3E9, (s32)&msg, 0);
+}
 INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100_2", func_actor_403100_8013D11C);
 INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100_2", func_actor_403100_8013D1B8);
 INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100_2", func_actor_403100_8013D24C);
