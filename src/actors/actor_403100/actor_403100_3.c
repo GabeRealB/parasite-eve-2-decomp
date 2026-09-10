47d91e083a14c35a3ef5c0c70d5e61343337ba3a
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-10 16:41:31 +0200

    matched func_actor_403100_8013EB68 4

--- src/actors/actor_403100/actor_403100_3.c
+++ src/actors/actor_403100/actor_403100_3.c
@@ -394,7 +394,13 @@ void func_actor_403100_8013EAD4(Task* arg0)
     func_actor_403100_801327CC(arg0);
     func_actor_403100_8013B5E0(arg0, D_actor_403100_80155808->field_61C);
 }
-INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100_3", func_actor_403100_8013EB68);
+void func_actor_403100_8013EB68(Task* arg0)
+{
+    TaskFuncTable3 sp;
+
+    sp = D_actor_403100_80131EE4;
+    sp.funcs[(s16)D_actor_403100_80155808->field_5FA](arg0);
+}
 void func_actor_403100_8013EBC8(Task* arg0)
 {
     TaskFuncTable4 sp;
