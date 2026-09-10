73c4dba655fd6d8aee35f4ba76ae8c0cd3f6e939
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-10 16:41:33 +0200

    matched func_actor_403100_8013F658 3

--- src/actors/actor_403100/actor_403100_3.c
+++ src/actors/actor_403100/actor_403100_3.c
@@ -873,7 +873,13 @@ void func_actor_403100_8013F610(void)
     D_actor_403100_80155808->field_5FA = D_actor_403100_80155808->field_5FA + 1;
 }
 
-INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100_3", func_actor_403100_8013F658);
+void func_actor_403100_8013F658(void)
+{
+    if (Actor403100_TestFlags104()) {
+        D_actor_403100_80155808->field_5F8 = 1;
+        D_actor_403100_80155808->field_5FA = 0;
+    }
+}
 void func_actor_403100_8013F6B0(void)
 {
     D_actor_403100_80155808->field_61C  = 2;
