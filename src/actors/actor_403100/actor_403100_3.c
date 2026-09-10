4fb900e26041d2fa473148770f5c24cc60cbb3aa
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-10 16:41:31 +0200

    matched func_actor_403100_8013DF0C 5

--- src/actors/actor_403100/actor_403100_3.c
+++ src/actors/actor_403100/actor_403100_3.c
@@ -173,7 +173,12 @@ INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100_3", func_actor_403100
 INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100_3", func_actor_403100_8013DD78);
 INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100_3", func_actor_403100_8013DE0C);
 INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100_3", func_actor_403100_8013DEA0);
-INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100_3", func_actor_403100_8013DF0C);
+void func_actor_403100_8013DF0C(void)
+{
+    void (*fns[2])(void) = { func_actor_403100_8013F610, func_actor_403100_8013F658 };
+
+    fns[(s16)D_actor_403100_80155808->field_5FA]();
+}
 void func_actor_403100_8013DF64(void)
 {
     void (*fns[2])(void) = { (void (*)(void))func_actor_403100_8013B128, (void (*)(void))func_actor_403100_8013B3C4 };
