d38a46484f2c1c3b2a75a9aba53ed17cbef810ad
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-10 10:52:44 +0200

    matched func_actor_503500_8013BCB4 1

--- src/actors/actor_503500/actor_503500_6.c
+++ src/actors/actor_503500/actor_503500_6.c
@@ -1184,7 +1184,21 @@ void func_actor_503500_8013BC54(Actor503500* arg0)
     Gp_DestroyEnemy(enemy, (Task*)arg0);
 }
 
-INCLUDE_ASM("actors/nonmatchings/actor_503500/actor_503500_6", func_actor_503500_8013BCB4);
+void func_actor_503500_8013BCB4(Actor503500* arg0)
+{
+    GpEnemy* enemy;
+
+    enemy = arg0->field_20;
+    if (enemy->field_4C & 1) {
+        enemy->field_4C &= ~1;
+    }
+    if (enemy->field_4C & 2) {
+        enemy->field_4C &= ~2;
+    }
+    if (enemy->field_4C & 0xC) {
+        enemy->field_4C &= ~0xC;
+    }
+}
 
 void func_actor_503500_8013BD0C(Actor503500* arg0)
 {
