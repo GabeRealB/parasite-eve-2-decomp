9d4aa7ac7a68f5946943f99aa5207bc2ebbe55dd
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-10 16:41:36 +0200

    matched func_actor_403100_80135F30 10

--- src/actors/actor_403100/actor_403100.c
+++ src/actors/actor_403100/actor_403100.c
@@ -566,7 +566,47 @@ static __inline__ s16 Actor403100_TestFlags12C(void)
 }
 
 INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100", func_actor_403100_80135C00);
-INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100", func_actor_403100_80135F30);
+void func_actor_403100_80135F30(Task* arg0)
+{
+    s32        sound;
+    s32        sound2;
+    s32        pan;
+    s32        pan2;
+    u16        frame;
+    s32        depth;
+    s32        depth2;
+    TmdObject* obj;
+
+    obj                                = arg0->extra;
+    frame                              = D_actor_403100_80155808->field_5EC + 1;
+    D_actor_403100_80155808->field_5EC = frame;
+    if ((s16)frame == 0xA) {
+        func_8018257C();
+        sound = (((u16)((GpEnemy*)arg0->spawnArg2)->field_8 >> 0xC) << 8) | 0x401F0004;
+        pan   = (s8)Gp_GetObjPan((GpObj38*)&((TmdObject*)arg0->extra)->field_8[4]);
+        depth = Gp_GetObjDepth((GpObj38*)&((TmdObject*)arg0->extra)->field_8[4]);
+        SndEvt_EnqueueType6(sound, (s32)pan, (s8)(depth / 2));
+    }
+    if ((s16)D_actor_403100_80155808->field_5EC == 0xE) {
+        SndEvt_EnqueueType7(0x401F0004, 0xA);
+    }
+    if ((s16)D_actor_403100_80155808->field_5EC == 0x26) {
+        sound2 = (((u16)((GpEnemy*)arg0->spawnArg2)->field_8 >> 0xC) << 8) | 0x401F0002;
+        pan2   = (s8)Gp_GetObjPan((GpObj38*)&((TmdObject*)arg0->extra)->field_8[4]);
+        depth2 = Gp_GetObjDepth((GpObj38*)&((TmdObject*)arg0->extra)->field_8[4]);
+        SndEvt_EnqueueType6(sound2, (s32)pan2, (s8)(depth2 / 2));
+    }
+    if (Actor403100_TestFlags12C()) {
+        D_actor_403100_80155808->field_5FC  = 0x18;
+        D_actor_403100_80155808->field_5E2  = 0x18;
+        D_actor_403100_80155808->field_5DE  = 0x16;
+        D_actor_403100_80155808->field_5DA  = 1;
+        D_actor_403100_80155808->field_5EC  = 0;
+        D_actor_403100_80155808->field_600  = 0;
+        D_actor_403100_80155808->field_5FA += 1;
+        obj->field_E                        = 0;
+    }
+}
 INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100", func_actor_403100_80136100);
 INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100", func_actor_403100_8013631C);
 INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100", func_actor_403100_80136610);
