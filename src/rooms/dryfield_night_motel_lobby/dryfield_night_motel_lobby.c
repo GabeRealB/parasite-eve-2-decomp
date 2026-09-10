e0ea75265337968415c64514a1c6a47be921d3cb
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-10 20:37:51 +0200

    dryfield_night_motel_lobby: drop inline addiu hack
    The function used hand-written asm to materialize constant one. Replace it with a plain assignment and an empty register touch that preserves the matched codegen.

--- src/rooms/dryfield_night_motel_lobby/dryfield_night_motel_lobby.c
+++ src/rooms/dryfield_night_motel_lobby/dryfield_night_motel_lobby.c
@@ -146,7 +146,8 @@ void func_dryfield_night_motel_lobby_8017F504(Task* task)
                 }
             }
             cmd = script->field_1;
-            __asm__ volatile("addiu %0, $zero, 1" : "=r"(one));
+            one = 1;
+            SOFT_TOUCH_REG(one);
             if (cmd != one) {
                 goto L_run_cmd;
             }
