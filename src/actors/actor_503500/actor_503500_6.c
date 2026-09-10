b423520e25bb5f9d008cd7d7dc508bbe51254c15
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-10 08:27:40 +0200

    matched func_actor_503500_8013D914 1

--- src/actors/actor_503500/actor_503500_6.c
+++ src/actors/actor_503500/actor_503500_6.c
@@ -863,7 +863,24 @@ void func_actor_503500_8013D85C(Actor503500* arg0)
 }
 INCLUDE_ASM("actors/nonmatchings/actor_503500/actor_503500_6", func_actor_503500_8013D8BC);
 
-INCLUDE_ASM("actors/nonmatchings/actor_503500/actor_503500_6", func_actor_503500_8013D914);
+void func_actor_503500_8013D914(Actor503500* arg0)
+{
+    Actor503500Work* work;
+    s16              timer;
+
+    work = arg0->field_1C;
+    if (work->field_E8 != 0) {
+        timer          = (u16)work->field_E8 - 1;
+        work->field_E8 = timer;
+        if (timer < 0) {
+            work->field_E8 = 0;
+        }
+    }
+    if (func_actor_503500_80136208() == 0) {
+        func_actor_503500_8013CCBC(arg0, work, &work->rec, 8);
+    }
+    Gp_ClearRec18Occupied(&work->rec);
+}
 void func_actor_503500_8013D990(Actor503500* arg0)
 {
     s8 temp_v1;
