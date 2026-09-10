41fc65de1fb8686d09540742c4bddc851e54d4ab
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-10 16:41:30 +0200

    matched func_actor_403100_8013F6B0 6

--- src/actors/actor_403100/actor_403100_3.c
+++ src/actors/actor_403100/actor_403100_3.c
@@ -570,7 +570,16 @@ void func_actor_403100_8013F610(void)
 }
 
 INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100_3", func_actor_403100_8013F658);
-INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100_3", func_actor_403100_8013F6B0);
+void func_actor_403100_8013F6B0(void)
+{
+    D_actor_403100_80155808->field_61C  = 2;
+    D_actor_403100_80155808->field_5F6  = 4;
+    D_actor_403100_80155808->field_5FC  = 0xA;
+    D_actor_403100_80155808->field_5E2  = 8;
+    D_actor_403100_80155808->field_5DE  = 0xA;
+    D_actor_403100_80155808->field_5DA  = 1;
+    D_actor_403100_80155808->field_5FA += 1;
+}
 INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100_3", func_actor_403100_8013F6F4);
 void func_actor_403100_8013F76C(void)
 {
