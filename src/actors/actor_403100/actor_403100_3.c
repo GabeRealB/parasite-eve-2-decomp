7a0f9380cc1c3c576c8c7c9bbb11a55cb73ca21e
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-10 16:41:30 +0200

    matched func_actor_403100_8013E9D8 5

--- src/actors/actor_403100/actor_403100_3.c
+++ src/actors/actor_403100/actor_403100_3.c
@@ -285,7 +285,15 @@ void func_actor_403100_8013E964(void)
 }
 
 INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100_3", func_actor_403100_8013E96C);
-INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100_3", func_actor_403100_8013E9D8);
+void func_actor_403100_8013E9D8(Task* arg0)
+{
+    TaskFuncTable3 sp;
+
+    sp = D_actor_403100_80131EBC;
+    sp.funcs[(s16)D_actor_403100_80155808->field_5FA](arg0);
+    func_actor_403100_801327CC(arg0);
+    func_actor_403100_8013B5E0(arg0, D_actor_403100_80155808->field_61C);
+}
 INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100_3", func_actor_403100_8013EA60);
 INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100_3", func_actor_403100_8013EAD4);
 INCLUDE_ASM("actors/nonmatchings/actor_403100/actor_403100_3", func_actor_403100_8013EB68);
