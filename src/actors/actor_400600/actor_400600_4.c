90738216c5663b66bbfbe153c2c19a728be8345b
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-11 16:20:54 +0200

    matched func_actor_400600_8013B984 1

--- src/actors/actor_400600/actor_400600_4.c
+++ src/actors/actor_400600/actor_400600_4.c
@@ -521,7 +521,24 @@ void func_actor_400600_8013B830(Task* arg0)
 
 INCLUDE_ASM("actors/nonmatchings/actor_400600/actor_400600_4", func_actor_400600_8013B8AC);
 
-INCLUDE_ASM("actors/nonmatchings/actor_400600/actor_400600_4", func_actor_400600_8013B984);
+void func_actor_400600_8013B984(Task* arg0)
+{
+    Actor400600Work* work = (Actor400600Work*)arg0->idMap;
+
+    if (work->field_769 == 0) {
+        work->field_720 = 3;
+        work->field_726 = 0x10;
+        work->field_746 = 0xA;
+        work->field_742 = 1;
+    } else {
+        work->field_720 = 3;
+        work->field_726 = 0x10;
+        work->field_746 = 0xC;
+        work->field_742 = 1;
+    }
+    func_actor_400600_80138B5C(arg0, 0);
+    work->field_71E = work->field_71E + 1;
+}
 
 void func_actor_400600_8013BA00(Task* arg0)
 {
