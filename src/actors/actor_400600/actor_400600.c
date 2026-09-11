35560246c0e4d0e0e710dc84a47c80656f4fe2a7
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-11 12:47:22 +0200

    matched func_actor_400600_80139E68 2

--- src/actors/actor_400600/actor_400600.c
+++ src/actors/actor_400600/actor_400600.c
@@ -399,7 +399,28 @@ void func_actor_400600_80139DB0(Task* arg0, s16 arg1, s16 arg2, s16 arg3)
 
 INCLUDE_ASM("actors/nonmatchings/actor_400600/actor_400600", func_actor_400600_80139DCC);
 
-INCLUDE_ASM("actors/nonmatchings/actor_400600/actor_400600", func_actor_400600_80139E68);
+void func_actor_400600_80139E68(Task* arg0, s16 arg1, Actor400600ViewPos* arg2)
+{
+    MATRIX         root;
+    MATRIX         local;
+    GsCOORDINATE2* coord;
+    GsCOORDINATE2* coords;
+
+    coords            = (GsCOORDINATE2*)((TmdObject*)arg0->extra)->field_8;
+    Gfx_ViewCoord.flg = 0;
+    coord             = &coords[arg1];
+    Gp_UpdateCoord(&Gfx_ViewCoord);
+    coord->flg = 0;
+    Gp_UpdateCoord(coord);
+    Gp_WorldToLocal(&Gfx_ViewCoord.workm, &coords[0].workm, &root);
+    Gp_WorldToLocal(&Gfx_ViewCoord.workm, &coord->workm, &local);
+    coords[0].coord.t[0] = arg2->x - (local.t[0] - root.t[0]);
+    coords[0].coord.t[2] = arg2->z - (local.t[2] - root.t[2]);
+    coords[0].flg        = 0;
+    coord->flg           = 0;
+    Gp_UpdateCoord(coord);
+    Gp_UpdateCoord(coords);
+}
 
 void func_actor_400600_80139F4C(Task* arg0, s16 arg1, Actor400600ViewPos* arg2)
 {
