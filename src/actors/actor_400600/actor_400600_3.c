075a181c822790cf353fb836d015570b71559df6
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-11 19:29:28 +0200

    matched func_actor_400600_80139CAC 2

--- src/actors/actor_400600/actor_400600_3.c
+++ src/actors/actor_400600/actor_400600_3.c
@@ -10,7 +10,37 @@
 
 #include "actors/actor_400600.h"
 
-INCLUDE_ASM("actors/nonmatchings/actor_400600/actor_400600_3", func_actor_400600_80139CAC);
+void func_actor_400600_80139A78(Task* arg0);
+void func_actor_400600_80139AE8(Task* arg0);
+s16  func_actor_400600_80139BA0(Task* arg0, s16 arg1);
+
+void func_actor_400600_80139CAC(Task* arg0)
+{
+    Actor400600Work* work = (Actor400600Work*)arg0->idMap;
+    s32              i;
+
+    if (work->field_742 == 1) {
+        if (work->field_744 != work->field_746) {
+            work->field_748 = 0;
+        } else {
+            work->field_748 = func_actor_400600_80139BA0(arg0, work->field_748);
+        }
+        func_actor_400600_80139AE8(arg0);
+        work->field_742 = 3;
+    } else if (work->field_742 == 2) {
+        func_actor_400600_80139A78(arg0);
+        work->field_742 = 3;
+        work->field_748 = 0;
+    } else if (work->field_742 == 3) {
+        work->field_748++;
+    }
+    i = 1;
+    do {
+        work->slots[i].field_9 = work->field_726;
+        Gp_AnimTickIndex(&work->anim, i);
+        i++;
+    } while (i < 0x12);
+}
 
 void func_actor_400600_80139D98(Task* arg0, s16 arg1, s16 arg2)
 {
