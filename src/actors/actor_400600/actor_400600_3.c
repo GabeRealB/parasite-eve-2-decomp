f73efc0e6a2e2d4e283d86b33272305bf0516627
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-11 13:53:02 +0200

    matched func_actor_400600_8013AE88 1

--- src/actors/actor_400600/actor_400600_3.c
+++ src/actors/actor_400600/actor_400600_3.c
@@ -294,7 +294,21 @@ void func_actor_400600_8013ADA4(Task* arg0)
     }
 }
 
-INCLUDE_ASM("actors/nonmatchings/actor_400600/actor_400600_3", func_actor_400600_8013AE88);
+void func_actor_400600_8013AE88(Task* arg0)
+{
+    Actor400600Work* work;
+    TmdObject*       model;
+
+    work            = (Actor400600Work*)arg0->idMap;
+    model           = (TmdObject*)arg0->extra;
+    work->field_73A = 0;
+    func_actor_400600_80138B5C(arg0, 1);
+    work->obj_4B4.flags &= 0x7FFF;
+    work->obj_594.flags &= 0x7FFF;
+    work->obj_5CC.flags &= 0x7FFF;
+    model->field_C      |= 0x80;
+    work->field_71C      = work->field_71C + 1;
+}
 
 INCLUDE_ASM("actors/nonmatchings/actor_400600/actor_400600_3", func_actor_400600_8013AF04);
 
