6fb0eb120050747cdea45345574f6fbe70e929ce
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-13 01:18:00 +0200

    matched func_actor_405800_80138E20 1

--- src/actors/actor_405800/actor_405800_6.c
+++ src/actors/actor_405800/actor_405800_6.c
@@ -1,6 +1,7 @@
 #include "common.h"
 #include "main/task.h"
 #include "main/tmd.h"
+#include "main/sound.h"
 #include "gameplay/1BC.h"
 #include "gameplay/3CD8.h"
 #include "gameplay/3FB8.h"
@@ -18,6 +19,7 @@ void func_actor_405800_80136E14(Task* arg0);
 void func_actor_405800_8013706C(Task* arg0, s16 arg1);
 void func_actor_405800_80137994(Task* arg0, s32 arg1);
 void func_actor_405800_801379F8(Task* task);
+void func_actor_405800_80139FB0(Task* task, s16 arg1);
 void func_actor_405800_8013A0F4(Task* arg0);
 void func_actor_405800_8013A1E0(Task* task, s16 arg1, s16 arg2);
 void func_actor_405800_8013A1F8(Task* task, s16 arg1, s16 arg2, s16 arg3);
@@ -191,7 +193,28 @@ void func_actor_405800_80138D54(Task* task)
     func_actor_405800_8013A0F4(task);
 }
 
-INCLUDE_ASM("actors/nonmatchings/actor_405800/actor_405800_6", func_actor_405800_80138E20);
+void func_actor_405800_80138E20(Task* task)
+{
+    Actor405800Work* work;
+    u32              sound;
+    s32              pan;
+
+    work = (Actor405800Work*)task->idMap;
+    if ((s16)work->field_842 == 0) {
+        sound   = ((GpEnemy*)task->spawnArg2)->field_8;
+        sound >>= 0xC;
+        sound <<= 8;
+        sound  |= 0x40050006;
+        pan     = Gp_GetObjPan((GpObj38*)((TmdObject*)task->extra)->field_8) << 24;
+        pan   >>= 24;
+        SndEvt_EnqueueType6(sound, pan, (s8)Gp_GetObjDepth((GpObj38*)((TmdObject*)task->extra)->field_8));
+        work->field_842++;
+    }
+    if ((ActorsShared8013a0b0(task) << 0x10) != 0) {
+        func_actor_405800_80139FB0(task, 1);
+    }
+    func_actor_405800_8013A0F4(task);
+}
 
 void func_actor_405800_80138EF0(Task* task)
 {
