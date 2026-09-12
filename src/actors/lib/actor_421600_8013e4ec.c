cb365ecbdd50a257bfc62274846d5530ff1c84b1
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-12 14:56:51 +0200

    matched func_actor_421600_8013E4EC 8

--- src/actors/lib/actor_421600_8013e4ec.c
+++ src/actors/lib/actor_421600_8013e4ec.c
@@ -1,3 +1,19 @@
 #include "common.h"
 
-INCLUDE_ASM("actors/nonmatchings/lib/actor_421600_8013e4ec", func_actor_421600_8013E4EC);
+#include "gameplay/1BC.h"
+#include "main/task.h"
+#include "main/tmd.h"
+
+s32 func_actor_421600_8013E4EC(Task* task)
+{
+    if (((GpEnemy*)task->spawnArg2)->field_40 > 0) {
+        goto return_one;
+    }
+
+    if ((((TmdObject*)task->extra)->field_C & 0x80) != 0) {
+        return 0;
+    }
+
+return_one:
+    return 1;
+}
