4444b08c701c7c81835974929d83b77080a50e7c
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-12 03:35:29 +0200

    matched func_actor_400500_801371A0 1

--- src/actors/actor_400500/actor_400500.c
+++ src/actors/actor_400500/actor_400500.c
@@ -627,7 +627,69 @@ void func_actor_400500_80137034(Task* arg0)
     }
 }
 
-INCLUDE_ASM("actors/nonmatchings/actor_400500/actor_400500", func_actor_400500_801371A0);
+void func_actor_400500_801371A0(Task* arg0)
+{
+    Actor400500Work* work;
+    Actor400500Work* work2;
+    GsCOORDINATE2*   coord;
+    s32              flag;
+    s32              flag2;
+    s32              heading;
+
+    work  = (Actor400500Work*)arg0->idMap;
+    coord = (GsCOORDINATE2*)((TmdObject*)arg0->extra)->field_8;
+    if (work->field_A4A != 0) {
+        work->field_A4A = 0;
+        func_actor_400500_8013DB64(arg0, 5);
+        flag = 1;
+    } else {
+        flag = 0;
+    }
+    if (flag == 0) {
+        work2 = (Actor400500Work*)arg0->idMap;
+        if (work2->field_A49 != 0) {
+            work2->field_A49 = 0;
+            if (work2->field_A1E & 1) {
+                flag2 = 0;
+            } else {
+                func_actor_400500_8013DB64(arg0, 4);
+                flag2 = 1;
+            }
+        } else {
+            flag2 = 0;
+        }
+        if ((flag2 == 0) && ((func_actor_400500_80132D74(arg0) << 0x10) == 0) &&
+            ((func_actor_400500_80133358(arg0) << 0x10) == 0) &&
+            ((func_actor_400500_80133160(arg0) << 0x10) == 0)) {
+            heading = (u16)work->field_94A;
+            if ((heading & 0xFFF) != 0x800) {
+                if (((0x800 - heading) << 0x14) > 0) {
+                    work2            = (Actor400500Work*)arg0->idMap;
+                    work2->field_A38 = 2;
+                } else {
+                    work2            = (Actor400500Work*)arg0->idMap;
+                    work2->field_A38 = 1;
+                }
+                work2->field_A3A = 0;
+            } else {
+                switch (work->field_A1A) {
+                    case 2:
+                        if (coord->coord.t[2] < -0x209E) {
+                            work->field_A08 = 9;
+                        }
+                        break;
+                    case 3:
+                        if ((work->field_A34 == 0) && (work->field_9E4 > 0)) {
+                            work->field_A08 = 0xA;
+                        }
+                        break;
+                }
+                func_actor_400500_801335E8(arg0);
+            }
+            coord->coord.t[0] = 0x4074;
+        }
+    }
+}
 
 void func_actor_400500_80137338(Task* arg0)
 {
