81efe0cd715f02dd01e77d0a8c151cf10df7c372
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-13 10:00:08 +0200

    matched func_actor_405800_80136A1C 3

--- src/actors/actor_405800/actor_405800_2.c
+++ src/actors/actor_405800/actor_405800_2.c
@@ -28,7 +28,6 @@ extern u8 D_actor_405800_8014086C[];
 extern u8 D_actor_405800_80140F10[];
 extern u8 D_actor_405800_80141430[];
 
-s32 func_actor_405800_80136A1C(Task* arg0);
 s32 func_actor_405800_801373E0(Task* arg0);
 
 /* Defined in another unit of this overlay, or still `INCLUDE_ASM`. */
@@ -41,7 +40,84 @@ void func_actor_405800_801392EC(Task* arg0);
 
 INCLUDE_ASM("actors/nonmatchings/actor_405800/actor_405800_2", func_actor_405800_80136388);
 
-INCLUDE_ASM("actors/nonmatchings/actor_405800/actor_405800_2", func_actor_405800_80136A1C);
+s32 func_actor_405800_80136A1C(Task* arg0)
+{
+    Actor405800Work* work;
+    Actor405800Work* work2;
+
+    work = (Actor405800Work*)arg0->idMap;
+    if (work->field_858 == 1) {
+        if (work->field_890 == 0) {
+            switch (work->field_85A) {
+                case 1:
+                    work->field_85A  = 0;
+                    work2            = (Actor405800Work*)arg0->idMap;
+                    work2->field_846 = 3;
+                    work2->field_848 = 0;
+                    return 1;
+                case 2:
+                    work->field_85A  = 0;
+                    work2            = (Actor405800Work*)arg0->idMap;
+                    work2->field_846 = 4;
+                    work2->field_848 = 0;
+                    return 1;
+                case 3:
+                    work->field_85A  = 0;
+                    work2            = (Actor405800Work*)arg0->idMap;
+                    work2->field_846 = 5;
+                    work2->field_848 = 0;
+                    return 1;
+                case 4:
+                    work->field_85A  = 0;
+                    work2            = (Actor405800Work*)arg0->idMap;
+                    work2->field_846 = 4;
+                    work2->field_848 = 0;
+                    return 1;
+                case 5:
+                    work->field_85A  = 0;
+                    work2            = (Actor405800Work*)arg0->idMap;
+                    work2->field_846 = 0xF;
+                    work2->field_848 = 0;
+                    return 1;
+            }
+            return 0;
+        } else {
+            switch (work->field_85A) {
+                case 1:
+                    work2            = (Actor405800Work*)arg0->idMap;
+                    work2->field_846 = 3;
+                    work2->field_848 = 0;
+                    work->field_85A  = 0;
+                    return 1;
+                case 2:
+                    work2            = (Actor405800Work*)arg0->idMap;
+                    work2->field_846 = 4;
+                    work2->field_848 = 0;
+                    work->field_85A  = 0;
+                    return 1;
+                case 3:
+                    work2            = (Actor405800Work*)arg0->idMap;
+                    work2->field_846 = 0xE;
+                    work2->field_848 = 0;
+                    return 1;
+                case 4:
+                    work2            = (Actor405800Work*)arg0->idMap;
+                    work2->field_846 = 4;
+                    work2->field_848 = 0;
+                    work->field_85A  = 0;
+                    return 1;
+                case 5:
+                    work2            = (Actor405800Work*)arg0->idMap;
+                    work2->field_846 = 0xE;
+                    work2->field_848 = 0;
+                    work->field_85A  = 0;
+                    return 1;
+            }
+            return 0;
+        }
+    }
+    return 0;
+}
 
 s32 func_actor_405800_80136B94(Task* arg0)
 {
