2b66fb5ed36f763e4e79fe2a992e188059ce172b
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-13 16:29:34 +0200

    matched Actor00300_Fn0505C 4

--- src/actors/lib/actor_100300_text.c
+++ src/actors/lib/actor_100300_text.c
@@ -2,6 +2,7 @@
 
 #include "actors/actor_100300.h"
 #include "actors/actors_shared_80132074.h"
+#include "actors/actors_shared_80135b58.h"
 
 #include "gameplay/3CD8.h"
 
@@ -231,7 +232,30 @@ void Actor00300_Fn05008(Actor100300* arg0)
     Gp_DrawEffGroundQuad(&vec, 0x300, 0x80);
 }
 
-INCLUDE_ASM("actors/nonmatchings/lib/actor_100300_text", Actor00300_Fn0505C);
+void Actor00300_Fn0505C(Actor100300* arg0, MATRIX* arg1, s16 arg2)
+{
+    GsCOORDINATE2*              coord;
+    MATRIX*                     head;
+    ActorShared80135b58Scratch* scratch;
+
+    head                       = *(MATRIX**)0x1F8003FC;
+    scratch                    = (ActorShared80135b58Scratch*)((u8*)head - 0x30);
+    *(void**)0x1F8003FC        = scratch;
+    coord                      = arg0->field_2C->field_8;
+    scratch->scale.vx          = 0x1000;
+    scratch->scale.vy          = arg2;
+    scratch->scale.vz          = 0x1000;
+    coord->coord               = *arg1;
+    scratch->mat.ident.m00_m01 = 0x1000;
+    scratch->mat.ident.m02_m10 = 0;
+    scratch->mat.ident.m11_m12 = 0x1000;
+    scratch->mat.ident.m20_m21 = 0;
+    scratch->mat.ident.m22     = 0x1000;
+    ScaleMatrix(&scratch->mat.mat, &scratch->scale);
+    MulMatrix(&coord->coord, &scratch->mat.mat);
+    coord->flg         = 0;
+    *(u8**)0x1F8003FC += 0x30;
+}
 
 void Actor00300_Fn05138(Actor100300* arg0)
 {
