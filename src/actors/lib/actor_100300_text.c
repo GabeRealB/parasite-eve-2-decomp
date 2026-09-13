691fbefda2d3b77f489080a9d4e25dd952e5664e
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-13 20:00:37 +0200

    matched Actor00300_Fn02620 9

--- src/actors/lib/actor_100300_text.c
+++ src/actors/lib/actor_100300_text.c
@@ -357,7 +357,79 @@ void Actor00300_Fn01D60(Actor100300* arg0)
 
 INCLUDE_ASM("actors/nonmatchings/lib/actor_100300_text", Actor00300_Fn01F9C);
 
-INCLUDE_ASM("actors/nonmatchings/lib/actor_100300_text", Actor00300_Fn02620);
+void Actor00300_Fn02620(Actor100300* arg0)
+{
+    Actor100300Work* work;
+    GsCOORDINATE2*   coord;
+    VECTOR*          scratchEnd;
+    VECTOR*          vec;
+    s16              delta;
+    s16              angle;
+    s16              timer;
+    s32              magnitude;
+    s32              sound;
+    s32              pan;
+    s32              random;
+
+    scratchEnd            = *(VECTOR**)0x1F8003FC;
+    vec                   = scratchEnd - 1;
+    *(VECTOR**)0x1F8003FC = vec;
+    work                  = arg0->field_1C;
+    coord                 = arg0->field_2C->field_8;
+    switch (work->field_686) {
+        case 0:
+            work->field_67C   = 0x3C;
+            work->field_67A   = 0;
+            work->field_66E   = 3;
+            scratchEnd[-1].vx = Wip_SysConfig.field_4->t[0] - coord->coord.t[0];
+            vec->vy           = 0;
+            vec->vz           = Wip_SysConfig.field_4->t[2] - coord->coord.t[2];
+            angle             = ratan2((s16)scratchEnd[-1].vx, (s16)vec->vz) & 0xFFF;
+            work->field_680   = angle;
+            delta             = (u16)angle - (u16)work->field_67E;
+            magnitude         = abs(delta);
+            angle             = magnitude >= 0x800 ? (delta > 0 ? 0x1000 - delta : delta + 0x1000) : magnitude;
+            if (angle < 0x80) {
+                work->field_686 = 1;
+                work->field_67C = 0;
+                work->field_66E = 7;
+            } else {
+                timer           = (u16)work->field_688 - 1;
+                work->field_688 = timer;
+                if (timer <= 0) {
+                    work->field_684 = 1;
+                    work->field_686 = 0;
+                    random          = Gp_LcgState * 5 + 0x71357911;
+                    Gp_LcgState     = random;
+                    work->field_688 = ((u32)random >> 16) & 15;
+                }
+            }
+            break;
+        case 1:
+            if ((u32)((u16)work->field_672 - 0x10) < 0x14U) {
+                work->field_676 += 0x100;
+            } else if ((s16)work->field_672 >= 0x33) {
+                work->field_676 -= 0x100;
+            }
+            if ((s16)work->field_672 == 0x20) {
+                work->obj5B8.flags |= 0x8000;
+                sound               = (((u16)arg0->field_20->field_8 >> 12) << 8) | 0x4003000A;
+                pan                 = (s8)Gp_GetObjPan((GpObj38*)coord);
+                SndEvt_EnqueueType6(sound, pan, (s8)Gp_GetObjDepth((GpObj38*)coord));
+            }
+            if ((s16)work->field_672 >= 0x37) {
+                work->field_676     = 0;
+                work->field_684     = 1;
+                work->field_686     = 0;
+                random              = Gp_LcgState * 5 + 0x71357911;
+                Gp_LcgState         = random;
+                work->field_688     = ((u32)random >> 16) & 31;
+                work->obj5B8.flags &= 0x7FFF;
+            }
+            break;
+    }
+    *(u32*)0x1F8003FC += 0x10;
+}
 
 void Actor00300_Fn028D0(Actor100300* arg0)
 {
