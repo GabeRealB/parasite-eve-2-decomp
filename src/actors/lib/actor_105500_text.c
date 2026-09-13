c8b585f637900e3cd57b8c36794b7abca6dc0581
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-13 10:50:21 +0200

    matched Actor05500_Fn01B30 16

--- src/actors/lib/actor_105500_text.c
+++ src/actors/lib/actor_105500_text.c
@@ -400,7 +400,144 @@ INCLUDE_ASM("actors/nonmatchings/lib/actor_105500_text", Actor05500_Fn0143C);
 
 #include "actors_shared_fn01a0c.c"
 
-INCLUDE_ASM("actors/nonmatchings/lib/actor_105500_text", Actor05500_Fn01B30);
+void Actor05500_Fn01B30(Actor105500* arg0)
+{
+    Actor105500Obj2C* obj;
+    Actor105500Work*  work;
+    GsCOORDINATE2*    coord;
+    s16(*motion)[2];
+    SVECTOR*        scratchEnd;
+    SVECTOR*        velocity;
+    s32             state;
+    s32             one;
+    s32             pan1;
+    s32             pan2;
+    s32             pan3;
+    s16             timer;
+    Actor105500Ctx* ctx;
+    s32             indexOrSound;
+    u32             randomY;
+    u32             randomZ;
+    u32             randomX;
+    u32             randomDelay;
+    u32             randomRise;
+
+    obj        = arg0->field_2C;
+    work       = arg0->field_1C;
+    ctx        = arg0->field_20;
+    scratchEnd = *(SVECTOR**)PSX_SCRATCH_ADDR(0x3FC);
+    velocity   = (*(SVECTOR**)PSX_SCRATCH_ADDR(0x3FC) = scratchEnd - 1);
+    state      = work->field_39C;
+    coord      = obj->field_8;
+    one        = 1;
+    switch (state) {
+        case 0:
+            work->field_294.flags &= 0x7FFF;
+            work->field_214.flags &= 0xBFFF;
+            obj->field_C           = (u16)obj->field_C | 0x84;
+            ctx->field_14          = one;
+            if (D_8011540E == one) {
+                if (work->field_3C2 == 0) {
+                    work->field_39E = Actor05500_D089B0[work->field_3C4];
+                } else {
+                    work->field_39E = Actor05500_D089E0[work->field_3C4];
+                }
+                work->field_39C = 1;
+            }
+            break;
+        case 1:
+            timer           = (u16)work->field_39E - 1;
+            work->field_39E = timer;
+            if (timer <= 0) {
+                work->field_39E = 0;
+                work->field_39C = 2;
+            }
+            break;
+        case 2:
+            Tmd_AllocBuffers((TmdObject*)obj);
+            obj->field_C = (u16)obj->field_C & 0xFFFB;
+            indexOrSound = 0;
+            if (work->field_3C2 == 0) {
+                work->field_39C = 3;
+                work->field_392 = 4;
+                work->field_398 = 0;
+                work->field_3A6 = 0;
+                work->field_3A2 = ratan2((s32)coord->coord.m[0][2], (s32)coord->coord.m[2][2]) & 0xFFF;
+            } else {
+                work->field_392        = 7;
+                work->field_39A        = state;
+                work->field_39C        = 1;
+                work->field_3A8        = Actor05500_D089E8[work->field_3C4];
+                work->field_294.flags |= 0x8000;
+                work->field_214.flags |= 0x4000;
+                do {
+                    velocity->vx = 0;
+                    velocity->vz = 0;
+                    randomRise   = (Gp_LcgState * 5) + 0x71357911;
+                    Gp_LcgState  = randomRise;
+                    velocity->vy = ((randomRise >> 0x10) & 0x1FF) + 0x2EE;
+                    Gp_SpawnEff(0x6017C, coord, 0, velocity);
+                    indexOrSound++;
+                } while (indexOrSound < 5);
+                indexOrSound = (((u16)((Actor105500Ctx*)arg0->field_20)->field_8 >> 0xC) << 8) | 0x510D0012;
+                pan1         = (s8)Gp_GetObjPan((GpObj38*)coord);
+                SndEvt_EnqueueType6(indexOrSound, (s32)pan1, (s8)Gp_GetObjDepth((GpObj38*)coord));
+            }
+            break;
+        case 3:
+            if ((s16)work->field_396 == 0x1E) {
+                indexOrSound = (((u16)((Actor105500Ctx*)arg0->field_20)->field_8 >> 0xC) << 8) | 0x51090007;
+                pan2         = (s8)Gp_GetObjPan((GpObj38*)coord);
+                SndEvt_EnqueueType6(indexOrSound, (s32)pan2, (s8)Gp_GetObjDepth((GpObj38*)coord));
+            }
+            indexOrSound = 0;
+            if ((s16)work->field_396 == 0x27) {
+                do {
+                    randomX      = (Gp_LcgState * 5) + 0x71357911;
+                    Gp_LcgState  = randomX;
+                    velocity->vx = -((coord->coord.m[0][2] * (s32)(((randomX >> 16) & 0x3F) + 0xAF)) >> 12);
+                    randomY      = (Gp_LcgState * 5) + 0x71357911;
+                    Gp_LcgState  = randomY;
+                    velocity->vy = ((randomY >> 16) & 0x1FF) - 0x6D6;
+                    randomZ      = (Gp_LcgState * 5) + 0x71357911;
+                    Gp_LcgState  = randomZ;
+                    velocity->vz = -((coord->coord.m[2][2] * (s32)(((randomZ >> 16) & 0x3F) + 0xAF)) >> 12);
+                    Gp_SpawnEff(0x60051, coord, 0, velocity);
+                    indexOrSound++;
+                } while (indexOrSound < 3);
+                indexOrSound = 0;
+            }
+            motion = Actor05500_D08A38;
+            do {
+                indexOrSound++;
+                if ((s16)work->field_396 <= ((*motion)[0] + Actor05500_D08A20)) {
+                    coord->coord.t[0] += ((*motion)[1] * rsin(work->field_3A2)) >> 12;
+                    coord->coord.t[2] += ((*motion)[1] * rcos(work->field_3A2)) >> 12;
+                    break;
+                }
+                motion++;
+            } while (indexOrSound < 9);
+            if ((s16)work->field_396 == 0x28) {
+                indexOrSound = (((u16)((Actor105500Ctx*)arg0->field_20)->field_8 >> 0xC) << 8) | 0x401A0002;
+                pan3         = (s8)Gp_GetObjPan((GpObj38*)coord);
+                SndEvt_EnqueueType6(indexOrSound, (s32)pan3, (s8)Gp_GetObjDepth((GpObj38*)coord));
+                work->field_3A8        = 0x80;
+                work->field_294.flags |= 0x8000;
+                work->field_214.flags |= 0x4000;
+            }
+            if ((s16)work->field_396 >= (Actor05500_D08A20 + 0x46)) {
+                work->field_39A = 3;
+                work->field_39C = 0;
+                work->field_392 = 1;
+                randomDelay     = (Gp_LcgState * 5) + 0x71357911;
+                work->field_39E = Actor05500_D08980[((Actor105500Ctx*)arg0->field_20)->field_3C->field_F] + ((randomDelay >> 0x10) & 0xF);
+                Gp_LcgState     = randomDelay;
+                Gp_ArmStateF0(1);
+            }
+            break;
+    }
+    *(SVECTOR**)PSX_SCRATCH_ADDR(0x3FC) = *(SVECTOR**)PSX_SCRATCH_ADDR(0x3FC) + 1;
+}
 
 #define ActorsSharedFn020d4 Actor05500_Fn020D4
 #include "actors_shared_fn020d4.c"
