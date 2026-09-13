545190cbef43093111ce6e24a68ebcd017cbb4d7
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-13 19:07:49 +0200

    matched func_actor_136100_80134964 2
    Ports the m2c baseline into the host TU using Actor136100Work field access
    through the task's idMap slot, replacing the INCLUDE_ASM stub.

--- src/actors/actor_136100/actor_136100_3.c
+++ src/actors/actor_136100/actor_136100_3.c
@@ -1,8 +1,13 @@
 #include "common.h"
+
+#include "actors/actor_136100.h"
+#include "gameplay/3CD8.h"
+#include "gameplay/3FB8.h"
 #include "main/task.h"
 
 extern s16      D_actor_136100_8013F17C;
 extern TaskDesc ActorsShared80134898Desc;
+extern Task*    D_actor_136100_8014078C;
 
 void func_actor_136100_801348C8(void)
 {
@@ -17,7 +22,16 @@ void func_actor_136100_801348F8(void)
 
 INCLUDE_ASM("actors/nonmatchings/actor_136100/actor_136100_3", func_actor_136100_80134924);
 
-INCLUDE_ASM("actors/nonmatchings/actor_136100/actor_136100_3", func_actor_136100_80134964);
+void func_actor_136100_80134964(void)
+{
+    Actor136100Work* work = (Actor136100Work*)D_actor_136100_8014078C->idMap;
+
+    if (work->field_4EC != 0) {
+        Gp_SpawnWeaponEff();
+        work->field_4EC = 0;
+        Gp_MsgPlayerWeapon(0);
+    }
+}
 
 INCLUDE_ASM("actors/nonmatchings/actor_136100/actor_136100_3", func_actor_136100_801349B4);
 
