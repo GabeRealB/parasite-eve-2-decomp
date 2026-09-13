3df01ae12897fd3950c6a35913e4da9bbe468acf
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-13 04:19:57 +0200

    matched func_actor_405800_80139188 2

--- src/actors/actor_405800/actor_405800_6.c
+++ src/actors/actor_405800/actor_405800_6.c
@@ -15,6 +15,7 @@ extern u8  D_actor_405800_801514D8[];
 extern s32 Gp_LcgState;
 
 void func_actor_405800_80135A3C(Task* arg0, s16 arg1);
+s32  func_actor_405800_80136A1C(Task* arg0);
 s32  func_actor_405800_80136B94(Task* arg0);
 void func_actor_405800_80136E14(Task* arg0);
 void func_actor_405800_8013706C(Task* arg0, s16 arg1);
@@ -289,7 +290,37 @@ INCLUDE_ASM("actors/nonmatchings/actor_405800/actor_405800_6", func_actor_405800
 
 INCLUDE_ASM("actors/nonmatchings/actor_405800/actor_405800_6", func_actor_405800_801390FC);
 
-INCLUDE_ASM("actors/nonmatchings/actor_405800/actor_405800_6", func_actor_405800_80139188);
+void func_actor_405800_80139188(Task* arg0)
+{
+    Actor405800Work* work;
+    Actor405800Work* work2;
+    Actor405800Work* work3;
+
+    work = (Actor405800Work*)arg0->idMap;
+    if (work->field_858 != 0 && work->field_85A == 1) {
+        if (work->field_891 == 0) {
+            work->field_850 = 0x20;
+            work->field_872 = 9;
+            work->field_86E = 2;
+        } else {
+            work->field_850 = 0x20;
+            work->field_872 = 0xB;
+            work->field_86E = 2;
+        }
+        return;
+    }
+    if ((func_actor_405800_80136A1C(arg0) << 0x10) == 0 && (ActorsShared8013a0b0(arg0) << 0x10) != 0) {
+        if (work->field_891 == 0) {
+            work2            = (Actor405800Work*)arg0->idMap;
+            work2->field_846 = 2;
+            work2->field_848 = 0;
+        } else {
+            work3            = (Actor405800Work*)arg0->idMap;
+            work3->field_846 = 0xA;
+            work3->field_848 = 0;
+        }
+    }
+}
 
 INCLUDE_ASM("actors/nonmatchings/actor_405800/actor_405800_6", func_actor_405800_80139260);
 
