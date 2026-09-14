5da2294643fe08442aad546b3895c7774185b673
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-14 17:44:08 +0200

    matched Actor00400_Fn09B44 2

--- src/actors/lib/actor_100400_text.c
+++ src/actors/lib/actor_100400_text.c
@@ -602,7 +602,15 @@ INCLUDE_ASM("actors/nonmatchings/lib/actor_100400_text", Actor00400_Fn09A8C);
 
 INCLUDE_ASM("actors/nonmatchings/lib/actor_100400_text", Actor00400_Fn09AE0);
 
-INCLUDE_ASM("actors/nonmatchings/lib/actor_100400_text", Actor00400_Fn09B44);
+void Actor00400_Fn09B44(Actor100400* arg0)
+{
+    Actor100400Work* work;
+
+    work = arg0->field_1C;
+    if (work->field_65E == 1) {
+        work->field_63A = work->field_63A + 1;
+    }
+}
 
 INCLUDE_ASM("actors/nonmatchings/lib/actor_100400_text", Actor00400_Fn09B74);
 
