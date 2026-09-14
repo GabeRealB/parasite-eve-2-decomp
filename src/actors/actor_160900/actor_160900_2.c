edcd33988f7d7567a2b0f3076c9534638052be69
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-14 11:28:17 +0200

    matched func_actor_160900_80134790 2

--- src/actors/actor_160900/actor_160900_2.c
+++ src/actors/actor_160900/actor_160900_2.c
@@ -8,7 +8,14 @@
 #include "actors/actor_160900.h"
 
 INCLUDE_ASM("actors/nonmatchings/actor_160900/actor_160900_2", func_actor_160900_80134710);
-INCLUDE_ASM("actors/nonmatchings/actor_160900/actor_160900_2", func_actor_160900_80134790);
+void func_actor_160900_80134790(s16 arg0)
+{
+    Actor160900Work* work;
+
+    work           = (Actor160900Work*)D_actor_160900_8013FBB4->idMap;
+    work->field_4C = arg0;
+    work->field_4E = 0;
+}
 void func_actor_160900_801347B0(s16 arg0)
 {
     Actor160900Work* work;
