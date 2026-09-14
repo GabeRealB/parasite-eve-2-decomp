ce61fd46f1dd0f961eef535764fb4e871ccb4df1
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-15 00:56:28 +0200

    matched func_actor_535700_80131F2C 1

--- src/actors/actor_535700/actor_535700.c
+++ src/actors/actor_535700/actor_535700.c
@@ -1,10 +1,27 @@
 #include "common.h"
 
+#include "gameplay/1BC.h"
+#include "main/mc.h"
+#include "main/task.h"
+
+extern s16 D_80071076;
+
 INCLUDE_ASM("actors/nonmatchings/actor_535700/actor_535700", func_actor_535700_80131E2C);
 
 INCLUDE_ASM("actors/nonmatchings/actor_535700/actor_535700", func_actor_535700_80131EF0);
 
-INCLUDE_ASM("actors/nonmatchings/actor_535700/actor_535700", func_actor_535700_80131F2C);
+void func_actor_535700_80131F2C(void)
+{
+    if (Mc_SaveData.field_23 != 9) {
+        Mc_SaveData.field_6 = 0x1D;
+        Mc_SaveData.field_8 = 5;
+        Mc_SaveData.field_5 = 2;
+        D_80071076          = 1;
+        Task_Spawn(0, 0x11, 0, 0);
+        Mc_SaveData.field_5C5 = 6;
+        Gp_RestoreStreamRng();
+    }
+}
 
 INCLUDE_ASM("actors/nonmatchings/actor_535700/actor_535700", ActorsShared80131f9cSub0);
 
