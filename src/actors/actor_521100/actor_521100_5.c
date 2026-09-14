5b7de8aae2d919761b35b0c989b22f1ad8eaf64a
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-15 01:47:58 +0200

    matched func_actor_521100_80136680 2

--- src/actors/actor_521100/actor_521100_5.c
+++ src/actors/actor_521100/actor_521100_5.c
@@ -1,8 +1,13 @@
 #include "common.h"
 
 #include "actors/actor_521100.h"
+#include "gameplay/gameplay.h"
+#include "main/task.h"
+#include "main/tmd.h"
 
 void Gp_IncStateF0Ref(s32 arg0);
+void func_800D7A9C(TmdObject* arg0, VECTOR* arg1, s32 arg2, s32 arg3);
+void func_actor_521100_80135F2C(Task* task);
 
 INCLUDE_ASM("actors/nonmatchings/actor_521100/actor_521100_5", func_actor_521100_80135D10);
 
@@ -32,4 +37,18 @@ INCLUDE_ASM("actors/nonmatchings/actor_521100/actor_521100_5", func_actor_521100
 
 INCLUDE_ASM("actors/nonmatchings/actor_521100/actor_521100_5", func_actor_521100_80136604);
 
-INCLUDE_ASM("actors/nonmatchings/actor_521100/actor_521100_5", func_actor_521100_80136680);
+void func_actor_521100_80136680(void* arg0, Task* task)
+{
+    TmdObject*     obj;
+    GsCOORDINATE2* coord;
+    VECTOR         vec;
+
+    obj   = task->extra;
+    coord = obj->field_8;
+    Gp_UpdateCoord(coord);
+    vec.vx = coord->workm.t[0];
+    vec.vy = coord->workm.t[1] - 0x320;
+    vec.vz = coord->workm.t[2];
+    func_800D7A9C(obj, &vec, 0, 3);
+    func_actor_521100_80135F2C(task);
+}
