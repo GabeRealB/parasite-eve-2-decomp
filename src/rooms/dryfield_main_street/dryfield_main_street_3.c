e6ec04957a6824c56b7e21644f2e8370549a88a2
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-16 00:15:13 +0200

    matched func_dryfield_main_street_8017E0D8 1

--- src/rooms/dryfield_main_street/dryfield_main_street_3.c
+++ src/rooms/dryfield_main_street/dryfield_main_street_3.c
@@ -2,13 +2,17 @@
 
 #include "gameplay/1A8.h"
 #include "gameplay/3CD8.h"
+#include "gameplay/D4.h"
 
 #include "main/gameflag.h"
 #include "main/task.h"
 
-extern s32 D_dryfield_main_street_80181624;
-extern s32 D_dryfield_main_street_80181A14;
-void       func_dryfield_main_street_8017E4A4(s32 arg0);
+extern u8         D_80115598;
+extern GpMsgEntry D_dryfield_main_street_80180EA0[];
+extern s32        D_dryfield_main_street_80180ED0;
+extern s32        D_dryfield_main_street_80181624;
+extern s32        D_dryfield_main_street_80181A14;
+void              func_dryfield_main_street_8017E4A4(s32 arg0);
 
 s32 func_dryfield_main_street_8017E05C(Task* task, s32 msgId, GpMsg13EF* msg, s32 arg3)
 {
@@ -22,4 +26,13 @@ s32 func_dryfield_main_street_8017E05C(Task* task, s32 msgId, GpMsg13EF* msg, s3
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_main_street/dryfield_main_street_3", func_dryfield_main_street_8017E0D8);
+void func_dryfield_main_street_8017E0D8(Task* task)
+{
+    task->field_24 = D_dryfield_main_street_80180EA0;
+    Game_SetPtrSlot(task, 7);
+    D_80115598 = 1;
+    if (GameFlag_GetNibble(0x5F) == 0) {
+        Gp_DispatchMsg(Game_GetPtrSlot(4), 0x7DA, (s32)&D_dryfield_main_street_80180ED0, 0x7DB);
+    }
+    task->state++;
+}
