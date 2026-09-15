bd8bd5cde6e5403683f92c4235d3b61a38206c1b
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-15 18:03:52 +0200

    matched func_mine_cavern_8017E0B4 1

--- src/rooms/mine_cavern/mine_cavern_3.c
+++ src/rooms/mine_cavern/mine_cavern_3.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "gameplay/3A34.h"
 #include "gameplay/3CD8.h"
 #include "main/gameflag.h"
 #include "main/task.h"
@@ -10,7 +11,14 @@ void func_mine_cavern_8017E088(s16 arg0)
     Gp_StartCapSlot(arg0, 1, 1);
 }
 
-INCLUDE_ASM("rooms/nonmatchings/mine_cavern/mine_cavern_3", func_mine_cavern_8017E0B4);
+void func_mine_cavern_8017E0B4(void)
+{
+    Gp_StateF0.field_0 = 0;
+    if (Gp_StateF0.field_6 == 0) {
+        ((void (*)(s32))Gp_IncStateF0Ref)(0);
+    }
+    Gp_ArmStateF0(1);
+}
 
 void func_mine_cavern_8017E0F4(s32 arg0)
 {
