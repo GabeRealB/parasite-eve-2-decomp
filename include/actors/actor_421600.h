8d7cc5b5b68f83c1cecfe9e275c60f3f461fc5ed
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-15 20:22:17 +0200

    Reformat actor source

--- include/actors/actor_421600.h
+++ include/actors/actor_421600.h
@@ -30,31 +30,31 @@
 /// `GpRec18` table `func_actor_421600_8013285C` walks after the 0x20-byte
 /// `field_B6C` node, matching `Actor00100Obj.field_20` after `objs[2]`.
 typedef struct Actor421600Work {
-    /* 0x000 */ s16   field_0;
-    /* 0x002 */ byte  pad_2[2];
-    /* 0x004 */ s16   field_4;
-    /* 0x006 */ byte  pad_6[0x62];
-    /* 0x068 */ u16   field_68;
-    /* 0x06A */ byte  pad_6A[0x7BE];
-    /* 0x828 */ u16   field_828;
-    /* 0x82A */ u16   field_82A;
-    /* 0x82C */ byte  pad_82C[2];
-    /* 0x82E */ u16   field_82E;
-    /* 0x830 */ byte  pad_830[2];
-    /* 0x832 */ u16   field_832;
-    /* 0x834 */ byte  pad_834[0xB8];
-    /* 0x8EC */ GpObj field_8EC;
-    /* 0x90C */ byte  pad_90C[0x120];
-    /* 0xA2C */ GpObj field_A2C;
-    /* 0xA4C */ byte  pad_A4C[0x120];
+    /* 0x000 */ s16     field_0;
+    /* 0x002 */ byte    pad_2[2];
+    /* 0x004 */ s16     field_4;
+    /* 0x006 */ byte    pad_6[0x62];
+    /* 0x068 */ u16     field_68;
+    /* 0x06A */ byte    pad_6A[0x7BE];
+    /* 0x828 */ u16     field_828;
+    /* 0x82A */ u16     field_82A;
+    /* 0x82C */ byte    pad_82C[2];
+    /* 0x82E */ u16     field_82E;
+    /* 0x830 */ byte    pad_830[2];
+    /* 0x832 */ u16     field_832;
+    /* 0x834 */ byte    pad_834[0xB8];
+    /* 0x8EC */ GpObj   field_8EC;
+    /* 0x90C */ byte    pad_90C[0x120];
+    /* 0xA2C */ GpObj   field_A2C;
+    /* 0xA4C */ byte    pad_A4C[0x120];
     /* 0xB6C */ GpObj   field_B6C;
     /* 0xB8C */ GpRec18 field_B8C;
     /* 0xBA4 */ byte    pad_BA4[0x2F0];
-    /* 0xE94 */ Task* field_E94;
-    /* 0xE98 */ Task* field_E98;
-    /* 0xE9C */ byte  pad_E9C[0x10];
-    /* 0xEAC */ s16   field_EAC;
-    /* 0xEAE */ byte  pad_EAE[2];
+    /* 0xE94 */ Task*   field_E94;
+    /* 0xE98 */ Task*   field_E98;
+    /* 0xE9C */ byte    pad_E9C[0x10];
+    /* 0xEAC */ s16     field_EAC;
+    /* 0xEAE */ byte    pad_EAE[2];
 } Actor421600Work;
 STATIC_ASSERT_SIZEOF(Actor421600Work, 0xEB0);
 
