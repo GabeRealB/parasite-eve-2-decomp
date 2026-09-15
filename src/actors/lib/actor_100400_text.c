930970d87f6e156bd36a2c6fd6a854bc44f769ac
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-15 16:27:56 +0200

    matched Actor00400_Fn09418 1

--- src/actors/lib/actor_100400_text.c
+++ src/actors/lib/actor_100400_text.c
@@ -32,6 +32,8 @@ s32        Gp_GetObjDepth(GsCOORDINATE2* arg0);
 s32        SndEvt_EnqueueType6(s32 arg0, s32 arg1, s32 arg2);
 void       Actor00400_Fn060CC(Actor100400* arg0);
 void       Actor00400_Fn09714(Actor100400* arg0);
+void       Actor00400_Fn0A940(Actor100400* arg0);
+void       Actor00400_Fn04A1C(Actor100400* arg0);
 void       Actor00400_Fn0A9F4(Actor100400* arg0);
 void       Actor00400_Fn0AA40(Actor100400* arg0);
 void       Actor00400_Fn0A3D4(Actor100400* arg0);
@@ -618,7 +620,16 @@ void Actor00400_Fn093BC(void)
 
 INCLUDE_ASM("actors/nonmatchings/lib/actor_100400_text", Actor00400_Fn093C4);
 
-INCLUDE_ASM("actors/nonmatchings/lib/actor_100400_text", Actor00400_Fn09418);
+void Actor00400_Fn09418(Actor100400* arg0)
+{
+    Actor100400Work* work                       = arg0->field_1C;
+    void             (*states[2])(Actor100400*) = {
+        Actor00400_Fn0A940,
+        Actor00400_Fn04A1C,
+    };
+
+    states[(s16)work->field_63A](arg0);
+}
 
 void Actor00400_Fn0946C(Actor100400* arg0)
 {
