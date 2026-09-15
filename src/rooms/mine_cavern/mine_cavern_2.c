a17952e1e07456470362242f7c54b9e3dd3b1a75
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-15 18:53:10 +0200

    matched func_mine_cavern_8017DEE4 1

--- src/rooms/mine_cavern/mine_cavern_2.c
+++ src/rooms/mine_cavern/mine_cavern_2.c
@@ -5,6 +5,13 @@
 #include "main/sound.h"
 #include "main/task.h"
 
+extern s32 D_mine_cavern_80188214;
+extern s32 D_mine_cavern_801887B4;
+extern s32 D_mine_cavern_8018EB50;
+
+/// Cutscene / "among us" mode flag in the main executable.
+extern s8 D_80114C12;
+
 s32 func_mine_cavern_8017DC58(Task* task, s32 msgId, GpMsg13EF* arg2)
 {
     if ((arg2->field_2 == 6) && (GameFlag_GetNibble(0xC4) == 1)) {
@@ -41,4 +48,13 @@ void func_mine_cavern_8017DD6C(Task* task)
 
 INCLUDE_ASM("rooms/nonmatchings/mine_cavern/mine_cavern_2", func_mine_cavern_8017DDFC);
 
-INCLUDE_ASM("rooms/nonmatchings/mine_cavern/mine_cavern_2", func_mine_cavern_8017DEE4);
+void func_mine_cavern_8017DEE4(void)
+{
+    s32 flag;
+
+    flag = GameFlag_GetNibble(0xE6);
+    if ((flag == 1) && (D_mine_cavern_8018EB50 == flag) && (D_80114C12 != D_mine_cavern_8018EB50)) {
+        func_800E8634((s32)&D_mine_cavern_80188214, 0, (s32)&D_mine_cavern_801887B4);
+        D_mine_cavern_8018EB50 = 2;
+    }
+}
