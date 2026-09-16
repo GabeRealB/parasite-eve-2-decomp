f1afb038d962f80d08f39124ef76c14c65a357a8
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-16 02:37:23 +0200

    matched func_dryfield_water_hole_8017D73C 1

--- src/rooms/dryfield_water_hole/dryfield_water_hole.c
+++ src/rooms/dryfield_water_hole/dryfield_water_hole.c
@@ -1,8 +1,21 @@
 #include "common.h"
 
+#include "gameplay/3CD8.h"
+
+#include "main/gameflag.h"
+#include "main/sound.h"
+
 INCLUDE_ASM("rooms/nonmatchings/dryfield_water_hole/dryfield_water_hole", func_dryfield_water_hole_8017D5F0);
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_water_hole/dryfield_water_hole", func_dryfield_water_hole_8017D73C);
+s32 func_dryfield_water_hole_8017D73C(s32 arg0, s32 arg1, s32 arg2)
+{
+    if (arg2 == 2) {
+        Gp_RunCapCmd1(2);
+        GameFlag_SetNibble(0x1BD, 2);
+        SndEvt_EnqueueType6(0x52200004, 0, 0);
+    }
+    return 0;
+}
 
 INCLUDE_RODATA("rooms/nonmatchings/dryfield_water_hole/dryfield_water_hole", D_dryfield_water_hole_8017D5C0);
 
