be8ad3540bc59468b40705aa9e0ae265e7298869
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-16 02:59:28 +0200

    matched func_neo_ark_north_promenade_8017D5D8 1

--- src/rooms/neo_ark_north_promenade/neo_ark_north_promenade.c
+++ src/rooms/neo_ark_north_promenade/neo_ark_north_promenade.c
@@ -1,8 +1,29 @@
 #include "common.h"
 
+#include "gameplay/3CD8.h"
+#include "main/gameflag.h"
 #include "main/task.h"
+#include "rooms/room_common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/neo_ark_north_promenade/neo_ark_north_promenade", func_neo_ark_north_promenade_8017D5D8);
+extern void func_80179B14(RoomEventMsg* in, RoomEventMsg* out);
+
+s32 func_neo_ark_north_promenade_8017D5D8(s32 arg0, s32 arg1, RoomEventMsg* in, RoomEventMsg* out)
+{
+    *out = *in;
+    func_80179B14(in, out);
+    if (in->msgId != 0xB) {
+        return 1;
+    }
+    if (GameFlag_GetNibble(0xF6) != 0) {
+        return 1;
+    }
+    if (in->field_5 != 0) {
+        return 0;
+    }
+    Gp_SetNibbleIf(in->field_6, 2);
+    Gp_RunCapCmd1(1);
+    return 0;
+}
 
 s32 func_neo_ark_north_promenade_8017D66C(void)
 {
