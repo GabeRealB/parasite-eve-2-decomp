68b0e210270fc302ab6ca3a7ccc4f36e2f834938
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-16 13:01:42 +0200

    matched func_actor_401300_8013CBAC 2

--- src/actors/actor_401300/actor_401300.c
+++ src/actors/actor_401300/actor_401300.c
@@ -2070,7 +2070,97 @@ void func_actor_401300_8013B6E8(Actor401300* arg0)
 
 INCLUDE_ASM("actors/nonmatchings/actor_401300/actor_401300", func_actor_401300_8013BB30);
 
-INCLUDE_ASM("actors/nonmatchings/actor_401300/actor_401300", func_actor_401300_8013CBAC);
+void func_actor_401300_8013CBAC(Actor401300* arg0)
+{
+    Actor401300Work*       work;
+    TmdObject*             obj;
+    GsCOORDINATE2*         coord;
+    GsCOORDINATE2*         facing;
+    GsCOORDINATE2*         root;
+    GsCOORDINATE2*         root2;
+    WipSysConfig*          config;
+    Actor401300AimScratch* head;
+    Actor401300AimScratch* aim;
+    s16                    yaw;
+    s32                    angle;
+
+    work = arg0->field_1C;
+    if (work->field_4 != 0) {
+        obj                          = arg0->field_2C;
+        arg0->field_20->node.field_4 = 0;
+        obj->field_C                 = 0;
+        Tmd_AllocBuffers(obj);
+        work->field_970.field_1C = 0x280;
+        work->field_89C          = 1;
+        work->field_8A6          = 0x24;
+        work->field_8A2          = 2;
+        work->field_89E          = 0;
+        work->field_D1C          = 0;
+        work->field_BF0.flags   &= 0x7FFF;
+        work->field_AB0.flags   |= 0x4000;
+        Gp_ArmStateF0(1);
+        work->field_6 = 0;
+        work->field_8 = 0;
+    }
+    work->field_6++;
+    head                                     = *(Actor401300AimScratch**)G_SCRATCH_HEAD;
+    *(Actor401300AimScratch**)G_SCRATCH_HEAD = head - 1;
+    aim                                      = head - 1;
+    if (func_actor_401300_80132C78(arg0->field_2C->field_8, work->field_AD0, 0xC, 0x57) == 0) {
+        func_actor_401300_80132910(arg0, (GpRec18*)work->field_990, 0xC);
+    }
+    config                       = &Wip_SysConfig;
+    root                         = arg0->field_2C->field_8;
+    head[-1].delta.vx            = config->field_4->t[0] - root->coord.t[0];
+    aim->delta.vy                = config->field_4->t[1] - root->coord.t[1];
+    aim->delta.vz                = config->field_4->t[2] - root->coord.t[2];
+    arg0->field_2C->field_8->flg = 0;
+    func_actor_401300_80133A3C(arg0);
+    aim->pad_8        = ratan2(-((TmdObject*)((Task*)Game_GetPtrSlot(3))->extra)->field_8->coord.m[2][0],
+                               ((TmdObject*)((Task*)Game_GetPtrSlot(3))->extra)->field_8->coord.m[2][2]);
+    root2             = arg0->field_2C->field_8;
+    head[-1].delta.vx = config->field_4->t[0] - root2->coord.t[0];
+    aim->delta.vy     = config->field_4->t[1] - root2->coord.t[1];
+    aim->delta.vz     = config->field_4->t[2] - root2->coord.t[2];
+    yaw               = ratan2(head[-1].delta.vx, aim->delta.vz) + 0x800;
+    aim->pad_A        = yaw;
+    aim->pad_A        = Actor401300_NormalizeYaw(yaw);
+    coord             = arg0->field_2C->field_8;
+    angle             = ratan2(aim->delta.vx, aim->delta.vz);
+    aim->angle        = Actor401300_NormalizeYaw(angle - ratan2(-coord->coord.m[2][0], coord->coord.m[2][2]));
+    work->field_8B2   = aim->angle;
+    if (aim->angle < 0x200) {
+        if (!Actor401300_OutOfRange(&aim->delta, 0x44C)) {
+            work->field_0 = 0xB;
+        }
+    }
+    if (aim->angle > 0x20) {
+        aim->angle = 0x20;
+    }
+    if (aim->angle < -0x20) {
+        aim->angle = -0x20;
+    }
+    facing      = arg0->field_2C->field_8;
+    aim->angle += ratan2(-facing->coord.m[2][0], facing->coord.m[2][2]);
+    Gfx_RotMatrixY(&arg0->field_2C->field_8->coord, aim->angle, 1);
+    Actor401300_RescaleYaw(arg0->field_2C->field_8, 0x1964);
+    arg0->field_2C->field_8->flg = 0;
+    if (work->field_8A2 == 2) {
+        if (work->field_89E == 0) {
+            if ((s16)func_actor_401300_8013267C(arg0->field_2C->field_8, 0x15E, 0x16) != 0) {
+                Actor401300_MoveForward(arg0->field_2C->field_8, 0x16);
+            }
+        } else {
+            if ((s16)func_actor_401300_8013267C(arg0->field_2C->field_8, 0x15E, 5) != 0) {
+                Actor401300_MoveForward(arg0->field_2C->field_8, 5);
+            }
+        }
+    } else if (work->field_6C & 0x100) {
+        work->field_8A2 = 2;
+        work->field_89C = 1;
+    }
+    *(Actor401300AimScratch**)G_SCRATCH_HEAD += 1;
+}
 
 void func_actor_401300_8013D2AC(Actor401300* arg0)
 {
