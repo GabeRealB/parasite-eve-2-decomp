d8acc9bca08b75f703eba2ca4d99565b70267ae3
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-16 02:08:06 +0200

    matched func_dryfield_night_parking_lot_8017DB34 1

--- src/rooms/dryfield_night_parking_lot/dryfield_night_parking_lot_2.c
+++ src/rooms/dryfield_night_parking_lot/dryfield_night_parking_lot_2.c
@@ -1,3 +1,19 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_night_parking_lot/dryfield_night_parking_lot_2", func_dryfield_night_parking_lot_8017DB34);
+#include "gameplay/1A8.h"
+#include "gameplay/3CD8.h"
+#include "main/gameflag.h"
+#include "main/session.h"
+#include "main/task.h"
+
+extern s32 D_dryfield_night_parking_lot_8017ECB4;
+
+s32 func_dryfield_night_parking_lot_8017DB34(Task* task, s32 msgId, GpMsg13EF* arg2)
+{
+    if ((arg2->field_2 == 1) && (Game_Session->field_9 == 3) && (GameFlag_GetNibble(0x79) == 0)) {
+        GameFlag_SetNibble(0x79, 1);
+        Gp_MsgPlayerWeapon(0);
+        func_800E8614((s32)&D_dryfield_night_parking_lot_8017ECB4, 1);
+    }
+    return 0;
+}
