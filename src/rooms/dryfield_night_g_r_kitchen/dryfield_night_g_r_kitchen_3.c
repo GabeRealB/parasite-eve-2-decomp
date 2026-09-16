fd5dd2d22fc3504db4dd6d7212a8c732e521ba3e
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-16 02:55:38 +0200

    matched func_dryfield_night_g_r_kitchen_8017E1E4 1

--- src/rooms/dryfield_night_g_r_kitchen/dryfield_night_g_r_kitchen_3.c
+++ src/rooms/dryfield_night_g_r_kitchen/dryfield_night_g_r_kitchen_3.c
@@ -1,3 +1,23 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_night_g_r_kitchen/dryfield_night_g_r_kitchen_3", func_dryfield_night_g_r_kitchen_8017E1E4);
+#include "main/session.h"
+#include "rooms/room_common.h"
+
+extern SVECTOR D_dryfield_night_g_r_kitchen_8017E27C[];
+extern SVECTOR D_dryfield_night_g_r_kitchen_8017E29C[];
+
+/// Picks the pair of wedges `Room_Draw08` sweeps from the current view index
+/// (`Game_Session->field_4`, 2 or 3); any other view draws nothing.
+void func_dryfield_night_g_r_kitchen_8017E1E4(void)
+{
+    u8 view;
+
+    view = Game_Session->field_4;
+    if (view == 2) {
+        Room_Draw08(&D_dryfield_night_g_r_kitchen_8017E27C[0], 0x100);
+        Room_Draw08(&D_dryfield_night_g_r_kitchen_8017E27C[2], 0x100);
+    } else if (view == 3) {
+        Room_Draw08(&D_dryfield_night_g_r_kitchen_8017E29C[0], 0x100);
+        Room_Draw08(&D_dryfield_night_g_r_kitchen_8017E29C[2], 0x100);
+    }
+}
