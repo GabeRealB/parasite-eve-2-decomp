1282a2c257313a1b9c71d76f417f05d1553a8021
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-16 15:52:18 +0200

    matched Actor00400_Fn08DFC 1

--- src/actors/lib/actor_100400_fn0805c.c
+++ src/actors/lib/actor_100400_fn0805c.c
@@ -339,7 +339,19 @@ void Actor00400_Fn08D70(Actor100400* arg0)
     work->field_638 = work->field_638 + 1;
 }
 
-INCLUDE_ASM("actors/nonmatchings/lib/actor_100400_fn0805c", Actor00400_Fn08DFC);
+void Actor00400_Fn08DFC(Actor100400* arg0)
+{
+    Actor100400Ctx*  ctx;
+    Actor100400Work* work;
+
+    work = arg0->field_1C;
+    ctx  = arg0->field_2C;
+    if (++work->field_636 >= 0x18) {
+        ctx->field_C   |= 2;
+        work->field_636 = 0;
+        work->field_638++;
+    }
+}
 
 INCLUDE_ASM("actors/nonmatchings/lib/actor_100400_fn0805c", Actor00400_Fn08E50);
 
