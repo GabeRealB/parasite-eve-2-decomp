3aed6b29f0f0c9db8d8d8da2549b0d7e0075adc9
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-16 13:34:46 +0200

    matched func_actor_113100_801333B8 1

--- src/actors/actor_113100/actor_113100_4.c
+++ src/actors/actor_113100/actor_113100_4.c
@@ -7,4 +7,38 @@
 
 #include "gameplay/1BC.h"
 
-INCLUDE_ASM("actors/nonmatchings/actor_113100/actor_113100_4", func_actor_113100_801333B8);
+s32 func_actor_113100_801333B8(Task* task, s32 msgId, Actor113100Msg7DB* msg)
+{
+    Actor113100Work* work;
+    TmdObject*       model;
+
+    work = (Actor113100Work*)task->idMap;
+
+    switch (msg->field_2) {
+        case 0:
+            if (work->field_534 != NULL) {
+                model           = (TmdObject*)work->field_534->extra;
+                model->field_C &= 0xFF7F;
+            }
+            break;
+
+        case 1:
+            if (work->field_534 != NULL) {
+                model           = (TmdObject*)work->field_534->extra;
+                model->field_C |= 0x80;
+            }
+            break;
+
+        case 2:
+            work->field_53C = 1;
+            break;
+
+        case 3:
+            work->field_53C = 0;
+            break;
+
+        default:
+            return 0;
+    }
+    return 0;
+}
