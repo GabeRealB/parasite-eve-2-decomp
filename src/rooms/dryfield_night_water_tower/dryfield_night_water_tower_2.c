8a99d4339ac387e758f530fa9cd84f443677132a
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-16 02:57:39 +0200

    matched func_dryfield_night_water_tower_8017DAA4 1

--- src/rooms/dryfield_night_water_tower/dryfield_night_water_tower_2.c
+++ src/rooms/dryfield_night_water_tower/dryfield_night_water_tower_2.c
@@ -1,11 +1,19 @@
 #include "common.h"
 
+#include "gameplay/3CD8.h"
+
 s32 func_dryfield_night_water_tower_8017DA9C(void)
 {
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/dryfield_night_water_tower/dryfield_night_water_tower_2", func_dryfield_night_water_tower_8017DAA4);
+s32 func_dryfield_night_water_tower_8017DAA4(s32 arg0, s32 arg1, s32 arg2)
+{
+    if (arg2 == 7) {
+        Gp_StartCapSlot(7, 1, 3);
+    }
+    return 0;
+}
 
 s32 func_dryfield_night_water_tower_8017DAD4(void)
 {
