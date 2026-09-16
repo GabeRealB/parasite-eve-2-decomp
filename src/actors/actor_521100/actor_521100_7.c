37d5e2d6e99da3a7009aca3e1153e5104821a220
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-17 00:21:05 +0200

    matched func_actor_521100_80136AE0 3

--- src/actors/actor_521100/actor_521100_7.c
+++ src/actors/actor_521100/actor_521100_7.c
@@ -11,7 +11,39 @@ extern Task*    D_actor_521100_8016A3E4;
 
 INCLUDE_ASM("actors/nonmatchings/actor_521100/actor_521100_7", func_actor_521100_80136A64);
 
-INCLUDE_ASM("actors/nonmatchings/actor_521100/actor_521100_7", func_actor_521100_80136AE0);
+s32 func_actor_521100_80136AE0(Task* task, s32 arg1, Actor521100Msg* msg)
+{
+    switch (msg->field_2) {
+        case 1:
+            D_actor_521100_8016A3E0 = Task_SpawnFromTable(&D_actor_521100_8016A388, 1, 0, (s32)task);
+            break;
+
+        case 2:
+            D_actor_521100_8016A3D8->field_484 = 0;
+            task->state                        = 2;
+            break;
+
+        case 3:
+            if (D_actor_521100_8016A3E0 != NULL) {
+                Task_Kill(D_actor_521100_8016A3E0);
+                D_actor_521100_8016A3E0 = NULL;
+            }
+            if (D_actor_521100_8016A3E4 != NULL) {
+                Task_Kill(D_actor_521100_8016A3E4);
+                D_actor_521100_8016A3E4 = NULL;
+            }
+            /* fall through -- the jump table's index 0 lands on the same store */
+
+        case 0:
+            task->state = 1;
+            break;
+
+        case 4:
+            D_actor_521100_8016A3E4 = Task_SpawnFromTable(&D_actor_521100_8016A388, 1, 1, (s32)task);
+            break;
+    }
+    return 0;
+}
 s32 func_actor_521100_80136BE8(Task* task, s32 arg1, Actor521100Target* target)
 {
     GsCOORDINATE2* coord;
