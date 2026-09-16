4d8cc10b62fabd16459cddad2fe1cd478dc43630
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-16 13:01:33 +0200

    matched func_actor_120300_80133F14 1

--- src/actors/actor_120300/actor_120300_4.c
+++ src/actors/actor_120300/actor_120300_4.c
@@ -1,3 +1,22 @@
 #include "common.h"
 
-INCLUDE_ASM("actors/nonmatchings/actor_120300/actor_120300_4", func_actor_120300_80133F14);
+#include "gameplay/3CD8.h"
+#include "main/session.h"
+#include "main/task.h"
+
+extern s32 D_actor_120300_8014195C;
+
+void func_actor_120300_80133F14(Task* arg0)
+{
+    switch (arg0->state) {
+        case 0:
+            func_800E8614((s32)&D_actor_120300_8014195C, 0);
+            arg0->state += 1;
+            break;
+        case 1:
+            if (Game_Session->field_1 == 0) {
+                Task_Kill(arg0);
+            }
+            break;
+    }
+}
