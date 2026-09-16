19b62946ae8cff85ce2bbec367842afaae861541
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-17 01:09:28 +0200

    matched func_actor_206100_8014D380 10

--- src/actors/actor_206100/actor_206100.c
+++ src/actors/actor_206100/actor_206100.c
@@ -622,7 +622,39 @@ const Actor206100StateTable4 D_actor_206100_80149EC0 = {
 /// It ends by handing the actor to `func_actor_206100_8014ED3C` with step 0x14,
 /// the walk that retires the actor back to `field_434` / `field_438` once it has
 /// travelled far enough.
-INCLUDE_ASM("actors/nonmatchings/actor_206100/actor_206100", func_actor_206100_8014D380);
+void func_actor_206100_8014D380(Task* task)
+{
+    Actor206100Work* sub    = (Actor206100Work*)task->idMap;
+    TaskFuncTable3   states = D_actor_206100_80149EA8;
+    Actor206100Work* work;
+    GsCOORDINATE2*   coord;
+    SVECTOR          vec;
+    s32              angle;
+    s32              yaw;
+    s32              limit;
+    s32              diff;
+
+    if (take_request(task) == 0) {
+        states.funcs[(s16)sub->field_522](task);
+        work       = (Actor206100Work*)task->idMap;
+        coord      = ((TmdObject*)task->extra)->field_8;
+        coord->flg = 0;
+        vec.vx     = sub->field_4D0 - (u16)coord->coord.t[0];
+        vec.vy     = 0;
+        vec.vz     = sub->field_4D4 - (u16)coord->coord.t[2];
+        VectorNormalSS(&vec, &vec);
+        yaw   = ratan2(vec.vx, vec.vz);
+        limit = 0x28;
+        angle = (u16)work->field_43E;
+        diff  = ((angle - yaw) << 20) >> 20;
+        if (diff > limit) {
+            work->field_43E = angle - 0x18;
+        } else if (diff < -0x28) {
+            work->field_43E = angle + 0x18;
+        }
+        func_actor_206100_8014ED3C(task, 0x14);
+    }
+}
 void func_actor_206100_8014D574(Task* task)
 {
     Actor206100Work* work;
