3c8ad8e71e7f532f772695a284d56127548a7c71
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-16 02:30:17 +0200

    matched func_neo_ark_substation_8017D768 2

--- src/rooms/neo_ark_substation/neo_ark_substation_2.c
+++ src/rooms/neo_ark_substation/neo_ark_substation_2.c
@@ -1,6 +1,15 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/neo_ark_substation/neo_ark_substation_2", func_neo_ark_substation_8017D768);
+#include "main/gameflag.h"
+#include "gameplay/3CD8.h"
+
+s32 func_neo_ark_substation_8017D768(s32 arg0, s32 arg1, s32 arg2)
+{
+    if (arg2 == 3) {
+        Gp_RunCapCmd1(GameFlag_GetNibble(0xDF) != 0 ? 3 : 5);
+    }
+    return 0;
+}
 
 s32 func_neo_ark_substation_8017D7A4(void)
 {
