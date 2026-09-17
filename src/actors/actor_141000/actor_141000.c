faed5d0ba83f50e88f4c8726bb5241673f00d85f
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-17 13:44:39 +0200

    matched func_actor_141000_80131E94 4

--- src/actors/actor_141000/actor_141000.c
+++ src/actors/actor_141000/actor_141000.c
@@ -5,6 +5,7 @@
 
 #include "gameplay/3CD8.h"
 
+#include "main/display.h"
 #include "main/mem.h"
 #include "main/session.h"
 #include "main/task.h"
@@ -25,7 +26,112 @@ extern TaskFuncTable4 D_actor_141000_80131E3C;
 /// spawns later, every eighth frame.
 extern TaskDesc D_actor_141000_801348D8[];
 
-INCLUDE_ASM("actors/nonmatchings/actor_141000/actor_141000", func_actor_141000_80131E94);
+/// Quad index table: sixteen quads, four point/colour indices each.
+extern s8 D_actor_141000_801347C8[][4];
+
+/// Base vertex colours, scaled by the controller's `field_0` each frame.
+extern u8 D_actor_141000_80134808[][4];
+
+/// Draws the sixteen gouraud quads, each followed by a semi-transparency
+/// tpage change, at `arg2` depth minus 20.
+void func_actor_141000_80131E94(Actor141000* arg0, Actor141000Point* arg1, s32 arg2)
+{
+    CVECTOR   colors[24];
+    s8*       quad;
+    CVECTOR*  col;
+    POLY_G4*  poly;
+    DR_TPAGE* tpage;
+    s32       i;
+    u16       scale;
+    s32       a, b, c, d;
+
+    quad  = D_actor_141000_801347C8[0];
+    scale = ((Actor141000CtrlWork*)((Task*)((Task*)arg0)->spawnArg2)->idMap)->field_0;
+    if (((Task*)arg0)->killCountdown >= 0x800) {
+        ((Task*)arg0)->killCountdown = 0;
+    }
+    rsin(((Task*)arg0)->killCountdown);
+    for (i = 0; i < 24; i++) {
+        colors[i].r = (D_actor_141000_80134808[i][0] * (s16)scale) >> 12;
+        colors[i].g = (D_actor_141000_80134808[i][1] * (s16)scale) >> 12;
+        colors[i].b = (D_actor_141000_80134808[i][2] * (s16)scale) >> 12;
+    }
+    col = colors;
+    for (i = 0; i < 6; i++) {
+        a              = quad[0];
+        b              = quad[1];
+        c              = quad[2];
+        d              = quad[3];
+        poly           = (POLY_G4*)Gpu_PrimCursor;
+        Gpu_PrimCursor = (DR_TPAGE*)(poly + 1);
+        setlen(poly, 8);
+        poly->code = 0x3A;
+        poly->r0   = col[a].r;
+        poly->g0   = col[a].g;
+        poly->b0   = col[a].b;
+        poly->r1   = col[b].r;
+        poly->g1   = col[b].g;
+        poly->b1   = col[b].b;
+        poly->r2   = col[c].r;
+        poly->g2   = col[c].g;
+        poly->b2   = col[c].b;
+        poly->r3   = col[d].r;
+        poly->g3   = col[d].g;
+        poly->b3   = col[d].b;
+        poly->x0   = arg1[a].field_0;
+        poly->y0   = arg1[a].field_2;
+        poly->x1   = arg1[b].field_0;
+        poly->y1   = arg1[b].field_2;
+        poly->x2   = arg1[c].field_0;
+        poly->y2   = arg1[c].field_2;
+        poly->x3   = arg1[d].field_0;
+        poly->y3   = arg1[d].field_2;
+        addPrim((u32*)((((u32)(arg2 << Display_State.field_128) >> 2) & 0xFFC) + (u32)Gpu_CurrentOt) - 20, poly);
+        tpage          = Gpu_PrimCursor;
+        Gpu_PrimCursor = tpage + 1;
+        setlen(tpage, 1);
+        tpage->code[0] = 0xE1000425;
+        addPrim((u32*)((((u32)(arg2 << Display_State.field_128) >> 2) & 0xFFC) + (u32)Gpu_CurrentOt) - 20, tpage);
+        quad += 4;
+    }
+    for (i = 6; i < 16; i++) {
+        a              = quad[0];
+        b              = quad[1];
+        c              = quad[2];
+        d              = quad[3];
+        poly           = (POLY_G4*)Gpu_PrimCursor;
+        Gpu_PrimCursor = (DR_TPAGE*)(poly + 1);
+        setlen(poly, 8);
+        poly->code = 0x3A;
+        poly->r0   = col[a].r;
+        poly->g0   = col[a].g;
+        poly->b0   = col[a].b;
+        poly->r1   = col[b].r;
+        poly->g1   = col[b].g;
+        poly->b1   = col[b].b;
+        poly->r2   = col[c].r;
+        poly->g2   = col[c].g;
+        poly->b2   = col[c].b;
+        poly->r3   = col[d].r;
+        poly->g3   = col[d].g;
+        poly->b3   = col[d].b;
+        poly->x0   = arg1[a].field_0;
+        poly->y0   = arg1[a].field_2;
+        poly->x1   = arg1[b].field_0;
+        poly->y1   = arg1[b].field_2;
+        poly->x2   = arg1[c].field_0;
+        poly->y2   = arg1[c].field_2;
+        poly->x3   = arg1[d].field_0;
+        poly->y3   = arg1[d].field_2;
+        addPrim((u32*)((((u32)(arg2 << Display_State.field_128) >> 2) & 0xFFC) + (u32)Gpu_CurrentOt) - 20, poly);
+        tpage          = Gpu_PrimCursor;
+        Gpu_PrimCursor = tpage + 1;
+        setlen(tpage, 1);
+        tpage->code[0] = 0xE1000425;
+        addPrim((u32*)((((u32)(arg2 << Display_State.field_128) >> 2) & 0xFFC) + (u32)Gpu_CurrentOt) - 20, tpage);
+        quad += 4;
+    }
+}
 
 INCLUDE_ASM("actors/nonmatchings/actor_141000/actor_141000", func_actor_141000_801323F0);
 
