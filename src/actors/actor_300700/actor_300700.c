29d0c4a7390983e85c50e75e580a7df82679b992
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-17 07:02:20 +0200

    matched func_actor_300700_801626C0 2

--- src/actors/actor_300700/actor_300700.c
+++ src/actors/actor_300700/actor_300700.c
@@ -10,6 +10,7 @@
 #include "main/sound.h"
 #include "main/task.h"
 #include "main/tmd.h"
+#include "main/wipsys.h"
 
 #define SCRATCH_SP (*(u32*)0x1F8003FC)
 
@@ -336,7 +337,77 @@ void func_actor_300700_8016252C(Actor300700* arg0)
     SCRATCH_SP   += 8;
 }
 
-INCLUDE_ASM("actors/nonmatchings/actor_300700/actor_300700", func_actor_300700_801626C0);
+void func_actor_300700_801626C0(Actor300700* arg0)
+{
+    Actor300700Work*       work;
+    GsCOORDINATE2*         coord;
+    Actor300700RotScratch* sc;
+    s32                    random;
+    s32                    amount;
+    s32                    cur;
+    s32                    cur2;
+    s32                    cur3;
+    s32                    random2;
+    s32                    amount2;
+    u16                    want;
+    s16                    diff;
+    s32                    adiff;
+    s16                    turn;
+    s16                    wrap;
+
+    sc    = (Actor300700RotScratch*)(SCRATCH_SP -= 0x18);
+    work  = arg0->field_1C;
+    coord = arg0->field_2C->field_8;
+    switch (work->field_2E6) {
+        case 0:
+            Gp_LcgState     = Gp_LcgState * 5 + 0x71357911;
+            random          = (u32)Gp_LcgState >> 16;
+            amount          = random & 0x1F;
+            cur             = work->field_2DC;
+            work->field_2DC = !(random & 0x20) ? cur - amount : cur + amount;
+            break;
+        case 1:
+            sc->vec.vx = Wip_SysConfig.field_4->t[0] - coord->coord.t[0];
+            sc->vec.vy = 0;
+            sc->vec.vz = Wip_SysConfig.field_4->t[2] - coord->coord.t[2];
+            want       = ratan2((s16)sc->vec.vx, (s16)sc->vec.vz) & 0xFFF;
+            diff       = want - (work->field_2DC & 0xFFF);
+            adiff      = diff >= 0 ? diff : -diff;
+            turn       = diff;
+            if (adiff < 0x11) {
+                work->field_2DC = want;
+            } else {
+                if (adiff >= 0x801) {
+                    wrap = diff - 0x1000;
+                    if (diff <= 0)
+                        wrap = 0x1000 - diff;
+                    turn = wrap;
+                }
+                cur2 = work->field_2DC;
+                if (turn > 0) {
+                    work->field_2DC = cur2 + 0x10;
+                } else {
+                    work->field_2DC = cur2 - 0x10;
+                }
+            }
+            break;
+    }
+    Gp_LcgState     = Gp_LcgState * 5 + 0x71357911;
+    random2         = (u32)Gp_LcgState >> 16;
+    amount2         = random2 & 0x3F;
+    cur3            = work->field_2DA;
+    work->field_2DA = !(random2 & 0x40) ? cur3 - amount2 : cur3 + amount2;
+    if (work->field_2DA > 0x100) {
+        work->field_2DA = 0x100;
+    } else if (work->field_2DA < -0x100) {
+        work->field_2DA = -0x100;
+    }
+    sc->rot.vx = work->field_2DA;
+    sc->rot.vy = work->field_2DC;
+    sc->rot.vz = 0;
+    RotMatrix(&sc->rot, &coord->coord);
+    SCRATCH_SP += 0x18;
+}
 
 INCLUDE_ASM("actors/nonmatchings/actor_300700/actor_300700", func_actor_300700_801628C8);
 
