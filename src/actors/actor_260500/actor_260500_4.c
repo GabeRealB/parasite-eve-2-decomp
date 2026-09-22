0c81cee5c9ec7be34365b979a7568a1bcd27c631
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-23 00:10:14 +0200

    matched ActorsShared80131f9cSub1 1

--- src/actors/actor_260500/actor_260500_4.c
+++ src/actors/actor_260500/actor_260500_4.c
@@ -1,3 +1,26 @@
 #include "common.h"
 
-INCLUDE_ASM("actors/nonmatchings/actor_260500/actor_260500_4", ActorsShared80131f9cSub1);
+#include "actors/actor_260500.h"
+#include "gameplay/1BC.h"
+#include "gameplay/3A34.h"
+#include "main/task.h"
+#include "main/tmd.h"
+
+void ActorsShared80132378(Task* task);
+
+void ActorsShared80131f9cSub1(GpEnemy* enemy, Task* task)
+{
+    TmdObject*     obj;
+    GsCOORDINATE2* coord;
+    VECTOR         pos;
+
+    obj   = (TmdObject*)task->extra;
+    coord = obj->coords;
+    Gp_UpdateCoord(coord);
+    pos.vx = coord->workm.t[0];
+    pos.vy = coord->workm.t[1] - 0x320;
+    pos.vz = coord->workm.t[2];
+    func_800D7A9C(obj, &pos, 0, 3);
+    func_actor_260500_8014A110(task);
+    ActorsShared80132378(task);
+}
