bd6dd24188bedc94893b16c554dadd1e388c5c01
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-22 21:54:13 +0200

    promote ActorsSharedFn01110 into actor_04600, actor_07000

--- src/actors/actor_04600/actor_04600_4.c
+++ src/actors/actor_04600/actor_04600_4.c
@@ -24,8 +24,6 @@ extern u8 D_801153F4;
 /// teardown - dispatched through by state.
 extern GpEnemyTaskFuncTable3 Actor04600_D00004;
 
-INCLUDE_ASM("actors/nonmatchings/actor_04600/actor_04600_4", Actor04600_Fn01110);
-
 INCLUDE_ASM("actors/nonmatchings/actor_04600/actor_04600_4", Actor04600_Fn01604);
 
 INCLUDE_ASM("actors/nonmatchings/actor_04600/actor_04600_4", Actor04600_Fn017CC);
