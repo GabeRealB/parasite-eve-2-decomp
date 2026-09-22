42158df16230f3e035d38b8b9ecfef72f30a83e4
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-22 19:12:15 +0200

    matched Actor04000_Fn06EA8 1

--- src/actors/actor_04000/actor_204000_6.c
+++ src/actors/actor_04000/actor_204000_6.c
@@ -8,11 +8,34 @@ typedef struct Actor204000Task {
     /* 0x30 */ s32  field_30;
 } Actor204000Task;
 
+/// Payload of message 0x7DA, sent to the `gameGetPtrSlot(4)` task.
+typedef struct Actor204000Msg7DA {
+    /* 0x0 */ u8  field_0;
+    /* 0x1 */ u8  field_1;
+    /* 0x2 */ s16 field_2;
+} Actor204000Msg7DA;
+
 extern u8           D_8007216C;
+extern Actor104000* Actor04000_D0C710[2];
 extern Actor104000* Actor04000_D0C718[6];
 extern void         (*Actor04000_D0C6EC[])(Actor204000Task*);
 
-INCLUDE_ASM("actors/nonmatchings/actor_04000/actor_204000_6", Actor04000_Fn06EA8);
+void Actor04000_Fn06EA8(Actor204000Task* arg0)
+{
+    Actor204000Msg7DA msg;
+    s16               i;
+
+    for (i = 0; i < 6; i++) {
+        Actor04000_D0C718[i] = NULL;
+    }
+    Actor04000_D0C710[1] = NULL;
+    msg.field_0          = 3;
+    msg.field_1          = 0x10;
+    Actor04000_D0C710[0] = NULL;
+    msg.field_2          = 1;
+    Gp_DispatchMsg(gameGetPtrSlot(4), 0x7DA, (s32)&msg, 0x7DB);
+    arg0->field_30++;
+}
 
 void Actor04000_Fn06F54(Actor204000Task* arg0)
 {
