566707ab2289d5983060cbf41ef73c7be409ed2b
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-22 23:47:25 +0200

    matched ActorsShared80131e24Sub1 1

--- src/actors/actor_451100/actor_451100_6.c
+++ src/actors/actor_451100/actor_451100_6.c
@@ -1,10 +1,33 @@
 #include "common.h"
 
+#include "actors/actor_451100.h"
 #include "gameplay/1BC.h"
-
+#include "gameplay/3A34.h"
+#include "gameplay/gameplay.h"
 #include "main/task.h"
+#include "main/tmd.h"
 
-INCLUDE_ASM("actors/nonmatchings/actor_451100/actor_451100_6", ActorsShared80131e24Sub1);
+void func_actor_451100_80132CD4(Task* task);
+
+/// Refreshes the model root's coordinate, feeds its world translation (raised
+/// by 800 on y) to `func_800D7A9C`, then runs the animation starter and
+/// `func_actor_451100_80132CD4`, which draws a ground quad.
+void ActorsShared80131e24Sub1(GpEnemy* enemy, Task* task)
+{
+    TmdObject*     obj;
+    GsCOORDINATE2* coord;
+    VECTOR         pos;
+
+    obj   = (TmdObject*)task->extra;
+    coord = obj->coords;
+    Gp_UpdateCoord(coord);
+    pos.vx = coord->workm.t[0];
+    pos.vy = coord->workm.t[1] - 800;
+    pos.vz = coord->workm.t[2];
+    func_800D7A9C(obj, &pos, 0, 3);
+    ActorsShared80132a1c(task);
+    func_actor_451100_80132CD4(task);
+}
 
 void func_actor_451100_80132CAC(Task* task)
 {
