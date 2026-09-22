7b18eaea375724357ff30330557ba72b9616fece
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-22 21:21:40 +0200

    promote RoomsShared8017f8f4 into dryfield_factory, dryfield_night_factory

---
+++ src/rooms/dryfield_factory/dryfield_factory_12.c
@@ -0,0 +1,3 @@
+#include "common.h"
+
+INCLUDE_ASM("rooms/nonmatchings/dryfield_factory/dryfield_factory_12", func_dryfield_factory_8017FAC4);
--- src/rooms/dryfield_factory/dryfield_factory_3.c
+++ src/rooms/dryfield_factory/dryfield_factory_3.c
@@ -21,7 +21,3 @@ INCLUDE_ASM("rooms/nonmatchings/dryfield_factory/dryfield_factory_3", func_dryfi
 INCLUDE_ASM("rooms/nonmatchings/dryfield_factory/dryfield_factory_3", func_dryfield_factory_8017F08C);
 
 INCLUDE_ASM("rooms/nonmatchings/dryfield_factory/dryfield_factory_3", func_dryfield_factory_8017F4BC);
-
-INCLUDE_ASM("rooms/nonmatchings/dryfield_factory/dryfield_factory_3", func_dryfield_factory_8017F8F4);
-
-INCLUDE_ASM("rooms/nonmatchings/dryfield_factory/dryfield_factory_3", func_dryfield_factory_8017FAC4);
