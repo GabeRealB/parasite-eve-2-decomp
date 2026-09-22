dacd4196ff12c34a7a3aa5ee9dac34772a6ad502
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-22 19:09:38 +0200

    matched Actor04000_Fn06704 1

--- src/actors/actor_04000/actor_204000_3.c
+++ src/actors/actor_04000/actor_204000_3.c
@@ -1,3 +1,15 @@
 #include "common.h"
+#include "actors/actor_104000.h"
 
-INCLUDE_ASM("actors/nonmatchings/actor_04000/actor_204000_3", Actor04000_Fn06704);
+/// Message handler for 0x7DE: advances the work block's state from 0xB to 0xD
+/// and leaves any other state alone.
+s32 Actor04000_Fn06704(Actor104000* arg0, s32 arg1, void* arg2)
+{
+    Actor104000Work* work;
+
+    work = arg0->field_1C;
+    if (work->field_0 == 0xB) {
+        work->field_0 = 0xD;
+    }
+    return 1;
+}
