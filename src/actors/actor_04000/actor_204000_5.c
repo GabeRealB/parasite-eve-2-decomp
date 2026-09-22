baef8faa2d311395a4881bfb9b5cb944c0d89cb0
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-22 19:13:37 +0200

    matched Actor04000_Fn06C80 2

--- src/actors/actor_04000/actor_204000_5.c
+++ src/actors/actor_04000/actor_204000_5.c
@@ -44,6 +44,30 @@ void Actor04000_Fn06AC4(Actor104000Ctx* arg0, Actor104000* arg1)
 
 INCLUDE_ASM("actors/nonmatchings/actor_04000/actor_204000_5", Actor04000_Fn06BC8);
 
-INCLUDE_ASM("actors/nonmatchings/actor_04000/actor_204000_5", Actor04000_Fn06C80);
+void Actor04000_Fn06C80(Actor104000Ctx* arg0, Actor104000* arg1)
+{
+    Actor104000Work*  work;
+    Actor104000Obj2C* obj;
+
+    work = arg1->field_1C;
+    if (work->field_4 != 0) {
+        obj                 = arg1->field_2C;
+        arg0->field_14      = 0;
+        obj->field_C        = 0;
+        work->field_174     = 6;
+        work->field_170     = 1;
+        work->field_178     = 0;
+        work->obj350.flags |= 0x8000;
+        work->obj388.flags &= 0x7FFF;
+        work->obj3C0.flags &= 0x7FFF;
+        work->obj270.flags |= 0x4000;
+        Actor04000_Fn00E6C(arg1);
+        return;
+    }
+    Actor04000_Fn00E6C(arg1);
+    if (work->field_58 & 1) {
+        work->field_0 = 7;
+    }
+}
 
 INCLUDE_ASM("actors/nonmatchings/actor_04000/actor_204000_5", Actor04000_Fn06D38);
