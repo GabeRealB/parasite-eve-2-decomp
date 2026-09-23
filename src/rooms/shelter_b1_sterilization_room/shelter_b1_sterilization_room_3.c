a2b103b668a5171a3606cf7b5eb20d91b9acabbb
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 00:54:56 +0200

    matched func_shelter_b1_sterilization_room_80180430 1

--- src/rooms/shelter_b1_sterilization_room/shelter_b1_sterilization_room_3.c
+++ src/rooms/shelter_b1_sterilization_room/shelter_b1_sterilization_room_3.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "main/sound.h"
 
 INCLUDE_RODATA("rooms/nonmatchings/shelter_b1_sterilization_room/shelter_b1_sterilization_room_3", RoomsShared8017ef20Title);
 INCLUDE_RODATA("rooms/nonmatchings/shelter_b1_sterilization_room/shelter_b1_sterilization_room_3", RoomsShared8017de9cHundred);
@@ -6,7 +7,13 @@ INCLUDE_RODATA("rooms/nonmatchings/shelter_b1_sterilization_room/shelter_b1_ster
 INCLUDE_RODATA("rooms/nonmatchings/shelter_b1_sterilization_room/shelter_b1_sterilization_room_3", RoomsShared8017e8b4PeTitle);
 INCLUDE_RODATA("rooms/nonmatchings/shelter_b1_sterilization_room/shelter_b1_sterilization_room_3", RoomsShared8017ea68Title);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b1_sterilization_room/shelter_b1_sterilization_room_3", func_shelter_b1_sterilization_room_80180430);
+s32 func_shelter_b1_sterilization_room_80180430(s32 arg0, s32 arg1, s32 arg2)
+{
+    if (arg2 == 0x63) {
+        SndEvt_EnqueueType6(0x54100016, 0, 0);
+    }
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b1_sterilization_room/shelter_b1_sterilization_room_3", func_shelter_b1_sterilization_room_80180464);
 
