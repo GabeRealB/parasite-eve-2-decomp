f3a091ffd49d75e4014404e5570c3f4ecda9e470
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 00:57:24 +0200

    matched func_shelter_b1_sterilization_room_80181244 1

--- src/rooms/shelter_b1_sterilization_room/shelter_b1_sterilization_room_6.c
+++ src/rooms/shelter_b1_sterilization_room/shelter_b1_sterilization_room_6.c
@@ -1,6 +1,18 @@
 #include "common.h"
 #include "main/task.h"
+#include "main/session.h"
+#include "rooms/rooms_shared_8017e92c.h"
 extern TaskDesc D_shelter_b1_sterilization_room_80188504;
 extern s32      D_shelter_b1_sterilization_room_8018C340;
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b1_sterilization_room/shelter_b1_sterilization_room_6", func_shelter_b1_sterilization_room_80181244);
+void RoomsShared8017df80(s32 shade);
+
+void func_shelter_b1_sterilization_room_80181244(Task* task)
+{
+    RoomsShared8017e144(0x80);
+    RoomsShared8017df80(0);
+    if (gGameSession->viewReady != 0) {
+        task->killCountdown = 0x80;
+        task->state++;
+    }
+}
