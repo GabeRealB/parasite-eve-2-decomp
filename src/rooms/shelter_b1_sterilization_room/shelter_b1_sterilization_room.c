3b358aadd5d17fdb8c3aea0953619e56f5ebe2d2
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 01:07:29 +0200

    matched func_shelter_b1_sterilization_room_8017FF80 2

--- src/rooms/shelter_b1_sterilization_room/shelter_b1_sterilization_room.c
+++ src/rooms/shelter_b1_sterilization_room/shelter_b1_sterilization_room.c
@@ -8,18 +8,23 @@
 #include "main/task.h"
 #include "gameplay/1A8.h"
 #include "main/gameflag.h"
+#include "rooms/rooms_shared_80181228.h"
 
-extern void           func_800E8634(s32 arg0, s32 arg1, s32 arg2);
-extern TaskDesc       D_shelter_b1_sterilization_room_80188504[];
-extern s32            D_shelter_b1_sterilization_room_8018873C;
-extern s32            D_shelter_b1_sterilization_room_80188AB4;
-extern s32            D_80135AC0;
-extern s32            D_80135D78;
-extern s32            D_80136258;
-extern s32            D_shelter_b1_sterilization_room_80184E40;
-extern s32            D_shelter_b1_sterilization_room_80184E7C;
-extern GpObj4A        D_shelter_b1_sterilization_room_8018BF30[];
-extern GpAreaApplyRec D_shelter_b1_sterilization_room_8018C334;
+extern void                   func_800E8634(s32 arg0, s32 arg1, s32 arg2);
+extern TaskDesc               D_shelter_b1_sterilization_room_80188504[];
+extern s32                    D_shelter_b1_sterilization_room_8018873C;
+extern s32                    D_shelter_b1_sterilization_room_80188AB4;
+extern s32                    D_80135AC0;
+extern s32                    D_80135D78;
+extern s32                    D_80136258;
+extern s32                    D_shelter_b1_sterilization_room_80184E40;
+extern s32                    D_shelter_b1_sterilization_room_80184E7C;
+extern GpObj4A                D_shelter_b1_sterilization_room_8018BF30[];
+extern GpAreaApplyRec         D_shelter_b1_sterilization_room_8018C334;
+extern TaskDesc               D_shelter_b1_sterilization_room_80184E70;
+extern s32                    D_shelter_b1_sterilization_room_80188ED4;
+extern s32                    D_shelter_b1_sterilization_room_80188FDC;
+extern RoomsShared80181228Rec D_shelter_b1_sterilization_room_8018C344;
 
 void func_shelter_b1_sterilization_room_80180340(s32 arg0);
 
@@ -159,7 +164,55 @@ s32 func_shelter_b1_sterilization_room_8017FC78(Task* task, s32 msgId, GpMsg13EF
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b1_sterilization_room/shelter_b1_sterilization_room", func_shelter_b1_sterilization_room_8017FF80);
+s32 func_shelter_b1_sterilization_room_8017FF80(s32 arg0, s32 arg1, s32 arg2)
+{
+    if (arg2 == 0x11) {
+        if (GameFlag_GetNibble(0x161) == 0) {
+            GameFlag_SetNibble(0x161, 1);
+            Gp_SpawnIfCapIdle(0x18, 1);
+            return;
+        }
+        D_shelter_b1_sterilization_room_8018C344.field_0  = 0x13;
+        D_shelter_b1_sterilization_room_8018C344.field_1  = 1;
+        D_shelter_b1_sterilization_room_8018C344.field_3  = 2;
+        D_shelter_b1_sterilization_room_8018C344.field_2  = 0;
+        D_shelter_b1_sterilization_room_8018C344.field_4  = 0x5410000C;
+        D_shelter_b1_sterilization_room_8018C344.field_8  = 0x5410000F;
+        D_shelter_b1_sterilization_room_8018C344.field_10 = 0x5410000D;
+        D_shelter_b1_sterilization_room_8018C344.field_C  = 0x5410000E;
+        Task_SpawnFromTable(&RoomsShared80181228Desc, 0, 6, (s32)&D_shelter_b1_sterilization_room_8018C344);
+    }
+    if (arg2 == 0x15) {
+        Gp_RunCapCmd1(arg2);
+    }
+    if (arg2 == 4) {
+        Task_SpawnFromTable(D_shelter_b1_sterilization_room_80188504, 7, 0, 0);
+    }
+    if (arg2 == 0x13) {
+        Task_SpawnFromTable(D_shelter_b1_sterilization_room_80188504, 7, 1, 0);
+    }
+    if (arg2 == 0xE) {
+        if (GameFlag_GetNibble(0x76) == 1 && GameFlag_GetNibble(0x77) == 0) {
+            if (GameFlag_GetNibble(0x14F) == 0) {
+                Gp_MsgPlayerWeapon(0);
+                func_800E8634((s32)&D_shelter_b1_sterilization_room_80188ED4, 0, (s32)&D_shelter_b1_sterilization_room_80188FDC);
+                GameFlag_SetNibble(0x14F, 1);
+            } else {
+                Task_SpawnFromTable(D_shelter_b1_sterilization_room_80188504, 8, 0xB, 0);
+                GameFlag_SetNibble(0x14F, 2);
+            }
+        } else {
+            Gp_RunCapCmd1(arg2);
+        }
+    }
+    if (arg2 == 0xC || arg2 == 0xD) {
+        if (gGameSession->at4.loc.room == 3) {
+            Gp_MsgPlayerWeapon(0);
+            Task_SpawnFromTable(&D_shelter_b1_sterilization_room_80184E70, 0, arg2, 0);
+        }
+    }
+    return 0;
+}
 
 void func_shelter_b1_sterilization_room_80180188(Task* task)
 {
