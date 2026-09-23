0c5e77615db0c9c3dacd493a4812b3b8c85b0436
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 00:36:16 +0200

    matched func_shelter_b4_reservoir_8017E264 1

--- src/rooms/shelter_b4_reservoir/shelter_b4_reservoir.c
+++ src/rooms/shelter_b4_reservoir/shelter_b4_reservoir.c
@@ -1,5 +1,7 @@
 #include "common.h"
+#include "gameplay/1A8.h"
 #include "gameplay/3CD8.h"
+#include "main/gameflag.h"
 #include "main/sound.h"
 #include "main/task.h"
 #include "rooms/shelter_b4_reservoir.h"
@@ -8,15 +10,18 @@ extern s16 D_shelter_b4_reservoir_80184F82;
 extern s32 D_shelter_b4_reservoir_8018492C;
 extern s16 D_shelter_b4_reservoir_80184F80;
 
-extern u8       D_shelter_b4_reservoir_80184F78;
-extern u8       D_shelter_b4_reservoir_80184F79;
-extern u8       D_shelter_b4_reservoir_80184F7A;
-extern s16      D_shelter_b4_reservoir_80184F7C;
-extern s32      D_shelter_b4_reservoir_80187510;
-extern Task*    D_shelter_b4_reservoir_80184930;
-extern u8       D_8007216C;
-extern u8       D_801153F4;
-extern TaskDesc D_shelter_b4_reservoir_801848EC;
+extern u8        D_shelter_b4_reservoir_80184F78;
+extern u8        D_shelter_b4_reservoir_80184F79;
+extern u8        D_shelter_b4_reservoir_80184F7A;
+extern s16       D_shelter_b4_reservoir_80184F7C;
+extern s32       D_shelter_b4_reservoir_80187510;
+extern Task*     D_shelter_b4_reservoir_80184930;
+extern u8        D_8007216C;
+extern u8        D_801153F4;
+extern TaskDesc  D_shelter_b4_reservoir_801848EC;
+extern GpSaveLoc D_shelter_b4_reservoir_80187508;
+
+extern s32 func_80179A04(GpSaveLoc* in, GpSaveLoc* out);
 INCLUDE_RODATA("rooms/nonmatchings/shelter_b4_reservoir/shelter_b4_reservoir", RoomsShared8017d878Table);
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b4_reservoir/shelter_b4_reservoir", func_shelter_b4_reservoir_8017DE8C);
@@ -33,7 +38,28 @@ s32 func_shelter_b4_reservoir_8017E25C(void)
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b4_reservoir/shelter_b4_reservoir", func_shelter_b4_reservoir_8017E264);
+s32 func_shelter_b4_reservoir_8017E264(Task* task, s32 msgId, GpSaveLoc* src, GpSaveLoc* dst)
+{
+    *dst = *src;
+    func_80179A04(src, dst);
+    if (*(u16*)src == 0x2C) {
+        if (GameFlag_GetNibble(0xB7) == 1) {
+            if (src->field_5 == 0) {
+                Gp_SetNibbleIf(src->field_6, 2);
+                Gp_RunCapCmd1(2);
+            }
+        } else {
+            if (src->field_5 == 0) {
+                D_shelter_b4_reservoir_80187508.field_2 = dst->field_0;
+                D_shelter_b4_reservoir_80187508.field_4 = dst->field_2;
+                D_shelter_b4_reservoir_80187508.field_1 = dst->field_3;
+                Task_SpawnFromTable(&D_shelter_b4_reservoir_801848EC, 3, 0xB, 0);
+            }
+        }
+        return 0;
+    }
+    return 1;
+}
 
 s32 func_shelter_b4_reservoir_8017E354(s32 arg0, s32 arg1, s32 arg2)
 {
