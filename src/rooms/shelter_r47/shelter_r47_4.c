3f7ca4faaa42472aad074294f8d64ba0f2e8be1c
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-23 17:32:53 +0200

    matched func_shelter_r47_80183E24 1

--- src/rooms/shelter_r47/shelter_r47_4.c
+++ src/rooms/shelter_r47/shelter_r47_4.c
@@ -297,7 +297,30 @@ INCLUDE_ASM("rooms/nonmatchings/shelter_r47/shelter_r47_4", func_shelter_r47_801
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_r47/shelter_r47_4", func_shelter_r47_80183B84);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_r47/shelter_r47_4", func_shelter_r47_80183E24);
+void func_shelter_r47_80183E24(void)
+{
+    GpTpageSprt* p;
+    SPRT*        sprt;
+
+    p              = (GpTpageSprt*)gGpuPrimCursor;
+    gGpuPrimCursor = (u8*)(p + 1);
+    setlen(&p->tpage, 1);
+    setlen(&p->sprt, 4);
+    p->tpage.code[0] = 0xE100002F;
+    setcode(&p->sprt, 0x64);
+    sprt = &p->sprt;
+    MargePrim(p, sprt);
+    sprt->clut        = 0x3FC4;
+    sprt->x0          = -0x96;
+    sprt->y0          = 0x3F;
+    sprt->u0          = 0x50;
+    sprt->w           = 0x38;
+    sprt->v0          = 0;
+    sprt->h           = 0x10;
+    sprt->code       |= 3;
+    p->tpage.tag      = (p->tpage.tag & 0xFF000000) | (gGpuCurrentOt[11] & 0xFFFFFF);
+    gGpuCurrentOt[11] = (gGpuCurrentOt[11] & 0xFF000000) | ((u32)p & 0xFFFFFF);
+}
 
 void func_shelter_r47_80183F0C(void)
 {
