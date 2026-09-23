1483229264207b09c59b63cabac122e9e1e9d0e0
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-23 21:45:52 +0200

    matched func_shelter_r48_8017E4C4 1

--- src/rooms/shelter_r48/shelter_r48_3.c
+++ src/rooms/shelter_r48/shelter_r48_3.c
@@ -3,10 +3,19 @@
 #include "gameplay/3CD8.h"
 #include "gameplay/3FB8.h"
 #include "gameplay/D4.h"
+#include "gameplay/gameplay.h"
 #include "main/gameflag.h"
 #include "main/task.h"
 #include "main/tmd.h"
 
+typedef struct {
+    u8             pad0[8];
+    GsCOORDINATE2* field_8;
+    u8             pad_C[0x16];
+    u16            field_22;
+} ClumpMem;
+
+void func_shelter_r48_8017FF74(GsCOORDINATE2* arg0, s32 arg1, s32 arg2);
 void func_shelter_r48_8017F124(GpEffWork* work, GsCOORDINATE2* coord, s32 part);
 void func_shelter_r48_8018258C(void* arg0, s32 arg1, s32 arg2);
 
@@ -41,7 +50,63 @@ void func_shelter_r48_8017E3B8(Task* task)
     }
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_r48/shelter_r48_3", func_shelter_r48_8017E4C4);
+void func_shelter_r48_8017E4C4(Task* arg0)
+{
+    ClumpMem*      mem;
+    GsCOORDINATE2* coord;
+    MATRIX*        m;
+    s32            i;
+
+    mem   = (ClumpMem*)arg0->spawnArg2;
+    coord = (GsCOORDINATE2*)((TmdObject*)arg0->extra)->coords;
+    if (Gp_State1C->eventState != 0) {
+        func_shelter_r48_8017FF74(coord, ((s16)mem->field_22 / 2) & 0xFFFF, 0x380);
+        if (Gp_State1C->eventState >= 4) {
+            Gp_ReleaseState1CMem(mem, arg0);
+        }
+        return;
+    }
+    if (arg0->state == 0) {
+        m                            = &coord->coord;
+        coord->sub                   = mem->field_8;
+        *(s32*)&coord->coord.m[0][0] = 0x1000;
+        *(s32*)&m->m[0][2]           = 0;
+        *(s32*)&m->m[1][1]           = 0x1000;
+        *(s32*)&m->m[2][0]           = 0;
+        m->m[2][2]                   = 0x1000;
+        coord->coord.t[2]            = 0;
+        coord->coord.t[1]            = 0;
+        coord->coord.t[0]            = 0;
+        coord->flg                   = 0;
+        Gp_UpdateCoord(coord);
+        arg0->state = 1;
+    }
+    mem->field_22 += 1;
+    switch (arg0->spawnArg1) {
+        case 0:
+            Gp_SpawnEff(0x6018B, coord, 0x14002400, NULL);
+            arg0->spawnArg1 = 1;
+            return;
+        case 1:
+            func_shelter_r48_8017FF74(coord, ((s16)mem->field_22 / 2) & 0xFFFF, 0x380);
+            if (!(mem->field_22 & 1)) {
+                Gp_SpawnEff(0x6018B, coord, 0x1001400, NULL);
+            }
+            mem->field_22 += 1;
+            return;
+        case 2:
+            Gp_SpawnEff(0x6018B, coord, 0x10002380, NULL);
+            for (i = 0; i < 4; i++) {
+                Gp_SpawnEff(0x6018B, coord, 0x2002400, NULL);
+                Gp_SpawnEff(0x6018C, coord, 0x2202300, NULL);
+            }
+            arg0->spawnArg1 = 3;
+            return;
+        case 3:
+            Gp_ReleaseState1CMem(mem, arg0);
+            return;
+    }
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_r48/shelter_r48_3", func_shelter_r48_8017E704);
 
