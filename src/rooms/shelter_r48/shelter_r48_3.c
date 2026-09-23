5b0be2468ca3ab372df5dfd44d275526f3f4d45d
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-23 21:38:29 +0200

    matched func_shelter_r48_8017EFD8 3

--- src/rooms/shelter_r48/shelter_r48_3.c
+++ src/rooms/shelter_r48/shelter_r48_3.c
@@ -1,5 +1,12 @@
 #include "common.h"
 
+#include "gameplay/3CD8.h"
+#include "gameplay/3FB8.h"
+#include "main/task.h"
+#include "main/tmd.h"
+
+void func_shelter_r48_8017F124(GpEffWork* work, GsCOORDINATE2* coord, s32 part);
+
 INCLUDE_ASM("rooms/nonmatchings/shelter_r48/shelter_r48_3", func_shelter_r48_8017E27C);
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_r48/shelter_r48_3", func_shelter_r48_8017E3B8);
@@ -12,7 +19,39 @@ INCLUDE_ASM("rooms/nonmatchings/shelter_r48/shelter_r48_3", func_shelter_r48_801
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_r48/shelter_r48_3", func_shelter_r48_8017EC18);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_r48/shelter_r48_3", func_shelter_r48_8017EFD8);
+void func_shelter_r48_8017EFD8(Task* task)
+{
+    GpEffWork*     work;
+    GsCOORDINATE2* coord;
+
+    work  = task->spawnArg2;
+    coord = ((TmdObject*)task->extra)->coords;
+    if (Gp_State1C->eventState != 0) {
+        func_shelter_r48_8017F124(work, coord, 0);
+        func_shelter_r48_8017F124(work, coord, 1);
+        func_shelter_r48_8017F124(work, coord, 2);
+        if (Gp_State1C->eventState < 4) {
+            return;
+        }
+    } else {
+        work->age++;
+        if (task->state == 0) {
+            work->age   = 1;
+            work->scale = 0x80;
+            task->state = 1;
+        }
+        if (work->scale >= 9) {
+            work->scale -= 8;
+            work->angle += 0x200;
+            work->step  += 0x100;
+            func_shelter_r48_8017F124(work, coord, 0);
+            func_shelter_r48_8017F124(work, coord, 1);
+            func_shelter_r48_8017F124(work, coord, 2);
+            return;
+        }
+    }
+    Gp_ReleaseState1CMem(work, task);
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_r48/shelter_r48_3", func_shelter_r48_8017F124);
 
