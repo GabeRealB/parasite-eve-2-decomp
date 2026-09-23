95c9f907aff2a2dc9097758596cacb303bcd5e93
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 00:35:01 +0200

    matched func_shelter_b4_reservoir_8017FADC 1

--- src/rooms/shelter_b4_reservoir/shelter_b4_reservoir_3.c
+++ src/rooms/shelter_b4_reservoir/shelter_b4_reservoir_3.c
@@ -1,7 +1,13 @@
 #include "common.h"
+#include "main/session.h"
+#include "main/task.h"
+#include "rooms/rooms_shared_8017e690.h"
 
 extern void func_shelter_b4_reservoir_8017E068(void);
 void        func_shelter_b4_reservoir_8017E8E4(void);
+void        func_shelter_b4_reservoir_8017E8EC(Task* task);
+
+extern s16 D_shelter_b4_reservoir_80184F80;
 
 void func_shelter_b4_reservoir_8017E8E4(void)
 {
@@ -17,4 +23,10 @@ INCLUDE_ASM("rooms/nonmatchings/shelter_b4_reservoir/shelter_b4_reservoir_3", fu
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b4_reservoir/shelter_b4_reservoir_3", func_shelter_b4_reservoir_8017F674);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b4_reservoir/shelter_b4_reservoir_3", func_shelter_b4_reservoir_8017FADC);
+void func_shelter_b4_reservoir_8017FADC(Task* task)
+{
+    TaskFunc states[2] = { RoomsShared8017e690, func_shelter_b4_reservoir_8017E8EC };
+
+    states[task->state](task);
+    gGameSession->waterY = D_shelter_b4_reservoir_80184F80;
+}
