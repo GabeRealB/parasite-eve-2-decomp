9453e16e2e6428a183738c4ec40d87e62575c9b6
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-23 19:31:10 +0200

    matched func_shelter_b3_garbage_incinerator_8017DB7C 1

--- src/rooms/shelter_b3_garbage_incinerator/shelter_b3_garbage_incinerator.c
+++ src/rooms/shelter_b3_garbage_incinerator/shelter_b3_garbage_incinerator.c
@@ -1,13 +1,20 @@
 #include "common.h"
+#include "gameplay/D4.h"
 #include "main/session.h"
 #include "main/sound.h"
 #include "main/task.h"
 #include "rooms/room_common.h"
 
-extern u16      D_shelter_b3_garbage_incinerator_801855DC;
-extern TaskDesc D_shelter_b3_garbage_incinerator_801855E0;
+extern u16        D_shelter_b3_garbage_incinerator_801855DC;
+extern TaskDesc   D_shelter_b3_garbage_incinerator_801855E0;
+extern TaskDesc   D_8016BFE0;
+extern GpMsgEntry D_shelter_b3_garbage_incinerator_80185594[];
+extern Task*      D_shelter_b3_garbage_incinerator_801855D8;
+extern TaskDesc   D_shelter_b3_garbage_incinerator_80185BA0;
+extern TaskDesc   D_shelter_b3_garbage_incinerator_80187150[];
 
 void RoomsShared801830f0(s16 arg0, s16 arg1, s16 arg2);
+void func_shelter_b3_garbage_incinerator_8018108C(s32 arg0, s32 arg1, s32 arg2);
 
 INCLUDE_RODATA("rooms/nonmatchings/shelter_b3_garbage_incinerator/shelter_b3_garbage_incinerator", RoomsShared8017d878Table);
 
@@ -55,7 +62,20 @@ s32 func_shelter_b3_garbage_incinerator_8017DB2C(s32 arg0, s32 arg1, s32 arg2)
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b3_garbage_incinerator/shelter_b3_garbage_incinerator", func_shelter_b3_garbage_incinerator_8017DB7C);
+void func_shelter_b3_garbage_incinerator_8017DB7C(Task* task)
+{
+    task->msgTable = D_shelter_b3_garbage_incinerator_80185594;
+    Game_SetPtrSlot(task, 7);
+    func_shelter_b3_garbage_incinerator_8018108C(0x180, 0, 0);
+    D_shelter_b3_garbage_incinerator_801855D8 = Task_SpawnFromTable(&D_shelter_b3_garbage_incinerator_80185BA0, 0, 0, 0);
+    if (gGameSession->at4.loc.room >= 4) {
+        Task_SpawnFromTable(D_shelter_b3_garbage_incinerator_80187150, 0, 0, 0);
+    }
+    if (gGameSession->at4.loc.place == 2) {
+        Task_SpawnFromTable(&D_8016BFE0, 0, 0, 0);
+    }
+    task->state = task->state + 1;
+}
 
 void func_shelter_b3_garbage_incinerator_8017DC54(void)
 {
