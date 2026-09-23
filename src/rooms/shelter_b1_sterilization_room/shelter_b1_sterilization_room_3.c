a38d022146dc0c68802860b9687662b705fe74b5
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 00:59:09 +0200

    matched func_shelter_b1_sterilization_room_8018049C 7

--- src/rooms/shelter_b1_sterilization_room/shelter_b1_sterilization_room_3.c
+++ src/rooms/shelter_b1_sterilization_room/shelter_b1_sterilization_room_3.c
@@ -1,6 +1,8 @@
 #include "common.h"
 #include "main/session.h"
 #include "main/sound.h"
+#include "main/gameflag.h"
+#include "gameplay/3CD8.h"
 
 void func_shelter_b1_sterilization_room_8018049C(void);
 
@@ -25,4 +27,16 @@ void func_shelter_b1_sterilization_room_80180464(void)
     }
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b1_sterilization_room/shelter_b1_sterilization_room_3", func_shelter_b1_sterilization_room_8018049C);
+void func_shelter_b1_sterilization_room_8018049C(void)
+{
+    s32 view;
+
+    view = gGameSession->at4.loc.view;
+    if ((GameFlag_GetNibble(0xEA) == 1) && (gGameSession->eventState == 0)) {
+        if (view == 2 || view == 3) {
+            Gp_MsgSlot4Chain(0, 1);
+        } else {
+            Gp_MsgSlot4Chain(0, 0);
+        }
+    }
+}
