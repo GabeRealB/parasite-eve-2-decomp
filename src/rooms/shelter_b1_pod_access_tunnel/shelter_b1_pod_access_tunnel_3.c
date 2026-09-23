9c6975a9e2ca31ad91217c49d818a43cb74688de
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 01:16:16 +0200

    matched func_shelter_b1_pod_access_tunnel_8017E44C 1

--- src/rooms/shelter_b1_pod_access_tunnel/shelter_b1_pod_access_tunnel_3.c
+++ src/rooms/shelter_b1_pod_access_tunnel/shelter_b1_pod_access_tunnel_3.c
@@ -1,5 +1,7 @@
 #include "common.h"
 #include "main/task.h"
+#include "main/session.h"
+#include "gameplay/gameplay.h"
 extern TaskDesc D_shelter_b1_pod_access_tunnel_80182D2C;
 extern TaskDesc D_801348D8;
 
@@ -8,7 +10,26 @@ void func_shelter_b1_pod_access_tunnel_8017E41C(s32 arg0)
     Task_SpawnFromTable(&D_shelter_b1_pod_access_tunnel_80182D2C, 0, arg0, 0);
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b1_pod_access_tunnel/shelter_b1_pod_access_tunnel_3", func_shelter_b1_pod_access_tunnel_8017E44C);
+void func_shelter_b1_pod_access_tunnel_8017E44C(Task* task)
+{
+    GpViewRec* view;
+    VECTOR     vec;
+
+    if (task->killCountdown < task->spawnArg1 && gGameSession->at4.loc.view == 0xB) {
+        view   = Gp_GetStageView(&gGameSession->at4.loc);
+        vec.vx = 0;
+        vec.vy = 0x10;
+        vec.vz = 0;
+        ApplyTransposeMatrixLV(&view->mtx, &vec, &vec);
+        view->mtx.t[0] += vec.vx;
+        view->mtx.t[1] += vec.vy;
+        view->mtx.t[2] += vec.vz;
+        Gp_TrySpawnViewTask((s32)view);
+        task->killCountdown++;
+        return;
+    }
+    taskKill(task);
+}
 
 void func_shelter_b1_pod_access_tunnel_8017E52C(s32 arg0)
 {
