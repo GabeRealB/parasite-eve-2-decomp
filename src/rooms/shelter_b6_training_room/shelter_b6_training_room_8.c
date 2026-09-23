41bd8f41bf056e35107bb6864b5ec06f1aa77f2a
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-23 23:15:32 +0200

    matched func_shelter_b6_training_room_8018245C 4

--- src/rooms/shelter_b6_training_room/shelter_b6_training_room_8.c
+++ src/rooms/shelter_b6_training_room/shelter_b6_training_room_8.c
@@ -7,6 +7,7 @@
 extern s32            D_80070F70;
 extern u32            Gp_LcgState;
 extern GsCOORDINATE2* D_shelter_b6_training_room_80185C94;
+extern u16            D_shelter_b6_training_room_80185C98;
 extern SVECTOR        D_shelter_b6_training_room_80184334[];
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b6_training_room/shelter_b6_training_room_8", func_shelter_b6_training_room_8017F8B8);
@@ -50,7 +51,48 @@ INCLUDE_ASM("rooms/nonmatchings/shelter_b6_training_room/shelter_b6_training_roo
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b6_training_room/shelter_b6_training_room_8", func_shelter_b6_training_room_80181FDC);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b6_training_room/shelter_b6_training_room_8", func_shelter_b6_training_room_8018245C);
+void func_shelter_b6_training_room_8018245C(Task* task)
+{
+    GpEffWork*     mem;
+    GsCOORDINATE2* coord;
+    s16            eventState;
+    u8             rgb[3];
+
+    mem        = task->spawnArg2;
+    eventState = Gp_State1C->eventState;
+    coord      = ((TmdObject*)task->extra)->coords;
+    if (eventState != 0) {
+        if (eventState < 4) {
+            return;
+        }
+        goto release;
+    }
+    mem->age++;
+    if (task->state == 0) {
+        mem->scale = 0xC0;
+        mem->angle = 0x200;
+        D_shelter_b6_training_room_80185C98++;
+        task->state     = 1;
+        task->spawnArg1 = D_shelter_b6_training_room_80185C98;
+    }
+    if (task->spawnArg1 != D_shelter_b6_training_room_80185C98) {
+        goto release;
+    }
+    rgb[0]      = mem->scale;
+    rgb[1]      = mem->scale;
+    rgb[2]      = (u16)mem->scale >> 1;
+    mem->angle += 0x18;
+    Gp_DrawArc(coord, (s16)(mem->angle * 2), 0, rgb);
+    Gp_DrawRing(coord, (s16)((u16)mem->angle * 4), rgb);
+    if (mem->age < 9) {
+        return;
+    }
+    mem->scale -= 0x18;
+    if (mem->scale < 0x18) {
+    release:
+        Gp_ReleaseState1CMem(mem, task);
+    }
+}
 
 void func_shelter_b6_training_room_801825C0(Task* task)
 {
