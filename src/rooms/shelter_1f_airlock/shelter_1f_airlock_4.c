a1ee35548a1119c6858eb570ab0a5154c7b67284
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 19:47:06 +0200

    rooms/shelter_1f_airlock: fill the stubs copied from matched twins
    The frameless return-0 message handler at 0x10 is written as C, from its identical matched twin.

--- src/rooms/shelter_1f_airlock/shelter_1f_airlock_4.c
+++ src/rooms/shelter_1f_airlock/shelter_1f_airlock_4.c
@@ -1,3 +1,6 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_airlock/shelter_1f_airlock_4", func_shelter_1f_airlock_8017D5D0);
+s32 func_shelter_1f_airlock_8017D5D0(void)
+{
+    return 0;
+}
