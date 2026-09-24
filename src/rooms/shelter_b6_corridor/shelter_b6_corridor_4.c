35906a83647d27e4e0f6f7d0932ddf360334fa6a
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 04:21:46 +0200

    matched func_shelter_b6_corridor_8017ECA8 1

--- src/rooms/shelter_b6_corridor/shelter_b6_corridor_4.c
+++ src/rooms/shelter_b6_corridor/shelter_b6_corridor_4.c
@@ -1,12 +1,14 @@
 #include "common.h"
 
 #include "gameplay/3CD8.h"
+#include "gameplay/3FB8.h"
 
 #include "main/task.h"
 #include "main/tmd.h"
 
 extern s32 D_80070F70;
 extern u32 Gp_LcgState;
+extern s32 D_shelter_b6_corridor_801851B8;
 
 void func_shelter_b6_corridor_8017EBA4(Task* task)
 {
@@ -30,6 +32,47 @@ void func_shelter_b6_corridor_8017EBA4(Task* task)
     }
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b6_corridor/shelter_b6_corridor_4", func_shelter_b6_corridor_8017ECA8);
+void func_shelter_b6_corridor_8017ECA8(Task* task)
+{
+    GpEffWork*     mem;
+    GsCOORDINATE2* coord;
+    s16            eventState;
+    u8             rgb[3];
+
+    mem        = task->spawnArg2;
+    eventState = Gp_State1C->eventState;
+    coord      = ((TmdObject*)task->extra)->coords;
+    if (eventState != 0) {
+        if (eventState < 4) {
+            return;
+        }
+        goto release;
+    }
+    mem->age++;
+    if (task->state == 0) {
+        mem->scale = 0xC0;
+        mem->angle = 0x200;
+        D_shelter_b6_corridor_801851B8++;
+        task->state     = 1;
+        task->spawnArg1 = D_shelter_b6_corridor_801851B8;
+    }
+    if (task->spawnArg1 != D_shelter_b6_corridor_801851B8) {
+        goto release;
+    }
+    rgb[0]      = mem->scale;
+    rgb[1]      = mem->scale;
+    rgb[2]      = (u16)mem->scale >> 1;
+    mem->angle += 0x18;
+    Gp_DrawArc(coord, (s16)(mem->angle * 2), 0, rgb);
+    Gp_DrawRing(coord, (s16)((u16)mem->angle * 4), rgb);
+    if (mem->age < 9) {
+        return;
+    }
+    mem->scale -= 0x18;
+    if (mem->scale < 0x18) {
+    release:
+        Gp_ReleaseState1CMem(mem, task);
+    }
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b6_corridor/shelter_b6_corridor_4", func_shelter_b6_corridor_8017EE08);
