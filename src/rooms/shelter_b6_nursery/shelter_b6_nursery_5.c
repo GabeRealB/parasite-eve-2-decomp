0197f1a5c71c10e6192325ee41733df05babd44e
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 08:15:57 +0200

    matched func_shelter_b6_nursery_80181EDC 2

--- src/rooms/shelter_b6_nursery/shelter_b6_nursery_5.c
+++ src/rooms/shelter_b6_nursery/shelter_b6_nursery_5.c
@@ -131,7 +131,72 @@ void func_shelter_b6_nursery_80181314(Task* task)
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b6_nursery/shelter_b6_nursery_5", func_shelter_b6_nursery_80181820);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b6_nursery/shelter_b6_nursery_5", func_shelter_b6_nursery_80181EDC);
+void func_shelter_b6_nursery_80181EDC(GsCOORDINATE2* coord, u16 arg1, s16 arg2, s16 arg3)
+{
+    void**             scratch;
+    u8*                head;
+    RoomDraw27Scratch* block;
+    POLY_FT4*          prim;
+    SVECTOR*           vec;
+    s32                u0;
+    s32                v0;
+    s32                ang;
+    s32                ang2;
+    u16                vz;
+    u16                col;
+    u16                row;
+
+    scratch                                     = (void**)G_SCRATCH_HEAD;
+    head                                        = *scratch;
+    ((RoomDraw27Scratch*)(head - 0x1C))->vec.vx = *(u16*)&coord->workm.t[0];
+    block                                       = (RoomDraw27Scratch*)(head - 0x1C);
+    block->vec.vy                               = *(u16*)&coord->workm.t[1];
+    vz                                          = *(u16*)&coord->workm.t[2];
+    *scratch                                    = block;
+    block->vec.vz                               = vz;
+    vec                                         = &block->vec;
+    gte_SetTransMatrix(&GsWSMATRIX);
+    gte_SetRotMatrix(&GsWSMATRIX);
+    gte_ldv0(vec);
+    gte_rtps_real();
+    prim           = (POLY_FT4*)gGpuPrimCursor;
+    gGpuPrimCursor = (u8*)(prim + 1);
+    setlen(prim, 9);
+    setcode(prim, 0x2C);
+    gte_stsxy(&((RoomDraw27Scratch*)(head - 0x1C))->sx);
+    gte_stflg(&((RoomDraw27Scratch*)(head - 0x1C))->flag);
+    if (block->flag >= 0) {
+        gte_stszotz(&((RoomDraw27Scratch*)(head - 0x1C))->otz);
+        if (block->otz >= 0x41) {
+            prim->tpage = 0x2B;
+            prim->clut  = 0x4384;
+            prim->code |= 3;
+            col         = arg1 % 5;
+            row         = arg1 / 5;
+            u0          = col * 48;
+            v0          = row * 48;
+            setUV4(prim, u0, v0 + 0x28, u0 + 0x2F, v0 + 0x28, u0, v0 + 0x57, u0 + 0x2F, v0 + 0x57);
+            ang       = arg3;
+            block->dx = (((arg2 * 47) / block->otz) * rsin(ang)) >> 12;
+            block->dy = (((arg2 * 47) / block->otz) * rcos(ang)) >> 12;
+            prim->x0  = *(u16*)&block->sx + *(u16*)&block->dx;
+            prim->x3  = *(u16*)&block->sx - *(u16*)&block->dx;
+            prim->y0  = *(u16*)&block->sy - *(u16*)&block->dy;
+            ang2      = ang + 0x400;
+            prim->y3  = *(u16*)&block->sy + *(u16*)&block->dy;
+            block->dx = (((arg2 * 47) / block->otz) * rsin(ang2)) >> 12;
+            block->dy = (((arg2 * 47) / block->otz) * rcos(ang2)) >> 12;
+            prim->x1  = *(u16*)&block->sx + *(u16*)&block->dx;
+            prim->x2  = *(u16*)&block->sx - *(u16*)&block->dx;
+            prim->y1  = *(u16*)&block->sy - *(u16*)&block->dy;
+            prim->y2  = *(u16*)&block->sy + *(u16*)&block->dy;
+            addPrim((u_long*)(((((u32)block->otz << gDisplayState.otDepthShift) >> 2) & 0xFFC) +
+                              (s32)gGpuCurrentOt),
+                    prim);
+        }
+    }
+    *(void**)G_SCRATCH_HEAD = (u8*)*(void**)G_SCRATCH_HEAD + 0x1C;
+}
 
 void func_shelter_b6_nursery_80182330(GsCOORDINATE2* coord, u16 arg1, s16 arg2, s16 arg3)
 {
