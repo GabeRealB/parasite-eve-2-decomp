430189449fbf3f5c0be63f521dd71d52c7f04b38
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 05:59:23 +0200

    matched func_shelter_b2_elevator_hall_8017DC88 1

--- src/rooms/shelter_b2_elevator_hall/shelter_b2_elevator_hall.c
+++ src/rooms/shelter_b2_elevator_hall/shelter_b2_elevator_hall.c
@@ -1,5 +1,7 @@
 #include "common.h"
 
+#include "main/sound.h"
+
 INCLUDE_ASM("rooms/nonmatchings/shelter_b2_elevator_hall/shelter_b2_elevator_hall", func_shelter_b2_elevator_hall_8017D8E4);
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b2_elevator_hall/shelter_b2_elevator_hall", func_shelter_b2_elevator_hall_8017DAD4);
@@ -19,7 +21,13 @@ s32 func_shelter_b2_elevator_hall_8017DC80(void)
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b2_elevator_hall/shelter_b2_elevator_hall", func_shelter_b2_elevator_hall_8017DC88);
+s32 func_shelter_b2_elevator_hall_8017DC88(s32 arg0, s32 arg1, s32 arg2)
+{
+    if (arg2 == 1) {
+        SndEvt_EnqueueType6(0x541B0000 | 1, 0, 0);
+    }
+    return 0;
+}
 
 INCLUDE_RODATA("rooms/nonmatchings/shelter_b2_elevator_hall/shelter_b2_elevator_hall", RoomsShared8017d878Table);
 
