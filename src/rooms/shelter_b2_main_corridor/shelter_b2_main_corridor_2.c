915db2202e0202b80c973a32ca0f4b3bdfa38aae
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 03:31:20 +0200

    matched func_shelter_b2_main_corridor_8017E264 1

--- src/rooms/shelter_b2_main_corridor/shelter_b2_main_corridor_2.c
+++ src/rooms/shelter_b2_main_corridor/shelter_b2_main_corridor_2.c
@@ -5,10 +5,19 @@
 #include "main/session.h"
 #include "main/task.h"
 
-extern GpMsgEntry D_shelter_b2_main_corridor_80182C14[];
-extern TaskDesc   D_shelter_b2_main_corridor_80182DE0[];
+#include "rooms/room_common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b2_main_corridor/shelter_b2_main_corridor_2", func_shelter_b2_main_corridor_8017E264);
+extern GpMsgEntry     D_shelter_b2_main_corridor_80182C14[];
+extern TaskDesc       D_shelter_b2_main_corridor_80182DE0[];
+extern GpAreaApplyRec D_shelter_b2_main_corridor_80189644;
+
+void func_shelter_b2_main_corridor_8017E264(RoomEventMsg* msg)
+{
+    if ((GameFlag_GetNibble(0x4C) == 9) && (GameFlag_GetNibble(0xD1) == 3) && (msg->field_5 == 0)) {
+        GameFlag_SetNibble(0x4C, 0);
+        Gp_ApplyAreaRecs(&D_shelter_b2_main_corridor_80189644);
+    }
+}
 
 void func_shelter_b2_main_corridor_8017E2D4(Task* arg0)
 {
