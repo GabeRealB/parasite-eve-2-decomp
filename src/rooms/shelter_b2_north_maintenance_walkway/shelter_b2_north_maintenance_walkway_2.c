606ba1b33b116b9783fea713541b6111bb8a3b11
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 05:16:31 +0200

    matched func_shelter_b2_north_maintenance_walkway_8017DCE4 1

--- src/rooms/shelter_b2_north_maintenance_walkway/shelter_b2_north_maintenance_walkway_2.c
+++ src/rooms/shelter_b2_north_maintenance_walkway/shelter_b2_north_maintenance_walkway_2.c
@@ -1,5 +1,7 @@
 #include "common.h"
 
+#include "main/sound.h"
+
 INCLUDE_ASM("rooms/nonmatchings/shelter_b2_north_maintenance_walkway/shelter_b2_north_maintenance_walkway_2", func_shelter_b2_north_maintenance_walkway_8017DA88);
 
 s32 func_shelter_b2_north_maintenance_walkway_8017DC44(void)
@@ -14,7 +16,13 @@ s32 func_shelter_b2_north_maintenance_walkway_8017DC4C(void)
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b2_north_maintenance_walkway/shelter_b2_north_maintenance_walkway_2", func_shelter_b2_north_maintenance_walkway_8017DC54);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b2_north_maintenance_walkway/shelter_b2_north_maintenance_walkway_2", func_shelter_b2_north_maintenance_walkway_8017DCE4);
+s32 func_shelter_b2_north_maintenance_walkway_8017DCE4(s32 arg0, s32 arg1, s32 arg2)
+{
+    if (arg2 == 7) {
+        SndEvt_EnqueueType6(0x541E0000 | 7, 0, 0);
+    }
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b2_north_maintenance_walkway/shelter_b2_north_maintenance_walkway_2", func_shelter_b2_north_maintenance_walkway_8017DD18);
 
