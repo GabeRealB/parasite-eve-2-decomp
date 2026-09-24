ba537668b557ca7b588b765ea3aa841f001d6578
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 03:35:29 +0200

    matched func_shelter_b1_elevator_hall_8017DBB8 1

--- src/rooms/shelter_b1_elevator_hall/shelter_b1_elevator_hall.c
+++ src/rooms/shelter_b1_elevator_hall/shelter_b1_elevator_hall.c
@@ -1,4 +1,15 @@
 #include "common.h"
+
+#include "gameplay/3CD8.h"
+#include "gameplay/D4.h"
+
+#include "main/gameflag.h"
+#include "main/task.h"
+
+#include "rooms/room_common.h"
+
+extern GpMsgEntry D_shelter_b1_elevator_hall_80182CB8[];
+
 INCLUDE_ASM("rooms/nonmatchings/shelter_b1_elevator_hall/shelter_b1_elevator_hall", func_shelter_b1_elevator_hall_8017D620);
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b1_elevator_hall/shelter_b1_elevator_hall", func_shelter_b1_elevator_hall_8017D810);
@@ -24,7 +35,16 @@ s32 func_shelter_b1_elevator_hall_8017DB64(void)
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b1_elevator_hall/shelter_b1_elevator_hall", func_shelter_b1_elevator_hall_8017DB6C);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b1_elevator_hall/shelter_b1_elevator_hall", func_shelter_b1_elevator_hall_8017DBB8);
+void func_shelter_b1_elevator_hall_8017DBB8(Task* arg0)
+{
+    arg0->msgTable = D_shelter_b1_elevator_hall_80182CB8;
+    Game_SetPtrSlot(arg0, 7);
+    if (GameFlag_GetNibble(0x122) == 0) {
+        GameFlag_SetNibble(0x122, 1);
+        func_800E3FAC(0xA2, 0x1D);
+    }
+    arg0->state++;
+}
 
 void func_shelter_b1_elevator_hall_8017DC20(void)
 {
