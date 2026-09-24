71cb3eb0751fae67683cc290d5b56090ec9af9da
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 03:10:12 +0200

    matched func_shelter_b6_growth_room_8017E564 7

--- src/rooms/shelter_b6_growth_room/shelter_b6_growth_room_3.c
+++ src/rooms/shelter_b6_growth_room/shelter_b6_growth_room_3.c
@@ -11,6 +11,7 @@
 
 extern u32 Gp_LcgState;
 
+void func_shelter_b6_growth_room_8017E7F0(GsCOORDINATE2* coord, u16 arg1, s16 arg2, u16 arg3);
 void func_shelter_b6_growth_room_8017ED28(GsCOORDINATE2* coord, u16 arg1, s16 arg2, s16 arg3);
 
 void func_shelter_b6_growth_room_8017E448(s16 arg0, s16 arg1)
@@ -29,7 +30,75 @@ void func_shelter_b6_growth_room_8017E448(s16 arg0, s16 arg1)
     Gp_AddTpageShift((P_TAG*)prim, 1, 0x40);
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b6_growth_room/shelter_b6_growth_room_3", func_shelter_b6_growth_room_8017E564);
+void func_shelter_b6_growth_room_8017E564(Task* task)
+{
+    RoomEffWork*   work  = task->spawnArg2;
+    GsCOORDINATE2* coord = ((TmdObject*)task->extra)->coords;
+    s32            vz;
+    s32            t;
+    s16            f2a;
+    u32            rng2;
+    u32            rng3;
+
+    work->field_22++;
+    if (task->state == 0) {
+        work->field_24 = (*(u16*)&task->spawnArg1) & 0xFFF;
+
+        if (task->spawnArg1 & 0xF000) {
+            work->field_28 = (task->spawnArg1 >> 12) & 0x7;
+        } else {
+            work->field_28 = 1;
+        }
+
+        work->field_22 = 0;
+        task->state    = 1;
+
+        if (task->spawnArg1 & 0xFF0000) {
+            f2a = (task->spawnArg1 >> 16) & 0xFF;
+        } else {
+            f2a = 0x40;
+        }
+
+        work->field_2A    = f2a;
+        work->field_10.vy = 0;
+        rng2              = Gp_LcgState * 5 + 0x71357911;
+        Gp_LcgState       = rng2;
+        work->field_10.vx = 0x80 - ((rng2 >> 16) & 0xFF);
+        rng3              = Gp_LcgState * 5 + 0x71357911;
+        Gp_LcgState       = rng3;
+        vz                = 0x80 - ((rng3 >> 16) & 0xFF);
+        work->field_10.vz = vz;
+        VectorNormalSS(&work->field_10, &work->field_10);
+
+        gte_lddp(work->field_2A);
+        gte_ldsv(&work->field_10);
+        gte_gpf12_real();
+        gte_stsv(&work->field_10);
+    }
+
+    if ((s16)work->field_22 < (s16)work->field_28 * 10 - 16) {
+        if ((s16)work->field_26 < 0x40) {
+            work->field_26 += 4;
+        }
+    } else {
+        t              = (s16)work->field_22 + 16;
+        work->field_26 = 0x40 - (t - (s16)work->field_28 * 10) * 4;
+    }
+
+    func_shelter_b6_growth_room_8017E7F0(coord, work->field_20, (s16)work->field_24, work->field_26);
+
+    coord->coord.t[0] += work->field_10.vx;
+    coord->coord.t[1] += work->field_10.vy;
+    coord->coord.t[2] += work->field_10.vz;
+    coord->flg         = 0;
+
+    if (((s16)work->field_22 % (s16)work->field_28) == 0) {
+        work->field_20++;
+        if ((s16)work->field_20 >= 0xA) {
+            Gp_ReleaseState1CMem(work, task);
+        }
+    }
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b6_growth_room/shelter_b6_growth_room_3", func_shelter_b6_growth_room_8017E7F0);
 
