495812624b443302b9ec12c076ce09899ba8f990
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 19:55:06 +0200

    rooms/mine_tunnel_entrance: fill the stubs copied from matched twins
    The frameless return-0 message handler at 0x28 is C, like the room's two other
    return-0 handlers.

--- src/rooms/mine_tunnel_entrance/mine_tunnel_entrance_4.c
+++ src/rooms/mine_tunnel_entrance/mine_tunnel_entrance_4.c
@@ -1,3 +1,6 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/mine_tunnel_entrance/mine_tunnel_entrance_4", func_mine_tunnel_entrance_8017D5E8);
+s32 func_mine_tunnel_entrance_8017D5E8(void)
+{
+    return 0;
+}
