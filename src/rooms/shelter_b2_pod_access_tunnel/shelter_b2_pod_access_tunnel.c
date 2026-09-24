06211c0554f9b548da52c04a90d27f962f22e034
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 04:55:15 +0200

    matched func_shelter_b2_pod_access_tunnel_8017DB78 1

--- src/rooms/shelter_b2_pod_access_tunnel/shelter_b2_pod_access_tunnel.c
+++ src/rooms/shelter_b2_pod_access_tunnel/shelter_b2_pod_access_tunnel.c
@@ -1,4 +1,6 @@
 #include "common.h"
+#include "main/sound.h"
+
 INCLUDE_ASM("rooms/nonmatchings/shelter_b2_pod_access_tunnel/shelter_b2_pod_access_tunnel", func_shelter_b2_pod_access_tunnel_8017D62C);
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b2_pod_access_tunnel/shelter_b2_pod_access_tunnel", func_shelter_b2_pod_access_tunnel_8017D7C4);
@@ -19,7 +21,13 @@ s32 func_shelter_b2_pod_access_tunnel_8017DB70(void)
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b2_pod_access_tunnel/shelter_b2_pod_access_tunnel", func_shelter_b2_pod_access_tunnel_8017DB78);
+s32 func_shelter_b2_pod_access_tunnel_8017DB78(s32 arg0, s32 arg1, s32 arg2)
+{
+    if (arg2 == 4) {
+        SndEvt_EnqueueType6(0x16, 0, 0);
+    }
+    return 0;
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b2_pod_access_tunnel/shelter_b2_pod_access_tunnel", func_shelter_b2_pod_access_tunnel_8017DBA8);
 
