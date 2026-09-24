bdc31697fb400e995ecf467eedf23ca99c077af5
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 03:37:51 +0200

    matched func_shelter_b1_elevator_hall_8017D810 1

--- src/rooms/shelter_b1_elevator_hall/shelter_b1_elevator_hall.c
+++ src/rooms/shelter_b1_elevator_hall/shelter_b1_elevator_hall.c
@@ -1,5 +1,6 @@
 #include "common.h"
 
+#include "gameplay/1A8.h"
 #include "gameplay/3CD8.h"
 #include "gameplay/D4.h"
 
@@ -10,10 +11,51 @@
 #include "rooms/room_common.h"
 
 extern GpMsgEntry D_shelter_b1_elevator_hall_80182CB8[];
+extern TaskDesc   D_shelter_b1_elevator_hall_80182CAC;
+extern TaskDesc   D_shelter_b1_elevator_hall_80182CE8;
+extern GpSaveLoc  D_shelter_b1_elevator_hall_801849F8;
+
+extern s32 func_80179A04(GpSaveLoc* in, GpSaveLoc* out);
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b1_elevator_hall/shelter_b1_elevator_hall", func_shelter_b1_elevator_hall_8017D620);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b1_elevator_hall/shelter_b1_elevator_hall", func_shelter_b1_elevator_hall_8017D810);
+s32 func_shelter_b1_elevator_hall_8017D810(Task* task, s32 msgId, GpSaveLoc* src, GpSaveLoc* dst)
+{
+    *dst = *src;
+    func_80179A04(src, dst);
+    if (*(u16*)src == 0xF && GameFlag_GetNibble(0xA5) == 0) {
+        if (src->field_5 == 0) {
+            Gp_SetNibbleIf(src->field_6, 2);
+            Gp_RunCapCmd1(2);
+        }
+        return 0;
+    }
+    if (*(u16*)src == 0x1A) {
+        if (GameFlag_GetNibble(0xBA) == 0) {
+            if (src->field_5 == 0) {
+                Gp_SetNibbleIf(src->field_6, 2);
+                Gp_RunCapCmd1(1);
+            }
+        } else {
+            if (src->field_5 == 0) {
+                Gp_RunCapCmd(4, 0);
+                Task_SpawnFromTable(&D_shelter_b1_elevator_hall_80182CAC, 0, 0x54090008, 0);
+            }
+        }
+        return 0;
+    }
+    if (*(u16*)src == 8) {
+        if (src->field_5 == 0) {
+            D_shelter_b1_elevator_hall_801849F8.field_2 = dst->field_0;
+            D_shelter_b1_elevator_hall_801849F8.field_4 = dst->field_2;
+            D_shelter_b1_elevator_hall_801849F8.field_1 = dst->field_3;
+            Gp_MsgPlayerWeapon(0);
+            Task_SpawnFromTable(&D_shelter_b1_elevator_hall_80182CE8, 0, 0, 0);
+        }
+        return 2;
+    }
+    return 1;
+}
 
 INCLUDE_RODATA("rooms/nonmatchings/shelter_b1_elevator_hall/shelter_b1_elevator_hall", RoomsShared8017d878Table);
 
