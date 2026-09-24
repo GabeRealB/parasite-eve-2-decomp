31aa294fa665d75e126539983f7bfb4c84e88d7d
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 05:48:07 +0200

    matched func_shelter_1f_guardroom_8017D8D8 2

--- src/rooms/shelter_1f_guardroom/shelter_1f_guardroom_3.c
+++ src/rooms/shelter_1f_guardroom/shelter_1f_guardroom_3.c
@@ -2,15 +2,42 @@
 
 #include "gameplay/D4.h"
 
+#include "main/fs.h"
 #include "main/gameflag.h"
 #include "main/session.h"
+#include "main/stream.h"
 #include "main/task.h"
 
 extern GpMsgEntry D_shelter_1f_guardroom_8017DA30[];
 
 void func_shelter_1f_guardroom_8017D9CC(s32);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_guardroom/shelter_1f_guardroom_3", func_shelter_1f_guardroom_8017D8D8);
+void func_shelter_1f_guardroom_8017D8D8(Task* arg0)
+{
+    u8          slotParam[4];
+    CdCmdQueue* queue;
+
+    queue = &CdCmd_Queue;
+    switch (arg0->state) {
+        case 0:
+            func_shelter_1f_guardroom_8017D9CC(0);
+            queue->field_1EA = 1;
+            slotParam[0]     = Stream_FindSlot(gGameSession->at4.raw.data, 0, 0);
+            CdCmd_Enqueue(0x61, 0, slotParam);
+            arg0->state++;
+            break;
+        case 1:
+            if (queue->field_1FA != 0) {
+                arg0->state = 2;
+            }
+            break;
+        case 2:
+            if (CdCmd_IsIdle() & 0xFFFF) {
+                Task_RequestKill(arg0, 0);
+            }
+            break;
+    }
+}
 
 void func_shelter_1f_guardroom_8017D9CC(s32 arg0)
 {
