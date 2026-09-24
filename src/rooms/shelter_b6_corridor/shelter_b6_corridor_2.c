3fe4bf42167f9fbda10152d1c9fa5791dbe1b85b
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 04:10:17 +0200

    matched func_shelter_b6_corridor_8017E19C 2

--- src/rooms/shelter_b6_corridor/shelter_b6_corridor_2.c
+++ src/rooms/shelter_b6_corridor/shelter_b6_corridor_2.c
@@ -4,10 +4,20 @@
 #include "gameplay/3CD8.h"
 
 #include "main/gameflag.h"
+#include "main/task.h"
 
+extern s8  D_801153F1;
 extern s16 D_800691CA;
 
 extern s32 D_shelter_b6_corridor_8017F354;
 extern s32 D_shelter_b6_corridor_8017F684;
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b6_corridor/shelter_b6_corridor_2", func_shelter_b6_corridor_8017E19C);
+void func_shelter_b6_corridor_8017E19C(s32 arg0)
+{
+    if (!(gGameSession->flowFlags & 0x80)) {
+        gGameSession->flowFlags |= 0x80;
+        D_801153F1               = arg0;
+        Gp_ReleaseStateF0Add((GpObj20E*)Gp_LookupSlot4(1), 0x31);
+        Task_CallExit((Task*)Gp_LookupSlot4(1));
+    }
+}
