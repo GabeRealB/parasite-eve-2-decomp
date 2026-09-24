7df09f1ae8ad4159553d62bd77d5211450a27031
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 02:12:40 +0200

    matched func_shelter_b4_water_supply_8017DC28 2

--- src/rooms/shelter_b4_water_supply/shelter_b4_water_supply.c
+++ src/rooms/shelter_b4_water_supply/shelter_b4_water_supply.c
@@ -99,7 +99,37 @@ void func_shelter_b4_water_supply_8017DB18(void)
     }
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b4_water_supply/shelter_b4_water_supply", func_shelter_b4_water_supply_8017DC28);
+void func_shelter_b4_water_supply_8017DC28(Task* arg0)
+{
+    ShelterB4WaterSupplyEventDesc work;
+    RoomEventMsg                  param;
+    s32                           (*resolve)(RoomEventMsg*, RoomEventMsg*);
+
+    if (Gp_CapBusy() == 0) {
+        resolve      = func_shelter_b4_water_supply_8017DDFC;
+        work.field_0 = 3;
+        work.field_1 = 0x20;
+        work.field_2 = 3;
+        work.field_3 = 1;
+        work.field_8 = 0x542E0003;
+        work.field_4 = 0x400;
+        Gp_MsgPlayerWeapon(0);
+        param.msgId   = work.field_1;
+        param.field_2 = work.field_2;
+        param.field_3 = work.field_3;
+        param.field_5 = 0;
+        resolve(&param, &param);
+        work.field_1                       = param.msgId;
+        work.field_2                       = param.field_2;
+        work.field_3                       = param.field_3;
+        D_shelter_b4_water_supply_80184E44 = work;
+        Task_SpawnFromTable(&D_shelter_b4_water_supply_801825E4, 0, 0, 0);
+        if (gameGetPtrSlot(0xA) != NULL && GameFlag_GetNibble(0xCF) == 0) {
+            GameFlag_SetNibble(0x4C, 6);
+        }
+        taskKill(arg0);
+    }
+}
 
 void func_shelter_b4_water_supply_8017DD40(Task* arg0)
 {
