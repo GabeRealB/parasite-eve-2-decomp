cf300b8c6e824e7f5f84de563269e926d3931df6
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 04:03:29 +0200

    matched func_shelter_1f_heliport_801807C0 2

--- src/rooms/shelter_1f_heliport/shelter_1f_heliport_4.c
+++ src/rooms/shelter_1f_heliport/shelter_1f_heliport_4.c
@@ -1,8 +1,24 @@
 #include "common.h"
 
-extern void func_shelter_1f_heliport_801807C0(void);
+#include "gameplay/3CD8.h"
+#include "main/session.h"
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_heliport/shelter_1f_heliport_4", func_shelter_1f_heliport_801807C0);
+extern u8 D_8007216C;
+extern u8 D_shelter_1f_heliport_801811D4[][4];
+
+void func_shelter_1f_heliport_801807C0(void)
+{
+    s32 i;
+    s32 idx = D_8007216C;
+
+    if (gGameSession->at4.loc.place < 3 && idx < 12) {
+        if (D_shelter_1f_heliport_801811D4[idx][0] != 0) {
+            for (i = 0; i < 4; i++) {
+                Gp_MsgSlot4Chain(i, D_shelter_1f_heliport_801811D4[idx][i]);
+            }
+        }
+    }
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_1f_heliport/shelter_1f_heliport_4", func_shelter_1f_heliport_8018085C);
 
