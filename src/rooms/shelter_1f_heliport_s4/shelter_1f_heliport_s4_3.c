595d0dd24f25e6425161708f9a3bdaec81620b85
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 20:29:04 +0200

    rooms/shelter_1f_heliport_s4: fill the stubs copied from matched twins
    The only stub was the 0x13F1 message handler at 0x10, a frameless return-0 leaf, written directly in C.

--- src/rooms/shelter_1f_heliport_s4/shelter_1f_heliport_s4_3.c
+++ src/rooms/shelter_1f_heliport_s4/shelter_1f_heliport_s4_3.c
@@ -1,3 +1,6 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_heliport_s4/shelter_1f_heliport_s4_3", func_shelter_1f_heliport_s4_8017D5D0);
+s32 func_shelter_1f_heliport_s4_8017D5D0(void)
+{
+    return 0;
+}
