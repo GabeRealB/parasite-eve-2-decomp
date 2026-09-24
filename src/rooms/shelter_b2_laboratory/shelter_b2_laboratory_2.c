67150e364c5a5162839f2bd6ad8454dd985cba02
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 03:29:57 +0200

    matched func_shelter_b2_laboratory_8017FEB8 2

--- src/rooms/shelter_b2_laboratory/shelter_b2_laboratory_2.c
+++ src/rooms/shelter_b2_laboratory/shelter_b2_laboratory_2.c
@@ -1,7 +1,11 @@
 #include "common.h"
 
+#include "gameplay/3A34.h"
 #include "gameplay/3CD8.h"
+#include "gameplay/gameplay.h"
 #include "main/gameflag.h"
+#include "main/gfx.h"
+#include "main/sound.h"
 #include "main/task.h"
 
 /// Parameter block passed as the fourth argument of `Task_SpawnFromTable` to
@@ -23,6 +27,9 @@ extern TaskDesc                   D_shelter_b2_laboratory_80182A08[];
 extern TaskDesc                   D_shelter_b2_laboratory_80182A6C[];
 extern s32                        D_shelter_b2_laboratory_801864B8;
 extern ShelterLaboratoryCapParams D_shelter_b2_laboratory_801864BC;
+extern GsCOORDINATE2              D_shelter_b2_laboratory_801864DC;
+extern s8                         D_shelter_b2_laboratory_80182A90[];
+extern u8                         D_8007216C;
 
 void func_shelter_b2_laboratory_801820F4(s16 arg0);
 
@@ -72,4 +79,47 @@ s32 func_shelter_b2_laboratory_8017FD18(s32 arg0, s32 arg1, s32 arg2)
 
 INCLUDE_RODATA("rooms/nonmatchings/shelter_b2_laboratory/shelter_b2_laboratory_2", RoomsShared8017d878Table);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b2_laboratory/shelter_b2_laboratory_2", func_shelter_b2_laboratory_8017FEB8);
+void func_shelter_b2_laboratory_8017FEB8(Task* arg0)
+{
+    s8  pan;
+    s8  depth;
+    s32 vol;
+
+    D_shelter_b2_laboratory_801864DC.coord.t[0] = 0xC1C;
+    D_shelter_b2_laboratory_801864DC.coord.t[1] = -0x5DC;
+    D_shelter_b2_laboratory_801864DC.coord.t[2] = -0xC80;
+    D_shelter_b2_laboratory_801864DC.sub        = &gGfxViewCoord;
+    D_shelter_b2_laboratory_801864DC.flg        = 0;
+    Gp_UpdateCoord(&D_shelter_b2_laboratory_801864DC);
+    pan   = Gp_GetObjPan(&D_shelter_b2_laboratory_801864DC);
+    depth = gpGetObjDepth(&D_shelter_b2_laboratory_801864DC);
+    switch (arg0->state) {
+        case 0:
+            SndEvt_EnqueueType6(0x541F000E, pan, depth);
+            arg0->state++;
+            break;
+        case 1:
+            if (D_shelter_b2_laboratory_801864B8 == 0) {
+                SndEvt_EnqueueType7(0x541F000E, 1);
+                taskKill(arg0);
+                return;
+            }
+            if (D_8007216C != gGameSession->at4.loc.view) {
+                arg0->state++;
+            }
+            break;
+        case 2:
+        case 3:
+        case 4:
+            arg0->state++;
+            break;
+        case 5:
+            vol = 0x7F - D_shelter_b2_laboratory_80182A90[gGameSession->at4.loc.view] * 0x7F / 100;
+            if (vol >= 0x80) {
+                vol = 0x7F;
+            }
+            SndEvt_EnqueueTypeA(0x541F000E, pan, (s8)vol);
+            arg0->state = 1;
+            break;
+    }
+}
