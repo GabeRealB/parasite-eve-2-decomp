f12016cb9d26b6f6736fb84f5d65be16fd232b04
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 04:51:40 +0200

    matched func_shelter_b2_septic_tank_8017D90C 2

--- src/rooms/shelter_b2_septic_tank/shelter_b2_septic_tank.c
+++ src/rooms/shelter_b2_septic_tank/shelter_b2_septic_tank.c
@@ -1,12 +1,18 @@
 #include "common.h"
 
+#include "gameplay/3CD8.h"
 #include "gameplay/D4.h"
 
+#include "main/gameflag.h"
 #include "main/session.h"
 #include "main/task.h"
 
+#include "rooms/room_common.h"
+
 extern GpMsgEntry D_shelter_b2_septic_tank_80182F4C[];
 extern TaskDesc   D_shelter_b2_septic_tank_801832C0[];
+extern u8         D_shelter_b2_septic_tank_8018310C;
+extern u8         D_shelter_b2_septic_tank_80187045;
 INCLUDE_ASM("rooms/nonmatchings/shelter_b2_septic_tank/shelter_b2_septic_tank", func_shelter_b2_septic_tank_8017D614);
 
 s32 func_shelter_b2_septic_tank_8017D7AC(void)
@@ -21,7 +27,21 @@ s32 func_shelter_b2_septic_tank_8017D904(void)
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b2_septic_tank/shelter_b2_septic_tank", func_shelter_b2_septic_tank_8017D90C);
+s32 func_shelter_b2_septic_tank_8017D90C(s32 arg0, s32 arg1, RoomEventMsg* arg2)
+{
+    u8  kind;
+    s32 flag;
+
+    kind = arg2->field_2;
+    if (kind == 2) {
+        flag = GameFlag_GetNibble(0xEB);
+        if (flag == 1 && D_shelter_b2_septic_tank_80187045 == flag) {
+            func_800E8614((s32)&D_shelter_b2_septic_tank_8018310C, 0);
+            D_shelter_b2_septic_tank_80187045 = kind;
+        }
+    }
+    return 0;
+}
 
 void func_shelter_b2_septic_tank_8017D97C(s32 arg0)
 {
