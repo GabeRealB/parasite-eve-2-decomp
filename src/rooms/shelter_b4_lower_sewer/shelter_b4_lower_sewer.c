9ed7f8e82d4907982d052916ea588769ebcd3026
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 05:26:28 +0200

    matched func_shelter_b4_lower_sewer_8017D664 1

--- src/rooms/shelter_b4_lower_sewer/shelter_b4_lower_sewer.c
+++ src/rooms/shelter_b4_lower_sewer/shelter_b4_lower_sewer.c
@@ -1,5 +1,14 @@
 #include "common.h"
 
+#include "main/gameflag.h"
+#include "main/session.h"
+#include "main/task.h"
+
+#include "gameplay/D4.h"
+
+extern GpMsgEntry D_shelter_b4_lower_sewer_80181E44[];
+extern TaskDesc   D_shelter_b4_lower_sewer_80181E70[];
+
 s32 func_shelter_b4_lower_sewer_8017D654(void)
 {
     return 0;
@@ -10,7 +19,15 @@ s32 func_shelter_b4_lower_sewer_8017D65C(void)
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b4_lower_sewer/shelter_b4_lower_sewer", func_shelter_b4_lower_sewer_8017D664);
+void func_shelter_b4_lower_sewer_8017D664(Task* task)
+{
+    task->msgTable = D_shelter_b4_lower_sewer_80181E44;
+    Game_SetPtrSlot(task, 7);
+    if (GameFlag_GetNibble(0xB7) != 0) {
+        Task_SpawnFromTable(D_shelter_b4_lower_sewer_80181E70, 0, 0, 0);
+    }
+    task->state = (s32)(task->state + 1);
+}
 
 void func_shelter_b4_lower_sewer_8017D6CC(void)
 {
