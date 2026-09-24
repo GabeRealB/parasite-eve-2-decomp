8a055ed4cf99e5e0df5278e9180af8b5a1286e47
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 04:56:19 +0200

    matched func_shelter_b2_pod_access_tunnel_8017DB30 1

--- src/rooms/shelter_b2_pod_access_tunnel/shelter_b2_pod_access_tunnel.c
+++ src/rooms/shelter_b2_pod_access_tunnel/shelter_b2_pod_access_tunnel.c
@@ -1,5 +1,9 @@
 #include "common.h"
+#include "gameplay/3CD8.h"
 #include "main/sound.h"
+#include "main/task.h"
+
+extern TaskDesc D_shelter_b2_pod_access_tunnel_80183BFC;
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b2_pod_access_tunnel/shelter_b2_pod_access_tunnel", func_shelter_b2_pod_access_tunnel_8017D62C);
 
@@ -14,7 +18,14 @@ s32 func_shelter_b2_pod_access_tunnel_8017DB28(void)
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b2_pod_access_tunnel/shelter_b2_pod_access_tunnel", func_shelter_b2_pod_access_tunnel_8017DB30);
+s32 func_shelter_b2_pod_access_tunnel_8017DB30(Task* arg0, s32 arg1, s32 arg2, s32 arg3)
+{
+    if (arg2 == 1) {
+        Gp_MsgPlayerWeapon(0);
+        Task_SpawnFromTable(&D_shelter_b2_pod_access_tunnel_80183BFC, 0, 0, 0);
+    }
+    return 0;
+}
 
 s32 func_shelter_b2_pod_access_tunnel_8017DB70(void)
 {
