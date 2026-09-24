aced0620a111807f4d5b909616289edb3e3fc298
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 05:22:42 +0200

    matched func_shelter_b2_north_maintenance_walkway_8017DDE8 1

--- src/rooms/shelter_b2_north_maintenance_walkway/shelter_b2_north_maintenance_walkway_3.c
+++ src/rooms/shelter_b2_north_maintenance_walkway/shelter_b2_north_maintenance_walkway_3.c
@@ -1,3 +1,102 @@
 #include "common.h"
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b2_north_maintenance_walkway/shelter_b2_north_maintenance_walkway_3", func_shelter_b2_north_maintenance_walkway_8017DDE8);
+#include <psyq/libgte.h>
+
+#include "gameplay/D4.h"
+#include "main/gameflag.h"
+#include "main/task.h"
+#include "rooms/room_common.h"
+
+extern s32 D_80115720;
+extern s32 D_80115728;
+extern s32 D_8011572C;
+extern s32 D_8011573C;
+extern s32 D_80115744;
+extern s32 D_80115750;
+extern s32 D_80115758;
+
+extern SVECTOR D_shelter_b2_north_maintenance_walkway_80183B90[];
+extern SVECTOR D_shelter_b2_north_maintenance_walkway_80183BB0[];
+extern SVECTOR D_shelter_b2_north_maintenance_walkway_80183C20[];
+extern SVECTOR D_shelter_b2_north_maintenance_walkway_80183C28[];
+extern SVECTOR D_shelter_b2_north_maintenance_walkway_80183C30[];
+
+void func_shelter_b2_north_maintenance_walkway_8017DDE8(Task* arg0)
+{
+    if (arg0->state == 0) {
+        D_80115728  = 0x6024B;
+        D_80115744  = 0x60257;
+        D_8011573C  = 0x60262;
+        D_80115720  = 0x6026E;
+        D_80115758  = 0x601D2;
+        D_8011572C  = 0x601EE;
+        D_80115750  = 0x6020A;
+        arg0->state = 1;
+    }
+
+    switch (Gp_GetViewIndex() & 0xFF) {
+        case 2: {
+            SVECTOR* p;
+            if (GameFlag_GetNibble(0xA8) != 0) {
+                Room_Draw15(D_shelter_b2_north_maintenance_walkway_80183C28, 0x100, 0x504C);
+            } else {
+                Room_Draw15(D_shelter_b2_north_maintenance_walkway_80183C30, 0x100, 0x5C40);
+            }
+            p = D_shelter_b2_north_maintenance_walkway_80183B90;
+            Room_Draw11(&p[0], 0x200, 0);
+            Room_Draw11(&p[6], 0x200, -0x400);
+            break;
+        }
+        case 3: {
+            SVECTOR* p;
+            p = D_shelter_b2_north_maintenance_walkway_80183C20;
+            Room_Draw25(p, 0x200);
+            Room_Draw11(&p[-18], 0x200, 0);
+            Room_Draw11(&p[-16], 0x200, 0);
+            Room_Draw11(&p[-14], 0x200, 0);
+            Room_Draw11(&p[-12], 0x200, 0x400);
+            Room_Draw11(&p[-10], 0x200, 0x400);
+            Room_Draw11(&p[-8], 0x200, 0x400);
+            Room_Draw11(&p[-4], 0x200, 0x800);
+            break;
+        }
+        case 4: {
+            SVECTOR* p;
+            p = D_shelter_b2_north_maintenance_walkway_80183C20;
+            Room_Draw25(p, 0x200);
+            Room_Draw11(&p[-14], 0x200, 0);
+            Room_Draw11(&p[-8], 0x200, 0x400);
+            Room_Draw11(&p[-4], 0x200, 0x800);
+            break;
+        }
+        case 5: {
+            SVECTOR* p;
+            p = D_shelter_b2_north_maintenance_walkway_80183C20;
+            Room_Draw25(p, 0x200);
+            Room_Draw11(&p[-14], 0x200, 0);
+            Room_Draw11(&p[-6], 0x200, 0x800);
+            Room_Draw11(&p[-4], 0x200, 0x400);
+            Room_Draw11(&p[-2], 0x200, -0x400);
+            break;
+        }
+        case 6: {
+            SVECTOR* p;
+            p = D_shelter_b2_north_maintenance_walkway_80183C20;
+            Room_Draw25(p, 0x200);
+            Room_Draw11(&p[-14], 0x200, 0);
+            Room_Draw11(&p[-4], 0x200, 0x800);
+            Room_Draw11(&p[-2], 0x200, 0);
+            break;
+        }
+        case 7:
+            if (GameFlag_GetNibble(0xA8) != 0) {
+                Room_Draw15(D_shelter_b2_north_maintenance_walkway_80183C28, 0x100, 0x504C);
+            } else {
+                Room_Draw15(D_shelter_b2_north_maintenance_walkway_80183C30, 0x100, 0x5C40);
+            }
+            break;
+        case 8:
+            Room_Draw11(D_shelter_b2_north_maintenance_walkway_80183BB0, 0x200, 0x400);
+            break;
+    }
+}
