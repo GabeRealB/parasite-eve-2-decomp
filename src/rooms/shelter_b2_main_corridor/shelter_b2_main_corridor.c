df8cf743c67e925df770a9c92348fa771e5df954
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 03:30:04 +0200

    matched func_shelter_b2_main_corridor_8017E1DC 1

--- src/rooms/shelter_b2_main_corridor/shelter_b2_main_corridor.c
+++ src/rooms/shelter_b2_main_corridor/shelter_b2_main_corridor.c
@@ -1,4 +1,8 @@
 #include "common.h"
+
+#include "main/sound.h"
+#include "main/task.h"
+
 INCLUDE_ASM("rooms/nonmatchings/shelter_b2_main_corridor/shelter_b2_main_corridor", func_shelter_b2_main_corridor_8017D6BC);
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b2_main_corridor/shelter_b2_main_corridor", func_shelter_b2_main_corridor_8017D82C);
@@ -23,4 +27,10 @@ s32 func_shelter_b2_main_corridor_8017E1D4(void)
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b2_main_corridor/shelter_b2_main_corridor", func_shelter_b2_main_corridor_8017E1DC);
+s32 func_shelter_b2_main_corridor_8017E1DC(Task* task, s32 msgId, s32 arg2, s32 arg3)
+{
+    if (arg2 == 9) {
+        SndEvt_EnqueueType6(0x54210000 | 9, 0, 0);
+    }
+    return 0;
+}
