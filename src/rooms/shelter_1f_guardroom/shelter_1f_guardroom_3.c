8933d35dd751f2209ece20b0a01b26fff3b26238
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 05:46:45 +0200

    matched func_shelter_1f_guardroom_8017D9CC 1

--- src/rooms/shelter_1f_guardroom/shelter_1f_guardroom_3.c
+++ src/rooms/shelter_1f_guardroom/shelter_1f_guardroom_3.c
@@ -12,7 +12,18 @@ void func_shelter_1f_guardroom_8017D9CC(s32);
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_1f_guardroom/shelter_1f_guardroom_3", func_shelter_1f_guardroom_8017D8D8);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_guardroom/shelter_1f_guardroom_3", func_shelter_1f_guardroom_8017D9CC);
+void func_shelter_1f_guardroom_8017D9CC(s32 arg0)
+{
+    GpAreaKey* sess = &gGameSession->at4.loc;
+    GpSprtCmd* cmd;
+
+    cmd = Gp_SprtTables[sess->stage - 1][0].field_0[sess->area - 1][2].field_4;
+    if ((arg0 & 0xFF) == 0) {
+        cmd[1].field_4 = 1;
+    } else {
+        cmd[1].field_4 = 0;
+    }
+}
 
 void func_shelter_1f_guardroom_8017DA28(void)
 {
