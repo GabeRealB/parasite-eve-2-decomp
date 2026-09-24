50594660c1819b83a11cfe44213bf01525d39d55
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 03:58:56 +0200

    matched func_shelter_1f_heliport_801802AC 1

--- src/rooms/shelter_1f_heliport/shelter_1f_heliport_2.c
+++ src/rooms/shelter_1f_heliport/shelter_1f_heliport_2.c
@@ -1,6 +1,9 @@
 #include "common.h"
 
+#include "main/gameflag.h"
 #include "main/session.h"
+#include "main/task.h"
+#include "main/tmd.h"
 
 #include "rooms/room_common.h"
 
@@ -11,11 +14,32 @@ extern void func_801322A0(void);
 extern void func_80149E38(void);
 extern void func_80149EBC(void);
 
+extern SVECTOR D_shelter_1f_heliport_80181204;
+extern s16     D_shelter_1f_heliport_80181206; // D_shelter_1f_heliport_80181204.vy
+
+void func_shelter_1f_heliport_8018085C(GsCOORDINATE2* coord, SVECTOR* offset);
+
 INCLUDE_ASM("rooms/nonmatchings/shelter_1f_heliport/shelter_1f_heliport_2", func_shelter_1f_heliport_8017FF08);
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_1f_heliport/shelter_1f_heliport_2", func_shelter_1f_heliport_801800A0);
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_1f_heliport/shelter_1f_heliport_2", func_shelter_1f_heliport_801802AC);
+void func_shelter_1f_heliport_801802AC(void)
+{
+    Task* task;
+    Task* slotA;
+
+    task  = gameGetPtrSlot(0xA);
+    slotA = task;
+    if (task == NULL) {
+        task = gameGetPtrSlot(3);
+    }
+    if (slotA != NULL && GameFlag_GetNibble(0xE4) == 1) {
+        D_shelter_1f_heliport_80181206 = 0;
+    } else {
+        D_shelter_1f_heliport_80181206 = 0x2710;
+    }
+    func_shelter_1f_heliport_8018085C(((TmdObject*)task->extra)->coords, &D_shelter_1f_heliport_80181204);
+}
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_1f_heliport/shelter_1f_heliport_2", func_shelter_1f_heliport_80180334);
 
