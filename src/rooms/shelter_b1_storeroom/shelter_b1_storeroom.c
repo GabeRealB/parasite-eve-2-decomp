3435e5330b8393d2d6814a9aefdb237e5e16c83d
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-24 06:01:31 +0200

    matched func_shelter_b1_storeroom_8017D6F0 1

--- src/rooms/shelter_b1_storeroom/shelter_b1_storeroom.c
+++ src/rooms/shelter_b1_storeroom/shelter_b1_storeroom.c
@@ -1,6 +1,7 @@
 #include "common.h"
 
 #include "main/session.h"
+#include "main/sound.h"
 #include "main/task.h"
 
 INCLUDE_ASM("rooms/nonmatchings/shelter_b1_storeroom/shelter_b1_storeroom", func_shelter_b1_storeroom_8017D604);
@@ -15,7 +16,19 @@ s32 func_shelter_b1_storeroom_8017D6E8(void)
     return 0;
 }
 
-INCLUDE_ASM("rooms/nonmatchings/shelter_b1_storeroom/shelter_b1_storeroom", func_shelter_b1_storeroom_8017D6F0);
+s32 func_shelter_b1_storeroom_8017D6F0(s32 arg0, s32 arg1, s32 arg2)
+{
+    switch (arg2) {
+        case 8:
+            SndEvt_EnqueueType6(0x540B0008, 0, 0);
+            break;
+        case 0x6A:
+            SndEvt_EnqueueType6(0x540B0009, 0, 0);
+            break;
+    }
+    return 0;
+}
+
 INCLUDE_RODATA("rooms/nonmatchings/shelter_b1_storeroom/shelter_b1_storeroom", RoomsShared8017d878Table);
 
 INCLUDE_RODATA("rooms/nonmatchings/shelter_b1_storeroom/shelter_b1_storeroom", jtbl_shelter_b1_storeroom_8017D5D0);
