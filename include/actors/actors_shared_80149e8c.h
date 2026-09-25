f76515121c72bf2b1b3e2e1fdba132fd8f81bdff
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-25 07:37:45 +0200

    include: delete 266 shared headers nothing includes
    The actor and room lib units they declared are all demoted; the headers declared only their shared names and view types.

--- include/actors/actors_shared_80149e8c.h
+++
@@ -1,8 +0,0 @@
-#ifndef ACTORS_SHARED_80149E8C_H
-#define ACTORS_SHARED_80149E8C_H
-
-struct Actor202600;
-
-void ActorsShared80149e8c(struct Actor202600* actor);
-
-#endif
--- include/actors/actors_shared_8014a1c4.h
+++
@@ -1,8 +0,0 @@
-#ifndef ACTORS_SHARED_8014A1C4_H
-#define ACTORS_SHARED_8014A1C4_H
-
-#include "main/task.h"
-
-void ActorsShared8014a1c4(Task* task);
-
-#endif
