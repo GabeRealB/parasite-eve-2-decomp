02ed5c06f349cce6cb1010d8f31b26c051f5d208
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2026-09-25 08:42:49 +0200

    actors: drop blank lines left by a merge in actor_403100 and actor_800100

--- src/actors/actor_403100/actor_403100.c
+++ src/actors/actor_403100/actor_403100.c
@@ -466,7 +466,6 @@ extern Actor403100AnimTable D_actor_403100_8015570C;
 extern GpAnimBlk*           Gp_PlayerAnimBlkTbl[];
 extern u16                  Gp_WeaponIdBase[];
 
-
 extern s16 D_80073BA0;
 extern u8  D_801153F4;
 extern u8  D_80165FC0;
